e01a2eb60487cde5f35c0e3fd2c3002911f168d3
akicho8 <akicho8@gmail.com>
2017-11-24 10:35:23 +0900

    URLのパス表記を改善

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,20 @@ Rails.application.routes.draw do
   # match 'new',             to: 'name_space1/kifu_convert_infos#new', via: :get
 
   resolve "KifuConvertInfo" do |kifu_convert_info, options|
-    [:name_space1, kifu_convert_info, options]
+    # [:name_space1, kifu_convert_info, options]
     # "/r/#{kifu_convert_info.unique_key}"
+    "x/#{kifu_convert_info.to_param}"
   end
 
   get "tops/show"
   get "swars_tops/show"
-  root "name_space1/kifu_convert_infos#new"
+
+  get "s", to: "swars_tops#show"
+
+  get "x", to: "name_space1/kifu_convert_infos#new"
+  match "x/:id", to: "name_space1/kifu_convert_infos#show", via: :get
+
+  # root "name_space1/kifu_convert_infos#new"
+  # root "swars_tops#show"
+  root "swars_tops#show"
 end
