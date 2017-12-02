cc402734f735f636c6b792a2a23bdac008f7f0a0
akicho8 <akicho8@gmail.com>
2017-12-03 01:52:31 +0900

    入玉勝ち→対応

--- app/models/battle_result_info.rb
+++ app/models/battle_result_info.rb
@@ -1,9 +1,10 @@
 class BattleResultInfo
   include ApplicationMemoryRecord
   memory_record [
-    {key: "TORYO",      name: "投了",     label_key: nil,     csa_key: "TORYO",   },
-    {key: "DISCONNECT", name: "切断",     label_key: :danger, csa_key: "CHUDAN",  },
-    {key: "TIMEOUT",    name: "時間切れ", label_key: nil,     csa_key: "TIMEOUT", },
-    {key: "CHECKMATE",  name: "詰み",     label_key: nil,     csa_key: "TSUMI",   },
+    {key: "TORYO",        name: "投了",     label_key: nil,      csa_key: "TORYO",   },
+    {key: "DISCONNECT",   name: "切断",     label_key: :danger,  csa_key: "CHUDAN",  },
+    {key: "TIMEOUT",      name: "時間切れ", label_key: nil,      csa_key: "TIMEOUT", },
+    {key: "CHECKMATE",    name: "詰み",     label_key: nil,      csa_key: "TSUMI",   },
+    {key: "ENTERINGKING", name: "入玉",     label_key: :warning, csa_key: "KACHI",   },
   ]
 end
