55eea38370c2f8c2c7182ab89915a539500ad6a2
akicho8 <akicho8@gmail.com>
2017-12-06 00:35:56 +0900

    ビューのリファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,66 +10,56 @@
 
 console.log('Hello World from Webpacker')
 
-// import SWFObject from "./swfobject.js"
-
 import "bootstrap-honoka/dist/css/bootstrap.min.css"
 import "bootstrap-honoka/dist/js/bootstrap.min.js"
 
-import "./modulable_crud.coffee"
-
-import "./basic.sass"
+import "./bootstrap_basic.sass"
 import "./bootstrap_tuning.sass"
 
 // require("bootstrap/dist/css/bootstrap")
 // require("bootstrap/dist/css/bootstrap-theme")
 
-console.log(`jQuery ${typeof(jQuery)}`)
+import "./modulable_crud.coffee"
+
+console.log(`jQuery in webpacker: ${typeof(jQuery)}`)
 
 document.addEventListener("DOMContentLoaded", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+
   const buttons = document.querySelectorAll(".kif_clipboard_copy_button")
-  for (var i = 0, len = buttons.length; i < len; i++) {
-    buttons[i].addEventListener("click", function(e) {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (e) => {
 
       const url = e.target.dataset.kifDirectAccessPath // kif_direct_access_path
 
       if (!url) {
-        alert("URLが取得できません")
+        alert("棋譜のURLが不明です")
       }
 
-      // ユーザーがクリックしたタイミングでないとクリップボードにコピーできないため同期している
+      // クリックのタイミングしかクリップボードへのコピーは作動しないため同期している(重要)
       const kifu_text = $.ajax({type: "GET", url: url, async: false}).responseText
 
+      // クリップボードにコピーする
       const text_area = document.createElement("textarea")
       text_area.value = kifu_text
       document.body.appendChild(text_area)
       text_area.select()
       const result = document.execCommand("copy")
       document.body.removeChild(text_area)
+
       if (result) {
         if (false) {
           alert("クリップボードにコピーしました")
         } else {
           $(e.target).tooltip({title: "棋譜をクリップボードにコピーしました", trigger: "manual"})
           $(e.target).tooltip("show")
-          setTimeout(() => $(e.target).tooltip("destroy"), 500)
+          setTimeout(() => $(e.target).tooltip("destroy"), 1000)
         }
       } else {
         alert("クリップボードへのコピーに失敗しました")
       }
 
       e.preventDefault()
-
     })
   }
-
-  // $(".kif_clipboard_copy_button").click()
-
-  $('[data-toggle="tooltip"]').tooltip()
-
-  // # animation: true          # フェイドイン・アウトするか？
-  // # placement: "bottom"      # 表示場所
-  // # title: "代替テキスト"    # title属性がない場合の代替テキスト
-  // # trigger: "hover"         # 表示するタイミング
-  // # delay: show:0, hide:100  # 表示までのディレイと消えるまでディレイ
-
 })
