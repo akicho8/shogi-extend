633d22eba1d93b60d3835b4b226bdbcde36c6a89
akicho8 <akicho8@gmail.com>
2017-12-07 01:19:44 +0900

    テストが転ける→修正

--- spec/system/convert_source_infos_spec.rb
+++ spec/system/convert_source_infos_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "ConvertSourceInfos", type: :system do
     fill_in "convert_source_info[kifu_body]", with: "76歩"
     click_button "変換"
 
-    expect(page).to have_content "変換完了"
+    expect(page).to have_content "結果"
     expect(page).to have_content "▲７六歩"
   end
 end
