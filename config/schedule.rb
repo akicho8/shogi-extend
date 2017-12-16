3a77b133792a823a60bdda8411845071f1060e96
akicho8 <akicho8@gmail.com>
2017-12-16 22:54:58 +0900

    wheneverが動かない→修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-# -*- compile-command: "cap production deploy:upload FILES=config/schedule.rb whenever:update_crontab crontab" -*-
+# -*- coding: utf-8; compile-command: "cap production deploy:upload FILES=config/schedule.rb whenever:update_crontab crontab" -*-
 # capp rails:cron_log
 
 set :output, {standard: "log/#{@environment}_cron.log"}
@@ -9,7 +9,7 @@ job_type :runner,  "cd :path && bin/rails runner -e :environment ':task' :output
 every "*/5 * * * *" do
   runner [
     %(p [Time.current.to_s, 'begin', BattleUser.count, BattleRecord.count]),
-    %(BattleRecord.import_batch(limit: 2, page_max: 2, sleep: 2, battle_grade_key_gteq: "初段")),
+    %(BattleRecord.import_batch(limit: 2, page_max: 2, sleep: 2, battle_grade_key_gteq: '初段')),
     %(p [Time.current.to_s, 'end__', BattleUser.count, BattleRecord.count]),
   ].join(";")
 end
