b5652d7717b02da46a0ac856013d5237dc1edec6
akicho8 <akicho8@gmail.com>
2018-01-06 19:57:14 +0900

    フリー棋譜変換で将棋ウォーズの対局URLからもが見たい→対応

--- app/controllers/resource_ns1/free_battle_records_controller.rb
+++ app/controllers/resource_ns1/free_battle_records_controller.rb
@@ -24,6 +24,18 @@ module ResourceNs1
     include ModulableCrud::All
     include SharedMethods
 
+    def create
+      if url = current_record_params[:kifu_url].presence || current_record_params[:kifu_body].presence
+        url
+        if url.match?(%r{https?://kif-pona.heroz.jp/games/})
+          battle_key = URI(url).path.split("/").last
+          redirect_to [:resource_ns1, :swars_battle_record, id: battle_key]
+          return
+        end
+      end
+      super
+    end
+
     private
 
     def current_filename
