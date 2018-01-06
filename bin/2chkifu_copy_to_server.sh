c1b1bc50279cb2e5e73b442f544abf79221d79c1
akicho8 <akicho8@gmail.com>
2018-01-06 19:58:45 +0900

    戦法別の棋譜をまとめて集める

--- bin/2chkifu_copy_to_server.sh
+++ bin/2chkifu_copy_to_server.sh
@@ -5,4 +5,6 @@ ssh s ls -al /tmp/README.org
 rsync -avzq --exclude=".git" --delete ~/src/2chkifu s:~/
 ssh s ls -al "~/2chkifu"
 
+# rsync -avz s:/var/www/shogi_web_production/current/README.org /tmp
+
 echo "done"
