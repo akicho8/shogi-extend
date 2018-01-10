32ed7156f24ffd71aba68e5d0d2aa400bb0ccc17
akicho8 <akicho8@gmail.com>
2018-01-10 13:46:47 +0900

    RAILS_ENV=production rails webpacker:clobber webpacker:compile が動かない→解決

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,15 @@
 const environment = require('./environment')
 
+// ~/src/shogi_web/node_modules/@rails/webpacker/package/environments/production.js
+if (true) {
+  const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+  environment.plugins.delete('UglifyJs')
+  environment.plugins.append(
+    'UglifyJs',
+    new UglifyJsPlugin({
+      sourceMap: true,
+    })
+  )
+}
+
 module.exports = environment.toWebpackConfig()
