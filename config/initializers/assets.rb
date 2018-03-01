89365e7fd1ff7aa149e632b57c308bd5f02dee12
akicho8 <akicho8@gmail.com>
2018-03-01 15:08:08 +0900

    [fix] webpaker:compile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules/shogi-player/src/assets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w( hero.jpg )
