c8dd085b1031001d6dd3327032b6427b852e783f
akicho8 <akicho8@gmail.com>
2018-03-14 20:34:59 +0900

    [refactor] ボットチェックをモジュール化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,4 @@
 class ApplicationController < ActionController::Base
-  def self.bot_regexp
-    # ▼HTTP_USER_AGENTでbot,Browser,Deviceチェック - Qiita
-    # https://qiita.com/haco99/items/c8321b2992080364d08c
-    @bot_regexp ||= Regexp.union([
-        'Googlebot',
-        'Yahoo! Slurp',
-        'Mediapartners-Google',
-        'msnbot',
-        'bingbot',
-        'MJ12bot',
-        'Ezooms',
-        'pirst; MSIE 8.0;',
-        'Google Web Preview',
-        'ia_archiver',
-        'Sogou web spider',
-        'Googlebot-Mobile',
-        'AhrefsBot',
-        'YandexBot',
-        'Purebot',
-        'Baiduspider',
-        'UnwindFetchor',
-        'TweetmemeBot',
-        'MetaURI',
-        'PaperLiBot',
-        'Showyoubot',
-        'JS-Kit',
-        'PostRank',
-        'Crowsnest',
-        'PycURL',
-        'bitlybot',
-        'Hatena',
-        'facebookexternalhit',
-      ])
-  end
-
   add_flash_types :success, :info, :warning, :danger
 
   def submitted?(name)
@@ -41,15 +6,28 @@ class ApplicationController < ActionController::Base
   end
   helper_method :submitted?
 
-  def bot_agent?
-    request.user_agent.to_s.match?(self.class.bot_regexp)
-  end
-  helper_method :bot_agent?
-
   private
 
   def h
     @h ||= view_context
   end
   delegate :tag, :link_to, :icon_tag, to: :h
+
+  concerning :BotCheckMethods do
+    included do
+      helper_method :bot_agent?
+    end
+
+    class_methods do
+      def bot_regexp
+        # ▼HTTP_USER_AGENTでbot,Browser,Deviceチェック - Qiita
+        # https://qiita.com/haco99/items/c8321b2992080364d08c
+        @bot_regexp ||= Regexp.union("Googlebot", "Yahoo! Slurp", "Mediapartners-Google", "msnbot", "bingbot", "MJ12bot", "Ezooms", "pirst; MSIE 8.0;", "Google Web Preview", "ia_archiver", "Sogou web spider", "Googlebot-Mobile", "AhrefsBot", "YandexBot", "Purebot", "Baiduspider", "UnwindFetchor", "TweetmemeBot", "MetaURI", "PaperLiBot", "Showyoubot", "JS-Kit", "PostRank", "Crowsnest", "PycURL", "bitlybot", "Hatena", "facebookexternalhit")
+      end
+    end
+
+    def bot_agent?
+      request.user_agent.to_s.match?(self.class.bot_regexp)
+    end
+  end
 end
