155a03f692eb5563ff6804981c2fd67b820bea83
akicho8 <akicho8@gmail.com>
2018-04-16 20:36:55 +0900

    [fix] レコードが削除できない

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import "rails-ujs"
+import Rails from "rails-ujs"
+Rails.start()
 
 import "./application_css.sass"
 import "./modulable_crud.coffee"
