411865f16e7c21f12d990ee98d7ab0fdd840407c
akicho8 <akicho8@gmail.com>
2018-04-19 20:56:28 +0900

    [fix] 合法手の生成が遅すぎて3秒以内に何も手が出せない

--- app/controllers/cpu_versus_controller.rb
+++ app/controllers/cpu_versus_controller.rb
@@ -17,11 +17,23 @@ class CpuVersusController < ApplicationController
         end
 
         puts mediator
-        brain = mediator.current_player.brain(diver_class: Warabi::NegaScoutDiver, evaluator_class: Warabi::EvaluatorAdvance)
-        records = brain.interactive_deepning(time_limit: 3, depth_range: 0..8)
+        brain = mediator.current_player.brain(diver_class: Warabi::NegaScoutDiver, evaluator_class: Warabi::EvaluatorAdvance, legal_moves_only: false)
+        records = []
+        time_limit = 3
+        begin
+          records = brain.interactive_deepning(time_limit: time_limit, depth_max_range: 0..8)
+        rescue Warabi::BrainHandsEmpty
+          time_limit += 1
+          retry
+        end
         tp Warabi::Brain.human_format(records)
-        record = records.first
 
+        if records.empty?
+          render json: {toryo_message: "指し手がないので負けました(T_T)"}
+          return
+        end
+
+        record = records.first
         if record[:score] <= -Warabi::INF_MAX
           render json: {toryo_message: "負けました(T_T)"}
           return
