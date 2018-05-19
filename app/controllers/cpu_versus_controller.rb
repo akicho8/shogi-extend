5a2894b9d5332a908e5e8cb9f486a023c0c314a4
akicho8 <akicho8@gmail.com>
2018-05-19 10:21:05 +0900

    [style] CPU対戦

--- app/controllers/cpu_versus_controller.rb
+++ app/controllers/cpu_versus_controller.rb
@@ -117,7 +117,7 @@ class CpuVersusController < ApplicationController
   class CpuBrainInfo
     include ApplicationMemoryRecord
     memory_record [
-      { key: :level1, name: "呆れるほど弱い",     time_limit: nil, depth_max_range: nil,  legal_only: false, }, # ランダム
+      { key: :level1, name: "あきれるほど弱い",   time_limit: nil, depth_max_range: nil,  legal_only: false, }, # ランダム
       { key: :level2, name: "ありえないほど弱い", time_limit: nil, depth_max_range: nil,  legal_only: true,  }, # 合法手のランダム
       { key: :level3, name: "めちゃくちゃ弱い",   time_limit: nil, depth_max_range: 0..0, legal_only: nil,   }, # 最初の合法手リストを最善手順に並べたもの
       { key: :level4, name: "かなり弱い",         time_limit:   3, depth_max_range: 0..9, legal_only: nil,   }, # 3秒まで深読みできる
