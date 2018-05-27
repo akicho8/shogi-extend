80b1101666055e8187011374c99ededdc74f7d33
akicho8 <akicho8@gmail.com>
2018-05-27 21:57:04 +0900

    [chore] ActiveStorage を使うときは storage を共有しとかないといけない

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ append :linked_files, "config/secrets.yml.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "storage"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
