259dabe8058e61eb538de058ae9569fc32f9dac6
akicho8 <akicho8@gmail.com>
2018-06-02 22:52:34 +0900

    [chore] fallbackアイコンの s, l とプライマリー色に合わせる

--- bin/fallback_icons_generate.rb
+++ bin/fallback_icons_generate.rb
@@ -2,33 +2,65 @@
 
 require "rgb"
 
-[
-  {key: :red,     h:   0, },
-  {key: :orange,  h:  38, },
-  {key: :yellow,  h:  60, },
-  {key: :green,   h: 120, },
-  {key: :cyan,    h: 180, },
-  {key: :blue,    h: 240, },
-  {key: :magenta, h: 300, },
-].each.with_index do |e, i|
-  c = RGB::Color.from_fractions(e[:h] / 360.0, 0.7, 0.7)
-  background = c.to_rgb_hex               # => "#E87D7D", "#E8C17D", "#E8E87D", "#7DE87D", "#7DE8E8", "#7D7DE8", "#E87DE8"
-  fill = c.lighten_percent(100).to_rgb_hex # => "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"
-  filename = "../app/assets/images/fallback_icons/%04d_fallback_face_icon_#{e[:key]}.png" % i
-  p `convert -background '#{background}' -fill '#{fill}' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:"?" #{filename}`
-  puts filename
+count = 32
+size = 256
+primary_color = RGB::Color.from_rgb_hex("#7957d5")
+
+count.times do |i|
+  c = RGB::Color.from_fractions((1.0 / count) * i, primary_color.s, primary_color.l)
+  background = c.to_rgb_hex               # => "#D55757", "#D58657", "#D5B657", "#C5D557", "#96D557", "#67D557", "#57D577", "#57D5A6", "#57D5D5", "#57A6D5", "#5776D5", "#6757D5", "#9657D5", "#C557D5", "#D557B5", "#D55786"
+  fill = c.lighten_percent(97).to_rgb_hex # => "#FEFAFA", "#FEFBFA", "#FEFDFA", "#FDFEFA", "#FCFEFA", "#FAFEFA", "#FAFEFB", "#FAFEFC", "#FAFEFE", "#FAFCFE", "#FAFBFE", "#FAFAFE", "#FCFAFE", "#FDFAFE", "#FEFAFD", "#FEFAFB"
+  filepath = "../app/assets/images/fallback_icons/%04d_fallback_face_icon.png" % i
+  command = "convert -background '#{background}' -fill '#{fill}' -size #{size}x#{size} -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' #{filepath}"
+  puts command
+  puts system(command)
+  puts filepath
 end
-# >> ""
-# >> ../app/assets/images/fallback_icons/0000_fallback_face_icon_red.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0001_fallback_face_icon_orange.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0002_fallback_face_icon_yellow.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0003_fallback_face_icon_green.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0004_fallback_face_icon_cyan.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0005_fallback_face_icon_blue.png
-# >> ""
-# >> ../app/assets/images/fallback_icons/0006_fallback_face_icon_magenta.png
+# >> convert -background '#D55757' -fill '#FEFAFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0000_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0000_fallback_face_icon.png
+# >> convert -background '#D58657' -fill '#FEFBFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0001_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0001_fallback_face_icon.png
+# >> convert -background '#D5B657' -fill '#FEFDFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0002_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0002_fallback_face_icon.png
+# >> convert -background '#C5D557' -fill '#FDFEFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0003_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0003_fallback_face_icon.png
+# >> convert -background '#96D557' -fill '#FCFEFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0004_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0004_fallback_face_icon.png
+# >> convert -background '#67D557' -fill '#FAFEFA' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0005_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0005_fallback_face_icon.png
+# >> convert -background '#57D577' -fill '#FAFEFB' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0006_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0006_fallback_face_icon.png
+# >> convert -background '#57D5A6' -fill '#FAFEFC' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0007_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0007_fallback_face_icon.png
+# >> convert -background '#57D5D5' -fill '#FAFEFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0008_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0008_fallback_face_icon.png
+# >> convert -background '#57A6D5' -fill '#FAFCFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0009_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0009_fallback_face_icon.png
+# >> convert -background '#5776D5' -fill '#FAFBFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0010_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0010_fallback_face_icon.png
+# >> convert -background '#6757D5' -fill '#FAFAFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0011_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0011_fallback_face_icon.png
+# >> convert -background '#9657D5' -fill '#FCFAFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0012_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0012_fallback_face_icon.png
+# >> convert -background '#C557D5' -fill '#FDFAFE' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0013_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0013_fallback_face_icon.png
+# >> convert -background '#D557B5' -fill '#FEFAFD' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0014_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0014_fallback_face_icon.png
+# >> convert -background '#D55786' -fill '#FEFAFB' -size 256x256 -gravity center -font /Library/Fonts/Ricty-Bold.ttf -pointsize 160 label:'?' ../app/assets/images/fallback_icons/0015_fallback_face_icon.png
+# >> true
+# >> ../app/assets/images/fallback_icons/0015_fallback_face_icon.png
