32ec5742612bfb27952cfbdf76d7aa6587474128
akicho8 <akicho8@gmail.com>
2018-06-17 22:56:03 +0900

    [refactor] experiment

--- experiment/0170_名前に段級がついたままの場合.rb
+++ experiment/0170_名前に段級がついたままの場合.rb
@@ -4,16 +4,17 @@ require File.expand_path('../../config/environment', __FILE__)
 General::User.destroy_all
 General::Battle.destroy_all
 
-general/battle = General::Battle.create!(kifu_body: <<~EOT)
+battle = General::Battle.create!(kifu_body: <<~EOT)
 先手：花村元司五段
 後手：阿久津主税
 *「花村元司五段」vs「阿久津主税七段」
 EOT
 
 tp General::User
+# >> ["/Users/ikeda/src/shogi_web/config/initializers/0180_active_model_serializers.rb:11", nil, :ams_sr]
 # >> |----+------------+---------------------------+---------------------------|
 # >> | id | name       | created_at                | updated_at                |
 # >> |----+------------+---------------------------+---------------------------|
-# >> | 14 | 花村元司   | 2017-12-27 17:48:15 +0900 | 2017-12-27 17:48:15 +0900 |
-# >> | 15 | 阿久津主税 | 2017-12-27 17:48:15 +0900 | 2017-12-27 17:48:15 +0900 |
+# >> | 11 | 花村元司   | 2018-06-17 22:54:55 +0900 | 2018-06-17 22:54:55 +0900 |
+# >> | 12 | 阿久津主税 | 2018-06-17 22:54:55 +0900 | 2018-06-17 22:54:55 +0900 |
 # >> |----+------------+---------------------------+---------------------------|
