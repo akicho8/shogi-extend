2100298fba9f9e52666af214163d67918adb52e2
akicho8 <akicho8@gmail.com>
2018-07-01 11:17:00 +0900

    [refactor] fanta/memberhip

--- app/models/fanta/membership.rb
+++ app/models/fanta/membership.rb
@@ -24,38 +24,18 @@ module Fanta
     belongs_to :battle
     belongs_to :user
 
-    scope :black, -> { where(location_key: "black") }
-    scope :white, -> { where(location_key: "white") }
+    scope :location_eq, -> e { where(location_key: e) }
+    scope :black, -> { location_eq(:black) }
+    scope :white, -> { location_eq(:white) }
 
     scope :standby_enable, -> { where.not(standby_at: nil) } # 準備ができている
 
-    acts_as_list top_of_list: 0, scope: :battle
-
     default_scope { order(:position) }
 
+    acts_as_list top_of_list: 0, scope: :battle
+
     before_validation on: :create do
-      # active = battle.memberships.active
-      # if active.count < Warabi::Location.count
-      #   if membership = active.first
-      #     location = membership.location
-      #   else
-      #     location = Warabi::Location[Battle.count.modulo(Warabi::Location.count)]
-      #   end
-      #   self.location_key ||= location.flip.key
-      # end
-
-      # create!(users: [user1, user2]) とされた場合を考慮する
-      # index = battle.users.find_index(user) || battle.users.count
-      # self.location_key ||= Warabi::Location.fetch(index).key
       self.location_key ||= Warabi::Location.fetch(battle.users.count).key
-
-      # if active.count < Warabi::Location.count
-      # if membership = active.first
-      #   location = membership.location
-      # else
-      #   location = Warabi::Location[Battle.count.modulo(Warabi::Location.count)]
-      # end
-      # self.location_key ||= Warabi::Location[active.count.modulo(Warabi::Location.count)].key
 
       if user
         self.preset_key ||= user.self_preset_key
@@ -63,34 +43,18 @@ module Fanta
       self.preset_key ||= "平手"
     end
 
-    after_save do
-      if saved_changes[:fighting_at]
-        user.update!(fighting_at: fighting_at)
-      end
-    end
-
-    # before_validation do
-    #   if location_key == "watching"
-    #     self.location_key = nil
-    #   end
-    # end
-
-    with_options presence: true do
-      # validates :location_key
-    end
-
     with_options allow_blank: true do
       validates :location_key, inclusion: Warabi::Location.keys.collect(&:to_s)
     end
 
-    def location
-      Warabi::Location[location_key]
+    after_save do
+      if saved_changes[:fighting_at]
+        user.update!(fighting_at: fighting_at) # これはいるかな？
+      end
     end
 
-    def location_flip!
-      if location
-        update!(location_key: location.flip.key)
-      end
+    def location
+      Warabi::Location[location_key]
     end
   end
 end
