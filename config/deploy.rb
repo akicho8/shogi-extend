2c547e5f2cf619c15192c6026636b13609c1c19d
akicho8 <akicho8@gmail.com>
2018-07-11 21:12:24 +0900

    [chore] keep_releases 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :default_env, -> { {"DISABLE_DATABASE_ENVIRONMENT_CHECK" => "1", "RAILS_ENV"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 1
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
