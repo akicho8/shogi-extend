3d4cafebf8d68c26e74f9ff5cd622ef5e4e83117
akicho8 <akicho8@gmail.com>
2018-07-12 13:26:20 +0900

    [refactor] query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,12 +10,9 @@ class Types::QueryType < Types::BaseObject
   end
 
   field :user, Types::UserType, null: true do
-    description 'ユーザーをidで1件取得' # fieldのdescription引数はブロック内で定義してもOK
-    argument :id, ID, 'ユーザーのID', required: true
+    description "指定IDのユーザーを取得"
+    argument :id, ID, "ユーザーID", required: true
   end
-  #
-  # argumentがあるとき、resolveメソッドは必須キーワード引数としてそれを受け取れる
-  #
   def user(id:)
     Fanta::User.find(id)
   end
