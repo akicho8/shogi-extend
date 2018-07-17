7db5022d3a291cbd5f8e8907288b240632a61f0f
akicho8 <akicho8@gmail.com>
2018-07-17 12:04:36 +0900

    [chore] devise setting change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'pinpon.ikeda@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -149,7 +149,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 1.years
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
@@ -193,13 +193,13 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 5
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
-  # config.last_attempt_warning = true
+  config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
   #
@@ -257,17 +257,14 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
-  omniauth = Rails.application.credentials[Rails.env.to_sym]
-  if v = omniauth[:google]
-    # name が xxx なら http://localhost:3000/xusers/auth/xxx に対応する
-    config.omniauth :google_oauth2, v[:id], v[:secret], name: "google", scope: ["email"] # provider を文字列にしたいので name は文字列で指定
-  end
-  if v = omniauth[:twitter]
-    config.omniauth :twitter, v[:id], v[:secret]
-  end
-  if v = omniauth[:github]
-    config.omniauth :github, v[:id], v[:secret]
+  # name が xxx なら http://localhost:3000/xusers/auth/xxx に対応する
+  [
+    {:key :google,  auth_key: :google_oauth2, args: {name: "google", scope: ["email"]}}, # provider を文字列にしたいので name は文字列で指定
+    {:key :twitter, auth_key: nil,            args: {}},
+    {:key :github,  auth_key: nil,            args: {}},
+  ].each do |e|
+    v = Rails.application.credentials[Rails.env.to_sym].fetch(e.key)
+    config.omniauth((e[:auth_key] || e[:key]), v[:id], v[:secret], e[:args])
   end
 
   # ==> Warden configuration
