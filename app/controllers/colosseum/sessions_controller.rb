9fdb3648cab118fdef28d5918ca518a5899772a1
akicho8 <akicho8@gmail.com>
2018-07-19 16:56:26 +0900

    [chore] 即席アカウントを作ったときだあなたの名前を教えてあげる

--- app/controllers/colosseum/sessions_controller.rb
+++ app/controllers/colosseum/sessions_controller.rb
@@ -2,7 +2,7 @@ module Colosseum
   class SessionsController < ApplicationController
     def create
       user = User.create!
-      flash[:notice] = "即席アカウントを作成してログインしました。"
+      flash[:notice] = "即席アカウントを作成してログインしました。あなたは「#{user.name}」です"
       user.lobby_chat_say("ログインしました", :msg_class => "has-text-info")
       sign_in_and_redirect user
     end
