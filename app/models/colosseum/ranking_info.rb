209476944e97dda78096da2d84c53d02cf142a98
akicho8 <akicho8@gmail.com>
2018-07-24 15:10:16 +0900

    [feat] 週間、月間、本日、ランキング追加

---
+++ app/models/colosseum/ranking_info.rb
@@ -0,0 +1,77 @@
+module Colosseum
+  class RankingInfo
+    include ApplicationMemoryRecord
+    memory_record [
+      { key: :total, name: "総合", begin_at: nil,                                    end_at: nil,                                               },
+      { key: :day,   name: "本日", begin_at: -> { Time.current.midnight           }, end_at: -> { Time.current.midnight.tomorrow },             },
+      { key: :week,  name: "週間", begin_at: -> { Time.current.beginning_of_week  }, end_at: -> { Time.current.beginning_of_week.next_week },   },
+      { key: :month, name: "月間", begin_at: -> { Time.current.beginning_of_month }, end_at: -> { Time.current.beginning_of_month.next_month }, },
+    ]
+
+    cattr_accessor(:rank_limit) { 2 } # 位まで表示
+    cattr_accessor(:accuracy)  { 1000 }
+
+    def all
+      current_clean
+      aggregate
+      redis.zrevrange(inside_key, 0, rank_limit - 1, with_scores: true).collect do |user_id, score|
+        rank = redis.zcount(inside_key, score + 1, "+inf") + 1
+        {rank: rank, user_id: user_id, win_ratio: score.fdiv(accuracy)}
+      end
+    end
+
+    private
+
+    def aggregate
+      win_ratio_scope.each do |row|
+        redis.zadd(inside_key, row["win_ratio"], row["user_id"])
+      end
+    end
+
+    def inside_key
+      "ranking_#{key}"
+    end
+
+    def current_clean
+      redis.del(inside_key)
+    end
+
+    def redis
+      @redis ||= Redis.new(host: "localhost", port: 6379, db: 1)
+    end
+
+    def win_ratio_scope
+      ActiveRecord::Base.connection.select_all("
+      select user_id, floor(win / (win + lose) * #{accuracy}) as win_ratio
+      from (#{chronicle_scope.to_sql}) as chronicles
+      order by win_ratio desc")
+    end
+
+    def chronicle_scope
+      scope = Chronicle.all
+      if begin_at && end_at
+        scope = scope.where(created_at: begin_at.call...end_at.call)
+      end
+      scope = scope.select("user_id, sum(case when judge_key = 'win' then 1 else 0 end) as win, sum(case when judge_key='lose' then 1 else 0 end) as lose")
+      scope = scope.group("user_id")
+    end
+  end
+
+  if $0 == __FILE__
+    User.destroy_all
+
+    user = User.create!
+
+    user = User.create!
+    user.judge_add(:win)
+    user.judge_add(:lose)
+
+    user = User.create!
+    user.judge_add(:win)
+    user.judge_add(:win)
+
+    aggregator = Aggregator.new
+    aggregator.aggregate
+    tp aggregator.all
+  end
+end
