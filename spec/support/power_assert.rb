f9329d501e60872bda985ef80e2d0bfb639fdf23
akicho8 <akicho8@gmail.com>
2018-07-27 13:03:18 +0900

    [refactor] send を取る

--- spec/support/power_assert.rb
+++ spec/support/power_assert.rb
@@ -1,8 +1,8 @@
 if defined?(RSpec::PowerAssert)
-  RSpec::PowerAssert.verbose_successful_report = false           # true にすると成功したブロックまで結果が表示される
-  RSpec::PowerAssert.example_assertion_alias :assert             # rspec-rails のせいで assert が上書きされてしまうため assert にできない
-  RSpec::PowerAssert.example_group_assertion_alias :assert       # 同じ名前でもいいみたい。でも使わない。
-  RSpec::Rails::Assertions.send(:remove_method, :assert)         # 干渉するというかこっちが使われてしまうので消しておく
+  RSpec::PowerAssert.verbose_successful_report = false     # true にすると成功したブロックまで結果が表示される
+  RSpec::PowerAssert.example_assertion_alias :assert       # rspec-rails のせいで assert が上書きされてしまうため assert にできない
+  RSpec::PowerAssert.example_group_assertion_alias :assert # 同じ名前でもいいみたい。でも使わない。
+  RSpec::Rails::Assertions.remove_method(:assert)          # 干渉するというかこっちが使われてしまうので消しておく
 
   PowerAssert.configure do |config|
     config.lazy_inspection = true # これは何？
