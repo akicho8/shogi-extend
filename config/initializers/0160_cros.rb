294bf26b053ecf77f3a52229993bc76a5c4da5bf
akicho8 <akicho8@gmail.com>
2018-09-16 20:34:14 +0900

    [chore] production では特定のサイトからのみ(意味あるの？)

--- config/initializers/0160_cros.rb
+++ config/initializers/0160_cros.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    if Rails.env.production?
+      origins 'tk2-221-20341.vs.sakura.ne.jp'
+    else
+      origins '*'
+    end
+
     resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end
