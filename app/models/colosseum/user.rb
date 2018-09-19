7e74e604696ab30fb08250dfa5804e4b085dc24b
akicho8 <akicho8@gmail.com>
2018-09-19 23:00:09 +0900

    [chore] 名前に絵文字が含まれる場合にMySQLが死ぬので絵文字を取る

--- app/models/colosseum/user.rb
+++ app/models/colosseum/user.rb
@@ -60,6 +60,12 @@ module Colosseum
           end
           self.email ||= default_emal
 
+          if changes[:name] && name
+            # 絵文字があると MySQL が死ぬ
+            # Mysql2::Error: Incorrect string value: '\xF0\x9F\xA6\x90b' になる
+            self.name = name.encode('EUC-JP', 'UTF-8', invalid: :replace, undef: :replace, replace: '(絵文字)').encode('UTF-8')
+          end
+
           if Rails.env.development?
             self.online_at ||= Time.current
           end
