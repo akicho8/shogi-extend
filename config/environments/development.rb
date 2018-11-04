5e1545cbb8a491a8f3e5b9c09a29c09285c2fb5f
akicho8 <akicho8@gmail.com>
2018-11-04 16:38:44 +0900

    [chore] slackへのエラー通知テスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,18 +60,30 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # ################################################################################ ActionMailer
-  config.action_mailer.show_previews = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = true
-  config.action_mailer.smtp_settings = {
-    enable_starttls_auto: true,
-    address: "smtp.gmail.com",
-    domain: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    user_name: "pinpon.ikeda",
-    password: Rails.application.credentials[:gmail_smtp_password],
-  }
+  if false
+    # ################################################################################ ActionMailer
+    config.action_mailer.show_previews = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_caching = true
+    config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address: "smtp.gmail.com",
+      domain: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      user_name: "pinpon.ikeda",
+      password: Rails.application.credentials[:gmail_smtp_password],
+    }
+
+    # ################################################################################ エラー通知
+    config.consider_all_requests_local = false
+    config.middleware.use(ExceptionNotification::Rack, {
+        slack: {
+          webhook_url: Rails.application.credentials.dig(:slack_webhook_url),
+          channel: '#general',
+          additional_parameters: { mrkdwn: true },
+        },
+      })
+  end
 end
