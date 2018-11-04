c0c207127e1bf21edf9efcc7107a5b4f59a2e277
akicho8 <akicho8@gmail.com>
2018-11-04 17:49:11 +0900

    [chore] slack api token を無効化されたので作り直し

--- config/initializers/0200_slack_ruby_client.rb
+++ config/initializers/0200_slack_ruby_client.rb
@@ -1,3 +1,6 @@
+# for slack-ruby-client gem
+# https://api.slack.com/apps/ADUGJCCFJ/oauth?success=1
+# rails credentials:edit
 Slack.configure do |config|
-  config.token = "xoxp-472688466998-472339572855-470695463489-d4e5bb6062210cfb4fd1cd6e9994ddaa"
+  config.token = Rails.application.credentials[:slack_api_token]
 end
