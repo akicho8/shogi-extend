1779117d0e6f7d40917b2b1ed311d32fa5b70fec
akicho8 <akicho8@gmail.com>
2018-11-06 22:13:16 +0900

    [chore] slackの発言チャンネル変更

--- app/models/slack_agent.rb
+++ app/models/slack_agent.rb
@@ -4,7 +4,7 @@ module SlackAgent
   def chat_post_message(key:, body:)
     if Rails.env.production? || Rails.env.development?
       Slack::Web::Client.new.tap do |client|
-        client.chat_postMessage(channel: "#exception", text: "【#{key}】#{body}")
+        client.chat_postMessage(channel: "#shogi_web", text: "【#{key}】#{body}")
       end
     end
   end
