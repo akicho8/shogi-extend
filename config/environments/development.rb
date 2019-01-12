494f5f88fe8d657e449ed9eae175eba287449a46
akicho8 <akicho8@gmail.com>
2019-01-12 14:03:37 +0900

    [chore] development のときのエラー通知は共通化したので除去

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,31 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  if false
-    # ################################################################################ ActionMailer
-    config.action_mailer.show_previews = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.perform_caching = true
-    config.action_mailer.smtp_settings = {
-      enable_starttls_auto: true,
-      address: "smtp.gmail.com",
-      domain: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      user_name: "pinpon.ikeda",
-      password: Rails.application.credentials[:gmail_smtp_password],
-    }
-
-    # ################################################################################ エラー通知
-    config.consider_all_requests_local = false
-    config.middleware.use(ExceptionNotification::Rack, {
-        slack: {
-          webhook_url: Rails.application.credentials.dig(:slack_webhook_url),
-          channel: '#exception',
-          additional_parameters: { mrkdwn: true },
-        },
-      })
-  end
 end
