e56b6c08b72415e870916af3cef379d328ea8e7b
akicho8 <akicho8@gmail.com>
2019-03-07 18:38:03 +0900

    [chore] setup時のslack通知抑制

--- app/models/slack_agent.rb
+++ app/models/slack_agent.rb
@@ -2,10 +2,16 @@ module SlackAgent
   extend self
 
   def chat_post_message(key:, body:)
-    if Rails.env.production? || Rails.env.development?
-      Slack::Web::Client.new.tap do |client|
-        client.chat_postMessage(channel: "#shogi_web", text: "【#{key}】#{body}")
-      end
+    if Rails.env.test?
+      return
+    end
+
+    if ENV["SETUP"]
+      return
+    end
+
+    Slack::Web::Client.new.tap do |client|
+      client.chat_postMessage(channel: "#shogi_web", text: "【#{key}】#{body}")
     end
   end
 end
