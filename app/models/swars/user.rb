2a04f6aaa95374e79245249e59f6ea444f9f348f
akicho8 <akicho8@gmail.com>
2019-03-08 19:53:53 +0900

    [refactor] 放置判定改善

--- app/models/swars/user.rb
+++ app/models/swars/user.rb
@@ -75,6 +75,10 @@ module Swars
 
           memberships.each { |e| e.summary_store_to(stat) }
 
+          parcentage_set(stat, "投了率", stat["投了した"], stat["負け"])
+          parcentage_set(stat, "棋神召喚疑惑率", stat["棋神召喚疑惑"], stat["勝ち"])
+          parcentage_set(stat, "*切断率", stat["切断した"], stat["負け"])
+
           [:ten_min, :three_min].each do |rule_key|
             rule_info = RuleInfo.fetch(rule_key)
             ships = memberships.find_all { |e| e.battle.rule_info == rule_info }
@@ -83,27 +87,27 @@ module Swars
               sec_set(stat, "【#{rule_info.name}】最大長考", sec)
 
               sec = ships.collect { |e| e.sec_list.last }.compact.max
-              sec_set(stat, "【#{rule_info.name}】最終着手の最長", sec)
+              sec_set(stat, "【#{rule_info.name}】最後の着手の最長", sec)
 
               scope = ships.find_all { |e| e.judge_info.key == :lose && e.sec_list.last.to_i >= rule_info.leave_alone_limit }
-              count_set(stat, "【#{rule_info.name}】最後の着手に#{sec_to_human(rule_info.leave_alone_limit)}以上かけて負けた", scope.count)
+              count_set(stat, "*【#{rule_info.name}】最後の着手に#{sec_to_human(rule_info.leave_alone_limit)}以上かけて負けた", scope.count)
+
+              scope = ships.find_all { |e| e.summary_key == "詰ました" && e.sec_list.last >= rule_info.leave_alone_limit }
+              count_set(stat, "*【#{rule_info.name}】1手詰を#{sec_to_human(rule_info.leave_alone_limit)}以上考えて詰ました", scope.count)
+
+              scope = ships.find_all { |e| e.summary_key == "詰ました" }
+              sec = scope.collect { |e| e.sec_list.last }.compact.max
+              sec_set(stat, "【#{rule_info.name}】1手詰勝ちのときの着手までの最長", sec)
 
               scope = ships.find_all { |e| e.summary_key == "切れ負け" }
               sec = scope.collect { |e| e.rest_sec }.max
               sec_set(stat, "【#{rule_info.name}】切れ負けたときの最長残り時間", sec)
 
               scope = ships.find_all { |e| e.summary_key == "切れ負け" && e.rest_sec >= rule_info.leave_alone_limit }
-              count_set(stat, "【#{rule_info.name}】#{sec_to_human(rule_info.leave_alone_limit)}以上考えたまま切れ負けた", scope.count)
+              count_set(stat, "*【#{rule_info.name}】#{sec_to_human(rule_info.leave_alone_limit)}以上考えたまま切れ負けた", scope.count)
 
-              scope = ships.find_all { |e| e.summary_key == "詰ました" }
-              sec = scope.collect { |e| e.sec_list.last }.compact.max
-              sec_set(stat, "【#{rule_info.name}】1手詰勝ちのときの着手までの最長", sec)
             end
           end
-
-          parcentage_set(stat, "投了率", stat["投了した"], stat["負け"])
-          parcentage_set(stat, "切断率", stat["切断した"], stat["負け"])
-          parcentage_set(stat, "棋神召喚疑惑率", stat["棋神召喚疑惑"], stat["勝ち"])
 
           stat = stat.transform_values do |e|
             if e.kind_of? Integer
@@ -130,34 +134,32 @@ module Swars
         end
 
         def count_set(stat, key, value)
-          if value.nonzero?
-            stat[key] = value
+          if value.zero?
+            # return
           end
+
+          stat[key] = value
         end
 
         def sec_set(stat, key, value)
-          value = sec_to_human(value)
-
-          if value.blank?
+          if value.nil?
             return
           end
 
-          stat[key] = value
+          stat[key] = sec_to_human(value)
         end
 
         def parcentage_set(stat, key, numerator, denominator)
-          value = parcentage(numerator, denominator)
-
-          if value.blank?
+          if denominator.zero?
             return
           end
 
-          stat[key] = value
+          stat[key] = parcentage(numerator, denominator)
         end
 
         def parcentage(numerator, denominator)
           if denominator.zero?
-            return ""
+            return
           end
           rate = (numerator * 100).fdiv(denominator).round(2)
           "#{rate} %"
