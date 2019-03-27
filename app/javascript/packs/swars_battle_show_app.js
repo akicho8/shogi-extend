8f9f40cbe418657a5891ad450be947204eb6b062
akicho8 <akicho8@gmail.com>
2019-03-27 13:37:32 +0900

    [fix] 消費時間チャートでN手目が知りたいのにわかりにくい

--- app/javascript/packs/swars_battle_show_app.js
+++ app/javascript/packs/swars_battle_show_app.js
@@ -7,8 +7,37 @@ document.addEventListener("DOMContentLoaded", () => {
         chartjs_params: this.$options.chartjs_params,
       }
     },
+
     mounted() {
-      new Chart(this.$refs.swars_battle_show_canvas, this.chartjs_params)
+      new Chart(this.$refs.swars_battle_show_canvas, this.chartjs_all_params)
+    },
+
+    computed: {
+      chartjs_all_params() {
+        return Object.assign({}, this.chartjs_params, {
+          options: {
+            // https://tr.you84815.space/chartjs/configuration/tooltip.html
+            legend: {
+              display: false,
+            },
+            tooltips: {
+              callbacks: {
+                title(tooltipItems, data) {
+                  return ""
+                },
+                label(tooltipItem, data) {
+                  return [
+                    // data.labels[tooltipItem.index]
+                    data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].x, "手目",
+                    " ",
+                    Math.abs(data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y), "秒",
+                  ].join("")
+                },
+              },
+            },
+          },
+        })
+      }
     },
   })
 })
