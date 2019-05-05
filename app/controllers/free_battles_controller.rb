69aa715c5ea84868a2a16fec1b17ea4b79d17122
akicho8 <akicho8@gmail.com>
2019-05-05 20:18:23 +0900

    [chore] twitter card でリダイレクトしないURLを指定

--- app/controllers/free_battles_controller.rb
+++ app/controllers/free_battles_controller.rb
@@ -135,7 +135,9 @@ class FreeBattlesController < ApplicationController
       let :twitter_options do
         options = { title: current_record.safe_title, description: current_record&.description }
         if current_record.thumbnail_image.attached?
-          options.update(image: rails_blob_url(current_record.thumbnail_image))
+          # options.update(image: rails_blob_url(current_record.thumbnail_image))
+          options.update(image: rails_representation_url(current_record.thumbnail_image.variant(resize: "1200x630>", type: :grayscale)))
+          # options.update(image: rails_blob_url(current_record.thumbnail_image.variant(resize: "1200x630>", type: :grayscale)))
         else
           options.update(card: "summary")
         end
