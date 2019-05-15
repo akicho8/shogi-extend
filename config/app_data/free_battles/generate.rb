d18cfcc78d77b08175f3f5f531e3a279f0e7198d
akicho8 <akicho8@gmail.com>
2019-05-15 22:31:00 +0900

    [chore] 棋譜追加

--- config/app_data/free_battles/generate.rb
+++ config/app_data/free_battles/generate.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 require "securerandom"
 
-# title = "実戦詰筋事典000"
+title = "実戦詰め筋事典000"
 # title = "極限早繰り銀"
-title = "石田流vs左美濃"
+# title = "石田流vs左美濃"
 
 base = "09000"
 (ARGV.first || 1).to_i.times do |i|
