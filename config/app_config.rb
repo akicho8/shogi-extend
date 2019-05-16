90201d0bb664bbc3a6327f1df5cca04dda9e6d9e
akicho8 <akicho8@gmail.com>
2019-05-16 20:48:35 +0900

    [chore] 「ZIPダウンロード」→「一括ダウンロード」

--- config/app_config.rb
+++ config/app_config.rb
@@ -3,6 +3,6 @@ AppConfig = {
   volume: 0.2,
   admin_email: "alice@localhost",
 
-  zip_download_limit_default: 100, # ZIPダウンロードするときの件数(初期値)
-  zip_download_limit_max: 200,     # ZIPダウンロードするときの件数(最大)
+  zip_download_limit_default: 100, # 一括ダウンロードするときの件数(初期値)
+  zip_download_limit_max: 200,     # 一括ダウンロードするときの件数(最大)
 }
