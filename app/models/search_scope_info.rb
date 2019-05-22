65a3b8358be0c541c456ed80b3a1083e34dcb30b
akicho8 <akicho8@gmail.com>
2019-05-22 19:02:39 +0900

    [chore] タブの「すべて」を消した

--- app/models/search_scope_info.rb
+++ app/models/search_scope_info.rb
@@ -4,6 +4,6 @@ class SearchScopeInfo
     { key: :ss_public,     name: "一般",   },
     { key: :ss_my_public,  name: "公開",   },
     { key: :ss_my_private, name: "非公開", },
-    { key: :ss_my_all,     name: "すべて", },
-  ]
+    # (!Rails.env.production? ? { key: :ss_my_all, name: "すべて", } : nil),
+  ].compact
 end
