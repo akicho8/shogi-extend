d22b1e02de59513b2ee2211f43489b27ef7b9c51
akicho8 <akicho8@gmail.com>
2019-05-23 19:02:16 +0900

    [chore] 音声をHowlerで再生

--- app/javascript/packs/app_helper.js
+++ app/javascript/packs/app_helper.js
@@ -1,6 +1,7 @@
 // どこに書いたらいいかわからない機能シリーズ
 
 import axios from "axios"
+import { Howl, Howler } from 'howler'
 
 // 指定 URL の結果をクリップボードにコピー
 export function kifu_copy_exec(params) {
@@ -147,8 +148,13 @@ export function talk(source_text) {
     }
 
     // FIFO形式で順次発声
-    if (true) {
+    if (false) {
       audio_queue.media_push(response.data.service_path)
+    }
+
+    // Howler
+    if (true) {
+      new Howl({src: response.data.service_path, autoplay: true, volume: 1.0})
     }
 
   }).catch((error) => {
