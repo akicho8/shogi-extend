f7539a33516ae9445e7dad63c12e9da62845fc5a
akicho8 <akicho8@gmail.com>
2019-05-25 01:26:24 +0900

    [chore] 棋譜追加

--- config/app_data/free_battles/generate.rb
+++ config/app_data/free_battles/generate.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 require "securerandom"
 
-title = "実戦詰め筋事典000"
+# title = "実戦詰め筋事典000"
 # title = "極限早繰り銀"
 # title = "石田流vs左美濃"
+title = "予約"
 
 base = "09000"
-(ARGV.first || 1).to_i.times do |i|
-  File.write([base, SecureRandom.hex, title].join("_") + ".kif", "")
+(ARGV.first || 30).to_i.times do |i|
+  File.write([base, SecureRandom.hex, 0, title].join("_") + ".kif", "")
   base.succ!
 end
 
