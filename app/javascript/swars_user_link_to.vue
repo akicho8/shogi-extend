9deb99c6cd3642e3dbc356bd8af16df82f17cccd
akicho8 <akicho8@gmail.com>
2019-05-26 09:47:44 +0900

    [chore] swars_user_link_to の dropdown 内にアイコン追加

--- app/javascript/swars_user_link_to.vue
+++ app/javascript/swars_user_link_to.vue
@@ -4,11 +4,20 @@
       a(slot="trigger")
         | {{membership.name_with_grade}}
       b-dropdown-item(:has-link="true" :paddingless="true")
-        a(@click.stop="() => {}" :href="membership.query_user_url") 検索
+        a(@click.stop="() => {}" :href="membership.query_user_url")
+          b-icon(icon="magnify" size="is-small")
+          | &nbsp;
+          | 検索
       b-dropdown-item(:has-link="true" :paddingless="true")
-        a(@click.stop="() => {}" :href="membership.player_info_path") プレイヤー情報
+        a(@click.stop="() => {}" :href="membership.player_info_path")
+          b-icon(icon="account" size="is-small")
+          | &nbsp;
+          | プレイヤー情報
       b-dropdown-item(:has-link="true" :paddingless="true")
-        a(@click.stop="() => {}" :href="membership.swars_home_url" target="_blank") ウォーズに移動
+        a(@click.stop="() => {}" :href="membership.swars_home_url" target="_blank")
+          b-icon(icon="open-in-new" size="is-small")
+          | &nbsp;
+          | ウォーズに移動
 </template>
 
 <script>
