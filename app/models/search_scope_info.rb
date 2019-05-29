e0fb56773104f4471eab476c4f37a41940814710
akicho8 <akicho8@gmail.com>
2019-05-29 13:01:08 +0900

    [chore] 非公開のタブには鍵アイコン

--- app/models/search_scope_info.rb
+++ app/models/search_scope_info.rb
@@ -1,9 +1,9 @@
 class SearchScopeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :ss_public,     name: "一般",   },
-    { key: :ss_my_public,  name: "公開",   },
-    { key: :ss_my_private, name: "非公開", },
+    { key: :ss_public,     name: "一般",   icon_names: nil,    },
+    { key: :ss_my_public,  name: "公開",   icon_names: nil,    },
+    { key: :ss_my_private, name: "非公開", icon_names: "lock", },
     # (!Rails.env.production? ? { key: :ss_my_all, name: "すべて", } : nil),
   ].compact
 end
