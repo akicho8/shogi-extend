75e0ac9526aaf8e01b3e45132a1e8a38dc1547a2
akicho8 <akicho8@gmail.com>
2019-06-02 19:38:57 +0900

    [chore] タブにアイコン追加

--- app/models/search_scope_info.rb
+++ app/models/search_scope_info.rb
@@ -1,9 +1,9 @@
 class SearchScopeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :ss_public,     name: "一般",   icon_names: nil,    },
-    { key: :ss_my_public,  name: "公開",   icon_names: nil,    },
-    { key: :ss_my_private, name: "非公開", icon_names: "lock", },
+    { key: :ss_public,     name: "みんなの", icon_names: "account-multiple", },
+    { key: :ss_my_public,  name: "公開",     icon_names: "account",          },
+    { key: :ss_my_private, name: "非公開",   icon_names: "lock",             },
     # (!Rails.env.production? ? { key: :ss_my_all, name: "すべて", } : nil),
   ].compact
 end
