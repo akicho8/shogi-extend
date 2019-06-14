75169396ce90ce26ed936ebda9b74e186808c153
akicho8 <akicho8@gmail.com>
2019-06-14 13:13:21 +0900

    [fix] iPhoneで棋譜コピーできない

--- app/javascript/packs/vue_basic_methods.js
+++ app/javascript/packs/vue_basic_methods.js
@@ -31,24 +31,33 @@ export default {
       }, params)
 
       const kc_url = params["kc_url"]
-      if (kc_url) {
-        const kc_format = params["kc_format"] || "kif"
-        this.$http.get(`${kc_url}.${kc_format}`, { params: { copy_trigger: true }}).then(response => {
-          params["text"] = response.data
-
-          const kc_title = params["kc_title"]
-          if (kc_title) {
-            params["success_yomiage"] = `${kc_title}の棋譜をクリップボードにコピーしました`
-          }
-
-          this.clipboard_copy(params)
-
-        }).catch(error => {
-          console.table([error.response])
-          this.$toast.open({message: error.message, position: "is-bottom", type: "is-danger"})
-        })
+      const kc_title = params["kc_title"]
+      if (kc_title) {
+        params["success_yomiage"] = `${kc_title}の棋譜をクリップボードにコピーしました`
       }
 
+      if (kc_url) {
+        const kc_format = params["kc_format"] || "kif"
+        const full_url = `${kc_url}.${kc_format}`
+
+        if (true) {
+          this.$http.get(full_url, { params: { copy_trigger: true }}).then(response => {
+            params["text"] = response.data
+            this.clipboard_copy(params)
+          }).catch(error => {
+            console.table([error.response])
+            this.$toast.open({message: error.message, position: "is-bottom", type: "is-danger"})
+          })
+        } else {
+          const kifu_text = $.ajax({ // このためだけに jQuery 使用
+            type: "GET",
+            url: `${full_url}?copy_trigger=true`,
+            async: false, // 実際のクリックのタイミングでしかクリップボードへのコピーは作動しないという鬼仕様のため同期(重要)
+          }).responseText
+          params["text"] = kifu_text
+          this.clipboard_copy(params)
+        }
+      }
     },
 
     // str をクリップボードにコピー
@@ -57,49 +66,95 @@ export default {
         success_message: "クリップボードにコピーしました",
         error_message: "クリップボードへのコピーに失敗しました",
         success_yomiage: "コピーしました",
-        error_yomiage: "失敗しました",
+        error_yomiage: "失敗したので自力でコピーしてください",
       }, params)
 
       let success = null
 
-      // この方法は iPhone で動かない。先に elem.select() を実行した時点で iPhone の方が作動しなくなる
       if (false) {
-        const elem = document.createElement("textarea")
-        elem.value = params["text"]
-        document.body.appendChild(elem)
-        elem.select() // この方法は Windows Chrome でのみ動く
-        success = document.execCommand("copy") // なんの嫌がらせか実際にクリックしていないと動作しないので注意
-        console.log(`クリップボードコピー試行1: select => ${success}`)
+        // この方法は iPhone で動かない。先に elem.select() を実行した時点で iPhone の方が作動しなくなる
+        if (false) {
+          const elem = document.createElement("textarea")
+          elem.value = params["text"]
+          document.body.appendChild(elem)
+          elem.select() // この方法は Windows Chrome でのみ動く
+          success = document.execCommand("copy") // なんの嫌がらせか実際にクリックしていないと動作しないので注意
+          console.log(`クリップボードコピー試行1: select => ${success}`)
 
-        if (!success) {
-          // この方法は iPhone と Mac の Chrome で動く。Mac の Safari では未検証
-          const range = document.createRange()
-          range.selectNode(elem)
-          window.getSelection().addRange(range)
-          success = document.execCommand("copy")
-          console.log(`クリップボードコピー試行2: selectNode => ${success}`)
+          if (!success) {
+            // この方法は iPhone と Mac の Chrome で動く。Mac の Safari では未検証
+            const range = document.createRange()
+            range.selectNode(elem)
+            window.getSelection().addRange(range)
+            success = document.execCommand("copy")
+            console.log(`クリップボードコピー試行2: selectNode => ${success}`)
+          }
+
+          document.body.removeChild(elem)
         }
 
-        document.body.removeChild(elem)
+        // https://marmooo.blogspot.com/2018/02/javascript.html
+        if (false) {
+          const elem = document.createElement('textarea')
+          document.body.appendChild(elem)
+          elem.value = params["text"]
+          success = this.corresponded_to_ios_pc_android_copy_to_clipboard(elem)
+          document.body.removeChild(elem)
+        }
+
+        if (!success) {
+          this.talk(params["error_yomiage"])
+          this.$toast.open({message: params["error_message"], position: "is-bottom", type: "is-danger"})
+          return
+        }
+
+        this.talk(params["success_yomiage"])
+        this.$toast.open({message: params["success_message"], position: "is-bottom", type: "is-success"})
       }
 
-      // https://marmooo.blogspot.com/2018/02/javascript.html
+      // https://alligator.io/js/async-clipboard-api/
       if (true) {
-        const elem = document.createElement('textarea')
-        document.body.appendChild(elem)
-        elem.value = params["text"]
-        success = this.corresponded_to_ios_pc_android_copy_to_clipboard(elem)
-        document.body.removeChild(elem)
+        if (navigator.clipboard) {
+          navigator.clipboard.writeText(params["text"]).then(() => {
+            this.talk(params["success_yomiage"])
+            this.$toast.open({message: params["success_message"], position: "is-bottom", type: "is-success"})
+          }).catch(err => {
+            this.clipboard_copy_myself(params)
+          })
+        } else {
+          this.clipboard_copy_myself(params)
+        }
       }
+    },
 
-      if (!success) {
-        this.talk(params["error_yomiage"])
-        this.$toast.open({message: params["error_message"], position: "is-bottom", type: "is-danger"})
-        return
+    clipboard_copy_myself(params) {
+      this.talk(params["error_yomiage"])
+      this.$toast.open({message: params["error_message"], position: "is-bottom", type: "is-danger"})
+
+      const ModalForm = {
+        template: `
+<form action="">
+<div class="modal-card" style="width: auto">
+<header class="modal-card-head">
+  <p class="modal-card-title">失敗したので自力でコピーしてください</p>
+</header>
+<section class="modal-card-body">
+  <b-field label="">
+    <b-input type="textarea" value="${params['text']}"></b-input>
+  </b-field>
+</section>
+<footer class="modal-card-foot">
+  <button class="button" type="button" @click="$parent.close()">閉じる</button>
+</footer>
+</div>
+</form>
+    `,
       }
-
-      this.talk(params["success_yomiage"])
-      this.$toast.open({message: params["success_message"], position: "is-bottom", type: "is-success"})
+      this.$modal.open({
+        parent: this,
+        component: ModalForm,
+        hasModalCard: true
+      })
     },
 
     // https://marmooo.blogspot.com/2018/02/javascript.html
