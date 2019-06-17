e6452a5b36a45e6d3ed4432274f2b65bd3612695
akicho8 <akicho8@gmail.com>
2019-06-17 22:34:08 +0900

    [chore] 棋譜追加

--- config/app_data/free_battles/generate.rb
+++ config/app_data/free_battles/generate.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 require "securerandom"
+require "pathname"
+require "fileutils"
 
 # title = "実戦詰め筋事典000"
 # title = "極限早繰り銀"
 # title = "石田流vs左美濃"
+# title = "寄せの手筋"
+title = "筋違い角のすべて"
 
 base = "09000"
-(77..92).each do |i|
-  title = "寄せの手筋%03d" % i
-  filename = "#{base}_#{SecureRandom.hex}_0_#{title}__s0.kif"
+(1..100).each do |i|
+  filename = Pathname("#{title}/#{base}_#{SecureRandom.hex}_0_#{title}%03d__s0.kif" % i)
+  FileUtils.mkdir_p(filename.dirname)
   File.write(filename, "")
   base.succ!
 end
