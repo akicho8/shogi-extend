6097c551129a1349efd6a6ac038137fc4e3d1796
akicho8 <akicho8@gmail.com>
2019-07-03 18:12:20 +0900

    [fix] systemテストが全滅

--- spec/support/system_support.rb
+++ spec/support/system_support.rb
@@ -22,12 +22,41 @@ Capybara.configure do |config|
   # config.threadsafe            = false
 end
 
+# 「Selenium::WebDriver::Error::UnknownCommandError: unknown command: Cannot call non W3C standard command while in W3C mode」対策
+# https://qiita.com/paranishian/items/17bd7a77dc953f8fbd63
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: {args: %w[headless disable-gpu window-size=1680,1050], w3c: false})
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+# end
+
+# Capybara.register_driver :chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: {'w3c' => false}
+#   )
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome, desired_capabilities: capabilities)
+# end
+
+# https://stackoverflow.com/questions/56111529/cannot-call-non-w3c-standard-command-while-in-w3c-mode-seleniumwebdrivererr
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w[headless disable-gpu], w3c: false })
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
 RSpec.configure do |config|
   # Chrome をヘッドレスモードで起動
   # https://qiita.com/jnchito/items/c7e6e7abf83598a6516d
   # 必須ではないが設定すると画面出てこなくなる
   config.before(:example, type: :system) do
-    driven_by :selenium_chrome_headless, screen_size: [1680, 1050]
+    # driven_by :selenium_chrome_headless, screen_size: [1680, 1050]
+
+    # caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: {w3c: false})
+    # driven_by(:selenium, using: :headless_chrome, screen_size: [1680, 1050], options: { desired_capabilities: caps })
+
+    # caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"w3c" => false})
+    # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }    
+    
+    driven_by :headless_chrome
+    
     # ↑ この書き方だと次のコードが実行され、resize_to になっていないのでスクリーンショットを撮ったときのサイズが変わらない
     # driver.browser.manage.window.size = Selenium::WebDriver::Dimension.new(*@screen_size)
 
