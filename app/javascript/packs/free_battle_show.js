31cb263e7860eaa0f6c0fee7e4bc42a5b5e21302
akicho8 <akicho8@gmail.com>
2019-07-04 21:40:08 +0900

    [fix] free_battle のほうで printer_handle メソッドがない

--- app/javascript/packs/free_battle_show.js
+++ app/javascript/packs/free_battle_show.js
@@ -1,7 +1,10 @@
+import battle_show_shared from "battle_show_shared.js"
+
 window.FreeBattleShow = Vue.extend({
+  mixins: [battle_show_shared],
+
   data() {
     return {
-      kifu_type_tab_index: 0,
     }
   },
 
