1c1c3c70de7290a8190e84c4ae2a9a43f908043c
akicho8 <akicho8@gmail.com>
2019-07-05 17:17:29 +0900

    [refactor] インライン編集機能は汎用性がないので削除

--- app/javascript/battle_show_shared.js
+++ app/javascript/battle_show_shared.js
@@ -4,7 +4,6 @@ import dayjs from "dayjs"
 export default {
   data() {
     return {
-      inline_edit_p: false,
       kifu_type_tab_index: 0,
       record: this.$options.record,
       decorator: this.$options.decorator,
@@ -21,23 +20,16 @@ export default {
     },
 
     desc_body_click_handle() {
-      if (this.inline_edit_p) {
-        if (!this.desc_body_edit_p) {
-          this.desc_body_edit_p = true
-          this.$nextTick(() => this.$refs.desc_body_edit_ref.focus())
-        }
-      } else {
-        this.$dialog.prompt({
-          title: "備考",
-          inputAttrs: { type: "text", value: this.decorator.desc_body },
-          confirmText: "更新",
-          cancelText: "キャンセル",
-          onConfirm: (value) => {
-            this.decorator.desc_body = value
-            this.$toast.open("備考を更新しました")
-          },
-        })
-      }
+      this.$dialog.prompt({
+        title: "備考",
+        inputAttrs: { type: "text", value: this.decorator.desc_body },
+        confirmText: "更新",
+        cancelText: "キャンセル",
+        onConfirm: (value) => {
+          this.decorator.desc_body = value
+          this.$toast.open("備考を更新しました")
+        },
+      })
     },
 
   },
