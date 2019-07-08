f45b229399c87e907a0463142aea964795a3aaad
akicho8 <akicho8@gmail.com>
2019-07-08 16:51:50 +0900

    [chore] 嬉野流vs中飛車 棋譜追加

--- config/app_data/free_battles/generate.rb
+++ config/app_data/free_battles/generate.rb
@@ -6,12 +6,12 @@ require "fileutils"
 # title = "実戦詰め筋事典000"
 # title = "極限早繰り銀"
 # title = "石田流vs左美濃"
-# title = "寄せの手筋"
+title = "寄せの手筋"
 # title = "筋違い角のすべて"
-title = "奇襲研究所嬉野流"
+# title = "奇襲研究所嬉野流"
 
-base = "09000"
-(1..50).each do |i|
+base = "02000"
+(14..36).each do |i|
   filename = Pathname("#{title}/#{base}_#{SecureRandom.hex}_0_#{title}%03d__s0.kif" % i)
   FileUtils.mkdir_p(filename.dirname)
   if filename.exist?
