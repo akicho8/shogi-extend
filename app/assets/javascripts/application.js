fd61e686663aebf9e3667ac0f291692c0382111c
akicho8 <akicho8@gmail.com>
2019-07-12 13:18:26 +0900

    [refactor] app/assets の方の不要なファイル削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,1 @@
-//  require rails-ujs
-//  require jquery
-//  require bootstrap-sprockets
 //= require_tree .
-
-////////////////////////////////////////////////////////////////////////////////
-if (typeof(jQuery) != "undefined") {
-  console.log('[Sprockets]jQuery: OK')
-  if (typeof($) != "undefined") {
-    console.log('[Sprockets]$: OK')
-  } else {
-    console.log('[Sprockets]$: Missing')
-  }
-} else {
-  console.log('[Sprockets]jQuery: Missing')
-}
