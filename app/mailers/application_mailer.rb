8f8eb0c1a5c4f4912b07023658328839cd796cfd
akicho8 <akicho8@gmail.com>
2019-07-18 12:59:45 +0900

    [fix] メール本文のテーブルが崩れてGmail上で読めない

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
+  MEASURES_AGAINST_DOUBLE_LINE_FEEDS_IN_GMAIL = true # GMailで改行が2重になる対策
+
   default from: AppConfig[:admin_email]
   default to: AppConfig[:admin_email]
 
@@ -6,7 +8,11 @@ class ApplicationMailer < ActionMailer::Base
 
   # ApplicationMailer.developper_notice.deliver_now
   def developper_notice(**params)
-    mail(subject: [subject_prefix, " ", params[:subject]].join, body: params[:body].to_s)
+    pre_body = params[:body].to_s
+    if MEASURES_AGAINST_DOUBLE_LINE_FEEDS_IN_GMAIL
+      pre_body = pre_body.gsub("\n", "<br>")
+    end
+    mail(subject: [subject_prefix, " ", params[:subject]].join, content_type: "text/html", body: "<pre style='white-space: pre; font-family: Osaka-mono, \"Osaka-等幅\", \"ＭＳ ゴシック\", \"Courier New\", Consolas, monospace'>#{pre_body}</pre>")
   end
 
   private
--- spec/mailers/previews/application_preview.rb
+++ spec/mailers/previews/application_preview.rb
@@ -2,6 +2,6 @@
 
 class ApplicationPreview < ActionMailer::Preview
   def developper_notice
-    ApplicationMailer.developper_notice
+    ApplicationMailer.developper_notice(body: ENV.to_h.to_t)
   end
 end
