45ac14fb3318791bcb30182b0c6fe2a546575e52
akicho8 <akicho8@gmail.com>
2019-07-19 14:41:10 +0900

    [chore] cros 設定

--- config/initializers/0160_cros.rb
+++ config/initializers/0160_cros.rb
@@ -1,11 +1,13 @@
+# FIXME: これは何のために入れてある？？？
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins 'tk2-221-20341.vs.sakura.ne.jp'
+      origins ENV["MY_APP_HOST"] || "tk2-221-20341.vs.sakura.ne.jp"
     else
-      origins '*'
+      origins "*"
     end
 
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    resource "*", headers: :any, methods: [:get, :post, :options]
   end
 end
