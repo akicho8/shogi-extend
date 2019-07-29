5db6bcf6544151804ed265f3e41eea7ff8352144
akicho8 <akicho8@gmail.com>
2019-07-29 13:42:30 +0900

    [chore] 「自分のすべて」タブを表示

--- app/models/search_scope_info.rb
+++ app/models/search_scope_info.rb
@@ -1,9 +1,9 @@
 class SearchScopeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :ss_public,     name: "みんなの", icon_key: "account-multiple", },
-    { key: :ss_my_public,  name: "公開",     icon_key: "account",          },
-    { key: :ss_my_private, name: "非公開",   icon_key: "lock",             },
-    # (!Rails.env.production? ? { key: :ss_my_all, name: "すべて", } : nil),
-  ].compact
+    { key: :ss_public,     name: "みんなの",     icon_key: "account-multiple", },
+    { key: :ss_my_public,  name: "公開",         icon_key: "account",          },
+    { key: :ss_my_private, name: "非公開",       icon_key: "lock",             },
+    { key: :ss_my_all,     name: "自分のすべて", icon_key: nil,                },
+  ]
 end
