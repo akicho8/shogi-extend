08733a4987e646c63ccc9ca740d058507ea2c9cf
akicho8 <akicho8@gmail.com>
2019-08-27 14:21:45 +0900

    [fix] 順位がバグった

--- app/models/xy_record.rb
+++ app/models/xy_record.rb
@@ -22,7 +22,7 @@
 #--------------------------------------------------------------------------------
 
 class XyRecord < ApplicationRecord
-  MSEC_ACCURACY = 1000_000
+  MSEC_ACCURACY = 1000
 
   belongs_to :user, class_name: "Colosseum::User", foreign_key: "colosseum_user_id", required: false
 
