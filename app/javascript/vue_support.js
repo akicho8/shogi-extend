c5bcd2d85ab9343eaf42e6ccef835a3bd1965335
akicho8 <akicho8@gmail.com>
2019-09-10 13:48:51 +0900

    [refactor] axios の then の部分をまとめる

--- app/javascript/vue_support.js
+++ app/javascript/vue_support.js
@@ -12,40 +12,28 @@ export default {
 
     http_command(method, url, data, callback = null) {
       const loading_instance = this.$buefy.loading.open()
-      this.$http({
-        method: method,
-        url: url,
-        data: data,
-      }).then((response) => {
-        loading_instance.close()
-        if (response.data.message) {
-          this.$buefy.toast.open({message: response.data.message})
-        }
-        if (callback) {
-          callback(response.data)
-        }
-      }).catch((error) => {
-        loading_instance.close()
-        console.table([error.response])
-        this.$buefy.toast.open({message: error.message, position: "is-bottom", type: "is-danger"})
-      })
+      this.$http({method: method, url: url, data: data}).then(response => this.http_command_then_process(response, loading_instance, callback)).catch(error => this.http_command_error_process(error, loading_instance))
     },
 
     http_get_command(url, params, callback = null) {
       const loading_instance = this.$buefy.loading.open()
-      this.$http.get(url, {params: params}).then((response) => {
-        loading_instance.close()
-        if (response.data.message) {
-          this.$buefy.toast.open({message: response.data.message})
-        }
-        if (callback) {
-          callback(response.data)
-        }
-      }).catch((error) => {
-        loading_instance.close()
-        console.table([error.response])
-        this.$buefy.toast.open({message: error.message, position: "is-bottom", type: "is-danger"})
-      })
+      this.$http.get(url, {params: params}).then(response => this.http_command_then_process(response, loading_instance, callback)).catch(error => this.http_command_error_process(error, loading_instance))
+    },
+
+    http_command_then_process(response, loading_instance, callback) {
+      loading_instance.close()
+      if (response.data.message) {
+        this.$buefy.toast.open({message: response.data.message})
+      }
+      if (callback) {
+        callback(response.data)
+      }
+    },
+
+    http_command_error_process(error, loading_instance) {
+      loading_instance.close()
+      console.table([error.response])
+      this.$buefy.toast.open({message: error.message, position: "is-bottom", type: "is-danger"})
     },
 
     process_now() {
