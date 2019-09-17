9072b95a703e030e0974a2a6f5e57dfbc57aff85
akicho8 <akicho8@gmail.com>
2019-09-17 14:53:30 +0900

    [fix] 本番でこけるのので alias_method_chain から prepend に変更

--- config/initializers/0250_mysql_utf8mb4_lteq_579_for_production.rb
+++ config/initializers/0250_mysql_utf8mb4_lteq_579_for_production.rb
@@ -7,18 +7,18 @@
 
 if Rails.env.production?
   # http://3.1415.jp/mgeu6lf5/
-  ActiveSupport.on_load :active_record do
-    module ActiveRecord::ConnectionAdapters
-      class AbstractMysqlAdapter
-        def create_table_with_innodb_row_format(table_name, options = {})
-          table_options = options.reverse_merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
-          create_table_without_innodb_row_format(table_name, table_options) do |td|
-            yield td if block_given?
-          end
-        end
-
-        alias_method_chain :create_table, :innodb_row_format
+  module InnodbRowFormat
+    def create_table(table_name, options = {}, **)
+      table_options = options.reverse_merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+      create_table_without_innodb_row_format(table_name, table_options) do |td|
+        yield td if block_given?
       end
+    end
+  end
+
+  ActiveSupport.on_load :active_record do
+    class ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter
+      prepend InnodbRowFormat
     end
   end
 end
