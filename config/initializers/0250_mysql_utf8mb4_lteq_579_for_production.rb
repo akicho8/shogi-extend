b0af08381517afcbf0035ce63cad827747595259
akicho8 <akicho8@gmail.com>
2019-09-17 14:41:31 +0900

    [chore] さくらサーバーで create_table するとき ROW_FORMAT=DYNMIAC になるようにする

---
+++ config/initializers/0250_mysql_utf8mb4_lteq_579_for_production.rb
@@ -0,0 +1,24 @@
+# /usr/local/var/rbenv/versions/2.6.1/lib/ruby/gems/2.6.0/gems/activerecord-6.0.0/lib/active_record/connection_adapters/mysql/schema_statements.rb:144
+# で、 MySQL 5.7.9 以上であれば自動的に ROW_FORMAT になる
+# しかしさくらサーバーでは
+# ssh s mysql --version
+# mysql  Ver 14.14 Distrib 5.5.45, for Linux (x86_64) using readline 5.1
+# なので次のコードが必要
+
+if Rails.env.production?
+  # http://3.1415.jp/mgeu6lf5/
+  ActiveSupport.on_load :active_record do
+    module ActiveRecord::ConnectionAdapters
+      class AbstractMysqlAdapter
+        def create_table_with_innodb_row_format(table_name, options = {})
+          table_options = options.reverse_merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+          create_table_without_innodb_row_format(table_name, table_options) do |td|
+            yield td if block_given?
+          end
+        end
+
+        alias_method_chain :create_table, :innodb_row_format
+      end
+    end
+  end
+end
