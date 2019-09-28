4a785fb9fc6f547eebb24f25750fbb6752f37813
akicho8 <akicho8@gmail.com>
2019-09-28 11:04:04 +0900

    [style] 「今日」を左側に変更

--- app/models/xy_scope_info.rb
+++ app/models/xy_scope_info.rb
@@ -1,7 +1,7 @@
 class XyScopeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: "xy_scope_all",   name: "総合", key_method: :table_key_for_all,   date_visible: true,  table_key_for: -> xy_record, xy_rule_info { xy_rule_info.table_key_for_all                    }, xy_record_scope: -> scope { scope }},
     { key: "xy_scope_today", name: "今日", key_method: :table_key_for_today, date_visible: false, table_key_for: -> xy_record, xy_rule_info { xy_rule_info.time_table_key(xy_record.created_at) }, xy_record_scope: -> scope { scope.where(created_at: Time.current.midnight...Time.current.tomorrow.midnight) }},
+    { key: "xy_scope_all",   name: "総合", key_method: :table_key_for_all,   date_visible: true,  table_key_for: -> xy_record, xy_rule_info { xy_rule_info.table_key_for_all                    }, xy_record_scope: -> scope { scope }},
   ]
 end
