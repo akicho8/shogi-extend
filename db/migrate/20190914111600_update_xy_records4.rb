948d118c0de36822cbc56831906384447ff28abf
akicho8 <akicho8@gmail.com>
2019-10-17 15:53:00 +0900

    [refactor] 嘘Schema情報削除

--- db/migrate/20190914111600_update_xy_records4.rb
+++ db/migrate/20190914111600_update_xy_records4.rb
@@ -1,26 +1,3 @@
-# -*- coding: utf-8 -*-
-# == Schema Information ==
-#
-# Xy record (xy_records as XyRecord)
-#
-# |-------------------+----------------+-------------+-------------+-----------------------------+-------|
-# | name              | desc           | type        | opts        | refs                        | index |
-# |-------------------+----------------+-------------+-------------+-----------------------------+-------|
-# | id                | ID             | integer(8)  | NOT NULL PK |                             |       |
-# | colosseum_user_id | Colosseum user | integer(8)  |             | :user => Colosseum::User#id | A     |
-# | entry_name        | Entry name     | string(255) |             |                             |       |
-# | summary           | Summary        | string(255) |             |                             |       |
-# | xy_rule_key       | Xy rule key    | string(255) | NOT NULL    |                             | B     |
-# | x_count           | X count        | integer(4)  | NOT NULL    |                             |       |
-# | spent_sec         | Spent sec      | float(24)   | NOT NULL    |                             |       |
-# | created_at        | 作成日時       | datetime    | NOT NULL    |                             |       |
-# | updated_at        | 更新日時       | datetime    | NOT NULL    |                             |       |
-# |-------------------+----------------+-------------+-------------+-----------------------------+-------|
-#
-#- Remarks ----------------------------------------------------------------------
-# Colosseum::User.has_many :free_battles, foreign_key: :colosseum_user_id
-#--------------------------------------------------------------------------------
-
 class UpdateXyRecords4 < ActiveRecord::Migration[5.1]
   def change
     add_index :xy_records, :entry_name
