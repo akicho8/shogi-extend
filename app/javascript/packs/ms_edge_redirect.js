56356e58735d2b78d797f2fd637f4a3d035c4bfe
akicho8 <akicho8@gmail.com>
2020-01-22 20:46:34 +0900

    [chore] [Edge問題] 直接ダウンロードページに飛ばす

--- app/javascript/packs/ms_edge_redirect.js
+++ app/javascript/packs/ms_edge_redirect.js
@@ -1,4 +1,5 @@
-if (!window.navigator.userAgent.match(/Windows.*Edge/)) {
+if (window.navigator.userAgent.match(/Windows.*Edge/)) {
   alert("ブラウザが古いため動作しません\n外部サイトに移動しますので新しい Microsoft Edge に更新してください\nもしくは Google Chrome をご利用ください")
-  location.href = "https://support.microsoft.com/ja-jp/help/4501095/download-the-new-microsoft-edge-based-on-chromium"
+  location.href = "https://www.microsoft.com/en-us/edge"
+  // location.href = "https://support.microsoft.com/ja-jp/help/4501095/download-the-new-microsoft-edge-based-on-chromium"
 }
