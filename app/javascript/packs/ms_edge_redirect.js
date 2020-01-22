cf7034c72011f83bb752d56f6c92ab0799c967a4
akicho8 <akicho8@gmail.com>
2020-01-22 20:36:31 +0900

    [refactor] 古い Edge 対策

--- app/javascript/packs/ms_edge_redirect.js
+++ app/javascript/packs/ms_edge_redirect.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  class MsEdgeRedirect {
-    static run() {
-      if (!window.navigator.userAgent.match(/Windows.*Edge/)) {
-        alert("ブラウザが古いため動作しません\n外部サイトに移動しますので新しい Microsoft Edge を入手してください")
-        location.href = "https://support.microsoft.com/ja-jp/help/4501095/download-the-new-microsoft-edge-based-on-chromium"
-      }
-    }
-  }
-  MsEdgeRedirect.run()
-})
+if (!window.navigator.userAgent.match(/Windows.*Edge/)) {
+  alert("ブラウザが古いため動作しません\n外部サイトに移動しますので新しい Microsoft Edge に更新してください\nもしくは Google Chrome をご利用ください")
+  location.href = "https://support.microsoft.com/ja-jp/help/4501095/download-the-new-microsoft-edge-based-on-chromium"
+}
