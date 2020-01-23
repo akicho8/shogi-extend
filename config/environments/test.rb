c1a5c2684ebddf387fdec21715133a3c9e28643a
akicho8 <akicho8@gmail.com>
2020-01-24 01:12:48 +0900

    [chore] generate montage

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # for AppConfig
+  config.to_prepare do
+    Rails.application.config.app_config.deep_merge!({
+        help_function: true, # 使い方の表示
+      })
+  end
 end
