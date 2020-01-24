4bacc13ad22a42379a2239458dc1e7dbd4fe7bab
akicho8 <akicho8@gmail.com>
2020-01-24 20:27:56 +0900

    [chore] 文言改善

--- app/javascript/packs/legacy_browser_verify.js
+++ app/javascript/packs/legacy_browser_verify.js
@@ -1,13 +1,13 @@
 if (window.navigator.userAgent.match(/Windows.*Edge/)) {
-  alert("ブラウザが古いため動作しません\n外部サイトに移動しますので新しい Microsoft Edge に更新してください\nもしくは Google Chrome をご利用ください")
+  alert("ブラウザが古いため表示できません\n外部サイトに移動しますので新しい Microsoft Edge に更新してください\nもしくは Google Chrome をご利用ください")
   location.href = "https://www.microsoft.com/en-us/edge"
 } else if (window.navigator.userAgent.match(/Chrome\/7[678]/)) {
   if (window.navigator.userAgent.match(/Android/)) {
-    alert("ブラウザが古いため動作しません\nスマホに向かって「おっけー ぐーぐる くろーむ こうしん！」と叫んでください")
+    alert("ブラウザが古いため表示できないかもしれません\n端末に向かって「おっけー ぐーぐる くろーむ こうしん！」と叫んでください")
   } else if (window.navigator.userAgent.match(/iPhone|iPad/)) {
-    alert("ブラウザが古いため動作しません\nスマホに向かって「へいしり くろーむ こうしん！」と叫んでください")
+    alert("ブラウザが古いため表示できないかもしれません\n端末に向かって「へいしり くろーむ こうしん！」と叫んでください")
   } else {
-    alert("ブラウザが古いため動作しません\n新しい Google Chrome に更新してください")
+    alert("ブラウザが古いため表示できないかもしれません\n新しい Google Chrome に更新してください")
     location.href = "https://support.google.com/chrome/answer/95414?co=GENIE.Platform%3DDesktop&hl=ja"
   }
 }
