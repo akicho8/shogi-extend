b5e4a0c98104877f5ea39df94e5e7901869308ee
akicho8 <akicho8@gmail.com>
2020-01-24 18:30:30 +0900

    [chore] 古いブラウザチェック

--- app/javascript/packs/ms_edge_redirect.js
+++
@@ -1,5 +0,0 @@
-if (window.navigator.userAgent.match(/Windows.*Edge/)) {
-  alert("ブラウザが古いため動作しません\n外部サイトに移動しますので新しい Microsoft Edge に更新してください\nもしくは Google Chrome をご利用ください")
-  location.href = "https://www.microsoft.com/en-us/edge"
-  // location.href = "https://support.microsoft.com/ja-jp/help/4501095/download-the-new-microsoft-edge-based-on-chromium"
-}
