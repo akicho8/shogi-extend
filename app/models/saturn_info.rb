243a9998683ee721f3013ea0641235b100ece022
akicho8 <akicho8@gmail.com>
2020-01-25 15:59:34 +0900

    [chore] 公開/非公開の文言改善

--- app/models/saturn_info.rb
+++ app/models/saturn_info.rb
@@ -1,7 +1,7 @@
 class SaturnInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :public,  name: "世界に公開", icon: "earth", char_key: "1", },
-    { key: :private, name: "自分だけ",   icon: "lock",  char_key: "0", },
+    { key: :public,  name: "みんな見れる",   icon: "lock-open-variant", char_key: "1", },
+    { key: :private, name: "自分だけ見れる", icon: "lock",              char_key: "0", },
   ]
 end
