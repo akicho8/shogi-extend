ee8ef85504c9585f565d4a3701cc1901d32d4be3
akicho8 <akicho8@gmail.com>
2020-01-29 11:42:28 +0900

    [chore] Chromeが古くても別サイトに飛ばさないようにした

--- app/javascript/packs/legacy_browser_verify.js
+++ app/javascript/packs/legacy_browser_verify.js
@@ -3,11 +3,13 @@ if (window.navigator.userAgent.match(/Windows.*Edge/)) {
   location.href = "https://www.microsoft.com/en-us/edge"
 } else if (window.navigator.userAgent.match(/Chrome\/7[678]\./)) {
   if (window.navigator.userAgent.match(/Android/)) {
-    alert("ブラウザが古いため動作しないかもしれません\n端末に向かって「OK Google くろーむ 更新」と叫んでください")
+    alert("ブラウザが古いため動作しないかもしれません\n端末に向かって「OK Google クローム 更新」と叫んでください")
   } else if (window.navigator.userAgent.match(/iPhone|iPad/)) {
-    alert("ブラウザが古いため動作しないかもしれません\n端末に向かって「Hey しり くろーむ 更新」と叫んでください")
+    alert("ブラウザが古いため動作しないかもしれません\n端末に向かって「Hey Siri クローム 更新」と叫んでください")
   } else {
-    alert("ブラウザが古いため動作しないかもしれません\n新しい Google Chrome に更新してください")
-    location.href = "https://support.google.com/chrome/answer/95414?co=GENIE.Platform%3DDesktop&hl=ja"
+    alert("ブラウザが古いため動作しないかもしれません\nGoogle Chrome を更新してください")
+    // if (window.confirm("ブラウザが古いため動作しないかもしれません\n新しい Google Chrome に更新しますか？")) {
+    //   location.href = "https:support.google.com/chrome/answer/95414?co=GENIE.Platform%3DDesktop&hl=ja"
+    // }
   }
 }
