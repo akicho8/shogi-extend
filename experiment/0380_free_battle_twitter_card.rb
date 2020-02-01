90a881b0a531a4ee436abcab931afe6a8c00998f
akicho8 <akicho8@gmail.com>
2020-02-01 09:59:34 +0900

    [chore] generate montage

--- experiment/0380_free_battle_twitter_card.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require File.expand_path('../../config/environment', __FILE__)
-Dir.chdir Rails.root
-
-free_battle = FreeBattle.first
-free_battle.id               # => 1
-free_battle.updated_at       # => Mon, 06 May 2019 20:27:01 JST +09:00
-free_battle.updated_at.to_i  # => 1557142021
