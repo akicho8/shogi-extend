1647f77914c681215b625cbe5463e006df6149d4
akicho8 <akicho8@gmail.com>
2020-02-06 15:40:27 +0900

    [chore] ダウンロードダイアログをシンプルにする

--- app/models/zip_kifu_info.rb
+++ app/models/zip_kifu_info.rb
@@ -1,8 +1,10 @@
 class ZipKifuInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :kif, },
-    { key: :ki2, },
+    { key: :kif,  },
+    { key: :ki2,  },
+    { key: :csa,  },
+    { key: :sfen, },
   ]
 
   def name
