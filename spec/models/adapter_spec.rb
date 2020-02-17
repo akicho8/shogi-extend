c5f36676c01304463427b410b5783832fcdd4bf3
akicho8 <akicho8@gmail.com>
2020-02-17 21:53:12 +0900

    [test]「局面図に手番が含まれていない」の修正に合わせてテスト変更

--- spec/models/adapter_spec.rb
+++ spec/models/adapter_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe "なんでも棋譜変換", type: :model do
   end
 
   it "shogidb2" do
-    assert { FreeBattle.create!(kifu_body: "https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202").to_cached_kifu(:kif).include?("まで1手で先手の勝ち") }
-    assert { FreeBattle.create!(kifu_body: "https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM").to_cached_kifu(:kif).include?("まで1手で先手の勝ち") }
+    assert { FreeBattle.create!(kifu_body: "https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202").to_cached_kifu(:kif).include?("まで0手で先手の勝ち") }
+    assert { FreeBattle.create!(kifu_body: "https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM").to_cached_kifu(:kif).include?("まで0手で先手の勝ち") }
   end
 
   it "棋王戦" do
--- spec/system/adapter_spec.rb
+++ spec/system/adapter_spec.rb
@@ -36,11 +36,11 @@ RSpec.describe "なんでも棋譜変換", type: :system do
 
   describe "shogidb2" do
     it "指定局面のURLをパースする" do
-      assert_convert("https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202", "まで1手で先手の勝ち")
+      assert_convert("https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202", "まで0手で先手の勝ち")
     end
 
     it "参考図のURLをパースする" do
-      assert_convert("https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM", "まで1手で先手の勝ち")
+      assert_convert("https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM", "まで0手で先手の勝ち")
     end
   end
 
