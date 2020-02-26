899f9d335b4b01d2b2dde18b1edd60e19941096a
akicho8 <akicho8@gmail.com>
2020-02-26 19:11:48 +0900

    [chore] ログインリンクを表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -162,10 +162,6 @@ class ApplicationController < ActionController::Base
     end
 
     def login_display?
-      if AppConfig[:login_link_show_enable]
-        return false
-      end
-
       v = false
       v ||= params[:controller].start_with?("colosseum")
       v ||= params[:controller].start_with?("xy_records")
