f7f0dd63770ebfba8f4566bcd56d1ac00e33c155
akicho8 <akicho8@gmail.com>
2020-02-27 16:24:05 +0900

    [refactor] 直近の対局をKENTOで見るときのURL作成

--- app/models/external_app_info.rb
+++ app/models/external_app_info.rb
@@ -1,8 +1,8 @@
 class ExternalAppInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :piyo_shogi, name: "ぴよ将棋", shortcut_name: "直前開くぴよ", redirect_in_controller: false, other_window: false, },
-    { key: :kento,      name: "KENTO",    shortcut_name: "直前KENTO",    redirect_in_controller: true,  other_window: true,  }, # iOS Safari では other_window が効かない
+    { key: :piyo_shogi, name: "ぴよ将棋", shortcut_name: "直前開くぴよ", redirect_in_controller: false, other_window: false, external_url_func: nil,                                             },
+    { key: :kento,      name: "KENTO",    shortcut_name: "直前KENTO",    redirect_in_controller: true,  other_window: true,  external_url_func: -> h, record { h.kento_app_url_switch(record) }, }, # iOS Safari では other_window が効かない
   ]
 
   def apple_touch_icon
@@ -10,17 +10,11 @@ class ExternalAppInfo
   end
 
   def external_url(h, record)
-
-    # if Rails.env.development?
-    #   return "https://www.example.com/?a=1&b=2"
-    # end
-
-    if key == :kento
-      # raise h.kento_app_url_switch(record).inspect
-      return h.kento_app_url_switch(record)
-      # return h.kento_app_url(kifu: h.full_url_for([record, format: "kif"]))
+    if func = external_url_func
+      return func.call(h, record)
     end
 
+    # デフォルトでは kif を渡す (kento_app_url, piyo_shogi_app_url)
     h.public_send("#{key}_app_url", h.full_url_for([record, format: "kif"]))
   end
 end
