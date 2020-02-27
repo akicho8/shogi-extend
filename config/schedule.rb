1ba140dc513c8daf66eb680176ff9351a253267b
akicho8 <akicho8@gmail.com>
2020-02-27 20:46:59 +0900

    [chore] SSL強制更新したせいで回数制限にひっかかってしまった

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,7 +27,7 @@ end
 
 if @environment == "production"
   every("0 3 * * 6") do
-    command "sudo certbot renew --force-renew --deploy-hook 'systemctl restart httpd'"
+    command "sudo certbot renew --deploy-hook 'systemctl restart httpd'"
   end
 end
 
