a4a521e961b11c7c57077b876bb3bef642cfdb19
akicho8 <akicho8@gmail.com>
2020-03-01 18:05:40 +0900

    [chore] session.delete(:admin_user) の位置移動

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -13,10 +13,9 @@ module Admin
           slack_message(key: "管理画面ログイン", body: [retv, name, password].inspect)
           nil
         end
+        session.delete(:admin_user)
         if retv
           session[:admin_user] ||= name.presence || "(admin_user)"
-        else
-          session.delete(:admin_user)
         end
         retv
       end
@@ -35,74 +34,5 @@ module Admin
     def admin_user
       session[:admin_user]
     end
-
-    # include ControllerShared
-
-    # 管理画面用ではないサーバーで管理画面にアクセスされたとき(ステージングのAPI側で/adminにアクセスしたときにエラーがでればOK)
-    # if Rails.env.production? || Rails.env.staging?
-    #   if AppInfo.api_server? && !AppInfo.dokku_server?
-    #     before_action(prepend: true) do
-    #       render :plain => "アクセスできません", :status => 404
-    #     end
-    #   end
-    # end
-
-    # helper LabelledFormHelper   # FIXME: 消す
-
-    # # 揺らがないように常に nil で実行する
-    # around_action {|_, block| Kamome.anchor(nil, &block) }
-    #
-    # before_action do
-    #   if params[:reset_session]
-    #     reset_session
-    #   end
-    # end
-    #
-    # before_action :staff_login_required
-    #
-    # before_action do |controller|
-    #   controller.asset_host = nil
-    # end
-    #
-    # before_action do |controller|
-    #   controller.instance_eval do
-    #     @module_name ||= "管理"
-    #   end
-    # end
-
-    # include GeneralSupport
-    # include StaffAuthorize
-
-    private
-
-    # def accessible_staff_only_if_login
-    #   if admin_user
-    #     unless admin_user.tag_list.include?("staff")
-    #       deny_access
-    #     end
-    #   end
-    # end
-
-    # # 内部から発生したエラーのみ捕捉
-    # if Rails.env.in?(["development", "test"])
-    # else
-    #   rescue_from ActionController::RoutingError, :with => :show_errors_and_rediret
-    #   rescue_from ActiveRecord::RecordInvalid, :with => :show_errors_and_rediret
-    #   rescue_from ActiveRecord::RecordNotFound, :with => :show_errors_and_rediret
-    #
-    #   def show_errors_and_rediret(exception)
-    #     # render :text => exception.message
-    #     # return
-    #     message = exception.message
-    #     case
-    #     when request.xhr?
-    #       render :ajax_error, {:locals => {:url => polymorphic_path(:admin), :message => message}}
-    #     when request.format.html?
-    #       redirect_to :admin, :alert => message
-    #     else
-    #       raise exception
-    #     end
-    #   end
-    # end
   end
 end
