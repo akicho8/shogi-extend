00f185b5e68673f1ce6c59fdeba07a480e690bb4
akicho8 <akicho8@gmail.com>
2020-03-07 14:05:22 +0900

    [feat] 指導対局の棋士を自動更新

--- app/models/frontend_script/professional_script.rb
+++ app/models/frontend_script/professional_script.rb
@@ -1,28 +1,94 @@
+# |-------------------------------+----------+----------------+------------------+------------------------+----------------------------------+------------------|
+# | 名前                          | 棋士番号 | 生年月日       | 出身地           | 師匠                   | 最終対局日時                     | key              |
+# |-------------------------------+----------+----------------+------------------+------------------------+----------------------------------+------------------|
+# | 黒沢怜生 <Reo Kurosawa>      |      298 | 1992年03月07日 | 埼玉県熊谷市     | 高橋道雄九段           | 17時間前 (2020/03/06 17:09) 棋譜 | Kurosawa_Reo     |
+# | 頼本奈菜 <Nana Yorimoto>     |       58 | 1997年03月12日 | 東京都目黒区     | 飯塚祐紀七段           | 2020/03/05 22:45 棋譜            | Yorimoto_Nana    |
+# | 山本博志 <Hiroshi Yamamoto>  |      316 | 1996年08月13日 | 東京都江東区     | 小倉久史七段           | 2020/02/29 20:55 棋譜            | Yamamoto_Hiroshi |
+# | 加藤一二三 <Hifumi Katoh>    |       64 | 1940年01月01日 | 福岡県嘉麻市     | （故）剱持松二九段     | -                                | Kato_Hifumi      |
+# | 長谷川優貴 <Yuuki Hasegawa>  |       44 | 1995年09月13日 | 兵庫県           | 野田敬三六段           | 2020/02/29 14:59 棋譜            | Hasegawa_Yuki    |
+# | 脇田菜々子 <Nanako Wakita>   |       66 | 1997年03月07日 | 愛知県一宮市     | 中田章道七段           | -                                | Wakita_Nanako    |
+# | 藤井奈々 <Nana Fujii>        |       63 | 1998年03月31日 | 京都府宇治市     | 伊藤博文七段           | -                                | Fujii_Nana       |
+# | 阪口悟 <Satoru Sakaguchi>    |      254 | 1978年08月16日 | 大阪府高石市     | （故）木下　晃七段     | -                                | Sakaguchi_Satoru |
+# | 佐藤紳哉 <Shinya Satoh>      |      224 | 1977年08月29日 | 神奈川県相模原市 | 安恵照剛八段           | -                                | Shinya_Sato      |
+# | 里見咲紀 <Saki Satomi>       |       56 | 1996年04月28日 | 島根県出雲市     | 森　けい二九段         | -                                | Saki_Satomi      |
+# | 窪田義行 <Yoshiyuki Kubota>  |      210 | 1972年05月18日 | 東京都足立区     | （故）花村元司九段     | -                                | Kubota_Yoshiyuki |
+# | 小高佐季子 <Sakiko Odaka>    |       61 | 2002年06月04日 | 千葉県佐倉市     | 田丸昇九段             | -                                | Odaka_Sakiko     |
+# | 大平武洋 <Takehiro Ohira>    |      243 | 1977年05月11日 | 東京都北区       | 桐谷広人七段           | -                                | Ohira_Takehiro   |
+# | 飯島栄治 <Eiji Iijima>       |      236 | 1979年09月16日 | 東京都江東区     | 桜井　昇八段           | -                                | Iijima_Eiji      |
+# | 宮宗紫野 <Shino Miyaso>      |       37 | 1988年04月23日 | 茨城県古河市     | 高橋道雄九段           | -                                | Miyaso_Shino     |
+# | 片上大輔 <Daisuke Katagami>  |      251 | 1981年08月28日 | 広島県広島市     | 森　信雄七段           | -                                | Katagami_Daisuke |
+# | 飯野愛 <Ai Iino>             |       48 | 1986年11月17日 | 東京都世田谷区   | 飯野健二七段           | -                                | Iino_Ai          |
+# | 佐藤慎一 <Shinichi Satoh>    |      271 | 1982年08月16日 | 東京都練馬区     | （故）剱持松二九段     | -                                | Sato_Shinichi    |
+# | 加藤結李愛 <Yuria Kato>      |       65 | 2003年02月15日 | 宮城県仙台市     | 石田和雄九段           | -                                | Kato_Yuria       |
+# | 山根ことみ <Kotomi Yamane>   |       49 | 1998年03月09日 | 愛媛県松山市     | 野田敬三六段           | -                                | Yamane_Kotomi    |
+# | 門倉啓太 <Keita Kadokura>    |      282 | 1987年06月03日 | 東京都豊島区     | 石田和雄九段           | -                                | Kadokura_Keita   |
+# | 高浜愛子 <Aiko Takahama>     |       54 | 1984年10月02日 | 大阪市福島区     | 山本真也六段           | -                                | Takahama_Aiko    |
+# | 村田顕弘 <Akihiro Murata>    |      267 | 1986年07月14日 | 富山県魚津市     | 中田章道七段           | -                                | Murata_Akihiro   |
+# | 相川春香 <Haruka Aikawa>     |       47 | 1994年04月18日 | 東京都           | 安恵照剛八段           | -                                | Aikawa_Haruka    |
+# | 田中悠一 <Yuichi Tanaka>     |      270 | 1985年03月14日 | 長野県長野市     | (故)関根茂九段         | -                                | Tanaka_Yuichi    |
+# | 中村真梨花 <Marika Nakamura> |       30 | 1987年05月20日 | 神奈川県         | 佐伯昌優九段           | -                                | NakamuraMarika   |
+# | 塚田恵梨花 <Erika Tsukada>   |       51 | 1998年08月27日 | 東京都杉並区     | 塚田泰明九段           | -                                | Tsukada_Erika    |
+# | 西田拓也 <Takuya Nishida>    |      309 | 1991年08月25日 | 京都府京都市     | 森信雄七段             | -                                | Nishida_Takuya   |
+# | 山口絵美菜 <Emina Yamaguchi> |       55 | 1994年05月04日 | 宮崎県都城市     | 森　信雄七段           | -                                | Yamaguchi_Emina  |
+# | 北浜健介 <Kensuke Kitahama>  |      211 | 1975年12月28日 | 神奈川県海老名市 | 佐伯昌優九段           | -                                | Kitahama_Kensuke |
+# | 伊藤果 <Hatasu Itou>         |      118 | 1950年09月16日 | 京都府京都市     | （故）高柳敏夫名誉九段 | -                                | Ito_Hatasu       |
+# | 田中寅彦 <Torahiko Tanaka>   |      127 | 1957年04月29日 | 大阪府豊中市     | （故）高柳敏夫名誉九段 | -                                | Tanaka_Torahiko  |
+# | 伊藤明日香 <Asuka Itou>      |       18 | 1982年03月16日 | 東京都           | 伊藤　果八段           | -                                | Ito_Asuka        |
+# | 竹部さゆり <Sayuri Takebe>   |       19 | 1978年06月04日 | 神奈川県逗子市   | 伊藤　果八段           | -                                | Takebe_Sayuri    |
+# | 斎藤明日斗 <Asuto Saito>     |      311 | 1998年07月17日 | 神奈川県川崎市   | 宮田利男八段           | -                                | Saito_Asuto      |
+# |-------------------------------+----------+----------------+------------------+------------------------+----------------------------------+------------------|
+
 module FrontendScript
   class ProfessionalScript < ::FrontendScript::Base
     self.script_name = "指導対局"
 
-    class ProfessionalInfo
-      include ApplicationMemoryRecord
-      memory_record [
-        { key: "Yorimoto_Nana",    name: "頼本奈菜 女流初段", },
-        { key: "Yamamoto_Hiroshi", name: "山本博志 四段",     },
-      ]
+    def script_body
+      user_infos_hash = user_infos_fetch.inject({}) { |a, e| a.merge(e[:key].downcase => e) }
+
+      grade = Swars::Grade.find_by!(key: "十段")
+      users = Swars::User.where(grade: grade).order(created_at: :desc).includes(:memberships) # joins(:memberships) するとデータがないレコードは表示されなくなる
+      users.collect do |user|
+        {}.tap do |row|
+          name = user.key
+          if user_info = user_infos_hash[user.key.downcase]
+            name = user_info["名前"].to_s.remove(/\s*\<.*?\>/)
+          end
+          row["指導棋士"] = h.link_to(name, [:swars, :battles, query: user.key])
+
+          ox = user.memberships.sort_by(&:created_at).collect { |e| e.judge_info.wb_mark }.join
+          row["勝敗"] = h.tag.span(ox, :class => "line_break_on")
+        end
+      end
     end
 
-    def script_body
-      ProfessionalInfo.collect do |e|
-        row = {}
-
-        row["指導棋士"] = h.link_to(e.name, [:swars, :battles, query: e.key])
-
-        count = 0
-        if user = Swars::User.find_by(key: e.key)
-          count = Swars::Membership.where(user: user).count
+    def user_infos_fetch
+      rows = []
+      256.times.with_index(1) do |_, page|
+        html = html_fetch(page) # 404 にはならず空データが返ってくる
+        doc = Nokogiri::HTML(html)
+        list = doc.search("#coach_list li")
+        if list.empty?
+          break
         end
-        row["棋譜数"] = count
+        rows += list.collect do |e|
+          {}.tap do |row|
+            row[:key] = e.at(".coach_name a").text
+            e.search("tr").each do |e|
+              key = e.at("th").text
+              val = e.at("td").text.squish
+              row[key] = val
+            end
+          end
+        end
+      end
+      rows
+    end
 
-        row
+    def html_fetch(page)
+      url = "https://shogiwars.heroz.jp/premium/coach_list?page=#{page}"
+      Rails.cache.fetch(url, :expires_in => 1.days) do
+        Rails.logger.debug(["#{__FILE__}:#{__LINE__}", __method__, "GET", url])
+        open(url, &:read)
       end
     end
   end
