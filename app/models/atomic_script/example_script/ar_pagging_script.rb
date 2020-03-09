843746797de1ef1f1b405b8e23a5a5d9dd9a7f21
akicho8 <akicho8@gmail.com>
2020-03-09 11:33:15 +0900

    [refactor] example_script を移動

---
+++ app/models/atomic_script/example_script/ar_pagging_script.rb
@@ -0,0 +1,22 @@
+module AtomicScript
+  module ExampleScript
+    class ArPaggingScript < ::AtomicScript::ExampleScript::Base
+      self.script_name = "ARページングのテスト"
+
+      def form_parts
+        [
+          {
+            :label   => "per",
+            :key     => :per,
+            :type    => :integer,
+            :default => params[:per],
+          },
+        ]
+      end
+
+      def script_body
+        page_per(AlertLog)
+      end
+    end
+  end
+end
