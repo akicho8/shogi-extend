26c48153d3cd1d53788d77a43630736dee43b3ec
akicho8 <akicho8@gmail.com>
2020-03-15 14:09:44 +0900

    [chore] 鍵外れているアイコンは不要

--- app/models/saturn_info.rb
+++ app/models/saturn_info.rb
@@ -1,7 +1,7 @@
 class SaturnInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :public,  name: "みんな見れる",   icon: "lock-open-variant", char_key: "1", },
-    { key: :private, name: "自分だけ見れる", icon: "lock",              char_key: "0", },
+    { key: :public,  name: "みんな見れる",   icon: nil,    char_key: "1", },
+    { key: :private, name: "自分だけ見れる", icon: "lock", char_key: "0", },
   ]
 end
