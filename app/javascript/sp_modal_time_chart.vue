013c5d624860e34a7b1e824c2f818c7f5cd9218b
akicho8 <akicho8@gmail.com>
2020-03-15 21:22:31 +0900

    [refactor][時間チャート] 縦線表示処理の密結合解消

--- app/javascript/sp_modal_time_chart.vue
+++ app/javascript/sp_modal_time_chart.vue
@@ -106,83 +106,18 @@ const CHART_CONFIG_DEFAULT = {
       },
     },
   },
-
-  // chart.js に縦線を入れる方法
-  // https://stackoverflow.com/questions/30256695/chart-js-drawing-an-arbitrary-vertical-line
-  plugins: [
-    {
-      // Plugin API
-      // https://misc.0o0o.org/chartjs-doc-ja/developers/plugins.html
-      afterDatasetsDraw(instance, easing) {
-        if (instance.config.chart_turn != null) {
-          this.vertical_line_render(instance, instance.config.chart_turn)
-        }
-      },
-
-      // private methods
-
-      line_position_get(instance, chart_turn) {
-        const index_info = instance.config.index_info_hash[chart_turn]
-        if (index_info) {
-          const meta = instance.getDatasetMeta(index_info.datasetIndex)
-          // meta:
-          //   type: "line"
-          //   data: Array(54)
-          //     0: ChartElement
-          //       _chart: Chart {id: 0, ctx: null, canvas: null, config: {…}, width: 828, …}
-          //       _datasetIndex: 1
-          //       _index: 0
-          //       hidden: false
-          //       _xScale: ChartElement {id: "x-axis-0", type: "category", options: {…}, ctx: CanvasRenderingContext2D, chart: Chart, …}
-          //       _yScale: ChartElement {id: "y-axis-0", type: "linear", options: {…}, ctx: CanvasRenderingContext2D, chart: Chart, …}
-          //       _options: {backgroundColor: "rgba(100.00%, 22.00%, 37.60%, 0.10)", borderColor: "rgba(100.00%, 22.00%, 37.60%, 0.60)", borderWidth: 1, hitRadius: 5, hoverBackgroundColor: undefined, …}
-          //       _model: {x: 49.173176518192996, y: 133.96444444444444, skip: false, radius: 1.2, pointStyle: "circle", …}
-          //       _view: {x: 49.173176518192996, y: 133.96444444444444, skip: false, radius: 1.2, pointStyle: "circle", …}
-          //       _start: null
-          //       __proto__: Element
-          //     1: ChartElement {_chart: Chart, _datasetIndex: 1, _index: 1, hidden: false, _xScale: ChartElement, …}
-          //     2: ChartElement {_chart: Chart, _datasetIndex: 1, _index: 2, hidden: false, _xScale: ChartElement, …}
-          //     ...snip...
-          //   dataset: ChartElement {_chart: Chart, _datasetIndex: 1, hidden: false, _scale: ChartElement, _children: Array(54), …}
-          //   controller: ChartElement {chart: Chart, index: 1, _data: Array(54)}
-          //   hidden: null
-          //   xAxisID: "x-axis-0"
-          //   yAxisID: "y-axis-0"
-          //   $filler: {visible: true, fill: "origin", chart: Chart, el: ChartElement, boundary: {…}, …}
-          const data = meta.data
-          return data[index_info.index]._model.x
-        }
-      },
-
-      vertical_line_render(instance, chart_turn) {
-        const lineLeftOffset = this.line_position_get(instance, chart_turn)
-        if (lineLeftOffset) {
-          const scale = instance.scales['y-axis-0']
-          const context = instance.chart.ctx
-
-          // render vertical line
-          context.beginPath()
-          // context.strokeStyle = "hsla(204, 86%,  53%, 0.3)" // $cyan
-          context.strokeStyle = "hsla(204, 86%,  53%, 0.3)" // $cyan
-          context.moveTo(lineLeftOffset, scale.top)
-          context.lineTo(lineLeftOffset, scale.bottom)
-          context.stroke()
-
-          // write label
-          // context.fillStyle = "#ff0000"
-          // context.textAlign = 'center'
-          // context.fillText('MY TEXT', lineLeftOffset, (scale.bottom - scale.top) / 2 + scale.top)
-        }
-      },
-    },
-  ],
 }
 
+import sp_modal_time_chart_vline from './sp_modal_time_chart_vline.js'
+
 export default {
+  mixins: [
+    sp_modal_time_chart_vline, // 縦線表示機能(コメントアウトでOFF)
+  ],
+
   props: {
     record: { required: true, }, // バトル情報
     show_p: { required: true, }, // 時間チャートを表示する？
-    chart_turn: { required: false, },
   },
 
   data() {
@@ -201,8 +136,11 @@ export default {
 
     // chart.js のインスタンスに他のデータを渡す
     // 予約キーと衝突しなかったら自由に引数を追加できる
-    this._chart_config.chart_turn = this.chart_turn
     this._chart_config.__record__ = this.record
+
+    if (this.vline_setup) {
+      this.vline_setup()
+    }
   },
 
   watch: {
@@ -239,13 +177,6 @@ export default {
       }
       window.chart_instance.update()
     },
-
-    chart_turn(v, ov) {
-      this.debug_alert(`watch chart_turn ${ov} => ${v}`)
-      this._chart_config.chart_turn = v
-      window.chart_instance.update()
-    },
-
   },
 
   beforeDestroy() {
@@ -263,47 +194,19 @@ export default {
         if (this.xhr_counter == 0) {
           this.xhr_counter += 1
           this.http_get_command(this.record.show_path, { time_chart_fetch: true }, data => {
-            this._chart_config.data = data.time_chart_params
-            this._chart_config.index_info_hash = this.index_info_hash(data.time_chart_params)
+            const time_chart_params = data.time_chart_params
+
+            this._chart_config.data = time_chart_params
+
+            if (this.index_info_hash) {
+              this._chart_config.index_info_hash = this.index_info_hash(time_chart_params)
+            }
 
             this.xhr_counter = 0
             this.chart_create()
           })
         }
       }
-    },
-
-    // time_chart_params:
-    //   labels: (5) [0, 1, 2, 3, 4]
-    //   datasets[0]
-    //     label: "devuser1"
-    //     data[]
-    //       0: {x: 1, y: +3}
-    //       1: {x: 3, y: +2}
-    //       ...
-    //   datasets[1]
-    //     label: "devuser2"
-    //     data[]
-    //       0: {x: 2, y: -3}
-    //       1: {x: 4, y: -2}
-    //
-    // ↓変換
-    //
-    // {
-    //   1 => {datasetIndex: 0, index: 0}
-    //   2 => {datasetIndex: 1, index: 0} ← 2手目は datasetIndex が 1 つまり後手で、並びの 0 番目のデータを参照すればいいことがわかる
-    //   3 => {datasetIndex: 0, index: 1}
-    //   4 => {datasetIndex: 1, index: 1}
-    // }
-    //
-    index_info_hash(time_chart_params) {
-      const hash = {}
-      time_chart_params.datasets.forEach((dataset, i) => {
-        dataset.data.forEach((e, j) => {
-          hash[e.x] = { datasetIndex: i, index: j }
-        })
-      })
-      return hash
     },
 
     // 時間チャート表示
