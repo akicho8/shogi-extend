3bd991be6163e4bb3a5699fa50c923ffb16c5704
akicho8 <akicho8@gmail.com>
2020-03-24 18:59:09 +0900

    [fix][swars_user_link_to] レアイウト調整

--- app/javascript/swars_user_link_to.vue
+++ app/javascript/swars_user_link_to.vue
@@ -1,57 +1,40 @@
 <template lang="pug">
 b-dropdown.swars_user_link_to(:hoverable="false")
-  a.swars_user_name_with_grade(slot="trigger")
+  a(slot="trigger")
     | {{membership.name_with_grade}}
 
   b-dropdown-item(@click="click_handle")
     b-icon(icon="account" size="is-small")
-    span.a_label プレイヤー情報
+    | プレイヤー情報
 
-  b-dropdown-item(:has-link="true")
-    a(@click="self_window_open(membership.query_user_url)")
-      b-icon(icon="magnify" size="is-small")
-      span.a_label 棋譜検索
+  b-dropdown-item(:href="membership.query_user_url")
+    b-icon(icon="magnify" size="is-small")
+    | 棋譜検索
 
-  b-dropdown-item(:has-link="true")
-    a(:href="membership.twitter_search_url")
-      b-icon(icon="twitter" size="is-small" type="is-info")
-      span.a_label Twitter検索
+  b-dropdown-item(:href="membership.player_info_path" v-if="membership.player_info_path")
+    b-icon(icon="account" size="is-small")
+    | プレイヤー情報
 
-  template(v-if="membership.player_info_path")
-    b-dropdown-item(:has-link="true")
-      a(@click="self_window_open(membership.player_info_path)")
-        b-icon(icon="account" size="is-small")
-        span.a_label プレイヤー情報
+  b-dropdown-item(separator v-if="false")
 
-  b-dropdown-item(:has-link="true")
-    a(:href="membership.swars_home_url")
-      b-icon(icon="open-in-new" size="is-small")
-      span.a_label ウォーズに飛ぶ
+  b-dropdown-item(:href="membership.twitter_search_url")
+    b-icon(icon="twitter" size="is-small" type="is-info")
+    | Twitter検索
 
-  b-dropdown-item(:has-link="true")
-    a(:href="membership.google_search_url")
-      b-icon(icon="google" size="is-small")
-      span.a_label ぐぐる
+  b-dropdown-item(:href="membership.swars_home_url")
+    b-icon(icon="link" size="is-small")
+    | ウォーズに飛ぶ
+
+  b-dropdown-item(:href="membership.google_search_url")
+    b-icon(icon="google" size="is-small")
+    | ぐぐる
 
   b-dropdown-item(v-if="development_p")
     pre
       | {{membership}}
-
-  //- script#user_info_template(type="text/x-template")
-  //-   .modal-card.is-size-7(style="width: auto")
-  //-     .modal-card-head
-  //-       .modal-card-title {{user_key}}
-  //-     .modal-card-body
-  //-       OK
-  //-       canvas#main_canvas(ref="main_canvas")
-  //-     .modal-card-foot
-  //-       button.button(type="button" @click="$parent.close()") 閉じる
 </template>
 
 <script>
-
-// import user_info_show from "./user_info_show.vue"
-
 export default {
   props: {
     membership: { required: true },
@@ -80,6 +63,8 @@ export default {
 </script>
 
 <style lang="sass">
-  @import "stylesheets/bulma_init.scss"
-  .swars_user_link_to
+@import "stylesheets/bulma_init.scss"
+.swars_user_link_to
+  .icon
+    margin-right: 0.5rem
 </style>
