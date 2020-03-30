9519824695ced2547ba51573ba2677b88d10b0e2
akicho8 <akicho8@gmail.com>
2020-03-30 22:44:28 +0900

    [chore] 「もっと取り込む」などを削除

--- app/javascript/packs/swars_battle_index.js
+++ app/javascript/packs/swars_battle_index.js
@@ -60,40 +60,6 @@ window.SwarsBattleIndex = Vue.extend({
       this.async_records_load()
     },
 
-    // // テーブルを表示する条件は検索文字列があること
-    // // フォームに割り当てられている this.query だと変動するので使ってはいけない
-    // table_display_p() {
-    //   return this.$options.query
-    // },
-
-    player_info_click_hadle(e) {
-      if (this.$options.player_info_path) {
-        this.$buefy.dialog.confirm({
-          message: "20秒ぐらいかかる場合がありますがよろしいですか？",
-          confirmText: "取得する",
-          cancelText: "やめとく",
-          onCancel: () => this.talk("やめときました"),
-          onConfirm: () => {
-            this.process_now()
-            location.href = this.$options.player_info_path
-          },
-        })
-      }
-    },
-
-    many_import_handle(e) {
-      this.$buefy.dialog.confirm({
-        message: "1分ぐらいかかる場合がありますがよろしいですか？",
-        confirmText: "取り込む",
-        cancelText: "やめとく",
-        onCancel: () => this.talk("やめときました"),
-        onConfirm: () => {
-          this.process_now()
-          this.$refs.many_import_link.click()
-        },
-      })
-    },
-
     form_submited(e) {
       this.process_now()
 
