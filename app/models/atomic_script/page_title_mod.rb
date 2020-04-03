3899ff85a6c4c0a2057d706f886d42b0d0f65fd7
akicho8 <akicho8@gmail.com>
2020-04-03 23:03:07 +0900

    [refactor] [style] .yumincho の定義除去

--- app/models/atomic_script/page_title_mod.rb
+++ app/models/atomic_script/page_title_mod.rb
@@ -2,8 +2,8 @@ module AtomicScript
   concern :PageTitleMod do
     def to_title_html
       # h.instance_variable_set(:@page_title, script_name)
-      # h.tag.div(h.instance_variable_get(:@page_title), :class => "title is-4 yumincho")
-      h.tag.div(script_name, :class => "title is-3 yumincho")
+      # h.tag.div(h.instance_variable_get(:@page_title), :class => "title is-4")
+      h.tag.div(script_name, :class => "title is-3")
     end
   end
 end
