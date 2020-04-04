a6e6d71b3d757c9730dbd3eddfccd7321e8a05f9
akicho8 <akicho8@gmail.com>
2020-04-04 17:14:02 +0900

    [chore][atomic_script] Vue.js を呼ぶ例を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -119,6 +119,7 @@ import message_link_to from "message_link_to.vue"
 import global_message_link from "global_message_link.vue"
 import swars_user_link_to from "swars_user_link_to.vue"
 import pulldown_menu from "pulldown_menu.vue"
+import buefy_table_wrapper from "buefy_table_wrapper.vue"
 import stopwatch from "stopwatch.vue"
 import simple_board from "simple_board.vue"
 import xy_master from "xy_master.vue"
@@ -169,6 +170,7 @@ Vue.mixin({
     sp_show,
     ox_modal,
     win_lose_circle,
+    buefy_table_wrapper,
 
     // for buefy modal
     tactic_show,
--- spec/controllers/admin/scripts_controller_spec.rb
+++ spec/controllers/admin/scripts_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Admin::ScriptsController, type: :controller do
+  def login!
+    request.env["HTTP_AUTHORIZATION"] = ActionController::HttpAuthentication::Basic.encode_credentials(SecureRandom.hex, Rails.application.credentials[:admin_password])
+  end
+
   it "ログイン必須になっている" do
     assert { Admin::ScriptsController.ancestors.include?(Admin::ApplicationController) }
   end
@@ -13,7 +17,7 @@ RSpec.describe Admin::ScriptsController, type: :controller do
   describe "すべてのスクリプト" do
     Admin::BackendScript.bundle_scripts.each do |e|
       it e.script_name do
-        request.env["HTTP_AUTHORIZATION"] = ActionController::HttpAuthentication::Basic.encode_credentials(SecureRandom.hex, Rails.application.credentials[:admin_password])
+        login!
         get :show, params: { id: e.key }
         expect(response).to have_http_status(:ok)
       end
