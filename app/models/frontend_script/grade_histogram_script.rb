d7b397d93a83f624816bbad85138d9cb8da1045b
akicho8 <akicho8@gmail.com>
2020-04-09 13:30:33 +0900

    [chore] 段級位ヒストグラムにチャート表示

--- app/models/frontend_script/grade_histogram_script.rb
+++ app/models/frontend_script/grade_histogram_script.rb
@@ -9,9 +9,18 @@ module FrontendScript
 
       counts_hash.delete("十段")
 
-      # 6級以下はまとめる
+      # まとめるんじゃなくて削除する
       if true
-        start = "6級"
+        range = "8級".."30級"
+        (Swars::GradeInfo[range.first].code..Swars::GradeInfo[range.last].code).each do |code|
+          grade_info = Swars::GradeInfo[code]
+          counts_hash.delete(grade_info.name)
+        end
+      end
+
+      # 6級以下はまとめる
+      if false
+        start = "10級"
         total = (Swars::GradeInfo[start].code..Swars::GradeInfo.values.last.code).sum do |code|
           counts_hash.delete(Swars::GradeInfo[code].name) || 0
         end
@@ -33,16 +42,36 @@ module FrontendScript
         return rows
       end
 
-      rows.collect do |e|
+      data = {
+        labels: rows.collect { |e| e[:name][0] },
+        datasets: [
+          {
+            label: nil,
+            data: rows.collect { |e| e[:ratio] },
+          },
+        ],
+      }
+
+      info = {
+        data: data,
+        scales_yAxes_ticks: {
+        },
+      }
+
+      out = ""
+      out += h.javascript_tag(%(document.addEventListener('DOMContentLoaded', () => { new Vue({}).$mount("#app") })))
+      out += %(<div id="app"><custom_chart :info='#{info.to_json}' /></div>)
+
+      out += rows.collect { |e|
         row = {}
         row["段級"]   = e[:name]
-        row["割合"]   = "%.3f %%" % [e[:ratio] * 100]
+        row["割合"]   = "%.2f %%" % [e[:ratio] * 100]
         if Rails.env.development? || params[:vervose]
           row["偏差値"] = "%.2f" % e[:deviation_score]
-          row["個数"] = e[:count]
+          row["人数"] = e[:count]
         end
         row
-      end
+      }.to_html
     end
   end
 end
