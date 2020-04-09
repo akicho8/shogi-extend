10a5741d57684a7d0de5c51122b1177f85555178
akicho8 <akicho8@gmail.com>
2020-04-09 19:41:32 +0900

    [fix] out += nil になる

--- app/models/frontend_script/grade_histogram_script.rb
+++ app/models/frontend_script/grade_histogram_script.rb
@@ -62,16 +62,20 @@ module FrontendScript
       out += h.javascript_tag(%(document.addEventListener('DOMContentLoaded', () => { new Vue({}).$mount("#app") })))
       out += %(<div id="app"><custom_chart :info='#{info.to_json}' /></div>)
 
-      out += rows.collect { |e|
-        row = {}
-        row["段級"]   = e[:name]
-        row["割合"]   = "%.2f %%" % [e[:ratio] * 100]
-        if Rails.env.development? || params[:vervose]
-          row["偏差値"] = "%.2f" % e[:deviation_score]
-          row["人数"] = e[:count]
-        end
-        row
-      }.to_html
+      if rows.present?
+        out += rows.collect { |e|
+          row = {}
+          row["段級"]   = e[:name]
+          row["割合"]   = "%.2f %%" % [e[:ratio] * 100]
+          if Rails.env.development? || params[:vervose]
+            row["偏差値"] = "%.2f" % e[:deviation_score]
+            row["人数"] = e[:count]
+          end
+          row
+        }.to_html
+      end
+
+      out
     end
   end
 end
