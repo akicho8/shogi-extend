eb2fac080ad13476c404f102d3f9b59b57048991
akicho8 <akicho8@gmail.com>
2020-04-09 19:26:54 +0900

    [chore] redis info 確認スクリプト追加

---
+++ app/models/backend_script/redis_info_script.rb
@@ -0,0 +1,10 @@
+module BackendScript
+  class RedisInfoScript < ::BackendScript::Base
+    self.category = "その他"
+    self.script_name = "Redis Info"
+
+    def script_body
+      tp Redis.new.info
+    end
+  end
+end
