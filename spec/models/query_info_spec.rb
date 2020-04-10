1234ca8551ab4ad420650a3a771fdd4d0d7a198f
akicho8 <akicho8@gmail.com>
2020-04-10 21:04:22 +0900

    [test] add query_info_spec

--- spec/models/query_info_spec.rb
+++ spec/models/query_info_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe QueryInfo, type: :model do
-  it do
-    assert { QueryInfo.parse("foo:a foo:b,c").attributes == {foo: ["a", "b", "c"]} }
-    assert { QueryInfo.parse("foo:a foo:a").attributes == {foo: ["a"]} }
+RSpec.describe QueryInfo do
+  let :query_info do
+    QueryInfo.parse("foo:a foo:a foo:b,c https://example.com/")
+  end
+
+  it "lookup" do
+    assert { query_info.lookup("foo") == ["a", "b", "c"] }
+  end
+
+  it "lookup_one" do
+    assert { query_info.lookup_one("foo") == "a" }
+  end
+
+  it "urls" do
+    assert { query_info.urls == ["https://example.com/"] }
   end
 end
