78dc537c64f01e0e6b7577cace929d7fb3b058fa
akicho8 <akicho8@gmail.com>
2020-04-12 10:23:50 +0900

    [style] json ボタンサイズ

--- app/models/atomic_script/add_json_link_mod.rb
+++ app/models/atomic_script/add_json_link_mod.rb
@@ -3,7 +3,7 @@ module AtomicScript
   concern :AddJsonLinkMod do
     def response_render(*)
       out = super
-      out << h.tag.div(:class => "buttons") { h.link_to("json", params.merge(format: :json), :class => "button") }
+      out << h.tag.div(:class => "buttons are-small") { h.link_to("json", params.merge(format: :json), :class => "button") }
       out
     end
   end
