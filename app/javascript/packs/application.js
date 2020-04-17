6add6026de08103f5acc7b7c5070a23dc4b68589
akicho8 <akicho8@gmail.com>
2020-04-17 21:37:56 +0900

    [chore] add vue-dump

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,11 @@ Vue.use(Vuex)                   // これは一箇所だけで実行すること
 import VueRouter from "vue-router"
 Vue.use(VueRouter)
 
+// https://www.npmjs.com/package/vue-dump
+import "vue-dump/dist/vue-dump.css"
+import VueDump from "vue-dump/dist/vue-dump.common"
+Vue.use(VueDump)
+
 import VueGtag from "vue-gtag"
 Vue.use(VueGtag, {
   config: {
