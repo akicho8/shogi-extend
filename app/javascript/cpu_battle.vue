32426efa195867a7b9616b48b8a6a75369294db6
akicho8 <akicho8@gmail.com>
2020-04-22 16:55:07 +0900

    [chore] update shogi-player

--- app/javascript/cpu_battle.vue
+++ app/javascript/cpu_battle.vue
@@ -46,7 +46,7 @@
           :flip.sync="flip"
           :setting_button_show="development_p"
           :summary_show="development_p || RAILS_ENV === 'test'"
-          @update:play_mode_long_sfen="play_mode_long_sfen_set"
+          @update:play_mode_advanced_full_moves_sfen="play_mode_long_sfen_set"
           ref="sp_vm"
         )
 
@@ -352,7 +352,7 @@ export default {
 
     // 1手実行
     one_hand_exec() {
-      this.play_mode_long_sfen_set(this.$refs.sp_vm.play_mode_current_sfen)
+      this.play_mode_long_sfen_set(this.$refs.sp_vm.play_mode_full_moves_sfen)
     },
 
     // 待った
