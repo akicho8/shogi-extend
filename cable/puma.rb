6018819e6eecbf5b6bbf5c9491aa19cfe84befb4
akicho8 <akicho8@gmail.com>
2020-05-04 16:00:27 +0900

    [chore] remove cable directory

--- cable/puma.rb
+++
@@ -1,9 +0,0 @@
-environment ENV.fetch("RAILS_ENV") { "development" }
-port ENV.fetch("WS_PORT") { 28081 }
-daemonize true
-state_path "tmp/pids/puma.state"
-pidfile "tmp/pids/puma.pid"
-threads 0,16
-workers 2
-rackup "cable/config.ru"
-stdout_redirect "log/puma_#{ENV["RAILS_ENV"]}.log", "log/puma_#{ENV["RAILS_ENV"]}_error.log", false
