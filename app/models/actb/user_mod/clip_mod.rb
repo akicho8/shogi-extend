f663839b37e21db6e2f5fa25627a568f34c3d748
akicho8 <akicho8@gmail.com>
2020-05-19 14:27:33 +0900

    [fix] モジュールの構造がおかしい

--- app/models/actb/user_mod/clip_mod.rb
+++ app/models/actb/user_mod/clip_mod.rb
@@ -1,42 +1,44 @@
-module Actb::UserMod
-  concern :ClipMod do
-    included do
-      has_many :actb_clip_marks, class_name: "Actb::ClipMark", dependent: :destroy
-    end
-
-    def clip_p(question)
-      actb_clip_marks.where(question: question).exists?
-    end
-
-    # from app/javascript/actb_app/the_history.vue
-    # clip_handle(question_id: question.id, clip_p: clip_p)
-    def clip_handle(params)
-      question = Actb::Question.find(params[:question_id])
-      clip_set(question, params[:clip_p])
-    end
-
-    private
-
-    def clip_set(question, enable)
-      s = actb_clip_marks.where(question: question)
-      if enable
-        if s.exists?
-          diff = 0
-        else
-          s.create!
-          diff = 1
-        end
-        enable = true
-      else
-        if s.exists?
-          s.destroy_all
-          diff = -1
-        else
-          diff = 0
-        end
-        enable = false
+module Actb
+  module UserMod
+    concern :ClipMod do
+      included do
+        has_many :actb_clip_marks, class_name: "ClipMark", dependent: :destroy
       end
-      { clip_p: enable, diff: diff }
+
+      def clip_p(question)
+        actb_clip_marks.where(question: question).exists?
+      end
+
+      # from app/javascript/actb_app/the_history.vue
+      # clip_handle(question_id: question.id, clip_p: clip_p)
+      def clip_handle(params)
+        question = Question.find(params[:question_id])
+        clip_set(question, params[:clip_p])
+      end
+
+      private
+
+      def clip_set(question, enable)
+        s = actb_clip_marks.where(question: question)
+        if enable
+          if s.exists?
+            diff = 0
+          else
+            s.create!
+            diff = 1
+          end
+          enable = true
+        else
+          if s.exists?
+            s.destroy_all
+            diff = -1
+          else
+            diff = 0
+          end
+          enable = false
+        end
+        { clip_p: enable, diff: diff }
+      end
     end
   end
 end
