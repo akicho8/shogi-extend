dfcbe1210749ec67ea01081dfc08e299489f5b8f
akicho8 <akicho8@gmail.com>
2020-06-08 23:31:50 +0900

    [fix] stagingでプロフィール編集後に保存できない

--- config/initializers/0160_cros.rb
+++ config/initializers/0160_cros.rb
@@ -39,6 +39,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # else
     # end
     origins "*"                 # クッキーが欲しいときはそのサーバーを明示的に指定する必要があり)
-    resource "*", headers: :any, methods: [:head, :get, :options]
+    resource "*", headers: :any, methods: [:head, :get, :post, :put, :options]
   end
 end
