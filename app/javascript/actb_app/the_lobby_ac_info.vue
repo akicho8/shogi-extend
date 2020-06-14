eec60181cf71f9046a498dd9c1c6ac5351efcda3
akicho8 <akicho8@gmail.com>
2020-06-14 22:07:20 +0900

    [refactor][style] ステイタスライン

--- app/javascript/actb_app/the_lobby_ac_info.vue
+++ app/javascript/actb_app/the_lobby_ac_info.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
-.the_lobby_ac_info.is-flex
-  p(v-if="development_p")
-    | 購読:{{app.ac_subscription_names}}
-  p(v-if="app.online_user_ids != null")
-    | オンライン:{{app.online_user_ids.length}}人
-  p(v-if="app.matching_list_hash")
-    | 対戦待ち:
-    | {{app.matching_list_hash.marathon_rule.length}},
-    | {{app.matching_list_hash.singleton_rule.length}},
-    | {{app.matching_list_hash.hybrid_rule.length}}
-    | 人
-  p(v-if="app.room_user_ids != null")
-    | 対戦中:{{app.room_user_ids.length}}人
+.the_lobby_ac_info
+  .container.is-flex.has-text-grey.is-size-7
+    .element(v-if="development_p")
+      | 購読{{app.ac_subscription_names}}
+    .element(v-if="app.online_user_ids != null")
+      | オンライン
+      .count {{app.online_user_ids.length}}
+      | 人
+    .element(v-if="app.matching_list_hash")
+      | 対戦待ち
+      .count
+        | {{app.matching_list_hash.marathon_rule.length}},
+        | {{app.matching_list_hash.singleton_rule.length}},
+        | {{app.matching_list_hash.hybrid_rule.length}}
+      | 人
+    .element(v-if="app.room_user_ids != null")
+      | 対戦中
+      .count {{app.room_user_ids.length}}
+      | 人
 </template>
 
 <script>
@@ -32,8 +38,13 @@ export default {
   top: $actb_primary_header_height
   left: 0
   right: 0
-  justify-content: center
-  p
-    font-size: $size-7
-    margin-left: 0.5rem
+  .container
+    justify-content: center
+    margin-top: 0.1rem
+    .element
+      margin-left: 0.5rem
+      .count
+        display: inline
+        font-weight: bold
+        color: $primary
 </style>
