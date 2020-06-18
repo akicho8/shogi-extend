8f87ccf17809656cc02da3bd01eec3b61548d340
akicho8 <akicho8@gmail.com>
2020-06-18 20:25:34 +0900

    [feat] 外部キー制約追加

--- db/migrate/20200505135600_create_actb.rb
+++ db/migrate/20200505135600_create_actb.rb
@@ -49,24 +49,24 @@ class CreateActb < ActiveRecord::Migration[6.0]
     create_table :actb_rooms do |t|
       t.datetime :begin_at,     null: false, index: true, comment: "対戦開始日時"
       t.datetime :end_at,       null: true,  index: true, comment: "対戦終了日時"
-      t.belongs_to :rule,       null: false,              comment: "ルール"
+      t.belongs_to :rule, foreign_key: true, null: false,              comment: "ルール"
       t.timestamps
       t.integer :battles_count, null: false, index: true, default: 0, comment: "連戦数"
     end
 
     create_table :actb_room_memberships do |t|
-      t.belongs_to :room,  null: false,              comment: "対戦部屋"
-      t.belongs_to :user,  null: false,              comment: "対戦者"
+      t.belongs_to :room, foreign_key: true,  null: false,              comment: "対戦部屋"
+      t.belongs_to :user, foreign_key: true,  null: false,              comment: "対戦者"
       t.integer :position, null: false, index: true, comment: "順序"
       t.timestamps
       t.index [:room_id, :user_id], unique: true
     end
 
     create_table :actb_battles do |t|
-      t.belongs_to :room,      null: false,               comment: "部屋"
-      t.belongs_to :parent,    null: true,                comment: "親"
-      t.belongs_to :rule,      null: false,               comment: "ルール"
-      t.belongs_to :final,     null: false,               comment: "結果"
+      t.belongs_to :room, foreign_key: true,      null: false,               comment: "部屋"
+      t.belongs_to :parent, foreign_key: true,    null: true,                comment: "親"
+      t.belongs_to :rule, foreign_key: true,      null: false,               comment: "ルール"
+      t.belongs_to :final, foreign_key: true,     null: false,               comment: "結果"
       t.datetime :begin_at,    null: false,  index: true, comment: "対戦開始日時"
       t.datetime :end_at,      null: true,   index: true, comment: "対戦終了日時"
       t.integer :rensen_index, null: false,  index: true, comment: "連戦数"
@@ -75,9 +75,9 @@ class CreateActb < ActiveRecord::Migration[6.0]
 
     # Actb::BattleMembership
     create_table :actb_battle_memberships do |t|
-      t.belongs_to :battle,    null: false,              comment: "対戦"
-      t.belongs_to :user,      null: false,              comment: "対戦者"
-      t.belongs_to :judge,     null: false,              comment: "勝敗"
+      t.belongs_to :battle, foreign_key: true,    null: false,              comment: "対戦"
+      t.belongs_to :user, foreign_key: true,      null: false,              comment: "対戦者"
+      t.belongs_to :judge, foreign_key: true,     null: false,              comment: "勝敗"
       t.integer :position,     null: false, index: true, comment: "順序"
       t.timestamps
 
@@ -85,15 +85,15 @@ class CreateActb < ActiveRecord::Migration[6.0]
     end
 
     create_table :actb_settings do |t|
-      t.belongs_to :user, null: false, comment: "自分"
-      t.belongs_to :rule, null: false, comment: "選択ルール"
+      t.belongs_to :user, foreign_key: true, null: false, comment: "自分"
+      t.belongs_to :rule, foreign_key: true, null: false, comment: "選択ルール"
       t.timestamps
     end
 
     # Actb::MasterXrecord
     common_columns_define = -> t {
-      t.belongs_to :judge,                                 null: false,               comment: "直前の勝敗"
-      t.belongs_to :final,                                 null: false,               comment: "直前の結果"
+      t.belongs_to :judge, foreign_key: true,                                 null: false,               comment: "直前の勝敗"
+      t.belongs_to :final, foreign_key: true,                                 null: false,               comment: "直前の結果"
       t.integer :battle_count,                             null: false, index: true,  comment: "対戦数"
       t.integer :win_count,                                null: false, index: true,  comment: "勝ち数"
       t.integer :lose_count,                               null: false, index: true,  comment: "負け数"
@@ -109,7 +109,7 @@ class CreateActb < ActiveRecord::Migration[6.0]
       t.integer :rensho_max,                               null: false, index: true,  comment: "連勝数(最大)"
       t.integer :renpai_max,                               null: false, index: true,  comment: "連敗数(最大)"
 
-      t.belongs_to :udemae,                                null: false, index: true,  comment: "ウデマエ"
+      t.belongs_to :udemae, foreign_key: true,                                null: false, index: true,  comment: "ウデマエ"
       t.decimal :udemae_point,     precision: 7, scale: 4, null: false, index: false, comment: "ウデマエの内部ポイント"
       t.decimal :udemae_last_diff, precision: 7, scale: 4, null: false, index: false, comment: "直近ウデマエ変化度"
 
@@ -120,15 +120,15 @@ class CreateActb < ActiveRecord::Migration[6.0]
     }
 
     create_table :actb_master_xrecords do |t|
-      t.belongs_to :user, null: false, index: { unique: true }, comment: "対戦者"
+      t.belongs_to :user, foreign_key: true, null: false, index: { unique: true }, comment: "対戦者"
       common_columns_define.call(t)
     end
 
     # Actb::SeasonXrecord
     create_table :actb_season_xrecords do |t|
       common_columns_define.call(t)
-      t.belongs_to :user,          null: false, index: true, comment: "対戦者"
-      t.belongs_to :season,        null: false,              comment: "期"
+      t.belongs_to :user, foreign_key: true,          null: false, index: true, comment: "対戦者"
+      t.belongs_to :season, foreign_key: true,        null: false,              comment: "期"
       t.integer :create_count,     null: false, index: true, comment: "users.actb_season_xrecord.create_count は users.actb_season_xrecords.count と一致"
       t.integer :generation,       null: false, index: true, comment: "世代(seasons.generationと一致)"
       t.index [:user_id, :season_id], unique: true
@@ -143,37 +143,37 @@ class CreateActb < ActiveRecord::Migration[6.0]
     end
 
     create_table :actb_histories do |t|
-      t.belongs_to :user,     null: false, comment: "自分"
-      t.belongs_to :question, null: false, comment: "出題"
+      t.belongs_to :user, foreign_key: true,     null: false, comment: "自分"
+      t.belongs_to :question, foreign_key: true, null: false, comment: "出題"
       t.timestamps
 
       #  おまけ
-      t.belongs_to :room,        null: false,               comment: "部屋"
-      t.belongs_to :battle,      null: false,               comment: "対戦"
-      t.belongs_to :membership,  null: false,               comment: "自分と相手"
-      t.belongs_to :ox_mark,     null: false,               comment: "解答"
+      t.belongs_to :room, foreign_key: true,        null: false,               comment: "部屋"
+      t.belongs_to :battle, foreign_key: true,      null: false,               comment: "対戦"
+      t.belongs_to :membership, foreign_key: true,  null: false,               comment: "自分と相手"
+      t.belongs_to :ox_mark, foreign_key: true,     null: false,               comment: "解答"
       t.decimal :rating, precision: 8, scale: 4,        null: false, index: false, comment: "レーティング"
     end
 
     ################################################################################
 
     create_table :actb_good_marks do |t|
-      t.belongs_to :user,      null: false,comment: "自分"
-      t.belongs_to :question,  null: false,comment: "出題"
+      t.belongs_to :user, foreign_key: true,      null: false,comment: "自分"
+      t.belongs_to :question, foreign_key: true,  null: false,comment: "出題"
       t.timestamps
       t.index [:user_id, :question_id], unique: true
     end
 
     create_table :actb_bad_marks do |t|
-      t.belongs_to :user,      null: false, comment: "自分"
-      t.belongs_to :question,  null: false, comment: "出題"
+      t.belongs_to :user, foreign_key: true,      null: false, comment: "自分"
+      t.belongs_to :question, foreign_key: true,  null: false, comment: "出題"
       t.timestamps
       t.index [:user_id, :question_id], unique: true
     end
 
     create_table :actb_clip_marks do |t|
-      t.belongs_to :user,      null: false,comment: "自分"
-      t.belongs_to :question,  null: false,comment: "出題"
+      t.belongs_to :user, foreign_key: true,      null: false,comment: "自分"
+      t.belongs_to :question, foreign_key: true,  null: false,comment: "出題"
       t.timestamps
       t.index [:user_id, :question_id], unique: true
     end
@@ -181,21 +181,21 @@ class CreateActb < ActiveRecord::Migration[6.0]
     ################################################################################
 
     create_table :actb_room_messages do |t|
-      t.belongs_to :user,         null: false, comment: "対戦者"
-      t.belongs_to :room,         null: false, comment: "対戦部屋"
+      t.belongs_to :user, foreign_key: true,         null: false, comment: "対戦者"
+      t.belongs_to :room, foreign_key: true,         null: false, comment: "対戦部屋"
       t.string :body, limit: 140, null: false, comment: "発言"
       t.timestamps
     end
 
     create_table :actb_lobby_messages do |t|
-      t.belongs_to :user,         null: false, comment: "対戦者"
+      t.belongs_to :user, foreign_key: true,         null: false, comment: "対戦者"
       t.string :body, limit: 140, null: false, comment: "発言"
       t.timestamps
     end
 
     create_table :actb_question_messages do |t|
-      t.belongs_to :user,          null: false, comment: "発言者"
-      t.belongs_to :question,      null: false, comment: "問題"
+      t.belongs_to :user, foreign_key: true,          null: false, comment: "発言者"
+      t.belongs_to :question, foreign_key: true,      null: false, comment: "問題"
       t.string :body, limit: 140,  null: false, comment: "発言"
       t.timestamps
     end
@@ -203,9 +203,9 @@ class CreateActb < ActiveRecord::Migration[6.0]
     create_table :actb_questions do |t|
       t.string :key, null: false, index: true
 
-      t.belongs_to :user,    null: false, comment: "作成者"
-      t.belongs_to :folder,  null: false, comment: "フォルダ"
-      t.belongs_to :lineage, null: false, comment: "種類"
+      t.belongs_to :user, foreign_key: true,    null: false, comment: "作成者"
+      t.belongs_to :folder, foreign_key: true,  null: false, comment: "フォルダ"
+      t.belongs_to :lineage, foreign_key: true, null: false, comment: "種類"
 
       t.string :init_sfen,               null: false, index: true,  comment: "問題"
       t.integer :time_limit_sec,         null: true,  index: true,  comment: "制限時間(秒)"
@@ -233,7 +233,7 @@ class CreateActb < ActiveRecord::Migration[6.0]
     end
 
     create_table :actb_ox_records do |t|
-      t.belongs_to :question,                    null: false, index: { unique: true }, comment: "問題"
+      t.belongs_to :question, foreign_key: true,                    null: false, index: { unique: true }, comment: "問題"
       t.integer :o_count,                        null: false, index: true,             comment: "正解数"
       t.integer :x_count,                        null: false, index: true,             comment: "不正解数"
       t.integer :ox_total,                       null: false, index: true,             comment: "出題数"
@@ -243,7 +243,7 @@ class CreateActb < ActiveRecord::Migration[6.0]
 
     # MovesAnswer
     create_table :actb_moves_answers do |t|
-      t.belongs_to :question, null: false,               comment: "問題"
+      t.belongs_to :question, foreign_key: true, null: false,               comment: "問題"
       t.integer :moves_count, null: false, index: true,  comment: "N手"
       t.string :moves_str,    null: false, index: false, comment: "連続した指し手"
       t.string :end_sfen,     null: true,  index: false, comment: "最後の局面"
@@ -252,7 +252,7 @@ class CreateActb < ActiveRecord::Migration[6.0]
 
     # 未使用
     create_table :actb_endpos_answers do |t|
-      t.belongs_to :question, null: false,               comment: "問題"
+      t.belongs_to :question, foreign_key: true, null: false,               comment: "問題"
       t.integer :moves_count, null: false, index: true,  comment: "N手"
       t.string :end_sfen,     null: false, index: false, comment: "最後の局面"
       t.timestamps
@@ -260,7 +260,7 @@ class CreateActb < ActiveRecord::Migration[6.0]
 
     # フォルダ
     create_table :actb_folders do |t|
-      t.belongs_to :user,  null: false
+      t.belongs_to :user, foreign_key: true,  null: false
       t.string :type, null: false, comment: "for STI"
       t.timestamps
       t.index [:type, :user_id], unique: true
