285f81757bd97b3f59f4c4006de90b2930eddd36
akicho8 <akicho8@gmail.com>
2020-06-20 18:26:21 +0900

    [chore] 何度も actb 関連の migrate を最初から実行できるようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_211300) do
+ActiveRecord::Schema.define(version: 2020_06_18_211302) do
 
   create_table "acns1_messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id"
