5f133cc1225bf8a9081bf9d49f3ba8bfc6b1de8a
akicho8 <akicho8@gmail.com>
2020-06-21 09:27:25 +0900

    [refactor] [style] [builder] [form] 調整

--- app/javascript/actb_app/the_builder/the_builder_form.vue
+++ app/javascript/actb_app/the_builder/the_builder_form.vue
@@ -1,30 +1,12 @@
 <template lang="pug">
 .the_builder_form
-  b-collapse(:open="tashaga_sakusha_p")
-    button.button(slot="trigger" @click="sound_play('click')") 他者が作者の場合
-    .box
-      b-field(label="作者" label-position="on-border")
-        b-input(v-model="$parent.question.other_author" placeholder="初代大橋宗桂")
-
-      b-field(label="出典メディア" label-position="on-border")
-        b-input(v-model="$parent.question.source_media_name" placeholder="詰パラ")
-
-      b-field(label="出典年月日" label-position="on-border")
-        b-datepicker(v-model="$parent.question.source_published_on" :mobile-native="false")
-
-      b-field(label="出典URL" label-position="on-border")
-        b-input(v-model="$parent.question.source_media_url" type="url")
-
   b-field(label="タイトル" label-position="on-border")
     b-input(v-model="$parent.question.title")
-
-  b-field(label="解説・作意" label-position="on-border")
-    b-input(v-model="$parent.question.description" size="is-small" type="textarea" rows="4")
 
   b-field(label="ヒント" label-position="on-border" v-if="app.config.hint_enable")
     b-input(v-model="$parent.question.hint_desc")
 
-  b-field(label="種類" custom-class="is-small" v-if="$parent.LineageInfo")
+  b-field.lineage_key(label="種類" custom-class="is-small" v-if="$parent.LineageInfo")
     b-select(v-model="$parent.question.lineage.key" expanded)
       option(v-for="row in $parent.LineageInfo.values" :value="row.key") {{row.name}}
 
@@ -40,6 +22,24 @@
         b-radio-button(v-model="$parent.question.folder_key" :native-value="row.key" :type="row.type")
           b-icon(:icon="row.icon" size="is-small")
           span {{row.name}}
+
+  b-field(label="解説" label-position="on-border")
+    b-input(v-model="$parent.question.description" size="is-small" type="textarea" rows="4")
+
+  b-collapse.mt-5(:open="tashaga_sakusha_p")
+    button.button.is-small(slot="trigger" @click="sound_play('click')") 他者が作者の場合
+    .box
+      b-field(label="作者" label-position="on-border")
+        b-input(v-model="$parent.question.other_author" placeholder="初代大橋宗桂")
+
+      b-field(label="出典メディア" label-position="on-border")
+        b-input(v-model="$parent.question.source_media_name" placeholder="詰パラ")
+
+      b-field(label="出典年月日" label-position="on-border")
+        b-datepicker(v-model="$parent.question.source_published_on" :mobile-native="false")
+
+      b-field(label="出典URL" label-position="on-border")
+        b-input(v-model="$parent.question.source_media_url" type="url")
 </template>
 
 <script>
@@ -91,9 +91,11 @@ export default {
 .the_builder_form
   margin: 2.4rem 1.0rem $margin_bottom
   .field:not(:first-child)
-    margin-top: 2rem
+    margin-top: 2.2rem
   .help
     font-size: $size-7
   .b-radio
     font-size: $size-7
+  .lineage_key
+    margin-top: 1.1rem !important // 種類の上が開きすぎているのでせばめる
 </style>
