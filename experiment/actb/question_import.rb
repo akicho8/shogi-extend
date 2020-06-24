bf4aa0b29e674f5f4ada941320b0fb2949b5d9ff
akicho8 <akicho8@gmail.com>
2020-06-24 21:06:48 +0900

    [refactor] 不要なコード削除

--- experiment/actb/question_import.rb
+++ experiment/actb/question_import.rb
@@ -1,8 +1,3 @@
 require "./setup"
-
-# Actb::Question.exists?                 # => true
-# Actb::Question.destroy_all
-
 Actb::Question.import_all
-Actb::Question.count            # => 10
-# >> load: /Users/ikeda/src/shogi_web/app/models/actb/questions.yml
+# >> load: /Users/ikeda/src/shogi_web/app/models/actb/questions.yml (13)
