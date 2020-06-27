2b612ccb26ae1ac705e50e0064c125846d9924e8
akicho8 <akicho8@gmail.com>
2020-06-27 10:19:18 +0900

    [chore][actb] 問題の出し方を調整

--- app/models/actb/best_questions_generator.rb
+++ app/models/actb/best_questions_generator.rb
@@ -4,7 +4,6 @@ module Actb
 
     def initialize(params)
       @params = {
-        :trace => false,
       }.merge(params)
 
       if block_given?
@@ -14,18 +13,18 @@ module Actb
 
     # FIXME: 二人のRの平均に合った問題を出題する
     def generate
-      # s = Question.all
-      # s = s.joins(:folder).where(Folder.arel_table[:type].eq("Actb::ActiveBox"))
-      # s = s.order("rand()")
-      # s = s.limit(Config[:best_questions_limit])
-      # s = Question.where(id: s.ids).order(:difficulty_level)
-      # s.collect(&:as_json_type3)
-
       s = Question.all
-      s = s.active_only
-      s = s.order(:id)
+      s = s.joins(:folder).where(Folder.arel_table[:type].eq("Actb::ActiveBox"))
+      s = s.order("rand()")
       s = s.limit(Config[:best_questions_limit])
+      s = Question.where(id: s.ids).order(:difficulty_level)
       s.collect(&:as_json_type3)
+
+      # s = Question.all
+      # s = s.active_only
+      # s = s.order(:id)
+      # s = s.limit(Config[:best_questions_limit])
+      # s.collect(&:as_json_type3)
     end
   end
 end
