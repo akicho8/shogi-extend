8fadfa471bfb9da530551fe5b0028d059f3b2240
akicho8 <akicho8@gmail.com>
2020-06-30 23:46:41 +0900

    [chore] シングルトンもONにする

--- app/models/actb/rule_info.rb
+++ app/models/actb/rule_info.rb
@@ -4,7 +4,7 @@ module Actb
     include ApplicationMemoryRecord
     memory_record [
       { key: :marathon_rule,  name: "マラソン",     display_p: true,  time_limit_sec: 60, },
-      { key: :singleton_rule, name: "シングルトン", display_p: false, time_limit_sec: 10, },
+      { key: :singleton_rule, name: "シングルトン", display_p: true,  time_limit_sec: 10, },
       { key: :hybrid_rule,    name: "ハイブリッド", display_p: false, time_limit_sec: 60, },
     ]
 
