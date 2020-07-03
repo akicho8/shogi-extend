de2e72387ff16c0a9c88950dd98b3bbcbb9d16cc
akicho8 <akicho8@gmail.com>
2020-07-03 09:00:57 +0900

    [feat] 問題一覧のタブを読み上げる

--- app/javascript/actb_app/the_builder/the_builder_index.vue
+++ app/javascript/actb_app/the_builder/the_builder_index.vue
@@ -148,6 +148,7 @@ export default {
     // タブが変更されたとき
     question_tab_index_change_handle() {
       this.sound_play("click")
+      this.talk2(this.question_current_tab_info.name)
       this.$parent.folder_change_handle(this.question_current_tab_info.key)
     },
 
