53f9f297ac64d09806f13ae39a91230bf5a2eac1
akicho8 <akicho8@gmail.com>
2020-07-05 08:54:19 +0900

    [refactor] [style] [問題詳細] Bulmaで用意された定義に置き換え

--- app/javascript/actb_app/the_question_show_message.vue
+++ app/javascript/actb_app/the_question_show_message.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .the_question_show_message
-  .articles_container(ref="articles_container")
+  .articles_container.mx-4(ref="articles_container")
     article.media(v-for="message in new_question.messages")
       figure.media-left.is_clickable(@click="app.ov_user_info_set(message.user.id)")
         p.image.is-64x64
@@ -13,11 +13,12 @@
             br
             span(v-html="simple_format(auto_link(message.body))")
             br
-            //- small
-            //-   a Like
-            //-   a Reply
-            //-   | · 3 hrs
-            span.diff_time_format
+            template(v-if="false")
+              small
+                a Like
+                a Reply
+                | · 3 hrs
+            span.diff_time_format.is-size-7.has-text-grey-light
               | {{diff_time_format(message.created_at)}}
         nav.level.is-mobile(v-if="false")
           .level-left
@@ -61,56 +62,38 @@ export default {
       new_question: this.question,
     }
   },
-
   created() {
     this.speak_init()
     this.question_subscribe()
   },
-
-  mounted() {
-    this.scroll_to_bottom(this.$refs.messages_box)
-  },
-
   beforeDestroy() {
     this.question_unsubscribe()
   },
-
-  watch: {
-    "new_question.messages": {
-      handler() { this.scroll_to_bottom(this.$refs.messages_box) },
-      immediate: true,
-    },
-  },
-
   methods: {
+    ////////////////////////////////////////////////////////////////////////////////
     question_unsubscribe() {
       this.ac_unsubscribe("$ac_question")
     },
-
     question_subscribe() {
-      this.__assert__(this.$ac_question == null)
+      this.__assert__(this.$ac_question == null, "this.$ac_question == null")
       this.$ac_question = this.ac_subscription_create({channel: "Actb::QuestionChannel", question_id: this.new_question.id})
     },
-
     ////////////////////////////////////////////////////////////////////////////////
-
     speak_init() {
       this.message_body = ""
     },
-
     speak_handle() {
       this.speak(this.message_body)
       this.message_body = ""
     },
-
     speak(message_body) {
       this.$ac_question.perform("speak", {message_body: message_body}) // --> app/channels/actb/question_channel.rb
     },
-
     speak_broadcasted(params) {
       this.app.lobby_speak_broadcasted_shared_process(params)
       this.new_question.messages.push(params.message)
     },
+    ////////////////////////////////////////////////////////////////////////////////
   },
 }
 </script>
