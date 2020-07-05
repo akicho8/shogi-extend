120c0c48956249458b6d0d613b2f5a43524f1594
akicho8 <akicho8@gmail.com>
2020-07-05 12:44:51 +0900

    [chore] b-datepicker 日本語化

--- app/javascript/actb_app/the_builder/the_builder_form.vue
+++ app/javascript/actb_app/the_builder/the_builder_form.vue
@@ -40,6 +40,8 @@
 
       b-field(label="出典年月日" label-position="on-border")
         b-datepicker(v-model="$parent.question.source_published_on" :mobile-native="false")
+          :month-names="[1,2,3,4,5,6,7,8,9,10,11,12]"
+          :day-names="['日','月','火','水','木','金', '土']"
 
       b-field(label="出典URL" label-position="on-border")
         b-input(v-model="$parent.question.source_media_url" type="url")
