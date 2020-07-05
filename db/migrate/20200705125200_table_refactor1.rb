60e185f0217a6a146da573548a36a2df7ad6cf84
akicho8 <akicho8@gmail.com>
2020-07-05 15:03:07 +0900

    [fix] migrate が実行できない

--- db/migrate/20200705125200_table_refactor1.rb
+++ db/migrate/20200705125200_table_refactor1.rb
@@ -3,5 +3,7 @@ class TableRefactor1 < ActiveRecord::Migration[6.0]
     change_table :actb_questions do |t|
       t.rename :other_author, :source_author
     end
+  rescue => error
+    p error
   end
 end
