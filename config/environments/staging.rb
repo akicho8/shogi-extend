1d0f9bc88912e6d67d55a6b11e85dc4073ade1ff
akicho8 <akicho8@gmail.com>
2020-07-09 16:14:27 +0900

    [chore] Rails Update 6.0.2.2 --> 6.0.3.2

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,17 +6,6 @@ Rails.application.configure do
   # ################################################################################ cache_store
   config.cache_store = :redis_cache_store, { db: 8 } # Rails.new
 
-  # ################################################################################ ActionCable
-  # ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.allowed_request_origins = [/https?:\/\/.*/]
-  # config.action_cable.allowed_request_origins = ["https://shogi-flow.xyz"]
-  # config.action_cable.url = "wss://shogi-flow.xyz:28081"
-  config.action_cable.mount_path = "/x-cable"
-
-  # ################################################################################ active_job
-  config.active_job.queue_adapter = :sidekiq
-
   # for AppConfig
   config.to_prepare do
     Rails.application.config.app_config.deep_merge!({
