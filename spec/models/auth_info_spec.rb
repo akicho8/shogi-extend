48cc3ae8fd3394386c9608bb7ff73bec4e03c79b
akicho8 <akicho8@gmail.com>
2020-07-09 16:43:12 +0900

    [chore] generate montage

--- spec/models/auth_info_spec.rb
+++ spec/models/auth_info_spec.rb
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# == Schema Information ==
+#
+# Auth info (auth_infos as AuthInfo)
+#
+# |-----------+--------------+-------------+-------------+------------+-------|
+# | name      | desc         | type        | opts        | refs       | index |
+# |-----------+--------------+-------------+-------------+------------+-------|
+# | id        | ID           | integer(8)  | NOT NULL PK |            |       |
+# | user_id   | User         | integer(8)  | NOT NULL    | => User#id | B     |
+# | provider  | Provider     | string(255) | NOT NULL    |            | A!    |
+# | uid       | Uid          | string(255) | NOT NULL    |            | A!    |
+# | meta_info | 棋譜ヘッダー | text(65535) |             |            |       |
+# |-----------+--------------+-------------+-------------+------------+-------|
+#
+#- Remarks ----------------------------------------------------------------------
+# User.has_many :actb_room_messages
+#--------------------------------------------------------------------------------
+
 require 'rails_helper'
 
 RSpec.describe AuthInfo, type: :model do
