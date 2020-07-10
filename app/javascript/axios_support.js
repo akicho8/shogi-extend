a2e4ced06399514f33a989b6d186098354580814
akicho8 <akicho8@gmail.com>
2020-07-10 23:35:11 +0900

    [fix][test] csrf-tokenがないのに取得しようとしてsystemテストが壊滅

--- app/javascript/axios_support.js
+++ app/javascript/axios_support.js
@@ -1,11 +1,20 @@
 import axios from "axios"
 
+function csrf_token_get() {
+  const el = document.querySelector('meta[name="csrf-token"]')
+  if (el) {
+    return el.getAttribute('content')
+  } else {
+    return ""
+  }
+}
+
 export default axios.create({
   // baseURL: process.env.NODE_ENV === "production" ? "https://www.shogi-extend.com/" : null,
   // baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : null,
   timeout: 1000 * 60 * 3,      // 3min
   headers: {
     "X-Requested-With": "XMLHttpRequest",
-    "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+    "X-CSRF-Token": csrf_token_get(),
   },
 })
