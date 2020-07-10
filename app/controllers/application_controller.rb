e210f6ff5d73619a5a60502ff68aa3f43907b5bd
akicho8 <akicho8@gmail.com>
2020-07-11 07:48:51 +0900

    [fix][staging] GoogleにクロールされているためBASIC認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -117,6 +117,7 @@ class ApplicationController < ActionController::Base
 
   concerning :AdminUserMethods do
     included do
+      before_action :admin_login_required, :if => proc { Rails.env.staging? }
       helper_method :admin_user
     end
 
