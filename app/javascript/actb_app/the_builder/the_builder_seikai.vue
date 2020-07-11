f46a643f26f966a25f7d70f286819a7016ad2b36
akicho8 <akicho8@gmail.com>
2020-07-12 08:24:25 +0900

    [chore] 不要なコードの残骸削除

--- app/javascript/actb_app/the_builder/the_builder_seikai.vue
+++ app/javascript/actb_app/the_builder/the_builder_seikai.vue
@@ -20,7 +20,6 @@
   .buttons.is-centered.konotejunsiikai
     b-button(@click="$parent.edit_stock_handle" :type="{'is-primary': $parent.answer_turn_offset >= 1}")
       | {{$parent.answer_turn_offset}}手目までの手順を正解とする
-    //- b-button(@click="$parent.edit_stock2_handle" :type="{'is-primary': $parent.answer_turn_offset >= 1}") この手順を正解とする2
 
   b-tabs.answer_tabs(v-model="$parent.answer_tab_index" position="is-centered" expanded :animated="true" v-if="$parent.question.moves_answers.length >= 1" @change="sound_play('click')")
     template(v-for="(e, i) in $parent.question.moves_answers")
