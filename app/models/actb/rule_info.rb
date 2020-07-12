57ca5bd1b3fe010029825b6de2e01348344c8766
akicho8 <akicho8@gmail.com>
2020-07-12 18:12:10 +0900

    [chore] 文言改善

--- app/models/actb/rule_info.rb
+++ app/models/actb/rule_info.rb
@@ -2,7 +2,7 @@ module Actb
   class RuleInfo
     include ApplicationMemoryRecord
     memory_record [
-      { key: :marathon_rule,  name: "マラソン",     display_p: true,  time_limit_sec: 60, description: "自分のペースで解いて先にゴールした方が勝ち (初心者向け)"},
+      { key: :marathon_rule,  name: "マラソン",     display_p: true,  time_limit_sec: 60, description: "自分のペースで解いて先にゴールした方が勝ち (はじめての方向け)"},
       { key: :singleton_rule, name: "シングルトン", display_p: true,  time_limit_sec: 10, description: "早押しクイズ形式"},
       { key: :hybrid_rule,    name: "ハイブリッド", display_p: false, time_limit_sec: 60, description: "マラソンルールだけど相手が解いたら次に進んじゃう"},
     ]
