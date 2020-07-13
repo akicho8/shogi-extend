1e978dc0521b692b8f2491cbb14a8c5fe874dda5
akicho8 <akicho8@gmail.com>
2020-07-13 14:17:41 +0900

    [chore] LOGINを見た瞬間に離脱する対策

--- app/javascript/actb_app/the_lobby.vue
+++ app/javascript/actb_app/the_lobby.vue
@@ -23,8 +23,7 @@
   the_lobby_ac_info
   .title.is-4.has-text-centered 将棋トレーニングバトル
   .buttons.is-centered.mt-5
-    b-button.has-text-weight-bold(@click="app.login_required2" type="is-primary" v-if="!app.current_user") LOGIN
-    b-button.has-text-weight-bold(@click="app.start_handle(false)" type="is-primary" v-if="app.current_user") START
+    b-button.has-text-weight-bold(@click="app.start_handle(false)" type="is-primary") START
     b-button.mt-3(@click="app.start_handle(true)") 練習
 
   the_lobby_message
