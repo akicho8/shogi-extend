b569a65952a0a6d478224509011e6990f7be02a7
akicho8 <akicho8@gmail.com>
2020-07-14 21:06:08 +0900

    [fix][atomic_script] page_title を設定しても表示されない

--- app/models/atomic_script/page_title_mod.rb
+++ app/models/atomic_script/page_title_mod.rb
@@ -14,7 +14,7 @@ module AtomicScript
       # h.instance_variable_set(:@page_title, script_name)
       # h.tag.div(h.instance_variable_get(:@page_title), :class => "title is-4")
       if v = (page_title || script_name).presence
-        h.tag.div(script_name, :class => "title is-3")
+        h.tag.div(v, :class => "title is-3")
       end
     end
   end
