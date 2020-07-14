ed7808a6af08bccbc2520fb18a57e0b0aa30a3b7
akicho8 <akicho8@gmail.com>
2020-07-14 22:25:40 +0900

    [chore] [三段リーグ] 紹介画像自動切り替え

--- app/models/service_info.rb
+++ app/models/service_info.rb
@@ -38,7 +38,10 @@ class ServiceInfo
       display_p: true,
       link_path: FrontendScript::ThreeStageLeagueScript.script_link_path,
       title: "三段リーグ成績早見表",
-      image_source: "frontend_script/three_stage_league_player_script_1200x630.png",
+      image_source: [
+        "frontend_script/three_stage_league_script_1200x630.png",
+        "frontend_script/three_stage_league_player_script_1200x630.png",
+      ],
       description: "奨励会三段リーグの成績を見やすくするサービスです",
       feature_items: [
         "スマホに最適化",
