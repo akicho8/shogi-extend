a36f928c7768c45b3aa185d20e88ced186879e8e
akicho8 <akicho8@gmail.com>
2020-07-22 19:18:43 +0900

    [chore] 結果のレーティングの小数を truncate する

--- app/javascript/actb_app/the_result_membership.vue
+++ app/javascript/actb_app/the_result_membership.vue
@@ -24,9 +24,9 @@
     .user_rating.has-text-weight-bold(v-if="app.config.rating_display_p")
       | {{rating_format(xrecord.rating)}}
       span.skill_last_diff.has-text-danger(v-if="xrecord.skill_last_diff > 0")
-        | (+{{xrecord.skill_last_diff}})
+        | (+{{rating_format(xrecord.skill_last_diff)}})
       span.skill_last_diff.has-text-success(v-if="xrecord.skill_last_diff < 0")
-        | ({{xrecord.skill_last_diff}})
+        | ({{rating_format(xrecord.skill_last_diff)}})
 
   .progress_container.mt-1
     the_result_membership_progress(:xrecord="xrecord")
