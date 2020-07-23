4cebfeb06d8beb110ae0a93313c8d0a3ffcb4dde
akicho8 <akicho8@gmail.com>
2020-07-23 09:52:13 +0900

    [chore] 作者不詳の場合は作者欄を空にしておく

--- db/migrate/20200711103812_table_refactor12.rb
+++ db/migrate/20200711103812_table_refactor12.rb
@@ -1,11 +1,16 @@
-class TableRefactor9 < ActiveRecord::Migration[6.0]
+class TableRefactor12 < ActiveRecord::Migration[6.0]
   def up
+    r = Actb::Question.first
+    r.source_author = "不明"
+    r.save!
+
     Actb::SourceAbout.reset_column_information
     Actb::Question.reset_column_information
 
     Actb::SourceAbout.setup
     Actb::Question.find_each do |e|
-      if e.source_author == "作者不詳"
+      if e.source_author == "作者不詳" || e.source_author.to_s.include?("不明")
+        e.source_author = nil
         e.source_about_key = "unknown"
       else
         e.source_about_key = "ascertained"
