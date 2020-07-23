44866f944a8f94da49acf3ee95b4346b9b3232a6
akicho8 <akicho8@gmail.com>
2020-07-23 10:00:11 +0900

    [refactor] migrate

--- db/migrate/20200711103812_table_refactor12.rb
+++ db/migrate/20200711103812_table_refactor12.rb
@@ -1,15 +1,11 @@
 class TableRefactor12 < ActiveRecord::Migration[6.0]
   def up
-    r = Actb::Question.first
-    r.source_author = "不明"
-    r.save!
-
     Actb::SourceAbout.reset_column_information
     Actb::Question.reset_column_information
 
     Actb::SourceAbout.setup
     Actb::Question.find_each do |e|
-      if e.source_author == "作者不詳" || e.source_author.to_s.include?("不明")
+      if e.source_author.to_s.match(/不詳|不明/)
         e.source_author = nil
         e.source_about_key = "unknown"
       else
