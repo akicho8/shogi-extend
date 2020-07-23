ef89199b551e30770819b0b3f98a0eac3a4d9a1c
akicho8 <akicho8@gmail.com>
2020-07-23 10:40:15 +0900

    Revert "[chore][talk] キーのソースを変換後のテキストに変更"
    This reverts commit 197347fb646d240d3a53675ce2f7390f2ae1dd3c.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -75,7 +75,7 @@ class Talk
   end
 
   def unique_key_source_string
-    [polly_params[:voice_id], polly_params[:sample_rate], normalized_text].join(":")
+    [polly_params[:voice_id], polly_params[:sample_rate], source_text].join(":")
   end
 
   def generate_if_not_exist
