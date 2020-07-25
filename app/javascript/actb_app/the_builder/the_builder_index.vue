2302b4bd5584a4e9c0864a7e2d870dc96c50756a
akicho8 <akicho8@gmail.com>
2020-07-25 09:53:41 +0900

    [chore][問題一覧] 長すぎるタイトルを truncate

--- app/javascript/actb_app/the_builder/the_builder_index.vue
+++ app/javascript/actb_app/the_builder/the_builder_index.vue
@@ -87,7 +87,8 @@
 
       b-table-column(custom-key="title"             field="title"             :label="QuestionIndexColumnInfo.fetch('title').short_name"            sortable         :visible="visible_hash.title")
         a(@click.stop="app.ov_question_info_set(props.row.id)")
-          | {{props.row.title}}
+          | {{string_truncate(props.row.title, {length: 20})}}
+
       b-table-column(custom-key="histories_count"   field="histories_count"   :label="QuestionIndexColumnInfo.fetch('histories_count').short_name"  sortable numeric :visible="visible_hash.histories_count")  {{props.row.histories_count}}
       b-table-column(custom-key="ox_record.o_rate"  field="ox_record.o_rate"  :label="QuestionIndexColumnInfo.fetch('o_rate').short_name"  sortable numeric :visible="visible_hash.o_rate")  {{float_to_perc(props.row.ox_record.o_rate)}} %
       b-table-column(custom-key="ox_record.o_count" field="ox_record.o_count" :label="QuestionIndexColumnInfo.fetch('o_count').short_name" sortable numeric :visible="visible_hash.o_count") {{props.row.ox_record.o_count}}
