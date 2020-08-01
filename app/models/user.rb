b17fd29556147f8668c576d1dec0c0b3c1676591
akicho8 <akicho8@gmail.com>
2020-08-01 19:50:02 +0900

    [chore] ゴーストユーザー全削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,23 @@
 class User < ApplicationRecord
   include ::Actb::UserMod
 
+  def self.ghost_destroy_all
+    ids = XyRecord.all.pluck("user_id").uniq
+    s = User.all
+    s = s.where.not(id: ids)
+    s = s.where(["email like ?", "%@localhost%"])
+    s = s.where("sign_in_count <= 1")
+    s = s.where(["name like ?", "%名無し%"])
+    s = s.limit(100)
+    Actb.count_diff do
+      s.each do |r|
+        if r.actb_histories.count == 0 && r.actb_questions.count == 0
+          r.destroy!
+        end
+      end
+    end
+  end
+
   concerning :BasicMethods do
     included do
       scope :random_order, -> { order(Arel.sql("rand()")) }
