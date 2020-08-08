b20ed2a7d9911eed6b22ff92d781050acfc09fb8
akicho8 <akicho8@gmail.com>
2020-08-08 10:24:28 +0900

    [chore] エモーションの種類を追加・厳選

--- app/javascript/actb_app/the_room_emotion.vue
+++ app/javascript/actb_app/the_room_emotion.vue
@@ -2,15 +2,17 @@
 .the_room_emotion.footer_nav.is-flex(v-if="!app.room.bot_user_id || development_p || true")
   //- .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '🥺', say: 'ぴえん'})") 🥺
   //- .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '❗❓', say: ''})") ❗❓
-  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '❗', say: 'びっくり'})") ❗
-  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '💯', say: '100点'})") 💯
   .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '👍', say: 'すごい！'})") 👍
-  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '🍣', say: 'どーぞ'})") 🍣
+  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '💯', say: '100点'})") 💯
+  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '❗', say: 'びっくり'})") ❗
+  //- .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '🍣', say: 'どーぞ'})") 🍣
+  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: 'むずい', say: 'むずい'})") 難
+  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: 'かんたん', say: 'かんたん'})") 簡
   //- .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '🍰', say: 'どーぞ'})") 🍰
-  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '❓', say: 'わからん？'})") ❓
+  //- .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '❓', say: 'わからん'})") ❓
   .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '対よろ', say: 'よろしく'})") よ
   .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '対あり', say: 'ありがとうございました'})") あ
-  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '相手の指し手は自分で操作するらしいよ', say: '相手の指し手は自分で操作するらしいよ。わかりにくいシステムですね'})") 教
+  .item.is_clickable.is-size-5(@click.stop="app.emotion_handle({type: 'is-light', plain: '相手の指し手は自分で操作するらしいよ', say: '相手の指し手は自分で操作するらしいよ。めんどくさいですね'})") 伝
   //- b-icon.item(icon="thumb-up" @click.stop.native="app.emotion_handle({type: 'is-light', plain: 'いいね'})")
 </template>
 
