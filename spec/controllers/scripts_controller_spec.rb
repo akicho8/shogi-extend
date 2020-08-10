cc5a0e3414642219787fd4a5d4fdd8250e4d1840
akicho8 <akicho8@gmail.com>
2020-08-10 09:05:34 +0900

    [refactor] actb_app_script_spec に分離

--- spec/controllers/scripts_controller_spec.rb
+++ spec/controllers/scripts_controller_spec.rb
@@ -25,38 +25,4 @@ RSpec.describe ScriptsController, type: :controller do
       assert { value.find { |e| e["name"] == "嬉野流" } }
     end
   end
-
-  describe "ActbAppScript" do
-    include ActbSupportMethods
-    before do
-      user_login
-    end
-
-    describe "profile_update" do
-      it do
-        put :update, params: { id: "actb-app", remote_action: "profile_update", name: "(user_name1)", profile_description: "a" * (512 + 1) }
-        expect(response).to have_http_status(:ok)
-        retv = JSON.parse(response.body)
-        assert { retv["error_message"] }
-      end
-    end
-
-    describe "battle_request_accept_handle" do
-      def test1
-        put :update, params: { id: "actb-app", remote_action: "battle_request_accept_handle", session_lock_token: SecureRandom.hex }
-        expect(response).to have_http_status(:ok)
-        hash = JSON.parse(response.body)
-        hash["status"]
-      end
-
-      it "相手がもういない" do
-        assert { test1 == "not_have_any_opponent" }
-      end
-
-      it "マッチングした" do
-        Actb::Rule[:marathon_rule].matching_users_add(user1)
-        assert { test1 == "success" }
-      end
-    end
-  end
 end
