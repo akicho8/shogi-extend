3c6ec27f80b5e1c510e1d9e2231fcfbd2bc10eea
akicho8 <akicho8@gmail.com>
2020-08-18 21:50:47 +0900

    [fix] 運営が名前未入力状態で生成される

--- app/models/concerns/user_staff_mod.rb
+++ app/models/concerns/user_staff_mod.rb
@@ -4,14 +4,14 @@ module UserStaffMod
   class_methods do
     # rails r "tp User.sysop"
     def sysop
-      staff_user_factory(key: "sysop", name: "運営", email: AppConfig[:admin_email])
+      staff_create!(key: "sysop", name: "運営", email: AppConfig[:admin_email])
     end
 
     def bot
-      staff_user_factory(key: "bot", name: "BOT", email: AppConfig[:bot_email], race_key: :robot)
+      staff_create!(key: "bot", name: "BOT", email: AppConfig[:bot_email], race_key: :robot)
     end
 
-    def staff_user_factory(attributes)
+    def staff_create!(attributes)
       key = attributes[:key]
       if user = find_by(key: key)
         return user
@@ -19,6 +19,7 @@ module UserStaffMod
       user = create!(attributes.merge(password: Rails.application.credentials.sysop_password))
 
       user.permit_tag_list = "staff"
+      user.name_input_at = Time.current
       user.save!
 
       user
