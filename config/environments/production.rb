7fe83b0c11bfd0fdca1207b6d464a496c87ce6ee
akicho8 <akicho8@gmail.com>
2020-08-18 18:56:07 +0900

    [chore] GitHubログインを有効化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -149,7 +149,7 @@ Rails.application.configure do
         :xy_master_custom_mode         => false,
         :columns_detail_show           => false,
 
-        :available_providers           => [:twitter, :google],
+        :available_providers           => [:twitter, :google, :github],
         :nanasi_login                  => false,
         :id_pw_login                   => false,
       })
