50e2d61a764a9daa43c64aa904aa16dd69b1595e
akicho8 <akicho8@gmail.com>
2020-08-28 22:10:01 +0900

    [refactor] 不要なコード削除

--- app/javascript/xclock_app/support.js
+++ app/javascript/xclock_app/support.js
@@ -1,85 +1,10 @@
-import Vuex from 'vuex'
-
-import Autolinker from 'autolinker'
+import Vuex from "vuex"
 
 export const support = {
   methods: {
-    warning_dialog(message_body) {
-      this.$buefy.dialog.alert({
-        title: "ERROR",
-        message: message_body,
-        canCancel: ["outside", "escape"],
-        type: "is-danger",
-        hasIcon: true,
-        icon: "times-circle",
-        iconPack: "fa",
-        trapFocus: true,
-      })
-    },
-
-    ok_notice(message_body, options = {}) {
-      this.$buefy.toast.open({message: message_body, position: "is-bottom", queue: false})
-      this.say(message_body, options)
-    },
-
-    warning_notice(message_body, options = {}) {
-      this.sound_play("x")
-      this.$buefy.toast.open({message: message_body, position: "is-bottom", type: "is-warning", queue: false})
-      this.say(message_body, options)
-    },
-
-    main_nav_set(display_p) {
-      return
-
-      const el = document.querySelector("#main_nav")
-      if (el) {
-        if (display_p) {
-          el.classList.remove("is-hidden")
-        } else {
-          el.classList.add("is-hidden")
-        }
-      }
-    },
-
-    delay(seconds, block) {
-      return setTimeout(block, 1000 * seconds)
-    },
-
-    delay_stop(delay_id) {
-      if (delay_id) {
-        clearTimeout(delay_id)
-      }
-    },
-
-    // { xxx: true, yyy: false } 形式に変換
-    as_visible_hash(v) {
-      return _.reduce(v, (a, e) => ({...a, [e.key]: e.visible}), {})
-    },
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    message_decorate(str) {
-      str = this.auto_link(str)
-      str = this.simple_format(str)
-      str = this.number_replace_to_question_link(str)
-      return str
-    },
-
-    // ../../../node_modules/autolinker/README.md
-    auto_link(str, options = {}) {
-      return Autolinker.link(str, {newWindow: true, truncate: 30, mention: "twitter", ...options})
-    },
-
-    number_replace_to_question_link(s) {
-      return s.replace(/#(\d+)/, '<a href="/training?question_id=$1">#$1</a>')
-    },
-
-    ////////////////////////////////////////////////////////////////////////////////
-
     say(str, options = {}) {
       this.talk(str, {rate: 1.5, ...options})
     },
-
   },
   computed: {
     ...Vuex.mapState([
