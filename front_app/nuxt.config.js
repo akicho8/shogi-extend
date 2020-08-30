7fb162e9f51c2ce3b85483c0d998a5b26216ee0b
akicho8 <akicho8@gmail.com>
2020-08-30 12:29:59 +0900

    [chore] add ogp tag

--- front_app/nuxt.config.js
+++ front_app/nuxt.config.js
@@ -20,7 +20,8 @@ const config = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: null,
+    titleTemplate: `%s - ${process.env.npm_package_name}`,
     htmlAttrs: {
       lang: "ja",
       class: process.env.NODE_ENV,
@@ -31,6 +32,18 @@ const config = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { name: "action-cable-url", content: (process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "") + "/x-cable" },
+
+      { hid: "og:site_name",   property: "og:site_name",   content: "SHOGI-EXTEND" },
+      { hid: "og:type",        property: "og:type",        content: "website" },
+      { hid: "og:url",         property: "og:url",         content: "https://example.com" },
+      { hid: "og:title",       property: "og:title",       content: "SHOGI-EXTEND" },
+      { hid: "og:description", property: "og:description", content: "将棋に関連する便利サービスを提供するサイトです" },
+      { hid: "og:image",       property: "og:image",       content: "https://example.com/img/ogp/common.jpg" },
+
+      { hid: "og:card",       property: "og:card",       content: "summary_large_image" },
+      { hid: "og:site",       property: "og:site",       content: "@sgkinakomochi" },
+      { hid: "og:creator",    property: "og:creator",    content: "@sgkinakomochi" },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -60,6 +73,11 @@ const config = {
     "~/plugins/axios_mod.js",
   ],
   /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
@@ -75,6 +93,7 @@ const config = {
     '@nuxtjs/proxy',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
+    // ['@nuxtjs/dotenv', { filename: `.env.${process.env.NODE_ENV}` }],
     '@nuxtjs/dotenv',
   ],
   /*
@@ -82,8 +101,8 @@ const config = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    debug: true,
-    proxy: true,
+    debug: process.env.NODE_ENV === 'development',
+    proxy: process.env.NODE_ENV === 'development',
     // // baseURL: process.env.API_URL,
     credentials: true,          // これを入れないと /talk のとき HTML が返ってきてしまう
   },
@@ -91,7 +110,7 @@ const config = {
   proxy: {
     // "/api": "http://localhost:3000",
     // "/api": "http://localhost:3000",
-    "/api": "http://localhost:3000",
+    // "/api": "http://localhost:3000",
     // "/api": {
     //   target: "http://localhost:3000",
     // },
@@ -119,6 +138,20 @@ const config = {
         },
       })
     },
+  },
+
+  // https://nuxtjs.org/guide/runtime-config
+  // 全体で共有する動的(？)な定義
+  publicRuntimeConfig: {
+    FOO: "",
+  },
+  // SSR側での定義で publicRuntimeConfig を上書きする
+  privateRuntimeConfig: {},
+
+  // ここで定義すると vue 側に渡せるが融通が効かないため publicRuntimeConfig を使う方がよい
+  env: {
+    MY_ENV: process.env.MY_ENV,
+    FOO: process.env.FOO,
   },
 }
 
