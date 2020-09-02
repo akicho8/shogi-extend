a036c8a22d74f44cf2218927aa8866194bb548e7
akicho8 <akicho8@gmail.com>
2020-09-03 07:43:49 +0900

    [chore] KENTOのアクセス元がわからないためいったんIPを収集

--- app/controllers/kento_json_mod.rb
+++ app/controllers/kento_json_mod.rb
@@ -37,6 +37,7 @@ module KentoJsonMod
         ip = request.remote_ip
         counter = Swars::Battle.continuity_run_counter("kento")
         if counter == 1
+          slack_message(key: "KENTOアクセス元IP", body: ip)
           Swars::Battle.sometimes_user_import(user_key: current_swars_user.key, page_max: 1)
         end
 
