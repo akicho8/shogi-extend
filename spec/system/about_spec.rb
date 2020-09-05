cc5af7cf3bd38dc2d930b81c148c003ea2acf8c3
akicho8 <akicho8@gmail.com>
2020-09-05 16:15:07 +0900

    [chore][test] /about/* は nuxt 側のテストにする

--- spec/system/about_spec.rb
+++ spec/system/about_spec.rb
@@ -2,20 +2,20 @@ require "rails_helper"
 
 RSpec.describe "about", type: :system do
   it "プライバシー" do
-    visit "/about/privacy_policy"
-    expect(page).to have_content "Rails"
+    visit "http://localhost:4000/about/privacy-policy"
+    expect(page).to have_content "プライバシー"
     doc_image
   end
 
   it "利用規約" do
-    visit "/about/terms"
-    expect(page).to have_content "Rails"
+    visit "http://localhost:4000/about/terms"
+    expect(page).to have_content "利用規約"
     doc_image
   end
 
   it "クレジット" do
-    visit "/about/credit"
-    expect(page).to have_content "Rails"
+    visit "http://localhost:4000/about/credit"
+    expect(page).to have_content "クレジット"
     doc_image
   end
 end
