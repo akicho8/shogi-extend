d89ecdc72e3c28cff4bdb6e257478d1ccf94f8d7
akicho8 <akicho8@gmail.com>
2020-09-09 12:32:26 +0900

    [chore] OGP_BASE_URL -> BASE_URL_OGP

--- front_app/pages/index.vue
+++ front_app/pages/index.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
-.section
-  .columns.is-marginless
-    .column
-      .tabs
-        ul
-          a(href="//0.0.0.0:3000") 3000(Rails)
-      Logo
-      .box
-        div(v-for="(item, key) of items" :key="key")
-          nuxt-link(:to="item.to" exact-active-class="is-active") {{item.title}}
+div
+  b-navbar(type="is-primary")
+    template(slot="brand")
+      b-navbar-item(tag="a" :href="$config.BASE_URL")
+        Logo
+        span.ml-2
+          b SHOGI-EXTEND
+    template(slot="start")
+  .section.pt-5
+    .columns.is-marginless
+      .column
+        .box
+          div(v-for="(item, key) of production_items" :key="key")
+            nuxt-link(:to="item.to" exact-active-class="is-active") {{item.title}}
+      .column
+        .box
+          div(v-for="(item, key) of experiment_items" :key="key")
+            nuxt-link(:to="item.to" exact-active-class="is-active") {{item.title}}
 </template>
 
 <script>
@@ -16,18 +24,22 @@ export default {
   name: "index",
   data () {
     return {
-      items: [
-        { title: 'Home',                     to: { name: 'index'                }, },
-        { title: 'Inspire',                  to: { name: 'inspire'              }, },
-        { title: 'privacy-policy',           to: { name: 'about-privacy-policy' }, },
-        { title: 'terms',                    to: { name: 'about-terms' }, },
-        { title: '対局時計',                 to: { name: 'vs-clock'             }, },
-        { title: 'ストップウォッチ',         to: { name: 'stopwatch'            }, },
-        { title: 'IP',                       to: { name: 'ip-show'              }, },
-        { title: 'async_data_test',          to: { name: 'async_data_test'      }, },
-        { title: 'bulma_test',               to: { name: 'bulma_test'           }, },
-        { title: '$nuxt.isOffline チェック', to: { name: 'online_offline'       }, },
-      ]
+      production_items: [
+        { title: 'Home',                     to: { name: 'index'                      }, },
+        { title: '利用規約',                 to: { name: 'about-terms'                }, },
+        { title: 'プライバシー  ',           to: { name: 'about-privacy-policy'       }, },
+        { title: 'クレジット',               to: { name: 'about-credit'               }, },
+        { title: '対局時計',                 to: { name: 'vs-clock'                   }, },
+        { title: 'ストップウォッチ',         to: { name: 'stopwatch'                  }, },
+      ],
+      experiment_items: [
+        { title: 'フルスクリーン',           to: { name: 'experiment-full_screen'     }, },
+        { title: 'Inspire',                  to: { name: 'experiment-inspire'         }, },
+        { title: 'IP',                       to: { name: 'experiment-ip-show'         }, },
+        { title: 'async_data_test',          to: { name: 'experiment-async_data_test' }, },
+        { title: 'bulma_test',               to: { name: 'experiment-bulma_test'      }, },
+        { title: '$nuxt.isOffline チェック', to: { name: 'experiment-online_offline'  }, },
+      ],
     }
   }
 }
