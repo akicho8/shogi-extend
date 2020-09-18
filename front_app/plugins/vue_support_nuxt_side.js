b974d6c9d9239727737b8758a673bd0166d28c30
akicho8 <akicho8@gmail.com>
2020-09-18 13:52:52 +0900

    [chore] ログインできないので console.log をしかける

--- front_app/plugins/vue_support_nuxt_side.js
+++ front_app/plugins/vue_support_nuxt_side.js
@@ -5,6 +5,7 @@ export default {
       const params = new URLSearchParams()
       params.set("return_to", location.href)
       console.log(`[login_handle][$config] ${this.$config}`)
+      console.log(this.$config)
       debugger
       location.href = this.$config.MY_SITE_URL + `/login?${params}`
     },
