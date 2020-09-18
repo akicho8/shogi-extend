287e2be3b451ec7e29187b8f13bc0cddc7b7a7b7
akicho8 <akicho8@gmail.com>
2020-09-18 13:43:00 +0900

    [chore] ログインできないので debugger をしかける

--- front_app/plugins/vue_support_nuxt_side.js
+++ front_app/plugins/vue_support_nuxt_side.js
@@ -4,6 +4,8 @@ export default {
       // location.href = this.$config.MY_SITE_URL + "/xusers/sign_in"
       const params = new URLSearchParams()
       params.set("return_to", location.href)
+      console.log(`[login_handle][$config] ${this.$config}`)
+      debugger
       location.href = this.$config.MY_SITE_URL + `/login?${params}`
     },
   },
