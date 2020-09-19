f052227aa9f7b6626de625119dad5e6ffd39c701
akicho8 <akicho8@gmail.com>
2020-09-19 12:36:52 +0900

    [chore] doctorのビューはclient-onlyにする

--- front_app/pages/experiment/doctor.vue
+++ front_app/pages/experiment/doctor.vue
@@ -1,33 +1,43 @@
 <template lang="pug">
-.doctor
-  b-navbar(type="is-primary")
-    template(slot="brand")
-      b-navbar-item(tag="nuxt-link" :to="{name:'index'}" exact-active-class="is-active") TOP
-    template(slot="start")
-      b-navbar-item(tag="nuxt-link" :to="{name:'experiment-doctor'}" exact-active-class="is-active") DOCTOR
-  .section
-    .columns
-      .column
-        .title Client
-        .box
-          .block
-            .title $config
-            pre {{$config}}
-          .block
-            .title ENV_BUILD_VERSION
-            pre {{CSR_ENV_BUILD_VERSION}}
-      .column
-        .title Server
-        .box
-          .block
-            .title $config
-            pre {{SSR_config}}
-          .block
-            .title ENV_BUILD_VERSION
-            pre {{SSR_ENV_BUILD_VERSION}}
+client-only
+  .doctor
+    b-navbar(type="is-primary")
+      template(slot="brand")
+        b-navbar-item(tag="nuxt-link" :to="{name:'experiment-doctor'}" exact-active-class="is-active") DOCTOR
+      template(slot="start")
+      template(slot="end")
+        b-navbar-item(tag="nuxt-link" :to="{name:'index'}" exact-active-class="is-active") TOP
+    .section
+      .columns
+        .column
+          .title Client
+          .box
+            .block
+              .title $config
+              pre {{$config}}
+            .block
+              .title ENV_BUILD_VERSION
+              pre {{CSR_ENV_BUILD_VERSION}}
+            .block
+              .title $route.query
+              pre {{$route.query}}
+        .column
+          .title Server
+          .box
+            .block
+              .title $config
+              pre {{SSR_config}}
+            .block
+              .title ENV_BUILD_VERSION
+              pre {{SSR_ENV_BUILD_VERSION}}
+            .block
+              .title CURRENT_TIME
+              pre {{CURRENT_TIME}}
 </template>
 
 <script>
+import dayjs from "dayjs"
+
 export default {
   name: "doctor",
   data() {
@@ -39,6 +49,7 @@ export default {
     return {
       SSR_config: $config,
       SSR_ENV_BUILD_VERSION: process.env.ENV_BUILD_VERSION,
+      CURRENT_TIME: dayjs().format("YYYY-MM-DD HH:mm:ss"),
     }
   },
 }
