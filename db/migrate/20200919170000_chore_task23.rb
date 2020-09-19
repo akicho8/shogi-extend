5ea3cdd99e2ed0ddf5bec3ba7c37781908408eb1
akicho8 <akicho8@gmail.com>
2020-09-19 21:12:45 +0900

    [fix] t.remove :break_through_p がこけてmigrateできない

--- db/migrate/20200919170000_chore_task23.rb
+++ db/migrate/20200919170000_chore_task23.rb
@@ -1,7 +1,6 @@
 class ChoreTask23 < ActiveRecord::Migration[6.0]
   def up
     change_table :tsl_users do |t|
-      t.remove :break_through_p
       t.integer :break_through_generation, index: true, null: true, comment: "プロになったか？"
     end
 
