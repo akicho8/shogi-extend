c140bc4f8a8c48e7698241747f7c46f59c6272db
akicho8 <akicho8@gmail.com>
2020-09-21 14:02:27 +0900

    [refactor] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :foo_articles
-  # resources :about, only: :show
+  root "tops#show"
 
   get "health" => HealthResponder
 
@@ -8,28 +7,16 @@ Rails.application.routes.draw do
     class_name: "::User",
     controllers: {
       omniauth_callbacks: "omniauth_callbacks",
-      # sessions: "users/sessions",
     },
   }
 
-  ################################################################################ 対戦
+  ################################################################################ プロフィール編集など
 
   resources :users
-  # namespace :colosseum do
-  #   resources :battles
-  #   resources :rankings, only: :index
-  # end
-
-  if Rails.env.development? || true
-    root "tops#show"
-  else
-    # root "colosseum/battles#index"
-    root "swars/battles#index"
-  end
 
   ################################################################################ Debug
 
-  match 'eval', to: "eval#run", via: [:get, :post, :put, :delete]
+  match "eval", to: "eval#run", via: [:get, :post, :put, :delete]
 
   ################################################################################ ログアウト
 
@@ -53,29 +40,14 @@ Rails.application.routes.draw do
 
   resources :free_battles, path: "x"
 
-  # resolve "FreeBattle" do |record, options|
-  #   [record, options]
-  # end
-
-  # resolve "FreeBattle" do |record, options|
-  #   [record, options]
-  # end
-
-  # get "adapter", to: "free_battles#new", defaults: { edit_mode: "adapter" }
-
-  # resolve "FreeBattle" do |free_battle, options|
-  #   [ns_prefix, free_battle, options]
-  # end
-
   ################################################################################ 戦法トリガー事典
 
   resources :tactic_notes, path: "tactics", only: [:index, :show]
 
   get "tactics-tree",    to: "tactic_notes#index", defaults: {mode: "tree"},    as: :tree
 
-  ################################################################################ 符号の鬼
+  ################################################################################ 他サービス
 
-  resource :stopwatch, only: [:show, :create]
   resource :simple_board, path: "board", only: [:show, :create]
   resource :share_board, path: "share-board", only: [:show, :create]
 
@@ -91,16 +63,62 @@ Rails.application.routes.draw do
   ################################################################################ 将棋トレーニングバトル
 
   match "training", to: "scripts#show", defaults: { id: "actb_app" }, via: [:get, :update]
-  get "tb" => redirect(path: "/training")
 
-  ################################################################################ Nuxt側
+  ################################################################################ scripts
 
-  direct :vs_clock do |options = {}|
-    if Rails.env.development?
-      "http://localhost:4000/vs-clock"
-    else
-      "/vs-clock"
+  resources :scripts, :path => "script", :only => [:show, :update]
+
+  ################################################################################ api
+
+  # http://0.0.0.0:3000/ping
+  # http://0.0.0.0:3000/ping.json
+  # http://0.0.0.0:3000/ping.txt
+  match "ping(.:format)", to: "api/etc#ping", via: :all, format: nil
+
+  namespace :api, format: "json" do
+    match "ping(.:format)", to: "etc#ping", via: :all, format: nil
+    match "echo(.:format)", to: "etc#echo", via: :all, format: nil
+
+    resource :general, only: [:show] do
+      match "any_source_to", via: [:get, :post]
     end
+    resource :talk, only: [:show, :create]
+    resources :xy_records, path: "xy", only: [:index, :create, :update]
+    resource :cpu_battle, only: [:show, :create]
+    resource :share_board, only: [:show, :create]
+    resource :three_stage_league, only: [:show]
+    resource :three_stage_league_player, only: [:show]
+  end
+
+  ################################################################################ admin
+
+  namespace :admin do
+    resource :session, only: :destroy
+    resource :home, only: :show
+
+    resources :scripts, path: "script", only: [:show, :update]
+
+    resources :example_scripts, path: "example-script", only: [:show, :update]
+
+    root "homes#show"
+  end
+
+  ################################################################################ sidekiq
+
+  # require "sidekiq/monitor/stats" # <mounted-path>/monitor-stats
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/admin/sidekiq" # authenticate :user {} で認証チェックする例がネットにあるが、それは devise のメソッド
+
+  if Rails.env.production?
+    Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+      [user, password] == ["admin", Rails.application.credentials[:admin_password]]
+    end
+  end
+
+  if false
+    # Sidekiq::Web.tabs.replace({"管理画面" => "/admin"}.merge(Sidekiq::Web.tabs))
+    # Sidekiq::Web.tabs["管理画面に戻る"] = "/admin"
+    Sidekiq::Web.tabs["管理画面に戻る"] = "http://localhost:3000/admin"
   end
 
   ################################################################################ 外部リンク
@@ -150,62 +168,5 @@ Rails.application.routes.draw do
 
   direct :staging_app do
     "https://shogi-flow.xyz/"
-  end
-
-  ################################################################################ scripts
-
-  resources :scripts, :path => "script", :only => [:show, :update]
-
-  ################################################################################ api
-
-  # http://0.0.0.0:3000/ping
-  # http://0.0.0.0:3000/ping.json
-  # http://0.0.0.0:3000/ping.txt
-  match "ping(.:format)", to: "api/etc#ping", via: :all, format: nil
-
-  namespace :api, format: "json" do
-    match "ping(.:format)", to: "etc#ping", via: :all, format: nil
-    match "echo(.:format)", to: "etc#echo", via: :all, format: nil
-
-    resource :general, only: [:show] do
-      match "any_source_to", via: [:get, :post]
-    end
-    resource :talk, only: [:show, :create]
-    resources :xy_records, path: "xy", only: [:index, :create, :update]
-    resource :cpu_battle, only: [:show, :create]
-    resource :share_board, only: [:show, :create]
-    resource :three_stage_league, only: [:show]
-    resource :three_stage_league_player, only: [:show]
-  end
-
-  ################################################################################ admin
-
-  namespace :admin do
-    resource :session, only: :destroy
-    resource :home, only: :show
-
-    resources :scripts, path: "script", only: [:show, :update]
-
-    resources :example_scripts, path: "example-script", only: [:show, :update]
-
-    root "homes#show"
-  end
-
-  ################################################################################ sidekiq
-
-  # require 'sidekiq/monitor/stats' # <mounted-path>/monitor-stats
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/admin/sidekiq' # authenticate :user {} で認証チェックする例がネットにあるが、それは devise のメソッド
-
-  if Rails.env.production?
-    Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-      [user, password] == ["admin", Rails.application.credentials[:admin_password]]
-    end
-  end
-
-  if false
-    # Sidekiq::Web.tabs.replace({"管理画面" => "/admin"}.merge(Sidekiq::Web.tabs))
-    # Sidekiq::Web.tabs["管理画面に戻る"] = "/admin"
-    Sidekiq::Web.tabs["管理画面に戻る"] = "http://localhost:3000/admin"
   end
 end
