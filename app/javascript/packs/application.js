e4402812533e7c4422e25dc8726b9170167438f2
akicho8 <akicho8@gmail.com>
2020-10-04 11:50:23 +0900

    [refactor] app/javascript/* 内の buefy_table_wrapper など削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,6 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "modulable_crud.js"
-import "global_variable_set.js"
 
 //////////////////////////////////////////////////////////////////////////////// Vue
 
