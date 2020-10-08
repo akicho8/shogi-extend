5609155500c7158a7d3ace049bf699bbd0763c04
akicho8 <akicho8@gmail.com>
2020-10-09 08:21:22 +0900

    [fix] isMobile の参照が遠回りで front_app の名前を変更すると動かない

--- front_app/plugins/vue_mixins/vue_piyo_shogi.js
+++ front_app/plugins/vue_mixins/vue_piyo_shogi.js
@@ -1,7 +1,7 @@
 // https://www.studiok-i.net/kifu/?sfen=position%20sfen%20lnsgkgsnl%2F1r5b1%2Fp1pppp1pp%2F1p4p2%2F9%2F2P4P1%2FPP1PPPP1P%2F1B5R1%2FLNSGKGSNL%20b%20-%201&game_name=&sente_name
 // https://www.studiok-i.net/ps/?sfen=position%20sfen%20lnsgkgsnl%2F1r5b1%2Fp1pppp1pp%2F1p4p2%2F9%2F2P4P1%2FPP1PPPP1P%2F1B5R1%2FLNSGKGSNL%20b%20-%201
 
-import { isMobile } from "../../../front_app/components/models/isMobile.js"
+import { isMobile } from "../../components/models/isMobile.js"
 
 export default {
   methods: {
--- front_app/plugins/vue_mixins/vue_support.js
+++ front_app/plugins/vue_mixins/vue_support.js
@@ -7,7 +7,7 @@ import SfenParser from "shogi-player/src/sfen_parser"
 
 const strip_tags = require('striptags')
 
-import { isMobile } from "../../../front_app/components/models/isMobile.js"
+import { isMobile } from "../../components/models/isMobile.js"
 
 import Autolinker from 'autolinker'
 
