b37240d4e43e5fda163517fb32e79055f90f32f2
akicho8 <akicho8@gmail.com>
2020-10-11 17:59:59 +0900

    [fix][style] プロフィール編集時にナビバーが赤い

--- front_app/components/User/UserProfileEditForm.vue
+++ front_app/components/User/UserProfileEditForm.vue
@@ -4,7 +4,7 @@
     div valid_p        = {{valid_p}}
     div name_invalid_p = {{name_invalid_p}}
 
-  b-navbar(type="is-danger" wrapper-class="container" :mobile-burger="false" spaced)
+  b-navbar(type="is-primary" wrapper-class="container" :mobile-burger="false" spaced)
     template(slot="start")
       b-navbar-item.has-text-weight-bold(@click="cancel_handle") キャンセル
     template(slot="end")
