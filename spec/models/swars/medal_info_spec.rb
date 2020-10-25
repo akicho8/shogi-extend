b70f7728ff4e0cffa2c0faa4ab10e6d0b65611e8
akicho8 <akicho8@gmail.com>
2020-10-25 13:49:42 +0900

    [chore][medal] チョコレート囲い

--- spec/models/swars/medal_info_spec.rb
+++ spec/models/swars/medal_info_spec.rb
@@ -51,6 +51,7 @@ module Swars
         assert { b("裏アヒル囲い").include?("レアマン")           }
         assert { b("カニカニ金").include?("カニ執着マン")         }
         assert { b("ダイヤモンド美濃").include?("ダイヤマン")     }
+        assert { b("チョコレート囲い").include?("チョコレートマン")   }
         assert { b("音無しの構え").include?("音無しマン")         }
         assert { b("筋違い角").include?("筋違い角おじさん")       }
         assert { b("いちご囲い").include?("スイーツマン")         }
