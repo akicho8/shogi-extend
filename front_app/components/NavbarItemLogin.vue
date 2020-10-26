15706a100a3575c5fec277a2dd4a5a0973bb4c17
akicho8 <akicho8@gmail.com>
2020-10-26 21:12:24 +0900

    [fix] ログインを押したときに音がでない

--- front_app/components/NavbarItemLogin.vue
+++ front_app/components/NavbarItemLogin.vue
@@ -1,4 +1,4 @@
 <template lang="pug">
-b-navbar-item.has-text-weight-bold(v-if="!g_current_user" :href="login_url_build()")
+b-navbar-item.has-text-weight-bold(v-if="!g_current_user" :href="login_url_build()" @click="sound_play('click')")
   | ログイン
 </template>
