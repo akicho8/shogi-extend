b250029d535cd5154fe43b2216df5454345c52ef
akicho8 <akicho8@gmail.com>
2020-11-05 08:59:17 +0900

    [refactor] base_channel の共通処理を channel.rb に移動

--- app/channels/actb/base_channel.rb
+++ app/channels/actb/base_channel.rb
@@ -1,46 +1,8 @@
 module Actb
   class BaseChannel < ApplicationCable::Channel
-    delegate :redis, :once_run, :already_run?, to: "self.class"
-
     class << self
-      def redis
-        @redis ||= Redis.new(db: AppConfig.fetch(:redis_db_for_actb))
-      end
-
-      def redis_clear
-        redis.flushdb
-      end
-
-      # if Actb::BaseChannel.once_run("shared key", expires_in: 1.minute)
-      #   ...
-      # end
-      def once_run(key, options = {})
-        raise ArgumentError unless key
-
-        if key.kind_of? Array
-          raise ArgumentError unless key.flatten.all?
-          key = key.join("/")
-        end
-
-        options = {
-          expires_in: 1.hours,
-        }.merge(options)
-
-        # https://qiita.com/shiozaki/items/b746dc4bb5e1e87c0528
-        values = redis.multi do
-          redis.incr(key)
-          redis.expire(key, options[:expires_in])
-        end
-
-        counter = values.first
-
-        Rails.logger.debug([__method__, {key: key, counter: counter, expires_in: redis.ttl(key)}])
-
-        counter == 1
-      end
-
-      def already_run?(key, options = {})
-        !once_run(key, options)
+      def redis_db_index
+        AppConfig.fetch(:redis_db_for_actb)
       end
     end
   end
