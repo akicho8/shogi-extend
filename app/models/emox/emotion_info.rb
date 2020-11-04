b6c87b133191c6d6d85a2fdb816346167f48fb4a
akicho8 <akicho8@gmail.com>
2020-11-04 23:38:56 +0900

    [chore] 文言改善

--- app/models/emox/emotion_info.rb
+++ app/models/emox/emotion_info.rb
@@ -2,12 +2,12 @@ module Emox
   class EmotionInfo
     include ApplicationMemoryRecord
     memory_record [
-      { name: "お",   message: "おまえはもう詰んでいる", voice: "おまえはもう詰んでいる", },
-      { name: "な",   message: "なんだと!?",             voice: "なんだと!?",             },
-      { name: "た",   message: "ただやん",               voice: "ただやん",               },
-      { name: "や",   message: "やばい",                 voice: "やばい",                 },
-      { name: "よ",   message: "対よろ",                 voice: "よろしく",               },
-      { name: "あ",   message: "対あり",                 voice: "ありがとうございました", },
+      { name: "つ",   message: "詰んでる",   voice: "詰んでる",    },
+      { name: "な",   message: "なんだと!?", voice: "なんだと!?",  },
+      { name: "た",   message: "ただやん",   voice: "ただやん",    },
+      { name: "や",   message: "やばい",     voice: "やばい",      },
+      { name: "よ",   message: "対よろ",     voice: "たいよろ",    },
+      { name: "あ",   message: "対あり",     voice: "たいあり",    },
     ]
   end
 end
