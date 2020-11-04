6e65b1b37fc1f2db47abb80919add5b7109ef0a6
akicho8 <akicho8@gmail.com>
2020-11-05 08:58:16 +0900

    [chore] 棋譜へのリンク追加

--- front_app/components/Emox/EmoxResult.vue
+++ front_app/components/Emox/EmoxResult.vue
@@ -2,7 +2,7 @@
 .EmoxResult
   a.delete.page_delete.is-large.is_top_left_fixed(@click="base.room_leave_handle")
 
-  .win_lose_container.has-text-centered.is-size-3.has-text-weight-bold.mt-5
+  .block.has-text-centered.is-size-3.has-text-weight-bold.mt-6
     template(v-if="base.current_membership.judge.key === 'win'")
       .has-text-danger
         | YOU WIN !
@@ -13,8 +13,11 @@
       .has-text-info
         | DRAW !
 
-  .final_container.has-text-centered.is-size-7(v-if="base.battle.final.key === 'f_disconnect' || base.battle.final.key === 'f_timeout'")
+  .block.has-text-centered.is-size-7(v-if="base.battle.final.key === 'f_disconnect' || base.battle.final.key === 'f_timeout'")
     | {{base.battle.final.name}}
+
+  .block.is-flex.is-justify-content-center
+    b-button(tag="nuxt-link" :to="{name: 'share-board', query: {body: base.vs_share_sfen, image_view_point: 'black'}}" size="is-small" type="is-text") 棋譜
 
   .box.is-shadowless(v-if="base.debug_read_p")
     .buttons.is-centered.are-small
