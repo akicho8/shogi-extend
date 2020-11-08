f2b3a4f3dfb541854419e747de623e46a0c07fb2
akicho8 <akicho8@gmail.com>
2020-11-08 13:17:36 +0900

    [chore] SNS経由ログインのとき loading を出す

--- front_app/components/SnsLoginContainer.vue
+++ front_app/components/SnsLoginContainer.vue
@@ -3,7 +3,7 @@
   .card-content
     .is-flex.is-justify-content-center.is-flex-direction-column
       template(v-for="e in SocialMediaInfo.values")
-        b-button.has-text-weight-bold(rounded :type="`is-${e.key}`" :icon-left="e.key" tag="a" :href="sns_auth_url(e)" @click="sound_play('click')")
+        b-button.has-text-weight-bold(rounded :type="`is-${e.key}`" :icon-left="e.key" @click="click_handle(e)")
           span {{e.name}} でログインする
 </template>
 
@@ -23,6 +23,12 @@ class SocialMediaInfo extends MemoryRecord {
 export default {
   name: "SnsLoginContainer",
   methods: {
+    click_handle(e) {
+      this.sound_play('click')
+      this.$emit("close")
+      this.$buefy.loading.open()
+      location.href = this.sns_auth_url(e)
+    },
     sns_auth_url(e) {
       return this.login_url_build({social_media_key: e.key})
     },
