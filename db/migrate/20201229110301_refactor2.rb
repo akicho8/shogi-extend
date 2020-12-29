e769978afd3ad1a8fb8392e2db0a672ee9536d63
akicho8 <akicho8@gmail.com>
2020-12-29 15:25:40 +0900

    [fix] まだmigrateがこける

--- db/migrate/20201229110301_refactor2.rb
+++ db/migrate/20201229110301_refactor2.rb
@@ -1,7 +1,7 @@
 class Refactor2 < ActiveRecord::Migration[6.0]
   def change
     change_table :xy_master_time_records do |t|
-      t.rename :xy_rule_key, :rule_key
+      t.rename :xy_rule_key, :rule_key rescue nil
     end
     XyMaster::TimeRecord.reset_column_information
     XyMaster::TimeRecord.find_each do |e|
