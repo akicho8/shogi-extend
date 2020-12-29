c442953009bbca037200529489540f8663e0971e
akicho8 <akicho8@gmail.com>
2020-12-29 15:22:03 +0900

    [fix] migrateがこける

--- db/migrate/20201229110301_refactor2.rb
+++ db/migrate/20201229110301_refactor2.rb
@@ -5,7 +5,7 @@ class Refactor2 < ActiveRecord::Migration[6.0]
     end
     XyMaster::TimeRecord.reset_column_information
     XyMaster::TimeRecord.find_each do |e|
-      e.rule_key = e.rule_key.sub(/xy_/, "")
+      e.rule_key = e.read_attribute(:rule_key).sub(/xy_/, "")
       e.save!
     end
   end
