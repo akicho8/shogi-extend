82b179bfd2204fced6353b7b1b412b2b6b4be2c0
akicho8 <akicho8@gmail.com>
2020-12-29 15:41:38 +0900

    [fix] それでもmigrateがこける(3)

--- db/migrate/20201229110304_refactor4.rb
+++ db/migrate/20201229110304_refactor4.rb
@@ -8,13 +8,14 @@ class Refactor4 < ActiveRecord::Migration[6.0]
 
     XyMaster::TimeRecord.find_each do |e|
       e.rule_id = XyMaster::Rule.fetch(e.rule_key).id
-      e.save!
+      e.save!(validate: false, touch: false)
     end
-
-    XyMaster::RuleInfo.rebuild
 
     change_table :xy_master_time_records do |t|
       t.remove :rule_key
     end
+    
+    XyMaster::RuleInfo.rebuild
+
   end
 end
