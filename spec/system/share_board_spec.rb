c6dc0e149591c475114037d1b5e46e57f39aea63
akicho8 <akicho8@gmail.com>
2021-01-09 10:44:26 +0900

    [test][share-board] テスト追加

--- spec/system/share_board_spec.rb
+++ spec/system/share_board_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe "共有将棋盤", type: :system do
     assert_selector(".CustomShogiPlayer .is_viewpoint_white")
     doc_image
   end
+
+  it "合言葉を含むURLから来てハンドルネーム入力して接続して駒を動かす" do
+    visit "http://localhost:4000/share-board?room_code=foo" # 合言葉を含むURLから来た
+    assert_selector(".RealtimeShareModal")                  # 「合言葉の設定と共有」のモーダル表示
+    expect(page).to have_content "合言葉の設定と共有"       # 〃
+    first(".RealtimeShareModal input").set("(漢字)")        # ハンドルネーム入力
+    first(".modal-card-foot .button.is-primary").click      # 確定
+    expect(page).to have_content "(漢字)"                   # 入力したハンドルネームの人が参加している
+    first(".place_7_7").click                               # 77の歩を持って
+    first(".place_7_6").click                               # 77に移動
+    expect(page).to have_content "☗7六歩(77)"              # 符号が表示されている
+    doc_image
+  end
 end
