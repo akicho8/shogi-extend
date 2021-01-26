a4f3b7f4da1789a0f3b38abc5e4955b703d92b50
akicho8 <akicho8@gmail.com>
2021-01-26 21:52:00 +0900

    [fix] iPad OS 13 では「ぴよ将棋」ではなく「ぴよ将棋ｗ」に遷移する

--- front_app/components/models/is_mobile.js
+++ front_app/components/models/is_mobile.js
@@ -15,10 +15,14 @@ export const isMobile = {
         window.navigator.userAgent.match(/BlackBerry/i)
     )
   },
+  // https://www.qam-web.com/?p=15115
+  // Safari on iPad + iPad OS13(デスクトップ用Webサイトを表示:On設定時 ← デフォルト)
+  // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15
   iOS() {
     return (
       typeof window !== 'undefined' &&
-        window.navigator.userAgent.match(/iPhone|iPad|iPod/i)
+        (window.navigator.userAgent.match(/iPhone|iPad|iPod/i) ||
+         (window.navigator.userAgent.match(/Macintosh/i) && 'ontouchend' in document))
     )
   },
   Opera() {
