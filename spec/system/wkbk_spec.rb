e76857618252db77e2e8c0d3f9cdfe0a04a3d47c
akicho8 <akicho8@gmail.com>
2021-02-01 22:00:37 +0900

    [test] library/article|book の system test 追加

--- spec/system/wkbk_spec.rb
+++ spec/system/wkbk_spec.rb
@@ -1,35 +1,47 @@
 require "rails_helper"
 
-RSpec.describe "将棋トレーニングバトル", type: :system do
+RSpec.describe "みんなの将棋問題集", type: :system do
   before do
+    Actb.setup
+    Emox.setup
     Wkbk.setup
+    Wkbk::Book.mock_setup
+    # tp Wkbk.info
+    # tp Wkbk::Book
   end
 
-  # from WkbkSupportMethods
-  let(:user1)     { User.create!                    }
-  let(:article1) { user1.wkbk_articles.create_mock1 }
-
-  xit "トップ(ログインなし)" do
-    visit "/training"
-    expect(page).to have_content "LOGIN"
+  it "問題集一覧" do
+    visit "http://0.0.0.0:4000/library/books"
+    expect(page).to have_content "問題集"
     doc_image
   end
 
-  xit "トップ(ログインあり)" do
-    visit "/training?_user_id=#{user1.id}"
-    expect(page).to have_content "対人戦"
+  it "問題集詳細" do
+    visit "http://0.0.0.0:4000/library/books/1"
+    expect(page).to have_content "sysop - public"
     doc_image
   end
 
-  xit "問題詳細(ログインなし)" do
-    visit "/training?article_id=#{article1.id}"
-    expect(page).to have_content "配置"
+  it "問題集編集" do
+    visit "http://0.0.0.0:4000/library/books/1/edit?_user_id=#{User.sysop.id}"
+    expect(page).to have_content ""
     doc_image
   end
 
-  xit "問題詳細(ログインあり)" do
-    visit "/training?article_id=#{article1.id}&_user_id=#{user1.id}"
-    expect(page).to have_content "配置"
+  it "問題一覧" do
+    visit "http://0.0.0.0:4000/library/articles"
+    expect(page).to have_content "問題"
+    doc_image
+  end
+
+  it "問題詳細" do
+    visit "http://0.0.0.0:4000/library/articles/1"
+    expect(page).to have_content "public"
+    doc_image
+  end
+
+  it "問題編集" do
+    visit "http://0.0.0.0:4000/library/articles/1/edit?_user_id=#{User.sysop.id}"
     doc_image
   end
 end
