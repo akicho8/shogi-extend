bb3d9e103d17d80982b97814f4161dbf55f4b370
akicho8 <akicho8@gmail.com>
2021-02-02 19:54:18 +0900

    [fix] keyがないため更新されない

--- front_app/components/Wkbk/WkbkBookShow/WkbkBookShowAnswer.vue
+++ front_app/components/Wkbk/WkbkBookShow/WkbkBookShowAnswer.vue
@@ -3,7 +3,7 @@
   .column
     b-tabs(v-model="answer_tab_index" position="is-centered" :vertical="false" :expanded="true" :animated="false" v-if="base.current_article.moves_answers.length >= 1" @input="sound_play('click')")
       template(v-for="(e, i) in base.current_article.moves_answers")
-        b-tab-item(:label="`${i + 1}`")
+        b-tab-item(:label="`${i + 1}`" :key="`tab_${i}_${e.moves_str}`")
           .CustomShogiPlayerWrap
             CustomShogiPlayer(
               sp_mobile_vertical="is_mobile_vertical_off"
