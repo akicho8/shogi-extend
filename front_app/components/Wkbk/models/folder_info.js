9ed4742ec9c0378ad3ee81e14c9fb8321ad59527
akicho8 <akicho8@gmail.com>
2021-02-06 17:34:06 +0900

    [feat] 公開設定のアイコン変更

--- front_app/components/Wkbk/models/folder_info.js
+++ front_app/components/Wkbk/models/folder_info.js
@@ -3,8 +3,8 @@ import MemoryRecord from 'js-memory-record'
 export class FolderInfo extends MemoryRecord {
   static get define() {
     return [
-      { key: "private",  name: "非公開", icon: "eye-off", pulldown_name: "",        }, // 🔒 https://lets-emoji.com/emojilist/emojilist-7/
-      { key: "public",   name: "公開",   icon: "eye",     pulldown_name: " (公開)", },
+      { key: "private",  name: "非公開", icon: "eye-off-outline", pulldown_name: "",        }, // 🔒 https://lets-emoji.com/emojilist/emojilist-7/
+      { key: "public",   name: "公開",   icon: "eye-outline",     pulldown_name: " (公開)", },
     ]
   }
 }
