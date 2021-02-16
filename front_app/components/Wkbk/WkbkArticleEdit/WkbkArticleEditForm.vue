b1e2373a27aa8a616de98f2b0f8cbdc6becc5872
akicho8 <akicho8@gmail.com>
2021-02-16 22:46:16 +0900

    [fix][style] is-gapless を使ってはいけない

--- front_app/components/Wkbk/WkbkArticleEdit/WkbkArticleEditForm.vue
+++ front_app/components/Wkbk/WkbkArticleEdit/WkbkArticleEditForm.vue
@@ -1,48 +1,60 @@
 <template lang="pug">
-.WkbkArticleEditForm
-  .columns.is-gapless
-    .column
-      b-field(label="タイトル" label-position="on-border")
-        b-input(v-model.trim="base.article.title" required :maxlength="100" placeholder="問題について説明するタイトルを追加しましょう")
+MainSection.is_mobile_padding_zero.WkbkArticleEditForm
+  .container
+    .columns
+      .column
+        b-field(label="タイトル" label-position="on-border")
+          b-input(v-model.trim="base.article.title" required :maxlength="100" placeholder="問題について説明するタイトルを追加しましょう")
 
-      b-field(label="解説" label-position="on-border")
-        b-input(v-model.trim="base.article.description" type="textarea" :maxlength="5000" placeholder="解答者に向けて問題の解答の解説をしましょう")
+        b-field(label="解説" label-position="on-border")
+          b-input(v-model.trim="base.article.description" type="textarea" :maxlength="5000" placeholder="解答者に向けて問題の解答の解説をしましょう")
 
-      b-field(label="種類" label-position="on-border" v-if="base.LineageInfo")
-        b-select(v-model="base.article.lineage_key" expanded)
-          option(v-for="e in base.LineageInfo.values" :value="e.key")
-            | {{e.name}}
+        b-field(label="種類" label-position="on-border" v-if="base.LineageInfo")
+          b-select(v-model="base.article.lineage_key" expanded)
+            option(v-for="e in base.LineageInfo.values" :value="e.key")
+              | {{e.name}}
 
-      b-field(label="出題時の一言" label-position="on-border" message="タイトルとは別に一言添えたいときの文章")
-        b-input(v-model.trim="base.article.direction_message" placeholder="3手指してください")
+        b-field(label="出題時の一言" label-position="on-border" message="タイトルとは別に一言添えたいときの文章")
+          b-input(v-model.trim="base.article.direction_message" placeholder="3手指してください")
 
-      b-field(label="難易度" custom-class="is-small")
-        b-rate(v-model="base.article.difficulty" spaced :max="5" :show-score="false")
+        b-field(label="難易度" custom-class="is-small")
+          b-rate(v-model="base.article.difficulty" spaced :max="5" :show-score="false")
 
-      b-field
-        b-switch(v-model="base.article.mate_skip" :disabled="!lineage_info.mate_validate_on")
-          span 最後は無駄合い
-          span.has-text-grey.is-size-7.ml-1 なので詰みチェックを省略
+        b-field
+          b-switch(v-model="base.article.mate_skip" :disabled="!lineage_info.mate_validate_on")
+            span 最後は無駄合い
+            span.has-text-grey.is-size-7.ml-1 なので詰みチェックを省略
 
-      b-field(label="タグ" label-position="on-border")
-        //- https://buefy.org/documentation/taginput
-        b-taginput(v-model="base.article.tag_list" rounded :confirm-key-codes="[13, 188, 9, 32]")
+        b-field(label="タグ" label-position="on-border")
+          //- https://buefy.org/documentation/taginput
+          b-taginput(v-model="base.article.tag_list" rounded :confirm-key-codes="[13, 188, 9, 32]")
 
-      b-field(label="公開設定" custom-class="is-small" :message="FolderInfo.fetch(base.article.folder_key).message.article")
-        b-field.is-marginless
-          template(v-for="e in FolderInfo.values")
-            b-radio-button(v-model="base.article.folder_key" :native-value="e.key" expanded)
-              b-icon(:icon="e.icon" size="is-small")
-              span {{e.name}}
+        b-field(label="公開設定" custom-class="is-small" :message="FolderInfo.fetch(base.article.folder_key).message.article")
+          b-field.is-marginless
+            template(v-for="e in FolderInfo.values")
+              b-radio-button(v-model="base.article.folder_key" :native-value="e.key" expanded)
+                b-icon(:icon="e.icon" size="is-small")
+                span {{e.name}}
 
-      .box
-        b-field(label="この問題を含める問題集" custom-class="is-medium")
-          .control.books
+        .box(v-if="false")
+          b-field(label="この問題を含める問題集" custom-class="is-medium")
+            .control.book_cb_buttons
+              template(v-for="e in base.books")
+                b-field
+                  b-checkbox-button(v-model="base.article.book_keys" :native-value="e.key")
+                    b-icon.ml-1(:icon="FolderInfo.fetch(e.folder_key).icon" size="is-small")
+                    p {{e.title}}
+
+        .panel.mb-0
+          .panel-heading
+            | この問題を含める問題集
+          .panel-block.book_cb_buttons.is-block
             template(v-for="e in base.books")
               b-field
-                b-checkbox-button(v-model="base.article.book_keys" :native-value="e.key")
-                  b-icon.ml-1(:icon="FolderInfo.fetch(e.folder_key).icon" size="is-small")
-                  p {{e.title}}
+                b-checkbox-button(v-model="base.article.book_keys" :native-value="e.key" expanded @input="sound_play('click')")
+                  b-icon(:icon="FolderInfo.fetch(e.folder_key).icon" size="is-small")
+                  p.ml-2 {{e.title}}
+
 </template>
 
 <script>
@@ -98,21 +110,20 @@ export default {
 @import "../support.sass"
 .WkbkArticleEditForm
   +mobile
-    margin: 0.5rem
+    margin: 1.0rem
   +tablet
     margin: 1.5rem
 
   .field:not(:last-child)
     margin-bottom: 1.5rem
 
-  .books
-    // +desktop
-    //   display: inline-block
-    margin-top: 0
-    .field:not(:last-child)
-      margin-bottom: 0.25rem
-    .button
-      justify-content: flex-start
+  .book_cb_buttons
+    padding: 1rem
+    .field
+      &:not(:last-child)
+        margin-bottom: 0.5rem
+      .button
+        justify-content: flex-start
 
   .help
     color: $grey
