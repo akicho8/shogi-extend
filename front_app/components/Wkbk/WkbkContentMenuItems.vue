946cd04106e57e33a9be24ca0d12bc58cb472002
akicho8 <akicho8@gmail.com>
2021-02-19 11:05:03 +0900

    [feat] サイドバーを共通化

---
+++ front_app/components/Wkbk/WkbkContentMenuItems.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+b-menu-list.WkbkContentMenuItems(label="コンテンツ")
+  b-menu-item(tag="nuxt-link" :to="{name: 'rack-articles'}" label="問題リスト"   @click.native="sound_play('click')" icon="menu")
+  b-menu-item(tag="nuxt-link" :to="{name: 'rack-books'}"    label="問題集リスト" @click.native="sound_play('click')")
+</template>
+
+<script>
+import { support } from "./support.js"
+
+export default {
+  name: "WkbkContentMenuItems",
+  mixins: [support],
+}
+</script>
+
+<style lang="sass">
+@import "./support.sass"
+.WkbkContentMenuItems
+</style>
