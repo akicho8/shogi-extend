11b02fd48c1fa13de09d7cc8dac333f43e510266
akicho8 <akicho8@gmail.com>
2021-02-21 09:38:00 +0900

    [feat] トップから直接問題作成画面にリンク

--- front_app/components/Wkbk/WkbkTop/WkbkTopNavbar.vue
+++ front_app/components/Wkbk/WkbkTop/WkbkTopNavbar.vue
@@ -12,11 +12,11 @@ MainNavbar.WkbkTopNavbar(:spaced="false")
     NavbarItemLogin
     NavbarItemProfileLink
 
-    //- b-navbar-item.has-text-weight-bold.px_5_if_tablet(tag="nuxt-link" :to="{name: 'rack-books-new'}" @click.native="sound_play('click')")
-    //-   b-icon(icon="plus")
-
     b-navbar-item.has-text-weight-bold.px_5_if_tablet(tag="nuxt-link" :to="{name: 'rack-articles'}" @click.native="sound_play('click')")
       b-icon(icon="cog")
+
+    b-navbar-item.has-text-weight-bold.px_5_if_tablet(tag="nuxt-link" :to="{name: 'rack-articles-new'}" @click.native="sound_play('click')")
+      b-icon(icon="plus")
 
     WkbkSidebarToggle(:base="base" @click="base.sidebar_toggle" v-if="false")
 
