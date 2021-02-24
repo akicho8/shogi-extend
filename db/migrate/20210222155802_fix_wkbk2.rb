643292485d21ddce4cf9f3799ae51bef6ec6a170
akicho8 <akicho8@gmail.com>
2021-02-24 21:53:41 +0900

    [chore] migrateでは実行しちゃいけない

--- db/migrate/20210222155802_fix_wkbk2.rb
+++
@@ -1,9 +0,0 @@
-class FixWkbk2 < ActiveRecord::Migration[6.0]
-  def change
-    if Rails.env.production? || Rails.env.staging?
-      key = "932ed39bb18095a2fc73e0002f94ecf1"
-      user = User.find_by!(key: key)
-      Wkbk::KifuDataImport.new(user: user).run
-    end
-  end
-end
