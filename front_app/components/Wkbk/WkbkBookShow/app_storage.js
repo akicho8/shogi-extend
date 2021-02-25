cb79f9dec3a0ef65fba77fba000d4e4fbeddc6ef
akicho8 <akicho8@gmail.com>
2021-02-25 11:18:59 +0900

    [refactor] 正解したら自動的に次に進む設定

--- front_app/components/Wkbk/WkbkBookShow/app_storage.js
+++ front_app/components/Wkbk/WkbkBookShow/app_storage.js
@@ -1,4 +1,6 @@
 import { ls_support_mixin } from "@/components/models/ls_support_mixin.js"
+import { ArticleTitleDisplayInfo } from "../models/article_title_display_info.js"
+import { CorrectBehaviorInfo } from "../models/correct_behavior_info.js"
 
 export const app_storage = {
   mixins: [
@@ -10,17 +12,19 @@ export const app_storage = {
       correct_behavior_key: null,
     }
   },
-
   beforeMount() {
     this.ls_setup()
   },
-
   computed: {
     ls_default() {
       return {
-        article_title_display_key: "display",
-        correct_behavior_key: "go_to_next",
+        article_title_display_key: this.ArticleTitleDisplayInfo.values[0].key,
+        correct_behavior_key: this.CorrectBehaviorInfo.values[0].key,
       }
     },
+    ArticleTitleDisplayInfo()    { return ArticleTitleDisplayInfo                                       },
+    article_title_display_info() { return ArticleTitleDisplayInfo.fetch(this.article_title_display_key) },
+    CorrectBehaviorInfo()        { return CorrectBehaviorInfo                                           },
+    correct_behavior_info()      { return CorrectBehaviorInfo.fetch(this.correct_behavior_key)          },
   },
 }
--- front_app/components/Wkbk/WkbkBookShow/app_support.js
+++ front_app/components/Wkbk/WkbkBookShow/app_support.js
@@ -1,6 +1,4 @@
 import WkbkBookShowDesc from "./WkbkBookShowDesc.vue"
-import { ArticleTitleDisplayInfo } from "../models/article_title_display_info.js"
-import { CorrectBehaviorInfo } from "../models/correct_behavior_info.js"
 
 // import dayjs from "dayjs"
 import _ from "lodash"
@@ -85,9 +83,5 @@ export const app_support = {
   },
   computed: {
     current_article_show_p() { return this.current_xitem },
-    ArticleTitleDisplayInfo() { return ArticleTitleDisplayInfo },
-    article_title_display_info() { return ArticleTitleDisplayInfo.fetch(this.article_title_display_key) },
-    CorrectBehaviorInfo() { return CorrectBehaviorInfo },
-    correct_behavior_info() { return CorrectBehaviorInfo.fetch(this.correct_behavior_key) },
   },
 }
