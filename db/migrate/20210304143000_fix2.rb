9756782643786f717363329fb05249c622229379
akicho8 <akicho8@gmail.com>
2021-03-05 13:52:37 +0900

    [feat] 「右玉」と「高田流左玉」を戦法から囲いカテゴリーに変更

--- db/migrate/20210304143000_fix2.rb
+++ db/migrate/20210304143000_fix2.rb
@@ -4,5 +4,10 @@ class Fix2 < ActiveRecord::Migration[6.0]
     ActsAsTaggableOn::Tag.find_by!(name: "片矢倉").update!(name: "天野矢倉")
     ActsAsTaggableOn::Tag.find_by!(name: "雁木囲い").update!(name: "オールド雁木")
     ActsAsTaggableOn::Tag.find_by!(name: "elmo囲い").update!(name: "エルモ囲い")
+
+    tag = ActsAsTaggableOn::Tag.find_by!(name: "右玉")
+    tag.taggings.where(context: "attack_tags").update_all("context = 'defense_tags'")
+    tag = ActsAsTaggableOn::Tag.find_by!(name: "高田流左玉")
+    tag.taggings.where(context: "attack_tags").update_all("context = 'defense_tags'")
   end
 end
