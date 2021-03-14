f69ad8624d0a745c34df7e24426fa8455beb54a6
akicho8 <akicho8@gmail.com>
2021-03-14 14:28:56 +0900

    [refactor][db] 戦法タグ名変更

--- db/migrate/20210307111406_fix6.rb
+++ db/migrate/20210307111406_fix6.rb
@@ -1,7 +1,8 @@
 class Fix6 < ActiveRecord::Migration[6.0]
   def change
     {
-      "超速" => "超速▲３七銀",
+      "超速"         => "超速▲３七銀",
+      "△３ニ飛戦法" => "2手目△３ニ飛戦法",
     }.each do |k, v|
       ActsAsTaggableOn::Tag.find_by(name: k)&.update!(name: v)
     end
