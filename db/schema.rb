676900a7c7e468978e88d78a93915dec8730ce1e
akicho8 <akicho8@gmail.com>
2021-03-17 13:31:06 +0900

    [feat] bundle update bioshogi (神吉流穴熊 判定追加)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_111406) do
+ActiveRecord::Schema.define(version: 2021_03_07_111407) do
 
   create_table "actb_bad_marks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.bigint "user_id", null: false, comment: "自分"
