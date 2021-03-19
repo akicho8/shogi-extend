f796f45286c0232013cf09fae405ce7f792a52bd
akicho8 <akicho8@gmail.com>
2021-03-19 19:36:10 +0900

    [fix] pのなかにdivを入れるとVue.jsが外に出してしまう

--- front_app/components/ShareBoard/RealtimeShareModal.vue
+++ front_app/components/ShareBoard/RealtimeShareModal.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 .modal-card.RealtimeShareModal(style="width:auto")
   header.modal-card-head
-    p.modal-card-title
-      .is-size-6.has-text-weight-bold.is-flex.is-align-items-center.is-justify-content-flex-start.is-flex-grow-0
-        | 合言葉の設定と共有
-        b-tag.mx-2.has-text-weight-bold(type="is-success" v-if="base.ac_room") 共有中
+    .modal-card-title.is-size-6.has-text-weight-bold.is-flex.is-align-items-center.is-flex-grow-0
+      | 合言葉の設定と共有
+      b-tag.mx-2.has-text-weight-bold(type="is-success" v-if="base.ac_room") 共有中
   section.modal-card-body
     .content
       ul
@@ -12,7 +11,7 @@
         li 合言葉を設定したら同じ合言葉をこっそり相手に伝えてください
         li メニューにある「合言葉だけを含むURL」を伝えてもかまいません
         li
-          | 共有のタイミングは
+          | 盤面の同期タイミングは
           b.is-size-5.mx-1 指したときだけ！
           | ← これ重要
         li
