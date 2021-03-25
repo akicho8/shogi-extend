a4bc8563ede07e9b470d10ed757128d1caca666e
akicho8 <akicho8@gmail.com>
2021-03-25 15:54:50 +0900

    [feat] ".*マン！" も敬称をつけない

--- front_app/plugins/vue_support.js
+++ front_app/plugins/vue_support.js
@@ -362,8 +362,8 @@ export default {
     // call_name("SOS団")  → "SOS団"
     // call_name("ありす") → "ありすさん"
     user_call_name(name) {
-      name = name.replace(/(\D+)\d+/, "$1") // "foo123" → "foo"
-      if (name.match(/.(さん|サン|ｻﾝ|くん|クン|ｸﾝ|ちゃん|チャン|ﾁｬﾝ|さま|サマ|ｻﾏ|様|氏|段|級|団|冠)$/)) {
+      name = name.replace(/(\D+)\d+/, "$1")    // "foo123" → "foo"
+      if (name.match(/.(さん|サン|ｻﾝ|くん|クン|ｸﾝ|ちゃん|チャン|ﾁｬﾝ|マン|さま|サマ|ｻﾏ|様|氏|段|級|団|冠)[!！]?$/)) {
         return name
       }
       if (name.match(/.(コ|ｺ|こ|子)$/)) {
