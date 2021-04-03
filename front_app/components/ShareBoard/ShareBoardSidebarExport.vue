638158bf1153a0ca9b177d825bdceb20a9e88251
akicho8 <akicho8@gmail.com>
2021-04-03 11:52:40 +0900

    [fix][adapter] "(SHIFT_JIS)" などが表示されなくなっている

--- front_app/components/ShareBoard/ShareBoardSidebarExport.vue
+++ front_app/components/ShareBoard/ShareBoardSidebarExport.vue
@@ -7,7 +7,7 @@
         b-icon.is-pulled-right(:icon="props.expanded ? 'menu-up' : 'menu-down'")
       template(v-for="e in base.FormatTypeInfo.values")
         template(v-if="e.show")
-          b-menu-item.is_active_unset(:label="e.name_fn(base.turn_offset)" @click.prevent="base.kifu_show_handle(e)" :href="base.kifu_show_url(e)")
+          b-menu-item.is_active_unset(:label="e.name_with_turn(base.turn_offset)" @click.prevent="base.kifu_show_handle(e)" :href="base.kifu_show_url(e)")
 
     b-menu-item.is_active_unset(@click="sound_play('click')")
       template(slot="label" slot-scope="props")
@@ -15,14 +15,14 @@
         b-icon.is-pulled-right(:icon="props.expanded ? 'menu-up' : 'menu-down'")
       template(v-for="e in base.FormatTypeInfo.values")
         template(v-if="e.clipboard")
-          b-menu-item.is_active_unset(:label="e.name_fn(base.turn_offset)" @click="base.kifu_copy_handle(e)")
+          b-menu-item.is_active_unset(:label="e.name_with_turn(base.turn_offset)" @click="base.kifu_copy_handle(e)")
 
     b-menu-item.is_active_unset(@click="sound_play('click')")
       template(slot="label" slot-scope="props")
         | ダウンロード
         b-icon.is-pulled-right(:icon="props.expanded ? 'menu-up' : 'menu-down'")
       template(v-for="e in base.FormatTypeInfo.values")
-        b-menu-item.is_active_unset(:label="e.name_fn(base.turn_offset)" @click.prevent="base.kifu_download_handle(e)" :href="base.kifu_download_url(e)")
+        b-menu-item.is_active_unset(:label="e.name_with_turn(base.turn_offset)" @click.prevent="base.kifu_download_handle(e)" :href="base.kifu_download_url(e)")
 </template>
 
 <script>
