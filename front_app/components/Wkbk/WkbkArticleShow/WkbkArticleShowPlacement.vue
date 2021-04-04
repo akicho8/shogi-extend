66fe4b7ac97ced3574b5de23dd0300b244800ba3
akicho8 <akicho8@gmail.com>
2021-04-04 09:59:20 +0900

    [feat][wkbk] 回答者が詰将棋をKENTOなどで検証できるようにする

--- front_app/components/Wkbk/WkbkArticleShow/WkbkArticleShowPlacement.vue
+++ front_app/components/Wkbk/WkbkArticleShow/WkbkArticleShowPlacement.vue
@@ -9,22 +9,37 @@ MainSection.WkbkArticleShowPlacement
             sp_run_mode="edit_mode"
             :sp_body="base.article.init_sfen"
             :sp_turn="0"
-            :sp_viewpoint="base.article.viewpoint"
+            :sp_viewpoint.sync="base.viewpoint"
             :sp_sound_body_changed="false"
             :sp_sound_enabled="false"
             sp_slider="is_slider_on"
             sp_controller="is_controller_on"
+            @update:edit_mode_snapshot_sfen="base.edit_mode_snapshot_sfen"
             ref="main_sp"
             )
           .footer_buttons
-            .buttons.is-centered.are-small.is-marginless.mt-3
+            .buttons.mb-0.is-centered.are-small.is-marginless.mt-3
               b-button(@click="king_formation_auto_set(true)") 詰将棋検討用玉配置
               b-button(@click="king_formation_auto_set(false)") 玉回収
 
-            .buttons.is-centered.are-small.is-marginless.mt-3
+            .buttons.mb-0.is-centered.are-small.is-marginless.mt-3
               PiyoShogiButton(:href="piyo_shogi_app_with_params_url")
               KentoButton(tag="a" :href="kento_app_with_params_url" target="_blank")
               KifCopyButton(@click="kifu_copy_handle") コピー
+
+            //- .buttons.mb-0.is-centered.are-small.is-marginless.mt-3
+            //-   b-button(@click="base.any_source_read_handle") 棋譜の読み込み
+            //-
+            //- .buttons.mb-0.has-addons.is-centered.are-small.mt-3(v-if="development_p")
+            //-   b-button(@click="$refs.main_sp.sp_object().mediator.shuffle_apply(3)") 3
+            //-   b-button(@click="$refs.main_sp.sp_object().mediator.shuffle_apply(4)") 4
+            //-   b-button(@click="$refs.main_sp.sp_object().mediator.shuffle_apply(5)") 5
+            //-   b-button(@click="$refs.main_sp.sp_object().mediator.shuffle_apply(6)") 6
+            //-   .ml-1
+            //-   b-button(icon-left="arrow-left"  @click="$refs.main_sp.sp_object().mediator.slide_xy(-1, 0)")
+            //-   b-button(icon-left="arrow-down"  @click="$refs.main_sp.sp_object().mediator.slide_xy(0, 1)")
+            //-   b-button(icon-left="arrow-up"    @click="$refs.main_sp.sp_object().mediator.slide_xy(0, -1)")
+            //-   b-button(icon-left="arrow-right" @click="$refs.main_sp.sp_object().mediator.slide_xy(1, 0)")
 </template>
 
 <script>
@@ -35,9 +50,11 @@ export default {
   mixins: [
     support_child,
   ],
-  // created() {
-  //   this.base.sp_body = this.base.article.init_sfen
-  // },
+
+  created() {
+    this.base.sp_body      = this.base.article.init_sfen
+    this.base.sp_viewpoint = this.base.viewpoint
+  },
 
   mounted() {
     this.base.piece_box_piece_counts_adjust()
@@ -57,18 +74,30 @@ export default {
         }
       }
     },
-
     // 棋譜コピー
     kifu_copy_handle() {
       this.sound_play("click")
-      this.general_kifu_copy(this.base.article.init_sfen, {to_format: "kif"})
+      this.general_kifu_copy(this.base.sp_body, {to_format: "kif"})
     },
-
   },
 
   computed: {
-    piyo_shogi_app_with_params_url() { return this.piyo_shogi_auto_url({sfen: this.base.article.init_sfen, turn: 0, viewpoint: "black"}) },
-    kento_app_with_params_url()      { return this.kento_full_url({sfen: this.base.article.init_sfen, turn: 0, viewpoint: "black"}) },
+    // ぴよ将棋で開く
+    piyo_shogi_app_with_params_url() {
+      return this.piyo_shogi_auto_url({
+        turn: 0,
+        sfen: this.base.sp_body,
+        viewpoint: this.base.viewpoint,
+      })
+    },
+    // KENTOで開く
+    kento_app_with_params_url() {
+      return this.kento_full_url({
+        turn: 0,
+        sfen: this.base.sp_body,
+        viewpoint: this.base.viewpoint,
+      })
+    },
   },
 }
 </script>
@@ -95,9 +124,10 @@ export default {
         margin: 0.75rem 0
       +tablet
         margin: $wkbk_share_gap 0
+
       width: 100%
       +tablet
-        max-width: 56vmin
+        max-width: 54vmin
       .footer_buttons
         .button
           margin-bottom: 0
--- front_app/components/Wkbk/WkbkArticleShow/app_placement.js
+++ front_app/components/Wkbk/WkbkArticleShow/app_placement.js
@@ -1,11 +1,19 @@
+// 配置タブ関連
+
 export const app_placement = {
   data() {
     return {
-      // sp_body: null,
+      sp_body:      null, // 配置の現在の状態
+      sp_viewpoint: null, // 配置の視点
     }
   },
 
   methods: {
+    // 配置で駒を動かしたときのフック。article.init_sfen は変更しないようにする
+    edit_mode_snapshot_sfen(sfen) {
+      this.sp_body = sfen
+    },
+
     // 駒箱正規化
     piece_box_piece_counts_adjust() {
       this.$refs.WkbkArticleShowPlacement?.$refs.main_sp.sp_object().mediator.piece_box_piece_counts_adjust()
