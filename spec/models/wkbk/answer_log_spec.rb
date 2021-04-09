a11e63efcddbb8690da2fb84033a729b14479c39
akicho8 <akicho8@gmail.com>
2021-04-09 19:05:58 +0900

    [test] add spec/models/wkbk/answer_log_spec.rb

--- spec/models/wkbk/answer_log_spec.rb
+++ spec/models/wkbk/answer_log_spec.rb
@@ -27,38 +27,11 @@ module Wkbk
     include ActiveJob::TestHelper # for perform_enqueued_jobs
 
     it "works" do
-      # user = User.create!
-      # book = user.wkbk_books.create!
-      # book.articles << user.wkbk_articles.create!(key: "a")
-      # book.articles << user.wkbk_articles.create!(key: "b")
-      # book.articles << user.wkbk_articles.create!(key: "c")
-      #
-      # answer_kind = ::Wkbk::AnswerKind.fetch("correct")
-      # answer_log = user.wkbk_answer_logs.create!(article: book.articles[0], answer_kind: answer_kind, book: book)
-      # answer_log = user.wkbk_answer_logs.create!(article: book.articles[0], answer_kind: answer_kind, book: book)
-      # answer_log = user.wkbk_answer_logs.create!(article: book.articles[1], answer_kind: answer_kind, book: book)
-      #
-      # tp user.wkbk_answer_logs
-      #
-      # correct_id = ::Wkbk::AnswerKind.fetch("correct").id
-      # mistake_id = ::Wkbk::AnswerKind.fetch("mistake").id
-      #
-      # # hash = s.joins(:grade).group("swars_grades.key").group("judge_key").count # => {["九段", "lose"]=>2, ["九段", "win"]=>1}
-      # o_count = "COUNT(answer_kind_id = #{correct_id} OR NULL) AS o_count"
-      # x_count = "COUNT(answer_kind_id = #{mistake_id} OR NULL) AS x_count"
-      # select = "article_id, #{o_count}, #{x_count}, MAX(created_at) AS created_at"
-      # records = user.wkbk_answer_logs.where(book: book).group("article_id").select(select).order("max(created_at) desc")
-      #
-      # records.collect { |e|
-      # }
-
-      # |----+------------+---------------------------+---------+---------|
-      # | id | article_id | created_at                | o_count | x_count |
-      # |----+------------+---------------------------+---------+---------|
-      # |    |        476 | 2000-01-01 00:00:00 +0900 |       2 |       0 |
-      # |    |        477 | 2000-01-01 00:00:00 +0900 |       1 |       0 |
-      # |----+------------+---------------------------+---------+---------|
-
+      user = User.create!
+      book = user.wkbk_books.create!
+      book.articles << user.wkbk_articles.create!
+      user.wkbk_answer_logs.create!(article: book.articles[0], answer_kind: AnswerKind.fetch("correct"), book: book, spent_sec: 1)
+      user.wkbk_answer_logs.create!(article: book.articles[0], answer_kind: AnswerKind.fetch("mistake"), book: book, spent_sec: 1)
     end
   end
 end
