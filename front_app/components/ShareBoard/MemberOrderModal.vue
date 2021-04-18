1b9221070fd18da57dfb74694c3e8c45193405de
akicho8 <akicho8@gmail.com>
2021-04-18 17:46:15 +0900

    [feat] チームトグル, 全体の順番

--- front_app/components/ShareBoard/MemberOrderModal.vue
+++ front_app/components/ShareBoard/MemberOrderModal.vue
@@ -17,9 +17,10 @@
     //-   template(v-for="row in default_ordered_members")
     //-     b-button(rounded type="is-primary" size="is-small" @click="user_name_click(row)")
     //-       | {{row.from_user_name}}
-    //- 
+    //-
     //- hr
     //- | {{new_ordered_members}}
+
     b-table(
       :data="new_ordered_members"
       )
@@ -33,7 +34,10 @@
       //- )
       //- default-sort-direction="desc"
       b-table-column(v-slot="{row}" field="user_name"    label="名前") {{row.user_name}}
-      b-table-column(v-slot="{row}" field="location_key" label="先後") {{Location.fetch(row.location_key)}}
+      b-table-column(v-slot="{row}" field="location_key" label="ﾁｰﾑ" centered)
+        b-button(@click="location_toggle_handle(row)")
+          | {{Location.fetch(row.location_key).name}}
+      b-table-column(v-slot="{row}" field="order_index"     label="ﾁｰﾑ内の順番" centered) {{row.order_index + 1}}
       //- b-table-column(v-slot="props" field="entry_name" label="名前"  sortable) {{string_truncate(props.row.entry_name || '？？？', {length: 15})}}
       //- b-table-column(v-slot="props" field="spent_sec"  label="タイム") {{base.time_format_from_msec(props.row.spent_sec)}}
       //- b-table-column(v-slot="props" field="created_at" label="日付" :visible="!!base.curent_scope.date_visible") {{base.time_default_format(props.row.created_at)}}
@@ -49,6 +53,17 @@
     //- p.has-text-centered.is-size-7.has-text-grey-light
     //-   | リレー将棋で自分の手番をまちがいがちな方向けのサポート機能です<br>
     //-   | 順序が固定されている場合に指定の上家が指し終わったときにお知らせします
+
+    .py-4
+      span.has-text-weight-bold 全体の順番
+      .is-inline-block.ml-2
+        template(v-for="(row, i) in real_order_members")
+          b-tag.has-text-weight-bold(rounded :type="foo_type(row)")
+            //- | {{Location.fetch(row.location_key).name}}
+            | {{row.user_name}}
+          span.mx-1.has-text-grey-light.is-size-7(v-if="i < real_order_members.length - 1")
+            | →
+
   footer.modal-card-foot
     b-button.close_button(@click="close_handle" icon-left="chevron-left") 閉じる
     b-button.test_button(@click="test_handle" v-if="development_p") テスト
@@ -58,7 +73,8 @@
 <script>
 import { support_child } from "./support_child.js"
 import _ from "lodash"
-import { Location   } from "shogi-player/components/models/location.js"
+import { Location } from "shogi-player/components/models/location.js"
+import { CycleIterator } from "@/components/models/cycle_iterator.js"
 
 export default {
   name: "MemberOrderModal",
@@ -72,6 +88,7 @@ export default {
   },
   beforeMount() {
     this.new_ordered_members = [...(this.base.ordered_members || this.default_ordered_members)]
+    this.order_index_update()
   },
   methods: {
     close_handle() {
@@ -103,12 +120,78 @@ export default {
     user_name_click(row) {
       this.new_ordered_members.push(row)
     },
+
+    location_toggle_handle(row) {
+      row.location_key = Location.fetch(row.location_key).flip.key
+      this.order_index_update()
+    },
+
+    order_index_update() {
+      this.new_ordered_members.forEach(e => {
+        e.order_index = this.group_list_hash[e.location_key].findIndex(v => e.user_name === v.user_name)
+      })
+      // return list
+      // const list = location_hash[row.location_key]
+      // const index = list.findIndex(e => e.user_name === row.user_name)
+      // if (index >= 0) {
+      //   return index + 1
+      // }
+    },
+
+    foo_type(row) {
+      if (row.location_key === 'black') {
+        return "is-primary"
+      }
+      if (row.location_key === 'white') {
+        return "is-primary is-light"
+      }
+      return
+    },
   },
   computed: {
+    Location() { return Location },
     form_changed_p() {
       return this.base.ordered_members !== this.new_ordered_members
     },
+    real_order_members() {
+      // const location_hash = Location.values.reduce((a, e, i) => ({...a, [e.key]: 0}), {})
+      // this.new_ordered_members.map(e => hash[e.location_key].push(e))
+
+      const tmp_names = this.new_ordered_members.map(e => e.user_name)
+
+      const cycle_list = Location.values.map(e => new CycleIterator(this.group_list_hash[e.key]))
+
+      const list = []
+      let done = false
+      while (!done) {
+        _.forEach(cycle_list, it => {
+          const v = it.next
+          list.push(v)
+          _.pull(tmp_names, v.user_name)
+          if (_.isEmpty(tmp_names)) {
+            done = true
+            return false        // break (_.forEachなら可能)
+          }
+        })
+      }
+      return list
+    },
+    group_list_hash() {
+      const hash = Location.values.reduce((a, e, i) => ({...a, [e.key]: []}), {})
+      this.new_ordered_members.forEach(e => hash[e.location_key].push(e))
+      return hash
+    },
     default_ordered_members() {
+      if (this.development_p) {
+        return [
+          { order_index: null, user_name: "alice", location_key: "black", },
+          { order_index: null, user_name: "bob",   location_key: "black", },
+          { order_index: null, user_name: "carol", location_key: "white", },
+          { order_index: null, user_name: "dave",  location_key: "white", },
+          { order_index: null, user_name: "ellen", location_key: "white", },
+        ]
+      }
+
       let v = this.base.member_infos
       v = _.uniqBy(v, "from_user_name")
       v = v.map((e, i) => {
