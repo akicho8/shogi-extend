e0bcb69db9cf9096d7d7020b317b590cd6e463ab
akicho8 <akicho8@gmail.com>
2021-04-23 20:54:19 +0900

    [style] b-iconで番号を表示すると2桁のときに困るためb-tagに変更

--- front_app/components/ShareBoard/ShareBoardMemberList.vue
+++ front_app/components/ShareBoard/ShareBoardMemberList.vue
@@ -2,8 +2,12 @@
 .ShareBoardMemberList.column
   .scroll_block(ref="scroll_block")
     template(v-for="(e, i) in member_infos")
-      .member_info.is_line_break_off.is-clickable.is-flex.is-align-items-center(:key="e.from_user_code" @click="row_click_handle(e)")
-        b-icon(:icon="icon_for(e)" :type="icon_type_for(e)")
+      .member_info.is_line_break_off.is-clickable.is-flex.is-align-items-center(:key="e.from_user_code" @click="row_click_handle(e)" :class="{is_zombie: !base.member_alive_p(e)}")
+        span.left_tag_or_icon.is-inline-flex.is-justify-content-center.is-align-items-center
+          template(v-if="order_lookup(e)")
+            b-tag(:type="tag_type_for(e)" rounded) {{tag_body_for(e)}}
+          template(v-else)
+            b-icon(:icon="icon_for(e)" :type="icon_type_for(e)")
         span.ml-1(:class="{'has-text-weight-bold': turn_active_p(e)}") {{e.from_user_name}}
         span.ml-1.is-size-7.time_format.has-text-grey-light(v-if="development_p") {{time_format(e)}}
         span.ml-1(v-if="development_p") r{{e.revision}}
@@ -36,25 +40,46 @@ export default {
     },
     // 自分のアイコン
     icon_for(e) {
-      if (this.base.order_func_p) {
-        if (this.base.ordered_members) {
-          const found = this.user_names_hash[e.from_user_name]
-          if (found) {
-            if (this.base.current_turn_user_name === e.from_user_name) {
-              return ["numeric", found.order_index + 1, "box"].join("-")
-            } else {
-              return ["numeric", found.order_index + 1, "box", "outline"].join("-")
-            }
-          }
-        }
-      }
       return "account"
     },
     // 自分のアイコンの色
     // 反応がなくなったら灰色になる
     icon_type_for(e) {
-      return this.base.member_alive_p(e) ? "is-primary" : "is-grey"
+      // if (!this.base.member_alive_p(e)) {
+      //   return "is-grey"
+      // }
+      return "is-primary"
     },
+
+    order_lookup(e) {
+      if (this.base.order_func_p) {
+        if (this.base.ordered_members) {
+          return this.user_names_hash[e.from_user_name]
+        }
+      }
+    },
+
+    tag_body_for(e) {
+      const found = this.order_lookup(e)
+      return found.order_index + 1
+    },
+
+    // 自分のアイコンの色
+    // 反応がなくなったら灰色になる
+    tag_type_for(e) {
+      // if (!this.base.member_alive_p(e)) {
+      //   return "is-grey"
+      // }
+      // const found = this.order_lookup(e)
+      // if (found) {
+      if (this.base.current_turn_user_name === e.from_user_name) {
+        return "is-primary"
+      } else {
+        return "is_inactive"
+      }
+      // }
+    },
+
   },
   computed: {
     member_infos() {
@@ -117,11 +142,21 @@ export default {
     .time_format
       vertical-align: middle
     .member_info
+      &.is_zombie
+        opacity: 0.3
       text-overflow: ellipsis
-      padding: 0.25rem 0.5rem
+      padding: 0.3rem 0.5rem
       color: inherit
       &:hover
         background-color: $grey-lighter
+    .left_tag_or_icon
+      min-width: 2rem
+      .tag
+        // font-size: unset
+        // height: unset
+        // padding: 0 0.5rem
+        &.is_inactive
+          background-color: unset
 
 .STAGE-development
   .ShareBoardMemberList
