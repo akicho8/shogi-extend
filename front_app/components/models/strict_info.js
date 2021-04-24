059303bd72f9212f44fe41a34a5d6e3a3004f937
akicho8 <akicho8@gmail.com>
2021-04-24 14:57:59 +0900

    [feat] フォーム文言改善

--- front_app/components/models/strict_info.js
+++ front_app/components/models/strict_info.js
@@ -3,8 +3,8 @@ import MemoryRecord from 'js-memory-record'
 export class StrictInfo extends MemoryRecord {
   static get define() {
     return [
-      { key: "turn_strict_on",  name: "あり", message: "手番の人だけが駒を動かせる",                             },
-      { key: "turn_strict_off", name: "なし", message: "動かそうと思えばいつでも誰でも駒を動かせる(以前の仕様)", },
+      { key: "turn_strict_on",  name: "あり", message: "手番の人だけが駒を動かせる",         },
+      { key: "turn_strict_off", name: "なし", message: "動かそうと思えば誰でも駒を動かせる", },
     ]
   }
 }
