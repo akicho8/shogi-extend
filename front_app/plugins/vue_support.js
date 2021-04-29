4799acb89965ac11490aaad090d3d83f8fa7cdfb
akicho8 <akicho8@gmail.com>
2021-04-29 23:50:33 +0900

    [test][fix] debug_alert が消えるのが速すぎてテストこける

--- front_app/plugins/vue_support.js
+++ front_app/plugins/vue_support.js
@@ -171,7 +171,7 @@ export default {
     debug_alert(message) {
       if (this.development_p) {
         if (message != null) {
-          this.$buefy.toast.open({message: message.toString(), position: "is-bottom", type: "is-danger", duration: 1000 * 1.0, queue: false})
+          this.$buefy.toast.open({message: message.toString(), position: "is-bottom", type: "is-danger", duration: 1000 * 2.0, queue: false})
         }
       }
     },
