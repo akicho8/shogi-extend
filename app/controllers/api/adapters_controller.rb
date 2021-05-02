dc12d9044b58de96488d77f3fa9463bb6928361a
akicho8 <akicho8@gmail.com>
2021-05-02 09:14:30 +0900

    [fix][adapter] なんでも棋譜変換のレコードの use_key が adapter ではなく basic になっているため定期削除外になっている

--- app/controllers/api/adapters_controller.rb
+++ app/controllers/api/adapters_controller.rb
@@ -2,13 +2,13 @@ module Api
   class AdaptersController < ::Api::ApplicationController
     # curl -d _method=post http://0.0.0.0:3000/api/adapter/record_create.json
     def record_create
-      record = FreeBattle.create!(kifu_body: params[:input_text])
+      record = FreeBattle.create!(kifu_body: params[:input_text], use_key: "adapter")
       render json: { record: record.as_json(methods: [:all_kifs, :display_turn, :piyo_shogi_base_params]) }
     end
 
     # curl http://0.0.0.0:3000/api/adapter/formal_sheet.json?key=xxx
     def formal_sheet
-      record = FreeBattle.find_by!(key: params[:key])
+      record = FreeBattle.find_by!(key: params[:key], use_key: "adapter")
       # record = FreeBattle.first
       render json: record.battle_decorator(params.to_unsafe_h.to_options.merge(view_context: view_context))
     end
