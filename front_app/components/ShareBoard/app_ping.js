5a1e83007e564146bdb3351b2a552aa458021922
akicho8 <akicho8@gmail.com>
2021-05-05 11:32:32 +0900

    [feat] 並列でsetTimeoutを呼びまくれる利点がないので1つだけ排他制御に変更

--- front_app/components/ShareBoard/app_ping.js
+++ front_app/components/ShareBoard/app_ping.js
@@ -1,33 +1,39 @@
 import dayjs from "dayjs"
 
-const PING_FAILED_DELAY = 3 // PINGを失敗とみなす秒数
-const PONG_DELAY        = 0 // PONGするまでの秒数(デバッグ時には PING_FAILED_DELAY 以上の値にする)
+const PING_OK_SEC = 3  // N秒以内ならPINGを成功とみなす
+const PONG_DELAY  = 0  // PONGするまでの秒数(デバッグ時には PING_OK_SEC 以上の値にする)
 
 export const app_ping = {
   data() {
     return {
-      ping_user_codes: {},
+      ping_runner_id: null, // 実行中なら null 以外
+      ping_success: false,  // 成功したら true
     }
   },
 
+  beforeDestroy() {
+    this.ping_callback_stop()
+  },
+
   methods: {
+    // メンバーをタップしたとき
     // --> ShareBoardMemberList.vue
     member_info_click_handle(e) {
       this.sound_play("click")
-      if (this.ping_process_p(e)) {
+      if (this.ping_running_p()) {
         this.toast_warn("PING実行中...")
       } else {
         this.ping_command(e)
       }
     },
 
-    ////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////// これだけあればいいけど失敗したかどうかわかりにくい
 
-    ping_command(member_info) {
-      this.ping_start_hook(member_info)
+    ping_command(e) {
+      this.ping_callback_set(e)
       this.ac_room_perform("ping_command", {
-        to_user_name: member_info.from_user_name,
-        to_user_code: member_info.from_user_code,
+        to_user_name: e.from_user_name,
+        to_user_code: e.from_user_code,
         ping_at: dayjs().valueOf(),
       }) // --> app/channels/share_board/room_channel.rb
     },
@@ -35,7 +41,6 @@ export const app_ping = {
       if (params.to_user_code === this.user_code) {
         const now = dayjs().valueOf()
         this.delay_block(this.PONG_DELAY, () => this.pong_command(params))
-        this.pong_command(params)
         this.aclog("PING", `${params.from_user_name} → ${this.user_name} ${now - params.ping_at}ms`)
       }
     },
@@ -49,7 +54,7 @@ export const app_ping = {
     },
     pong_command_broadcasted(params) {
       if (params.to_user_code === this.user_code) {
-        this.ping_success(params)
+        this.ping_done()
         const now = dayjs().valueOf()
         const gap = now - params.ping_at
         this.toast_ok(`${this.user_call_name(params.from_user_name)}の反応速度は${gap}ミリ秒です`, {toast_only: true})
@@ -57,38 +62,42 @@ export const app_ping = {
       }
     },
 
-    //////////////////////////////////////////////////////////////////////////////// 一定時間待って反応がなければ通知
+    //////////////////////////////////////////////////////////////////////////////// 一定時間待って失敗したら(反応がなければ)通知
 
-    // 処理中か？ ややこしくなるので排他制御すること
-    ping_process_p(e) {
-      return (this.ping_user_codes[e.from_user_code] || 0) >= 1
+    // PING実行中？
+    ping_running_p() {
+      return this.present_p(this.ping_runner_id)
     },
-    // 開始
-    ping_start_hook(e) {
-      if (this.ping_user_codes[e.from_user_code] == null) {
-        this.ping_user_codes[e.from_user_code] = 0
-      }
-      this.ping_user_codes[e.from_user_code] += 1
-      this.delay_block(this.PING_FAILED_DELAY, () => {
-        if (this.ping_user_codes) {
-          if (this.ping_user_codes[e.from_user_code] >= 1) {
-            this.ping_failed(e)
-          }
+
+    // PING実行と同時に呼んどく
+    ping_callback_set(e) {
+      this.ping_success = false
+      this.ping_callback_stop()
+      this.ping_runner_id = this.delay_block(this.PING_OK_SEC, () => {
+        if (!this.ping_success) {
+          this.toast_ok(`${this.user_call_name(e.from_user_name)}の反応がありません`)
         }
+        this.ping_callback_stop()
       })
     },
-    // 失敗
-    ping_failed(e) {
-      this.toast_ok(`${this.user_call_name(e.from_user_name)}の反応がありません`)
+
+    // 途中でやめる
+    ping_callback_stop() {
+      if (this.ping_runner_id) {
+        this.delay_stop(this.ping_runner_id)
+        this.ping_runner_id = null
+      }
     },
+
     // 成功
-    ping_success(e) {
-      this.ping_user_codes[e.from_user_code] = 0
+    ping_done() {
+      this.ping_success = true
+      this.ping_callback_stop()
     },
   },
 
   computed: {
-    PING_FAILED_DELAY() { return this.$route.PING_FAILED_DELAY || PING_FAILED_DELAY },
-    PONG_DELAY()        { return this.$route.PONG_DELAY || PONG_DELAY },
+    PING_OK_SEC() { return this.$route.PING_OK_SEC || PING_OK_SEC },
+    PONG_DELAY()  { return this.$route.PONG_DELAY || PONG_DELAY   },
   },
 }
