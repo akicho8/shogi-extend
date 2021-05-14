c12a09cda7534ce68e36c9edafdd9715bcecaeb1
akicho8 <akicho8@gmail.com>
2021-05-14 20:49:14 +0900

    [fix] Slackのエラーが無限生成される危険

--- app/jobs/slack_agent_message_send_job.rb
+++ app/jobs/slack_agent_message_send_job.rb
@@ -2,11 +2,24 @@ class SlackAgentMessageSendJob < ApplicationJob
   queue_as :default
 
   def perform(params)
-    Slack::Web::Client.new.tap do |client|
-      client.chat_postMessage(params)
+    begin
+      Slack::Web::Client.new.tap do |client|
+        client.chat_postMessage(params)
+      end
+    rescue Slack::Web::Api::Errors::TooManyRequestsError => error
+      # Slack通知がこれ以上できないときにSlack通知すると無限エラーを発生させてしまうためメール通知だけに絞る(重要)
+      ExceptionNotifier.notify_exception(error, notifiers: [:email])
+
+      # rescue *[
+      #     Slack::Web::Api::Errors::SlackError, # 指定のチャンネルがない (Slack側で手動で作る必要がある)
+      #     Faraday::ConnectionFailed,           # Wi-Fi が切れた
+      #     Faraday::ParsingError,               # SlackがJSON形式のエラーを返せないほど壊れた (WEBサーバーのエラーがHTML形式でがくる)
+      #   ] => error
+      #   ExceptionNotifier.notify_exception(error) # email と slack に通知
+
+    rescue => error
+      # Slack通知はリトライしなくてよいのでここですべてのエラーを捕捉する
+      ExceptionNotifier.notify_exception(error) # email と slack に通知
     end
-  rescue Slack::Web::Api::Errors::TooManyRequestsError, Faraday::ConnectionFailed, Faraday::ParsingError => error
-    # Faraday::ParsingError: Slackが完全に動いていないときHTMLを返してHTMLをJSONとして読み込もうとしたときのエラー
-    ExceptionNotifier.notify_exception(error)
   end
 end
