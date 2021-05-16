494569b06158440944ece5da9f6a3c695d0d15ec
akicho8 <akicho8@gmail.com>
2021-05-16 10:00:06 +0900

    [fix] 対局時計の初期値をlocalStorageに保存するときuser_nameがnullのまま保存される

--- front_app/components/ShareBoard/app_storage.js
+++ front_app/components/ShareBoard/app_storage.js
@@ -12,8 +12,10 @@ export const app_storage = {
   },
   computed: {
     // http://0.0.0.0:4000/share-board?default_user_name=foo でハンドルネームを設定できる(主にテスト用)
+    // persistent_cc_params の保存のタイミングで user_name が null のまま保存されると
+    // (自分が仕掛けたチェックで)でエラーになるので空文字列を設定すること
     default_user_name() {
-      return this.$route.query.default_user_name || this.g_current_user_name
+      return this.$route.query.default_user_name || this.g_current_user_name || ""
     },
 
     // ログイン名
