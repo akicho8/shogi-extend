b5c424e81287b8af1edbbf5d1174200671ab5691
akicho8 <akicho8@gmail.com>
2021-05-17 23:05:36 +0900

    [feat] ua_icon_detect の実行タイミングを beforeMount に移動

--- front_app/components/ShareBoard/app_devise.js
+++ front_app/components/ShareBoard/app_devise.js
@@ -2,10 +2,10 @@ export const app_devise = {
   data() {
     return {
       ua_notify_count: 0, // ua_notify_once が呼ばれた回数
-      ua_icon: null,      // 端末種別
+      ua_icon: null,      // 端末情報
     }
   },
-  mounted() {
+  beforeMount() {
     this.ua_icon = this.ua_icon_detect() // 何回も実行する必要がないので最初だけ
   },
   methods: {
@@ -13,8 +13,7 @@ export const app_devise = {
       if (this.ua_notify_count === 0) {
         this.ua_notify_count += 1
         if (typeof window !== 'undefined') {
-          // this.ac_log("端末種別", `${this.ua_icon_detect()} ${window.navigator.userAgent}`)
-          this.ac_log("端末種別", window.navigator.userAgent || "")
+          this.ac_log("端末情報", window.navigator.userAgent || "")
         }
       }
     },
