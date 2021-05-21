833ad76a68abf14979b87bd807fbca021c8b3799
akicho8 <akicho8@gmail.com>
2021-05-21 09:32:46 +0900

    [refactor] environment.rb で例外チェックをしていたのを元に戻す

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,4 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-begin
-  Rails.application.initialize!
-rescue => error
-  puts error.backtrace
-  raise error
-end
+Rails.application.initialize!
