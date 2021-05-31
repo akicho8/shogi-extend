74bc112785de9922d14c0e3816c6175fb22f440b
akicho8 <akicho8@gmail.com>
2021-05-31 11:43:50 +0900

    Revert "[fix][なんでも棋譜変換] 容量が大きすぎるときのエラーが出ない"
    This reverts commit 29b297d40b56157e82c703e036adf88ef9da07a8.

--- app/controllers/api/adapters_controller.rb
+++ app/controllers/api/adapters_controller.rb
@@ -2,10 +2,6 @@ module Api
   class AdaptersController < ::Api::ApplicationController
     # curl -d _method=post http://0.0.0.0:3000/api/adapter/record_create.json
     def record_create
-      if params[:input_text].to_s.strip == "ValueTooLong"
-        raise ActiveRecord::ValueTooLong, "(message)"
-      end
-
       record = FreeBattle.create!(kifu_body: params[:input_text], use_key: "adapter")
       render json: { record: record.as_json(methods: [:all_kifs, :display_turn, :piyo_shogi_base_params]) }
     end
