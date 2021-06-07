d7f59d9146bcdfc97096e04c86d3e2a8bd6ff1df
akicho8 <akicho8@gmail.com>
2021-06-07 14:10:24 +0900

    [feat][development] スマホからaxiosが動かない

--- config/initializers/0160_cros.rb
+++ config/initializers/0160_cros.rb
@@ -46,6 +46,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       # resource "*", headers: :any, methods: [:head, :get, :post, :patch, :put, :options], credentials: true
       origins *[
         "0.0.0.0:4000",
+        "10.0.1.2:4000",        # スマホを 10.0.1.2:4000 で見ているとき、これを設定するとしゃべるようになる
         "localhost:4000",
       ]
       resource "*", {
