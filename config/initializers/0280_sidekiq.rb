1af9e78ef4db166701f41b3e2a0beea8d2092aa6
akicho8 <akicho8@gmail.com>
2021-06-16 20:32:37 +0900

    [fix] rails app:update できない

--- config/initializers/0280_sidekiq.rb
+++ config/initializers/0280_sidekiq.rb
@@ -26,63 +26,65 @@
 #
 # require 'sidekiq-status'
 
-redis_db_for_sidekiq = AppConfig[:redis_db_for_sidekiq]
+if defined? AppConfig
+  redis_db_for_sidekiq = AppConfig[:redis_db_for_sidekiq]
 
-# tp({redis_db_for_sidekiq: redis_db_for_sidekiq})
+  # tp({redis_db_for_sidekiq: redis_db_for_sidekiq})
 
-redis_options = {
-  :url       => "redis://localhost:6379/#{redis_db_for_sidekiq}",
-  :namespace => "shogi_web_#{Rails.env}_sidekiq",
-}
+  redis_options = {
+    :url       => "redis://localhost:6379/#{redis_db_for_sidekiq}",
+    :namespace => "shogi_web_#{Rails.env}_sidekiq",
+  }
 
-# default_worker_options = {
-#   'backtrace' => true,
-# }
+  # default_worker_options = {
+  #   'backtrace' => true,
+  # }
 
-################################################################################
-# Sidekiq.configure_client と configure_server の両方に設定すること！
-################################################################################
+  ################################################################################
+  # Sidekiq.configure_client と configure_server の両方に設定すること！
+  ################################################################################
 
-# 表側 (WEBの方)
-Sidekiq.configure_client do |config|
-  config.redis = redis_options
-  # config.default_worker_options = default_worker_options
+  # 表側 (WEBの方)
+  Sidekiq.configure_client do |config|
+    config.redis = redis_options
+    # config.default_worker_options = default_worker_options
 
-  # #== sidekiq-status setting
-  # config.client_middleware do |chain|
-  #   chain.add Sidekiq::Status::ClientMiddleware
+    # #== sidekiq-status setting
+    # config.client_middleware do |chain|
+    #   chain.add Sidekiq::Status::ClientMiddleware
+    # end
+  end
+
+  # 裏側 (バックグラウンドで動いている側)
+  Sidekiq.configure_server do |config|
+    config.redis = redis_options
+    # config.default_worker_options = default_worker_options # これ重要
+
+    #== sidekiq-failures setting
+    # 失敗jobを保持する最大数、容易にunlimitにするのは危ない
+    # config.failures_max_count    = 1000  # default: 1000, false(unlimit)
+    # config.failures_default_mode = :all  # default :all, :exhausted or :off.
+
+    # #== sidekiq-status setting
+    # config.server_middleware do |chain|
+    #   # expiration == ステータス情報を保持しておく時間 (default: 30.minutes)
+    #   chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
+    # end
+    # config.client_middleware do |chain|
+    #   chain.add Sidekiq::Status::ClientMiddleware
+    # end
+  end
+
+  # 出力ログを日本標準時にする
+  # http://qiita.com/h_hys/items/250dce4d972295937318
+  # class JSTTimestampFormatter < Sidekiq::Logging::WithoutTimestamp
+  #   def call(severity, time, program_name, message)
+  #     "#{time.localtime.iso8601(3)} #{super}"
+  #   end
   # end
-end
+  # Sidekiq::Logging.logger.formatter = JSTTimestampFormatter.new
 
-# 裏側 (バックグラウンドで動いている側)
-Sidekiq.configure_server do |config|
-  config.redis = redis_options
-  # config.default_worker_options = default_worker_options # これ重要
-
-  #== sidekiq-failures setting
-  # 失敗jobを保持する最大数、容易にunlimitにするのは危ない
-  # config.failures_max_count    = 1000  # default: 1000, false(unlimit)
-  # config.failures_default_mode = :all  # default :all, :exhausted or :off.
-
-  # #== sidekiq-status setting
-  # config.server_middleware do |chain|
-  #   # expiration == ステータス情報を保持しておく時間 (default: 30.minutes)
-  #   chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
-  # end
-  # config.client_middleware do |chain|
-  #   chain.add Sidekiq::Status::ClientMiddleware
-  # end
-end
-
-# 出力ログを日本標準時にする
-# http://qiita.com/h_hys/items/250dce4d972295937318
-# class JSTTimestampFormatter < Sidekiq::Logging::WithoutTimestamp
-#   def call(severity, time, program_name, message)
-#     "#{time.localtime.iso8601(3)} #{super}"
-#   end
-# end
-# Sidekiq::Logging.logger.formatter = JSTTimestampFormatter.new
-
-if Rails.env.test?
-  require 'sidekiq/testing/inline'
+  if Rails.env.test?
+    require 'sidekiq/testing/inline'
+  end
 end
