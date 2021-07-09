6b0a3e1c7a4394b46afc41341c6937608b31ab0c
akicho8 <akicho8@gmail.com>
2021-07-09 11:20:58 +0900

    [fix] cap が動かない

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.0"
+lock "~> 3.16.0"
 
 set :application, "shogi_web"
 set :repo_url, -> { "git@github.com:akicho8/shogi-extend.git" }
