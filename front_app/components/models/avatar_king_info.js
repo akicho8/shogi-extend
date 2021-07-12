b9b7667a255b099d60141260b93c523bc72cdd87
akicho8 <akicho8@gmail.com>
2021-07-13 08:52:25 +0900

    [feat] アバターとシャウトでon/offを切り替えたときのメッセージを変更

--- front_app/components/models/avatar_king_info.js
+++ front_app/components/models/avatar_king_info.js
@@ -1,12 +1,12 @@
 import MemoryRecord from 'js-memory-record'
 
 export class AvatarKingInfo extends MemoryRecord {
-  static get message() { return "ﾌﾟﾛﾌ画像が玉になる" }
+  static get message() { return null }
 
   static get define() {
     return [
-      { key: "is_avatar_king_off", name: "なし", message: null, },
-      { key: "is_avatar_king_on",  name: "あり", message: null, },
+      { key: "is_avatar_king_off", name: "なし", message: "普通に駒を表示", },
+      { key: "is_avatar_king_on",  name: "あり", message: "自分が玉になる", },
     ]
   }
 }
--- front_app/components/models/shout_mode_info.js
+++ front_app/components/models/shout_mode_info.js
@@ -1,12 +1,12 @@
 import MemoryRecord from 'js-memory-record'
 
 export class ShoutModeInfo extends MemoryRecord {
-  static get message() { return "駒が叫ぶ" }
+  static get message() { return null }
 
   static get define() {
     return [
-      { key: "is_shout_mode_off", name: "なし", message: null, },
-      { key: "is_shout_mode_on",  name: "あり", message: null, },
+      { key: "is_shout_mode_off", name: "なし", message: "叫ばない",         },
+      { key: "is_shout_mode_on",  name: "あり", message: "いちいち駒が叫ぶ", },
     ]
   }
 }
