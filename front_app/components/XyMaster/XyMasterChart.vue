6f722ec91d83366de4261571b9b414bf5afbf36b
akicho8 <akicho8@gmail.com>
2021-08-02 23:26:00 +0900

    [feat][xy_master] チャート調整

--- front_app/components/XyMaster/XyMasterChart.vue
+++ front_app/components/XyMaster/XyMasterChart.vue
@@ -89,11 +89,12 @@ const CHART_CONFIG_DEFAULT = {
           labelString: "タイム",
         },
         ticks: {
-          maxTicksLimit: 7, // 最大横N個の目盛りにする
+          maxTicksLimit: 5, // 最大横N個の目盛りにする
           // suggestedMax: 1,
-          // suggestedMin: 1,
-          // stepSize: 15,
+          suggestedMin: 60,
+          stepSize: 30,
           // max: 60*3,
+          // min: 60,
           callback(value, index, values) {
             return dayjs.unix(value).format("mm:ss")
             // return Math.trunc(value / 60) + "時"
