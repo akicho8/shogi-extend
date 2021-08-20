bbd4578dd956a8e242030f72412c998e974c8b82
akicho8 <akicho8@gmail.com>
2021-08-20 19:33:15 +0900

    [feat] mp4_generate -> mp4_create_method

---
+++ front_app/components/Xconv/models/mp4_create_method_info.js
@@ -0,0 +1,18 @@
+import MemoryRecord from 'js-memory-record'
+
+export class Mp4CreateMethodInfo extends MemoryRecord {
+  static get field_label() {
+    return "mp4生成方法"
+  }
+
+  static get field_message() {
+    return ""
+  }
+
+  static get define() {
+    return [
+      { key: "ffmpeg",   name: "ffmpeg",  type: "is-primary", message: "連番ファイルから生成。トータルでは遅いけど安定", },
+      { key: "rmagick",  name: "rmagick", type: "is-primary", message: "内部のリストからmp4生成。結局ffmpegでyuv420変換の必要あり。やや速いけどとても不安定", },
+    ]
+  }
+}
