f8665f493f13998905bb4e801e543451166585b0
akicho8 <akicho8@gmail.com>
2021-09-02 09:29:50 +0900

    [fix] Full HD が指定できない

--- app/models/image_size_info.rb
+++ app/models/image_size_info.rb
@@ -1,7 +1,7 @@
 class ImageSizeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :width,  default: 1200, max: Rails.env.development? ? 3200 : 1600, },
-    { key: :height, default:  630, max: Rails.env.development? ? 3200 : 1200, },
+    { key: :width,  default: 1200, max: 4096, },
+    { key: :height, default:  630, max: 4096, },
   ]
 end
