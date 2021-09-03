03b613801acf412e9b2030a476eda7fd81a8af7d
akicho8 <akicho8@gmail.com>
2021-09-03 10:52:20 +0900

    [feat] 480p 追加

--- front_app/components/Xmovie/models/animation_size_info.js
+++ front_app/components/Xmovie/models/animation_size_info.js
@@ -13,8 +13,9 @@ export class AnimationSizeInfo extends MemoryRecord {
   static get define() {
     return [
       // https://ja.wikipedia.org/wiki/%E7%94%BB%E9%9D%A2%E8%A7%A3%E5%83%8F%E5%BA%A6
-      { key: "is1280x720",  name: "1280x720",       aspect_ratio: "16:9",   icon: "youtube", type: "is-danger", recommend: "◎", general_name: "HD",          width: 1280, height:  720,  type: "is-primary", environment: ["development", "staging", "production"], message: "動画サイト向け",        },
-      { key: "is1920x1080", name: "1920x1080",      aspect_ratio: "16:9",   icon: "youtube", type: "is-danger", recommend: "◎", general_name: "FULL HD",     width: 1920, height: 1080,  type: "is-primary", environment: ["development", "staging", "production"], message: "動画サイト向け 高画質", },
+      { key: "is854x480",   name: "480p",           aspect_ratio: "16:9",   icon: "youtube", type: "is-danger", recommend: "◎", general_name: "FULL HD",     width: 854, height: 480,    type: "is-primary", environment: ["development", "staging", "production"], message: "動画サイト向け 低画質", },
+      { key: "is1280x720",  name: "720p",           aspect_ratio: "16:9",   icon: "youtube", type: "is-danger", recommend: "◎", general_name: "HD",          width: 1280, height:  720,  type: "is-primary", environment: ["development", "staging", "production"], message: "動画サイト向け 一般的", },
+      { key: "is1920x1080", name: "1080p",          aspect_ratio: "16:9",   icon: "youtube", type: "is-danger", recommend: "◎", general_name: "FULL HD",     width: 1920, height: 1080,  type: "is-primary", environment: ["development", "staging", "production"], message: "動画サイト向け 高画質", },
       { separator: true },
       { key: "is320x240",   name: "320x240",        aspect_ratio: "4:3",    icon: "rss",     type: "is-orange", recommend: "×", general_name: "Quarter VGA", width:  320, height:  240,  type: "is-primary", environment: ["development",                        ], message: "フォント呆け",          },
       { key: "is640x480",   name: "640x480",        aspect_ratio: "4:3",    icon: "rss",     type: "is-orange", recommend: "×", general_name: "VGA",         width:  640, height:  480,  type: "is-primary", environment: ["development",                        ], message: "フォント呆け",          },
