b3c6cef0d319cb3e8aa856ae9e6f6193f95ef522
akicho8 <akicho8@gmail.com>
2021-09-05 12:33:58 +0900

    [feat] 1手 0.01 秒が指定できない

--- nuxt_side/components/Xmovie/XmovieForm2.vue
+++ nuxt_side/components/Xmovie/XmovieForm2.vue
@@ -8,7 +8,7 @@
   //- SimpleRadioButtons.one_block(:base="base" :model="base.AudioThemeInfo" var_name="audio_theme_key")
 
   b-field.one_block(label="1手N秒")
-    b-numberinput(v-model="base.one_frame_duration_sec" :min="0" :max="3" :step="0.1" exponential @input="sound_play('click')")
+    b-numberinput(v-model="base.one_frame_duration_sec" :min="0" :max="3" :step="one_frame_duration_sec_step" exponential @input="sound_play('click')")
 
   b-field.one_block(label="最後N秒停止")
     b-numberinput(v-model="base.end_duration_sec" :min="0" :max="10" :step="1" exponential @input="sound_play('click')")
@@ -20,6 +20,16 @@ import { support_child } from "./support_child.js"
 export default {
   name: "XmovieForm2",
   mixins: [support_child],
+  computed: {
+    one_frame_duration_sec_step() {
+      if (this.base.one_frame_duration_sec <= 0.1) {
+        return 0.01
+      } else {
+        return 0.1
+      }
+    },
+  },
+
 }
 </script>
 
