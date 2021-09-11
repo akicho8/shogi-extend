f43f77d8500679b851b3b0c408f4332cd4ab4794
akicho8 <akicho8@gmail.com>
2021-09-11 21:05:28 +0900

    [feat] 使い方 追記

--- nuxt_side/components/Xmovie/XmovieHelpBody.vue
+++ nuxt_side/components/Xmovie/XmovieHelpBody.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.XmovieHelpBody
+.XmovieHelpBody.is_line_break_on
   .content
     article
       h2 簡単な使い方
@@ -16,9 +16,71 @@
         | 終わったらメールするので待たなくてもいい
 
     article
-      h2 出来たファイルを直リンしていい？
+      h2 出来たファイルに直リンしていい？
       .article_body
-        p 定期的に古いのは削除するので直リンしない方が良い
+        p 容量の関係で古いのは消す場合もあるのでなるべくダウンロードしてほしい
+
+    article
+      h2 動画の長さと手数の関係
+      .article_body
+        .content
+          p
+            code 動画の長さ
+            | は
+            code 1手の秒数 * (1 + 手数) + 最後に停止する秒数
+            | で求まる
+          p
+            | なので動画の長さから含めることのできる手数を逆算したい場合などは
+            ul
+              li 2分20秒の動画
+              li 最後に7秒停止する
+              li 1手1秒
+            | なので
+            code (2分20秒 - 7秒) / 1秒 - 1
+            | として最長 132 手までいけることがわかる
+
+    article
+      h2 動画から棋譜に変換するには？
+      .article_body
+        | メタデータにSFEN形式の棋譜を入れてあるので ffprobe に読ませたり、
+        a.mx-1(href="https://mediaarea.net/MediaInfoOnline" target="_blank") https://mediaarea.net/MediaInfoOnline
+        | にアップロードして内容を取得し、「なんでも棋譜変換」等に投げる
+
+    article
+      h2 音にハメるには？
+      .article_body
+        | 1手1秒としたときは 120bpm の曲に合わせるとはまる
+        br
+        | つまり
+        code 1手の秒数 * 120
+        | ではまる bpm がわかる
+        pre
+          | 1.5 * 120 = 180 bpm
+          | 1.4 * 120 = 168 bpm
+          | 1.3 * 120 = 156 bpm
+          | 1.2 * 120 = 144 bpm
+          | 1.1 * 120 = 132 bpm
+          | 1.0 * 120 = 120 bpm
+          | 0.9 * 120 = 108 bpm
+          | 0.8 * 120 =  96 bpm
+          | 0.7 * 120 =  84 bpm
+          | 0.6 * 120 =  72 bpm
+          | 0.5 * 120 =  60 bpm
+        | 逆に bpm にハメる1手の秒数を求めるには
+        code bpm / 120
+        | とする
+        pre
+          | 160 / 120 = 1.333 s
+          | 150 / 120 = 1.25 s
+          | 140 / 120 = 1.167 s
+          | 130 / 120 = 1.083 s
+          | 120 / 120 = 1.0 s
+          | 110 / 120 = 0.917 s
+          | 100 / 120 = 0.833 s
+          |  90 / 120 = 0.75 s
+          |  80 / 120 = 0.667 s
+          |  70 / 120 = 0.583 s
+          |  60 / 120 = 0.5 s
 
     article(v-if="development_p")
       h2 Twitterのアップロード条件は本当？
@@ -77,4 +139,6 @@ export default {
       margin-left: 1rem
       th
         white-space: nowrap
+    pre
+      margin-top: 0.75rem
 </style>
