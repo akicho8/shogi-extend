eb76d40d6c89987d6114cc625c7560c608b676e7
akicho8 <akicho8@gmail.com>
2021-09-22 15:15:41 +0900

    [refactor] MainSettingModal

--- nuxt_side/components/ShareBoard/MainSettingModal.vue
+++ nuxt_side/components/ShareBoard/MainSettingModal.vue
@@ -1,12 +1,8 @@
 <template lang="pug">
-//- (style="width:auto")
 .modal-card
-  ////////////////////////////////////////////////////////////////////////////////
   .modal-card-head
     .modal-card-title
       | 設定
-
-  ////////////////////////////////////////////////////////////////////////////////
   .modal-card-body
     .columns.is-multiline
       template(v-for="m in MainSettingInfo.values")
@@ -15,11 +11,8 @@
             template(v-for="e in base[m.model].values")
               b-radio-button(:class="e.key" @input="sound_play('click')" size="is-small" v-model="base[m.key]" :native-value="e.key" :type="e.type")
                 | {{e.name}}
-
   .modal-card-foot
     b-button.close_button(@click="close_handle" icon-left="chevron-left") 閉じる
-    //- b-button.test_button(@click="test_handle" v-if="development_p") 追加
-    //- b-button.send_button(@click="send_handle" type="is-primary") 送信
 </template>
 
 <script>
@@ -28,29 +21,12 @@ import { MainSettingInfo } from "./main_setting_info.js"
 
 export default {
   name: "MainSettingModal",
-  mixins: [
-    support_child,
-  ],
+  mixins: [support_child],
   methods: {
     close_handle() {
       this.sound_play("click")
       this.$emit("close")
     },
-    // test_handle() {
-    //   this.sound_play("click")
-    //   this.base.ml_add_test()
-    // },
-    // send_handle() {
-    //   if (this.present_p(this.base.message_body2)) {
-    //     this.sound_play("click")
-    //     this.base.message_share({message: this.base.message_body2})
-    //     this.base.message_body2 = ""
-    //     this.input_focus()
-    //   }
-    // },
-    // input_focus() {
-    //   this.desktop_focus_to(this.$refs.message_input_tag)
-    // },
   },
   computed: {
     MainSettingInfo() { return MainSettingInfo },
@@ -61,16 +37,9 @@ export default {
 <style lang="sass">
 @import "support.sass"
 .MainSettingModal
-  // +tablet
-  //   width: 100%
+  +modal_max_width(800px)
   .modal-card-body
     padding: 1.75rem
     .field:not(:first-child)
       margin-top: 1.25rem
-
-  .modal-card-foot
-    justify-content: space-between
-    .button
-      min-width: 6rem
-      font-weight: bold
 </style>
--- nuxt_side/components/ShareBoard/app_main_setting.js
+++ nuxt_side/components/ShareBoard/app_main_setting.js
@@ -13,21 +13,9 @@ export const app_main_setting = {
     general_setting_modal_handle() {
       this.sidebar_p = false
       this.sound_play("click")
-
-      this.$buefy.modal.open({
-        customClass: "MainSettingModal",
+      this.modal_card_open({
         component: MainSettingModal,
-        parent: this,
-        trapFocus: true,
-        hasModalCard: true,
-        animation: "",
-        canCancel: ["escape", "outside"],
-        onCancel: () => {
-          this.sound_play("click")
-        },
-        props: {
-          base: this.base,
-        },
+        props: { base: this.base },
       })
     },
   },
