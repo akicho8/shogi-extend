a53c58416e366b067d454f7382774262119ed8ce
akicho8 <akicho8@gmail.com>
2021-10-07 12:40:24 +0900

    [fix] 背景アップロードサムネの上のマージンがない

--- nuxt_side/components/Kiwi/KiwiLemonNew/KiwiLemonNewImageUpload.vue
+++ nuxt_side/components/Kiwi/KiwiLemonNew/KiwiLemonNewImageUpload.vue
@@ -6,7 +6,7 @@
         b-icon(icon="upload" size="is-small")
         .is-size-7.ml-2 {{label}}
 
-  .image_preview(v-if="new_file_info")
+  .image_preview.mt-2(v-if="new_file_info")
     img(:src="new_file_info.url")
     button.delete(size="is-small" @click="delete_handle")
 </template>
