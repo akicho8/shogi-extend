0d11dd537f168c898fffe6069bd1dcc7ec561560
akicho8 <akicho8@gmail.com>
2021-10-07 12:53:31 +0900

    [fix] 先手/後手を選択しても発音しない

--- nuxt_side/components/SimpleRadioButtons.vue
+++ nuxt_side/components/SimpleRadioButtons.vue
@@ -2,7 +2,7 @@
 b-field(:label="model.field_label" :message="model.fetch(base[var_name]).message || model.field_message")
   template(v-for="e in model.values")
     template(v-if="e.environment == null || e.environment.includes($config.STAGE)")
-      b-radio-button(@input="sound_play('click')" v-model="base[var_name]" :native-value="e.key" :type="e.type")
+      b-radio-button(@input="click_handle" v-model="base[var_name]" :native-value="e.key" :type="e.type" v-on="$listeners")
         | {{e.name}}
 </template>
 
@@ -11,8 +11,14 @@ export default {
   name: "SimpleRadioButtons",
   props: {
     model:    { type: Function, required: true, },
-    base:     { type: Object, required: true, },
-    var_name: { type: String, required: true, },
+    base:     { type: Object,   required: true, },
+    var_name: { type: String,   required: true, },
+  },
+  methods: {
+    click_handle(e) {
+      this.sound_play("click")
+      this.talk(this.model.fetch(this.base[this.var_name]).name)
+    },
   },
 }
 </script>
