c543c72fa6d2490828699ccf1f9038484cebaf56
akicho8 <akicho8@gmail.com>
2021-10-08 09:55:22 +0900

    [feat] 切替えリンクはヘッダーにも置く

--- nuxt_side/components/Kiwi/KiwiBookShow/KiwiBookShowNavbar.vue
+++ nuxt_side/components/Kiwi/KiwiBookShow/KiwiBookShowNavbar.vue
@@ -4,6 +4,8 @@ MainNavbar.KiwiBookShowNavbar(:spaced="false" centered wrapper-class="container 
     b-navbar-item.px_5_if_tablet(tag="nuxt-link" :to="{name: 'video'}" @click.native="sound_play('click')")
       b-icon(icon="chevron-left")
   template(slot="end")
+    b-navbar-item.px_5_if_tablet(@click="base.switch_handle" v-if="base.book")
+      b-icon(icon="hand-pointing-up")
     b-navbar-item.px_5_if_tablet(@click.native="sound_play('click')" tag="nuxt-link" :to="{name: 'video-studio-book_key-edit', params: {book_key: base.book.key}}" v-if="base.book && base.owner_p")
       b-icon(icon="pencil")
     b-navbar-item.px_5_if_tablet(@click.native="sound_play('click')" tag="nuxt-link" :to="{name: 'share-board', query: {...base.current_share_board_params, title: base.book.title}}" v-if="base.book")
