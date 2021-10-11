b561c470cc28b5600c71a65f8ee6730e23092882
akicho8 <akicho8@gmail.com>
2021-10-11 14:16:41 +0900

    [fix] 厳選 → 特選

--- app/models/kiwi/banana/alpha_zero_vs_elmo_build_methods.rb
+++ app/models/kiwi/banana/alpha_zero_vs_elmo_build_methods.rb
@@ -17,17 +17,17 @@ module Kiwi
 
           tp Rails.env
           [
-            # { key: "Z8Yc9dPZek9", number: 1, color_theme_key: :is_color_theme_radial_gradiention1 },
-            { key: "RMl6paUAYM9", number: 12, color_theme_key: :is_color_theme_radial_gradiention1 },
-            { key: "TAs1CTDPsvd", number: 17, color_theme_key: :is_color_theme_radial_gradiention2 },
-            { key: "wIW3tnZMnUD", number: 18, color_theme_key: :is_color_theme_radial_gradiention3 },
-            { key: "nVxSJI7tlap", number: 23, color_theme_key: :is_color_theme_radial_gradiention4 },
-            { key: "qgeYRTJJ6tc", number: 32, color_theme_key: :is_color_theme_gradiention1 },
-            { key: "TW5vDpMXgXW", number: 35, color_theme_key: :is_color_theme_gradiention2 },
-            { key: "GDARn1MSPsu", number: 52, color_theme_key: :is_color_theme_gradiention3 },
-            { key: "TISYdaPlwhQ", number: 76, color_theme_key: :is_color_theme_gradiention4 },
-            { key: "q8Fsa72oSOs", number: 78, color_theme_key: :is_color_theme_plasma_blur1 },
-            { key: "Fyq37jDjjxV", number: 81, color_theme_key: :is_color_theme_plasma_blur2 },
+            { key: "Z8Yc9dPZek9", number: 1, color_theme_key: :is_color_theme_radial_gradiention1 },
+            # { key: "RMl6paUAYM9", number: 12, color_theme_key: :is_color_theme_radial_gradiention1 },
+            # { key: "TAs1CTDPsvd", number: 17, color_theme_key: :is_color_theme_radial_gradiention2 },
+            # { key: "wIW3tnZMnUD", number: 18, color_theme_key: :is_color_theme_radial_gradiention3 },
+            # { key: "nVxSJI7tlap", number: 23, color_theme_key: :is_color_theme_radial_gradiention4 },
+            # { key: "qgeYRTJJ6tc", number: 32, color_theme_key: :is_color_theme_gradiention1 },
+            # { key: "TW5vDpMXgXW", number: 35, color_theme_key: :is_color_theme_gradiention2 },
+            # { key: "GDARn1MSPsu", number: 52, color_theme_key: :is_color_theme_gradiention3 },
+            # { key: "TISYdaPlwhQ", number: 76, color_theme_key: :is_color_theme_gradiention4 },
+            # { key: "q8Fsa72oSOs", number: 78, color_theme_key: :is_color_theme_plasma_blur1 },
+            # { key: "Fyq37jDjjxV", number: 81, color_theme_key: :is_color_theme_plasma_blur2 },
           ].each.with_index do |params, i|
             tp params
 
@@ -47,7 +47,7 @@ module Kiwi
                 :recipe_key      => "is_recipe_mp4",
                 :color_theme_key => params[:color_theme_key], # ColorGradientInfo.fetch(i.modulo(ColorGradientInfo.count)).key,
                 :audio_theme_key => "is_audio_theme_ds3479",
-                :cover_text      => "羽生善治厳選 ##{i.next}\n#{versus} 百番勝負 第#{params[:number]}局\n#{black_white}\n#{judgment_message}",
+                :cover_text      => "羽生善治特選 ##{i.next}\n#{versus} 百番勝負 第#{params[:number]}局\n#{black_white}\n#{judgment_message}",
                 :page_duration   => 1.0,
                 :end_duration    => 7,
                 :width           => 1920,
@@ -73,9 +73,9 @@ module Kiwi
             banana1 ||= user.kiwi_bananas.build(lemon: lemon1, key: params[:key])
             banana1.update!({
                 :folder_key    => "public",
-                :title         => "羽生善治厳選 ##{i.next} #{versus} 百番勝負 第#{params[:number]}局",
+                :title         => "羽生善治特選 ##{i.next} #{versus} 百番勝負 第#{params[:number]}局",
                 :description   => "#{black_white}\n#{judgment_message}",
-                :tag_list      => ["AlphaZero", "elmo", "羽生善治", *lemon1.recordable.all_tag_names],
+                :tag_list      => ["AlphaZero", "elmo", "羽生善治", "百番勝負", *lemon1.recordable.all_tag_names],
                 :thumbnail_pos => 1 + lemon1.recordable.adjust_turn,
               })
 
