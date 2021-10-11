ec1c46809e1a1f7e59f795692d412faf624f060f
akicho8 <akicho8@gmail.com>
2021-10-12 08:03:48 +0900

    [feat] ライブラリ登録のラベルは消す

--- nuxt_side/components/Kiwi/KiwiLemonNew/KiwiLemonNewReview.vue
+++ nuxt_side/components/Kiwi/KiwiLemonNew/KiwiLemonNewReview.vue
@@ -18,7 +18,7 @@
 
     .buttons.is-centered.mt-3
       b-button.has-text-weight-bold(@click="base.download_handle" tag="a" :href="base.done_record.browser_path"            type="is-primary" icon-left="download"    :download="base.done_record.filename_human") ダウンロード
-      b-button(@click="base.banana_new_handle(base.done_record)"            type=""                                      icon-left="upload") ライブラリ登録
+      b-button(@click="base.banana_new_handle(base.done_record)"            type=""                                      icon-left="upload")
       b-button(v-if="development_or_staging_p" @click="sound_play('click')" tag="a" :href="base.done_record.browser_path"            type=""           icon-left="eye-outline" target="_blank" )
       b-button(v-if="development_or_staging_p" @click="base.__main_download_handle(base.done_record)"     type="is-light"   icon-left="download"            )
       b-button(v-if="development_or_staging_p" @click="base.__main_show_handle(base.done_record)"         type="is-light"   icon-left="eye-outline"         )
