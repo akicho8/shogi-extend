07aa5d810ddcab583f8d7ce6ca416f9fb4237311
akicho8 <akicho8@gmail.com>
2021-10-13 20:49:51 +0900

    [feat] 木目盤が多すぎる

--- alpha_zero_vs_elmo/normalize.rb
+++ alpha_zero_vs_elmo/normalize.rb
@@ -11,7 +11,7 @@ Pathname.glob("*.csa") do |e|
   mp4 = info.to_animation_mp4({
       :page_duration   => 1.0,
       :end_duration    => 7,
-      :color_theme_key => "is_color_theme_real_wood1",
+      :color_theme_key => "is_color_theme_groovy_board_texture08",
       :audio_theme_key => "is_audio_theme_ds3479",
     })
   e.sub_ext(".mp4").write(mp4)
