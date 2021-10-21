fcaec3040f49be7217a0471f5f1ed14967cd4518
akicho8 <akicho8@gmail.com>
2021-10-21 18:40:41 +0900

    [feat] ミクロコスモスは自動生成しない

--- app/models/kiwi/banana/import_items.rb
+++ app/models/kiwi/banana/import_items.rb
@@ -1,31 +1,31 @@
 module Kiwi
   class Banana
     ImportItems = [
-      {
-        :source_file => "kifu_files/microcosmos.sfen",
-        :key  => "KmmhXy2S4aG",
-        :body => nil,
-        :all_params => {
-          :media_builder_params => {
-            :recipe_key      => :is_recipe_mp4,
-            :color_theme_key => :is_color_theme_groovy_board_texture1,
-            :audio_theme_key => :is_audio_theme_ds3480,
-            :cover_text      => "最長手数詰将棋\nミクロコスモス 1525手詰\n作者: 橋本孝治\n『詰将棋探検隊』1995年発表",
-            :page_duration   => 0.016666666666666666,
-            :end_duration    => 7,
-            :width           => 1920,
-            :height          => 1080,
-            :main_volume     => 0.3,
-          },
-        },
-        :banana_params => {
-          :folder_key    => :public,
-          :title         => "ミクロコスモス 作:橋本孝治",
-          :description   => "",
-          :tag_list      => ["ミクロコスモス", "橋本孝治", "詰将棋"],
-          :thumbnail_pos => 1,
-        },
-      },
+      # {
+      #   :source_file => "kifu_files/microcosmos.sfen",
+      #   :key  => "KmmhXy2S4aG",
+      #   :body => nil,
+      #   :all_params => {
+      #     :media_builder_params => {
+      #       :recipe_key      => :is_recipe_mp4,
+      #       :color_theme_key => :is_color_theme_groovy_board_texture1,
+      #       :audio_theme_key => :is_audio_theme_ds3480,
+      #       :cover_text      => "最長手数詰将棋\nミクロコスモス 1525手詰\n作者: 橋本孝治\n『詰将棋探検隊』1995年発表",
+      #       :page_duration   => 0.016666666666666666,
+      #       :end_duration    => 7,
+      #       :width           => 1920,
+      #       :height          => 1080,
+      #       :main_volume     => 0.3,
+      #     },
+      #   },
+      #   :banana_params => {
+      #     :folder_key    => :public,
+      #     :title         => "ミクロコスモス 作:橋本孝治",
+      #     :description   => "",
+      #     :tag_list      => ["ミクロコスモス", "橋本孝治", "詰将棋"],
+      #     :thumbnail_pos => 1,
+      #   },
+      # },
     ]
   end
 end
