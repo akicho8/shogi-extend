1d070cd7bd7138f17cd542c8a360db022dc885ec
akicho8 <akicho8@gmail.com>
2021-11-08 14:25:05 +0900

    [feat] サイドバーの整理

--- nuxt_side/components/ShareBoard/ShareBoardSidebar.vue
+++ nuxt_side/components/ShareBoard/ShareBoardSidebar.vue
@@ -27,19 +27,19 @@ b-sidebar.is-unselectable.ShareBoardSidebar(fullheight right overlay v-model="ba
         b-menu-list(label="検討")
           //- b-menu-item.is_active_unset(label="ぴよ将棋" :href="base.piyo_shogi_app_with_params_url" :target="target_default" @click="sound_play_click()")
           //- b-menu-item.is_active_unset(label="KENTO"    :href="base.kento_app_with_params_url"      :target="target_default" @click="sound_play_click()")
-          b-menu-item.is_active_unset(icon="duck"                   label="ぴよ将棋" :href="base.piyo_shogi_app_with_params_url" :target="target_default" @click="base.other_app_click_handle('ぴよ将棋')")
-          b-menu-item.is_active_unset(icon="alpha-k-box-outline"    label="KENTO"    :href="base.kento_app_with_params_url"      :target="target_default" @click="base.other_app_click_handle('KENTO')")
           b-menu-item.is_active_unset(icon="clipboard-plus-outline" label="棋譜コピー" @click="base.kifu_copy_handle(base.FormatTypeInfo.fetch('kif'))")
           b-menu-item.is_active_unset(icon="link"                   label="棋譜リンクコピー" @click="base.room_code_except_url_copy_handle")
+          b-menu-item.is_active_unset(icon="duck"                   label="ぴよ将棋" :href="base.piyo_shogi_app_with_params_url" :target="target_default" @click="base.other_app_click_handle('ぴよ将棋')")
+          b-menu-item.is_active_unset(icon="alpha-k-box-outline"    label="KENTO"    :href="base.kento_app_with_params_url"      :target="target_default" @click="base.other_app_click_handle('KENTO')")
 
-        b-menu-list(label="詰将棋や特殊な手合割の作成")
+        b-menu-list(label="詰将棋・課題局面・特殊手合割の作成")
           b-menu-item.is_active_unset(icon="pencil-box-outline" label="局面編集"       @click="base.edit_mode_handle")
           b-menu-item.is_active_unset(icon="import"             label="棋譜の読み込み" @click="base.any_source_read_handle")
 
         b-menu-list(label="SNS共有")
-          b-menu-item.is_active_unset(icon="twitter" label="ツイートする"           @click="base.tweet_modal_handle")
-          b-menu-item.is_active_unset(icon="link"    label="ツイートリンクのコピー" @click="base.current_url_copy_handle")
-          b-menu-item.is_active_unset(icon="eye"     label="ツイート画像の視点設定" @click="base.abstract_viewpoint_key_select_modal_handle")
+          b-menu-item.is_active_unset(icon="twitter" label="ツイートする"              @click="base.tweet_modal_handle")
+          b-menu-item.is_active_unset(icon="link"    label="ツイートリンクのコピー"    @click="base.current_url_copy_handle")
+          b-menu-item.is_active_unset(icon="eye"     label="ツイート画像の視点設定"    @click="base.abstract_viewpoint_key_select_modal_handle")
 
         ShareBoardSidebarExport(:base="base")
 
