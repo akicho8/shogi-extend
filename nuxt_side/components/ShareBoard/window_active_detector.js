ea58022c552bf0d4c823ce4a27ece9079ab59e84
akicho8 <akicho8@gmail.com>
2021-11-23 12:34:49 +0900

    [refactor] USE_VISIBILITYCHANGE を使えるようにする

--- nuxt_side/components/ShareBoard/window_active_detector.js
+++ nuxt_side/components/ShareBoard/window_active_detector.js
@@ -2,8 +2,12 @@
 // ・focus のとき +1 して blur のとき -1 することで 0 か 1 になるのか？
 // ・mobile safari の共有リンクで Twitter App に飛んで戻ったとき 0 でも 1 でもなくなる
 // ・なので focus や blur の一方が連続で呼ばれるのを考慮しないといけない
-
-// const USE_VISIBILITYCHANGE = true
+//
+// 2021-11-23 スマホの focus, blur 問題
+// ・上下にスクロールした時点で blur になってしまう
+// ・なので visibilitychange に変更
+//
+const USE_VISIBILITYCHANGE = false
 
 export const window_active_detector = {
   data() {
@@ -16,34 +20,48 @@ export const window_active_detector = {
   mounted() {
     this.window_active_p = this.native_window_active_p()
 
-    // document.addEventListener("visibilitychange", this.visibilitychange_hook)
-    window.addEventListener("focus", this.window_focus_hook)
-    window.addEventListener("blur", this.window_blur_hook)
+    if (USE_VISIBILITYCHANGE) {
+      document.addEventListener("visibilitychange", this.visibilitychange_hook)
+    } else {
+      window.addEventListener("focus", this.window_focus_hook)
+      window.addEventListener("blur", this.window_blur_hook)
+    }
   },
 
   beforeDestroy() {
-    // document.removeEventListener("visibilitychange", this.visibilitychange_hook)
-    window.removeEventListener("focus", this.window_focus_hook)
-    window.removeEventListener("blur", this.window_blur_hook)
+    if (USE_VISIBILITYCHANGE) {
+      document.removeEventListener("visibilitychange", this.visibilitychange_hook)
+    } else {
+      window.removeEventListener("focus", this.window_focus_hook)
+      window.removeEventListener("blur", this.window_blur_hook)
+    }
   },
 
   methods: {
     // private
     native_window_active_p() {
-      // return document.visibilityState === "visible" && document.hasFocus()
-      // return document.visibilityState === "visible"
-      return document.hasFocus()
+      if (USE_VISIBILITYCHANGE) {
+        return document.visibilityState === "visible"
+      } else {
+        return document.hasFocus()
+      }
     },
     window_focus_hook() {
-      // this.tl_add("HOOK", "focus")
+      if (this.debug_mode_p) {
+        this.tl_add("HOOK", "focus")
+      }
       this.window_active_change_hook(true)
     },
     window_blur_hook() {
-      // this.tl_add("HOOK", "blur")
+      if (this.debug_mode_p) {
+        this.tl_add("HOOK", "blur")
+      }
       this.window_active_change_hook(false)
     },
     visibilitychange_hook() {
-      // this.tl_add("HOOK", `visibilitychange: ${focus_p}`)
+      if (this.debug_mode_p) {
+        this.tl_add("HOOK", document.visibilityState)
+      }
       this.window_active_change_hook(this.native_window_active_p())
     },
     window_active_change_hook(focus_p) {
