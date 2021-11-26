0f47ef816d6d09af44f4b20e2d595ccc87a5f9a8
akicho8 <akicho8@gmail.com>
2021-11-26 14:01:42 +0900

    [feat] URLに依存せずパラメータ名から棋譜を読み取る

--- app/models/url_embed_kifu_parser.rb
+++ app/models/url_embed_kifu_parser.rb
@@ -1,144 +1,185 @@
-# shogidb2 の URL から棋譜を抽出
-#
-# require "uri"
-# require "rack"
-#
-# url1 = "https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202"
-# url2 = "https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM"
-#
-# # URI.extract(url1, ["http", "https"]).first # => "https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202"
-#
-# uri = URI(url1)
-# uri.scheme   # => "https"
-# uri.userinfo # => nil
-# uri.host     # => "shogidb2.com"
-# uri.port     # => 443
-# uri.path     # => "/games/018d3d1ee6594c34c677260002621417c8f75221"
-# uri.query    # => nil
-# uri.fragment # => "lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202"
-# uri.to_s     # => "https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202"
-# if uri.fragment
-#   s = Rack::Utils.unescape(uri.fragment) # => "lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
-#   ["position sfen", s].join(" ")         # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
-# end
-#
-# uri = URI(url2)
-# uri.scheme   # => "https"
-# uri.userinfo # => nil
-# uri.host     # => "shogidb2.com"
-# uri.port     # => 443
-# uri.path     # => "/board"
-# uri.query    # => "sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM"
-# uri.fragment # => nil
-# uri.to_s     # => "https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM"
-#
-# hash = Rack::Utils.parse_query(uri.query) # => {"sfen"=>"lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2", "moves"=>"-3334FU+2726FU-8384FU+2625FU-8485FU+5958OU-4132KI+6978KI-8586FU+8786FU-8286HI+2524FU-2324FU+2824HI-8684HI+0087FU-0023FU+2428HI-2233KA+5868OU-7172GI+9796FU-3142GI+8833UM"}
-# ["position sfen", hash["sfen"]].join(" ") # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
-
-# URLが含まれるテキストをSFENに変換する
-#  sfen = UrlEmbedKifuParser.parse("https://www.kento-shogi.com/?moves=5g5f.8c8d.7g7f.8d8e.8h7g.6a5b.2h5h.7a6b.7i6h.5a4b.6h5g.4b3b.5i4h.1c1d.1g1f.6c6d.5f5e.6b6c.5g5f.3a4b.4h3h.4c4d.3h2h.4b4c.3i3h.7c7d.9g9f.9c9d.9i9g.3c3d.4g4f.2b3c.5h9h.8e8f.8g8f.8b8d.6i5h.3b2b.9f9e.9d9e.9g9e.P%2A9d.9e9d.9a9d.P%2A9e.7d7e.7f7e.3c2d.5h4g.4a3b.9e9d.5c5d.L%2A8e.5d5e.5f5e.P%2A7f.7g9i.8d8e.8f8e.2d3c.4f4e.L%2A8f.5e4d.4c4d.9i4d.8f8i%2B.S%2A4a.S%2A4c.4a3b%2B.4c3b.9d9c%2B.3c4d.4e4d.L%2A4e.4d4c%2B.4e4g%2B.4c3b.2b3b.3h4g.7f7g%2B.9c8c.B%2A6e.9h9b%2B.6e4g%2B.L%2A4h.P%2A4f.4h4g.4f4g%2B.P%2A4h.S%2A5h.4i3i.L%2A3h.3i3h.4g3h.2h3h.S%2A1g.B%2A5d.G%2A4c.5d4c%2B.3b4c.L%2A4f.N%2A4d.2i1g.B%2A5g.S%2A5e.G%2A5d.R%2A4a.4c3c.5e4d#34")
-#  assert { sfen == "position startpos moves 5g5f 8c8d 7g7f 8d8e 8h7g 6a5b 2h5h 7a6b 7i6h 5a4b 6h5g 4b3b 5i4h 1c1d 1g1f 6c6d 5f5e 6b6c 5g5f 3a4b 4h3h 4c4d 3h2h 4b4c 3i3h 7c7d 9g9f 9c9d 9i9g 3c3d 4g4f 2b3c 5h9h 8e8f 8g8f 8b8d 6i5h 3b2b 9f9e 9d9e 9g9e P*9d 9e9d 9a9d P*9e 7d7e 7f7e 3c2d 5h4g 4a3b 9e9d 5c5d L*8e 5d5e 5f5e P*7f 7g9i 8d8e 8f8e 2d3c 4f4e L*8f 5e4d 4c4d 9i4d 8f8i+ S*4a S*4c 4a3b+ 4c3b 9d9c+ 3c4d 4e4d L*4e 4d4c+ 4e4g+ 4c3b 2b3b 3h4g 7f7g+ 9c8c B*6e 9h9b+ 6e4g+ L*4h P*4f 4h4g 4f4g+ P*4h S*5h 4i3i L*3h 3i3h 4g3h 2h3h S*1g B*5d G*4c 5d4c+ 3b4c L*4f N*4d 2i1g B*5g S*5e G*5d R*4a 4c3c 5e4d" }
 class UrlEmbedKifuParser
-  def self.http_get_body(url)
-    connection = Faraday.new do |builder|
-      builder.response :follow_redirects # リダイレクト先をおっかける
-      builder.adapter :net_http
+  class << self
+    def http_get_body(url)
+      connection = Faraday.new do |builder|
+        builder.response :follow_redirects # リダイレクト先をおっかける
+        builder.adapter :net_http
+      end
+
+      response = connection.get(url)
+      s = response.body
+
+      s = s.toutf8
+      s = s.gsub(/\\n/, "") # 棋王戦のKIFには備考に改行コードではない '\n' という文字が入っていることがある
     end
 
-    response = connection.get(url)
-    s = response.body
+    def convert(source)
+      parse(source) || source
+    end
 
-    s = s.toutf8
-    s = s.gsub(/\\n/, "") # 棋王戦のKIFには備考に改行コードではない '\n' という文字が入っていることがある
+    def parse(source)
+      new(source).parse
+    end
   end
 
-  URL_CHECK_HEAD_LINES = 4
-
-  attr_accessor :dirty_text
+  attr_accessor :source
   attr_accessor :body
 
   delegate :http_get_body, to: "self.class"
 
-  def self.convert(dirty_text)
-    parse(dirty_text) || dirty_text
+  def initialize(source, options = {})
+    @options = {
+      url_check_head_lines: 4,
+    }.merge(options)
+
+    @source = source.to_s
   end
 
-  def self.parse(dirty_text)
-    new(dirty_text).parse
-  end
-
-  def initialize(dirty_text)
-    @dirty_text = dirty_text.to_s
-  end
-
-  # 抽出した本体をSFENに変換してエラーにならないなら「SFENではなく」本体を返す
+  # 抽出した本体が読み込めて最後の局面まで行けるなら「SFENではなく」本体を返す
   def parse
     extract_body
-    if body
-      Bioshogi::Parser.parse(body).to_sfen
+    if @body
+      Bioshogi::Parser.parse(@body, {
+          :skill_monitor_enable           => false,
+          :skill_monitor_technique_enable => false,
+          :candidate_enable               => false,
+          :validate_enable                => true,
+        }).mediator_run_once
       body
     end
   rescue Bioshogi::BioshogiError => error
-    Rails.logger.info(error)
+    SlackAgent.notify_exception(error)
     nil
   end
 
+  private
+
   def extract_body
-    if e = Bioshogi::TacticInfo.flat_lookup(@dirty_text.strip)
+    @body = nil
+    [
+      :extract_body_if_tactic,
+      :extract_body_if_preset,
+      :extract_body_if_swars_url,
+      :extract_body_if_kiousen_url,
+      :extract_body_if_kento_url,
+      :extract_body_if_shogidb2_url,
+      :extract_body_if_url_params,
+      :extract_body_if_other_url,
+    ].each do |e|
+      send(e)
+      if @body
+        break
+      end
+    end
+  end
+
+  def extract_body_if_tactic
+    if e = Bioshogi::TacticInfo.flat_lookup(@source.strip)
       @body = e.sample_kif_file.read
-      return
     end
+  end
 
-    if e = Bioshogi::PresetInfo.lookup(@dirty_text.strip)
+  def extract_body_if_preset
+    if e = Bioshogi::PresetInfo.lookup(@source.strip)
       @body = e.to_position_sfen
-      return
     end
+  end
 
-    if dirty_text.strip.lines.count <= URL_CHECK_HEAD_LINES && dirty_text.match?(%{^https?://})
-      # ウォーズのURL
-      if key = Swars::Battle.battle_key_extract(dirty_text)
+  def extract_body_if_swars_url
+    if url_type?
+      if key = Swars::Battle.battle_key_extract(@source)
         Swars::Battle.single_battle_import(key: key)
         if battle = Swars::Battle.find_by(key: key)
           @body = battle.kifu_body
-          return
         end
       end
+    end
+  end
 
-      url = URI.extract(dirty_text, ["http", "https"]).first
-      if url
-        uri = URI(url)
-
-        # 棋王戦
-        # http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.html
-        if uri.host.end_with?("live.shogi.or.jp")
-          uri.path = uri.path.sub(/html\z/, "kif")
-          @body = http_get_body(uri.to_s)
-          return
-        end
-
-        if uri.host.end_with?("shogidb2.com")
-          sfen = nil
-          if uri.fragment
-            sfen = Rack::Utils.unescape(uri.fragment) # => "lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
-          end
-          if uri.query
-            hash = Rack::Utils.parse_query(uri.query) # => {"sfen"=>"lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2", "moves"=>"-3334FU+2726FU-8384FU+2625FU-8485FU+5958OU-4132KI+6978KI-8586FU+8786FU-8286HI+2524FU-2324FU+2824HI-8684HI+0087FU-0023FU+2428HI-2233KA+5868OU-7172GI+9796FU-3142GI+8833UM"}
-            sfen = hash["sfen"]
-          end
-          if sfen.present?
-            @body = ["position", "sfen", sfen].join(" ")
-            return
-          end
-        end
-
-        if uri.host.end_with?("kento-shogi.com")
-          @body = KentoParamsParser.parse(url).to_sfen
-          return
-        end
-
-        # http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.kif
-        @body = http_get_body(url)
+  # 棋王戦
+  # http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.html
+  def extract_body_if_kiousen_url
+    if uri = extracted_uri
+      if uri.host.end_with?("live.shogi.or.jp")
+        uri.path = uri.path.sub(/html\z/, "kif")
+        @body = http_get_body(uri.to_s)
       end
+    end
+  end
+
+  def extract_body_if_shogidb2_url
+    if uri = extracted_uri
+      if uri.host.end_with?("shogidb2.com")
+        sfen = nil
+        if uri.fragment
+          sfen = Rack::Utils.unescape(uri.fragment)
+        end
+        if uri.query
+          hash = Rack::Utils.parse_query(uri.query)
+          sfen = hash["sfen"]
+        end
+        if sfen.present?
+          @body = ["position", "sfen", sfen].join(" ")
+        end
+      end
+    end
+  end
+
+  def extract_body_if_kento_url
+    if uri = extracted_uri
+      if uri.host.end_with?("kento-shogi.com")
+        @body = KentoParamsParser.parse(uri.to_s).to_sfen
+      end
+    end
+  end
+
+  # https://example.com/?body=68S
+  # https://example.com/?sfen=68S
+  # https://example.com/#68S
+  def extract_body_if_url_params
+    if uri = extracted_uri
+      if uri.query || uri.fragment
+        hash = {}
+        if uri.query
+          hash.update(Rack::Utils.parse_query(uri.query))
+        end
+        if uri.fragment
+          hash.update("__fragment__" => Rack::Utils.unescape(uri.fragment))
+        end
+        ["kif", "ki2", "csa", "sfen", "bod", "body", "kifu", "text", "content", "__fragment__"].each do |e|
+          if v = hash[e]
+            v = DotSfen.unescape(v.to_s)
+            if v.present?
+              if Bioshogi::Parser.accepted_class(v)
+                @body = v
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  # http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.kif
+  def extract_body_if_other_url
+    if url = extracted_url
+      @body = http_get_body(url)
+    end
+  end
+
+  def url_type?
+    @source.strip.lines.count <= @options[:url_check_head_lines] && @source.match?(%{^https?://})
+  end
+
+  def extracted_url
+    if url_type?
+      @extracted_url ||= URI.extract(@source, ["http", "https"]).first
+    end
+  end
+
+  def extracted_uri
+    if extracted_url
+      @uri ||= URI(extracted_url)
     end
   end
 end
--- spec/models/url_embed_kifu_parser_spec.rb
+++ spec/models/url_embed_kifu_parser_spec.rb
@@ -1,14 +1,73 @@
 require "rails_helper"
 
 RSpec.describe UrlEmbedKifuParser, type: :model do
-  it do
-    sfen = UrlEmbedKifuParser.parse("https://www.kento-shogi.com/?moves=5g5f.8c8d.7g7f.8d8e.8h7g.6a5b.2h5h.7a6b.7i6h.5a4b.6h5g.4b3b.5i4h.1c1d.1g1f.6c6d.5f5e.6b6c.5g5f.3a4b.4h3h.4c4d.3h2h.4b4c.3i3h.7c7d.9g9f.9c9d.9i9g.3c3d.4g4f.2b3c.5h9h.8e8f.8g8f.8b8d.6i5h.3b2b.9f9e.9d9e.9g9e.P%2A9d.9e9d.9a9d.P%2A9e.7d7e.7f7e.3c2d.5h4g.4a3b.9e9d.5c5d.L%2A8e.5d5e.5f5e.P%2A7f.7g9i.8d8e.8f8e.2d3c.4f4e.L%2A8f.5e4d.4c4d.9i4d.8f8i%2B.S%2A4a.S%2A4c.4a3b%2B.4c3b.9d9c%2B.3c4d.4e4d.L%2A4e.4d4c%2B.4e4g%2B.4c3b.2b3b.3h4g.7f7g%2B.9c8c.B%2A6e.9h9b%2B.6e4g%2B.L%2A4h.P%2A4f.4h4g.4f4g%2B.P%2A4h.S%2A5h.4i3i.L%2A3h.3i3h.4g3h.2h3h.S%2A1g.B%2A5d.G%2A4c.5d4c%2B.3b4c.L%2A4f.N%2A4d.2i1g.B%2A5g.S%2A5e.G%2A5d.R%2A4a.4c3c.5e4d#34")
-    assert { sfen == "position startpos moves 5g5f 8c8d 7g7f 8d8e 8h7g 6a5b 2h5h 7a6b 7i6h 5a4b 6h5g 4b3b 5i4h 1c1d 1g1f 6c6d 5f5e 6b6c 5g5f 3a4b 4h3h 4c4d 3h2h 4b4c 3i3h 7c7d 9g9f 9c9d 9i9g 3c3d 4g4f 2b3c 5h9h 8e8f 8g8f 8b8d 6i5h 3b2b 9f9e 9d9e 9g9e P*9d 9e9d 9a9d P*9e 7d7e 7f7e 3c2d 5h4g 4a3b 9e9d 5c5d L*8e 5d5e 5f5e P*7f 7g9i 8d8e 8f8e 2d3c 4f4e L*8f 5e4d 4c4d 9i4d 8f8i+ S*4a S*4c 4a3b+ 4c3b 9d9c+ 3c4d 4e4d L*4e 4d4c+ 4e4g+ 4c3b 2b3b 3h4g 7f7g+ 9c8c B*6e 9h9b+ 6e4g+ L*4h P*4f 4h4g 4f4g+ P*4h S*5h 4i3i L*3h 3i3h 4g3h 2h3h S*1g B*5d G*4c 5d4c+ 3b4c L*4f N*4d 2i1g B*5g S*5e G*5d R*4a 4c3c 5e4d" }
+  def test1(text)
+    UrlEmbedKifuParser.parse(text).truncate(512)
+  end
+
+  it "works" do
+    if $0 == "-"
+      test1("https://www.kento-shogi.com/?moves=7g7f.3c3d.8h2b%2B#3") # => "position startpos moves 7g7f 3c3d 8h2b+"
+      test1("https://www.kento-shogi.com/?branch=N%2A7e.7d7e.B%2A7d.8c9c.G%2A9b.9a9b.7d9b%2B.9c9b.6c7b.R%2A8b.G%2A8c.9b9a.7b8b.7c8b.R%2A9b&branchFrom=0&initpos=ln7%2F2g6%2F1ks%2BR5%2Fpppp5%2F9%2F9%2F%2Bp%2Bp%2Bp6%2F2%2Bp6%2FK1%2Bp6%20b%20NGB9p3l2n3s2gbr%201#6") # => "position sfen ln7/2g6/1ks+R5/pppp5/9/9/+p+p+p6/2+p6/K1+p6 b NGB9p3l2n3s2gbr 1"
+      test1("https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202") # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
+      test1("https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM") # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2"
+      test1("https://shogiwars.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900?tw=1") # => "N+Kato_Hifumi 三段\nN-SiroChannel 四段\n$START_TIME:2020/09/27 18:09:00\n$EVENT:将棋ウォーズ(10分切れ負け)\n$TIME_LIMIT:00:10+00\n+\n+5756FU\nT0\n-8384FU\nT1\n+7776FU\nT2\n-8485FU\nT1\n+8877KA\nT1\n-6152KI\nT1\n+2858HI\nT2\n-7162GI\nT9\n+7968GI\nT1\n-5142OU\nT0\n+6857GI\nT1\n-4232OU\nT2\n+5948OU\nT1\n-1314FU\nT7\n+1716FU\nT1\n-6364FU\nT1\n+5655FU\nT1\n-6263GI\nT3\n+5756GI\nT1\n-3142GI\nT1\n+4838OU\nT1\n-4344FU\nT2\n+3828OU\nT1\n-4243GI\nT1\n+3938GI\nT2\n-7374FU\nT0\n+9796FU\nT2\n-9394FU\nT2\n+9997KY\nT1\n-3334FU\nT7\n+4746FU\nT2\n-2233KA\nT5\n+5898HI\nT2\n-8586FU\nT7\n+8786FU\nT2\n-8284HI\nT2\n+..."
+      test1("https://kif-pona.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900?tw=1") # => "N+Kato_Hifumi 三段\nN-SiroChannel 四段\n$START_TIME:2020/09/27 18:09:00\n$EVENT:将棋ウォーズ(10分切れ負け)\n$TIME_LIMIT:00:10+00\n+\n+5756FU\nT0\n-8384FU\nT1\n+7776FU\nT2\n-8485FU\nT1\n+8877KA\nT1\n-6152KI\nT1\n+2858HI\nT2\n-7162GI\nT9\n+7968GI\nT1\n-5142OU\nT0\n+6857GI\nT1\n-4232OU\nT2\n+5948OU\nT1\n-1314FU\nT7\n+1716FU\nT1\n-6364FU\nT1\n+5655FU\nT1\n-6263GI\nT3\n+5756GI\nT1\n-3142GI\nT1\n+4838OU\nT1\n-4344FU\nT2\n+3828OU\nT1\n-4243GI\nT1\n+3938GI\nT2\n-7374FU\nT0\n+9796FU\nT2\n-9394FU\nT2\n+9997KY\nT1\n-3334FU\nT7\n+4746FU\nT2\n-2233KA\nT5\n+5898HI\nT2\n-8586FU\nT7\n+8786FU\nT2\n-8284HI\nT2\n+..."
+      test1("http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.html") # => "# --- Kifu for Windows (Pro) V6.65.47 棋譜ファイル ---\r\n対局ID：10156\r\n記録ID：5e2559475d617f00041e7a4a\r\n開始日時：2020/02/01 09:00\r\n終了日時：2020/02/01 17:04\r\n表題：棋王戦\r\n棋戦：第４５期棋王戦五番勝負　第１局\r\n戦型：矢倉\r\n持ち時間：各４時間\r\n消費時間：95▲171△207\r\n場所：石川・北國新聞会館\r\n備考：昼休前48手目14分\r\n振り駒：5,0,渡\r\n先手消費時間加算：0\r\n後手消費時間加算：0\r\n昼食休憩：12:00〜13:00\r\n昼休前消費時間：48手14分\r\n手合割：平手　　\r\n先手：渡辺明棋王\r\n後手：本田奎五段\r\n先手省略名：渡辺明\r\n手数----指手---------消費時間--\r\n*渡辺明棋王に本田奎五段が挑戦する第45期棋王戦五番勝負が２月１日（土）石川県金沢市「北國新聞会館」で開幕する。自己記録更新の８連覇を目指す棋界の第一人者に、史上初となる棋戦初参加にしてタイトル挑戦を決めた期待の超新星が挑む五番勝負。両者は本局が初手合いとなる。対局は９時開始。持ち時間..."
+      test1("http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.kif") # => "# --- Kifu for Windows (Pro) V6.65.47 棋譜ファイル ---\r\n対局ID：10156\r\n記録ID：5e2559475d617f00041e7a4a\r\n開始日時：2020/02/01 09:00\r\n終了日時：2020/02/01 17:04\r\n表題：棋王戦\r\n棋戦：第４５期棋王戦五番勝負　第１局\r\n戦型：矢倉\r\n持ち時間：各４時間\r\n消費時間：95▲171△207\r\n場所：石川・北國新聞会館\r\n備考：昼休前48手目14分\r\n振り駒：5,0,渡\r\n先手消費時間加算：0\r\n後手消費時間加算：0\r\n昼食休憩：12:00〜13:00\r\n昼休前消費時間：48手14分\r\n手合割：平手　　\r\n先手：渡辺明棋王\r\n後手：本田奎五段\r\n先手省略名：渡辺明\r\n手数----指手---------消費時間--\r\n*渡辺明棋王に本田奎五段が挑戦する第45期棋王戦五番勝負が２月１日（土）石川県金沢市「北國新聞会館」で開幕する。自己記録更新の８連覇を目指す棋界の第一人者に、史上初となる棋戦初参加にしてタイトル挑戦を決めた期待の超新星が挑む五番勝負。両者は本局が初手合いとなる。対局は９時開始。持ち時間..."
+      test1("https://www.shogi-extend.com/share-board?body=position.sfen.lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F9%2FPPPPPPPPP%2F1B5R1%2FLNSGKGSNL.b.-.1.moves.7g7f&turn=1&abstract_viewpoint=black&color_theme_key=is_color_theme_groovy_board_texture1") # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 7g7f"
+
+      test1("https://example.com/?body=68S")                    # => "68S"
+      test1("https://example.com/?body=position.sfen.startpos") # => "position sfen startpos"
+      test1("https://example.com/?sfen=68S")                    # => "68S"
+      test1("https://example.com/?csa=68S")                     # => "68S"
+      test1("https://example.com/?kif=68S")                     # => "68S"
+      test1("https://example.com/?ki2=68S")                     # => "68S"
+      test1("https://example.com/?content=68S")                 # => "68S"
+      test1("https://example.com/?text=68S")                    # => "68S"
+      test1("https://example.com/#68S")                         # => "68S"
+    end
+
+    assert { test1("https://www.kento-shogi.com/?moves=7g7f.3c3d.8h2b%2B#3") == "position startpos moves 7g7f 3c3d 8h2b+" }
+    assert { test1("https://www.kento-shogi.com/?branch=N%2A7e.7d7e.B%2A7d.8c9c.G%2A9b.9a9b.7d9b%2B.9c9b.6c7b.R%2A8b.G%2A8c.9b9a.7b8b.7c8b.R%2A9b&branchFrom=0&initpos=ln7%2F2g6%2F1ks%2BR5%2Fpppp5%2F9%2F9%2F%2Bp%2Bp%2Bp6%2F2%2Bp6%2FK1%2Bp6%20b%20NGB9p3l2n3s2gbr%201#6") == "position sfen ln7/2g6/1ks+R5/pppp5/9/9/+p+p+p6/2+p6/K1+p6 b NGB9p3l2n3s2gbr 1" }
+    assert { test1("https://shogidb2.com/games/018d3d1ee6594c34c677260002621417c8f75221#lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202") == "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2" }
+    assert { test1("https://shogidb2.com/board?sfen=lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F2P6%2FPP1PPPPPP%2F1B5R1%2FLNSGKGSNL%20w%20-%202&moves=-3334FU%2B2726FU-8384FU%2B2625FU-8485FU%2B5958OU-4132KI%2B6978KI-8586FU%2B8786FU-8286HI%2B2524FU-2324FU%2B2824HI-8684HI%2B0087FU-0023FU%2B2428HI-2233KA%2B5868OU-7172GI%2B9796FU-3142GI%2B8833UM") == "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL w - 2" }
+    assert { test1("https://shogiwars.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900?tw=1").match?(/SiroChannel 四段/) }
+    assert { test1("https://kif-pona.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900?tw=1").match?(/SiroChannel 四段/) }
+    assert { test1("http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.html").match?(/Kifu for Windows/) }
+    assert { test1("http://live.shogi.or.jp/kiou/kifu/45/kiou202002010101.kif").match?(/Kifu for Windows/) }
+    assert { test1("https://www.shogi-extend.com/share-board?body=position.sfen.lnsgkgsnl%2F1r5b1%2Fppppppppp%2F9%2F9%2F9%2FPPPPPPPPP%2F1B5R1%2FLNSGKGSNL.b.-.1.moves.7g7f&turn=1&abstract_viewpoint=black&color_theme_key=is_color_theme_groovy_board_texture1") == "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 7g7f" }
+
+    assert { test1("https://example.com/?body=68S")                    == "68S" }
+    assert { test1("https://example.com/?body=position.sfen.startpos") == "position sfen startpos" }
+    assert { test1("https://example.com/?sfen=68S")                    == "68S" }
+    assert { test1("https://example.com/?csa=68S")                     == "68S" }
+    assert { test1("https://example.com/?kif=68S")                     == "68S" }
+    assert { test1("https://example.com/?ki2=68S")                     == "68S" }
+    assert { test1("https://example.com/?content=68S")                 == "68S" }
+    assert { test1("https://example.com/?text=68S")                    == "68S" }
+    assert { test1("https://example.com/#68S")                         == "68S" }
   end
 end
-# >> Run options: exclude {:slow_spec=>true}
-# >> .
+# >> Run options: exclude {:login_spec=>true, :slow_spec=>true}
+# >> F
 # >> 
-# >> Finished in 0.60663 seconds (files took 2.15 seconds to load)
-# >> 1 example, 0 failures
+# >> Failures:
+# >> 
+# >>   1) UrlEmbedKifuParser works
+# >>      Failure/Error: Unable to find - to read failed line
+# >>      # -:31:in `block (2 levels) in <main>'
+# >>      # ./spec/support/database_cleaner.rb:22:in `block (3 levels) in <main>'
+# >>      # ./spec/support/database_cleaner.rb:22:in `block (2 levels) in <main>'
+# >> 
+# >> Top 1 slowest examples (1.42 seconds, 45.0% of total time):
+# >>   UrlEmbedKifuParser works
+# >>     1.42 seconds -:8
+# >> 
+# >> Finished in 3.15 seconds (files took 3.59 seconds to load)
+# >> 1 example, 1 failure
+# >> 
+# >> Failed examples:
+# >> 
+# >> rspec -:8 # UrlEmbedKifuParser works
 # >> 
