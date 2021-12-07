54510de3da35eecaa37c67eb1c721f00a03c6adb
akicho8 <akicho8@gmail.com>
2021-12-07 19:55:11 +0900

    [feat] 指手不達のエラーメール通知はしない

--- app/channels/share_board/room_channel.rb
+++ app/channels/share_board/room_channel.rb
@@ -39,7 +39,9 @@ module ShareBoard
     def sfen_share_not_reach(data)
       x_retry_count = data['x_retry_count']
       track(data, "指手不達", "#{x_retry_count}回目", ":指手不達:")
-      raise SfenNotReachError, "指手不達(#{x_retry_count}回目) : #{data}"
+      if Rails.env.development?
+        raise SfenNotReachError, "指手不達(#{x_retry_count}回目) : #{data}"
+      end
     end
 
     def title_share(data)
