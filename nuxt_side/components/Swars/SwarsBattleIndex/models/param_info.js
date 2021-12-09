beb97bea5f9b6a6a21afa896762183f2a190398c
akicho8 <akicho8@gmail.com>
2021-12-09 19:52:15 +0900

    [fix] 最初の状態でテーブルにならない

--- nuxt_side/components/Swars/SwarsBattleIndex/models/param_info.js
+++ nuxt_side/components/Swars/SwarsBattleIndex/models/param_info.js
@@ -6,7 +6,7 @@ export class ParamInfo extends ParamBase {
   static get define() {
     return [
       { key: "remember_vs_user_keys", type: "array",   name: "対戦相手の補完リスト", defaults: { development: null, production: [],                        }, permanent: true,  relation: null,          desc: null, },
-      { key: "display_key",           type: "string",  name: "局面表示タイプ",       defaults: { development: null, production: "critical",                }, permanent: true,  relation: "DisplayInfo", desc: null, },
+      { key: "display_key",           type: "string",  name: "局面表示タイプ",       defaults: { development: null, production: "table",                   }, permanent: true,  relation: "DisplayInfo", desc: null, },
       { key: "visible_hash",          type: "hash",    name: "カラム表示",           defaults: { development: null, production: this.visible_hash_default, }, permanent: true,  relation: null,          desc: null, },
     ]
   }
