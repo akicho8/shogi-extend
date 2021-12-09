051134eb50dd332d3944a965a99373928c35772f
akicho8 <akicho8@gmail.com>
2021-12-09 19:48:39 +0900

    [fix][test] development のつもりが test 環境を更新していた

--- spec/support/swars_system_support.rb
+++ spec/support/swars_system_support.rb
@@ -4,11 +4,11 @@ module SwarsSystemSupport
   included do
     before(:context) do
       command = <<~EOT
-        Swars.setup
         Swars::Battle.destroy_all
+        Swars.setup
         Swars::Battle.user_import(user_key: "devuser1")
       EOT
-      %x(rails r '#{command}')
+      %x(rails runner -e development '#{command}')
     end
   end
 end
