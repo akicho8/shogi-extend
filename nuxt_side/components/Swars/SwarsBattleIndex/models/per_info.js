5b01e0abd80e445c8090e15bb9db419fa43b8db9
akicho8 <akicho8@gmail.com>
2021-12-15 11:37:04 +0900

    [feat] 何も考えずに200件にしてしまう人向けの文言追加

--- nuxt_side/components/Swars/SwarsBattleIndex/models/per_info.js
+++ nuxt_side/components/Swars/SwarsBattleIndex/models/per_info.js
@@ -5,12 +5,12 @@ export class PerInfo extends ApplicationMemoryRecord {
 
   static get define() {
     return [
-      { key: "is_per1",   name: "1",   per:   1, available_env: { development: true, staging: false, production: false, }, },
-      { key: "is_per10",  name: "10",  per:  10, available_env: { development: true, staging: true,  production: true,  }, },
-      { key: "is_per25",  name: "25",  per:  25, available_env: { development: true, staging: true,  production: true,  }, },
-      { key: "is_per50",  name: "50",  per:  50, available_env: { development: true, staging: true,  production: true,  }, },
-      { key: "is_per100", name: "100", per: 100, available_env: { development: true, staging: true,  production: true,  }, },
-      { key: "is_per200", name: "200", per: 200, available_env: { development: true, staging: true,  production: true,  }, },
+      { key: "is_per1",   name: "1",           per:   1, available_env: { development: true, staging: false, production: false, }, },
+      { key: "is_per10",  name: "10 (速い)",   per:  10, available_env: { development: true, staging: true,  production: true,  }, },
+      { key: "is_per25",  name: "25",          per:  25, available_env: { development: true, staging: true,  production: true,  }, },
+      { key: "is_per50",  name: "50",          per:  50, available_env: { development: true, staging: true,  production: true,  }, },
+      { key: "is_per100", name: "100",         per: 100, available_env: { development: true, staging: true,  production: true,  }, },
+      { key: "is_per200", name: "200 (遅い)",  per: 200, available_env: { development: true, staging: true,  production: true,  }, },
     ]
   }
 
