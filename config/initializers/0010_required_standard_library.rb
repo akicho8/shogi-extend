86ce01d553e8b93bf8bb936cb19015a01c7c66fe
akicho8 <akicho8@gmail.com>
2021-12-20 19:02:33 +0900

    [refactor] require "matrix" の読み込み場所がおかしい

--- config/initializers/0010_required_standard_library.rb
+++ config/initializers/0010_required_standard_library.rb
@@ -1,3 +1,4 @@
-require "pp" # ruby 2.5.0 から require しなくていいというのは迷信。pretty_inspect がないと怒られる
+require "pp" # ruby 2.5.0 から require しなくていいというのは迷信。pretty_inspect が無くて怒られる
 require "nkf"
 require "kconv"
+require "matrix"
