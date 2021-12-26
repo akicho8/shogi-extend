3bf9e064393e741e6ea4206d3b45a9038d7cf654
akicho8 <akicho8@gmail.com>
2021-12-26 23:11:01 +0900

    [feat] SlackAgentNotifyJob retry_on

--- app/jobs/slack_agent_notify_job.rb
+++ app/jobs/slack_agent_notify_job.rb
@@ -1,16 +1,16 @@
 class SlackAgentNotifyJob < ApplicationJob
   queue_as :default
 
-  # rails r 'SlackAgent.notify(subject: "(subject)", body: "(body)")'
+  # 2 4 8 16 32 64 128 256 raise
+  retry_on(::Faraday::Error, wait: -> (executions) { 2**executions }, attempts: 8) do |job, error|
+    ExceptionNotifier.notify_exception(error, data: {slack: job}, notifiers: [:email])
+  end
+
+  # EXCEPTION_NOTIFICATION_ENABLE=1 rails r 'SlackAgent.notify(subject: "(subject)", body: "(body)")'
   def perform(params)
-    begin
-      Slack::Web::Client.new.tap do |client|
-        client.chat_postMessage(params)
-      end
-    rescue ::Faraday::Error => error
-      # Slack::Web::Api::Errors::*Error の親は ::Faraday::Error
-      # Slack通知がこれ以上できないときにSlack通知すると無限エラーを発生させてしまうためメール通知だけに絞る(重要)
-      ExceptionNotifier.notify_exception(error, data: {slack: params}, notifiers: [:email])
+    Slack::Web::Client.new.tap do |client|
+      client.chat_postMessage(params)
+      # raise ::Faraday::Error, "(fake)"
     end
   end
 end
