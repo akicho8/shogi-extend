d9fbd8ef29bbffb0307af1fa17b9162730a70ef7
akicho8 <akicho8@gmail.com>
2022-01-02 22:43:13 +0900

    [feat][wkbk] 問題を解くときにログイン必須としない

--- nuxt_side/components/Wkbk/WkbkBookShow/WkbkBookShowApp.vue
+++ nuxt_side/components/Wkbk/WkbkBookShow/WkbkBookShowApp.vue
@@ -125,7 +125,8 @@ export default {
   mounted() {
     // this.clog("book", this.book)
     // this.ga_click("インスタント将棋問題集")
-    if (this.nuxt_login_required()) { return }
+
+    // if (this.nuxt_login_required()) { return }
   },
 
   computed: {
