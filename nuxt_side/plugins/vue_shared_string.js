715e5322ec22e220475574a5c0bf425347827474
akicho8 <akicho8@gmail.com>
2022-01-19 15:34:21 +0900

    [feat] placeholder にスポニチ追加

--- nuxt_side/plugins/vue_shared_string.js
+++ nuxt_side/plugins/vue_shared_string.js
@@ -2,7 +2,7 @@ export const vue_shared_string = {
   computed: {
     SHARED_STRING() {
       return {
-        kifu_body_placeholder: "KIF KI2 CSA SFEN BOD の本体かURL。共有将棋盤・ぴよ将棋・KENTO・lishogi・将棋DB2・将棋ウォーズのURL。棋譜自体を引数に含むURL。棋譜URLをコンテンツに含むサイトURL。戦法名・囲い名などを入力してください",
+        kifu_body_placeholder: "KIF KI2 CSA SFEN BOD の本体かURL。共有将棋盤・ぴよ将棋・KENTO・lishogi・将棋DB2・スポニチ棋譜速報・将棋ウォーズのURL。棋譜自体を引数に含むURL。棋譜URLをコンテンツに含むサイトURL。戦法名・囲い名などを入力してください",
       }
     },
   },
