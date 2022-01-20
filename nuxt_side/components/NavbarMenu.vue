179658df00627b06f2dc8e509bbcd02bd2618b82
akicho8 <akicho8@gmail.com>
2022-01-20 15:31:31 +0900

    [feat] add NavbarMenu.vue

---
+++ nuxt_side/components/NavbarMenu.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+b-navbar-item.NavbarMenu.px_5_if_tablet.sidebar_toggle_navbar_item(
+  v-bind="$attrs"
+  v-on="$listeners"
+  )
+  b-icon(icon="menu")
+</template>
+
+<script>
+export default {
+  name: "NavbarMenu",
+}
+</script>
