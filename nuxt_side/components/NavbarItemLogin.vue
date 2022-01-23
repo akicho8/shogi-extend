f0a8096b9514a2330d0d6c605136ae256beadaa1
akicho8 <akicho8@gmail.com>
2022-01-23 19:13:02 +0900

    [fix] SSR でずっと未定義の nuxt_login_modal_handle を呼んでいた

--- nuxt_side/components/NavbarItemLogin.vue
+++ nuxt_side/components/NavbarItemLogin.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
-component(:is="component" class="NavbarItemLogin has-text-weight-bold" v-if="!g_current_user" @click="nuxt_login_modal_handle")
-  | ログイン
+client-only
+  component(:is="component" class="NavbarItemLogin has-text-weight-bold" v-if="!g_current_user" @click="nuxt_login_modal_handle")
+    | ログイン
 </template>
 
 <script>
