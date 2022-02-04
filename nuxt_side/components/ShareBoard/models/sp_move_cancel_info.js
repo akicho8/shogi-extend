7918d8cf8ca6b2af1859ab85f51046a99d5c8839
akicho8 <akicho8@gmail.com>
2022-02-04 13:00:24 +0900

    [feat][share-board] 文言改善

--- nuxt_side/components/ShareBoard/models/sp_move_cancel_info.js
+++ nuxt_side/components/ShareBoard/models/sp_move_cancel_info.js
@@ -7,7 +7,7 @@ export class SpMoveCancelInfo extends ApplicationMemoryRecord {
   static get define() {
     return [
       { key: "is_move_cancel_standard", name: "移動先以外", type: "is-primary", message: "将棋ウォーズやぴよ将棋はこれ。右クリックやESCキーでもキャンセルできる", },
-      { key: "is_move_cancel_reality",  name: "元の位置",   type: "is-primary", message: "従来の方法。リアルで指すのと同じように元の位置に戻してほしかったのそうしてたが他のアプリに慣れすぎた者は戸惑うようだ。右クリックやESCキーでもキャンセルできる", },
+      { key: "is_move_cancel_reality",  name: "元の位置",   type: "is-primary", message: "従来の方法。リアルで指すのと同じように元の位置に戻す。そうしてほしかった。しかし他のアプリに慣れすぎてしまい順応できない者たちが多かったため仕方なく初期値を変更した。右クリックやESCキーでもキャンセルできる", },
     ]
   }
 }
