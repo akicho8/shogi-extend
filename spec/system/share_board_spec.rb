43a3079449cae7141e49388daf7f36d261e3a333
akicho8 <akicho8@gmail.com>
2022-02-11 06:56:14 +0900

    [test][fix][system] テストが不安定

--- spec/system/share_board_spec.rb
+++ spec/system/share_board_spec.rb
@@ -1496,12 +1496,12 @@ RSpec.describe "共有将棋盤", type: :system, share_board_spec: true do
     it "works" do
       a_block do
         room_setup("my_room", "alice")
-        assert_text "aliceさんが入室しました"
+        assert_member_exist("alice")
         assert_text "部屋のリンクを仲間に伝えよう"
       end
       b_block do
         room_setup("my_room", "bob")
-        assert_text "bobさんが入室しました"
+        assert_member_exist("bob")
         assert_no_text "部屋のリンクを仲間に伝えよう"
       end
     end
