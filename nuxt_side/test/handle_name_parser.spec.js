fd8c3acd67df640d7e10b56aa60d996807839a27
akicho8 <akicho8@gmail.com>
2022-02-23 13:07:10 +0900

    [fix] "foochan" は "foochanさん" ではなく "foochan"

--- nuxt_side/test/handle_name_parser.spec.js
+++ nuxt_side/test/handle_name_parser.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import { HandleNameParser } from '@/components/models/handle_name_parser.js'
 
 describe('HandleNameParser', () => {
-  test('呼名', () => {
+  test('基本', () => {
     expect(HandleNameParser.call_name("foo123(456)")).toEqual("fooさん")
     expect(HandleNameParser.call_name("女王")).toEqual("女王様")
     expect(HandleNameParser.call_name("ココ")).toEqual("ココちゃん")
@@ -16,5 +16,13 @@ describe('HandleNameParser', () => {
     expect(HandleNameParser.call_name("alice!!")).toEqual("aliceさん")
     expect(HandleNameParser.call_name("中の人")).toEqual("中の人")
     expect(HandleNameParser.call_name("alice.")).toEqual("aliceさん")
+  })
+
+  test('ちゃん', () => {
+    expect(HandleNameParser.call_name("aliceちゃん")).toEqual("aliceちゃん") // 最後が "ん" の影響でスルーされているだけ
+  })
+
+  test('chan', () => {
+    expect(HandleNameParser.call_name("alicechan")).toEqual("alicechan")
   })
 })
