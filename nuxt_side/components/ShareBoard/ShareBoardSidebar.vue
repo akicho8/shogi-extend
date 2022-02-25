e0180b4c3b510d930cf912adc3735bbf6e25f244
akicho8 <akicho8@gmail.com>
2022-02-25 11:14:33 +0900

    [fix][share-board] 指アイコンの位置がずれて見える

--- nuxt_side/components/ShareBoard/ShareBoardSidebar.vue
+++ nuxt_side/components/ShareBoard/ShareBoardSidebar.vue
@@ -18,17 +18,17 @@ b-sidebar.is-unselectable.ShareBoardSidebar(fullheight right overlay v-model="ba
           b-menu-item.is_active_unset.important.room_setup_modal_handle(:class="base.bold_if(mi1_bold_p)" icon="numeric-1-circle-outline" @click="base.room_setup_modal_handle")
             template(#label)
               | 部屋に入る
-              b-icon.hand_blink(icon="hand-pointing-left" v-if="mi1_hand_p")
+              b-icon.is_hand_blink(size="is-small" icon="hand-pointing-left" v-if="mi1_hand_p")
 
           b-menu-item.is_active_unset.important.os_modal_handle(:class="base.bold_if(mi2_bold_p)" icon="numeric-2-circle-outline" @click="base.os_modal_handle")
             template(#label)
               | 順番設定
-              b-icon.hand_blink(icon="hand-pointing-left" v-if="mi2_hand_p")
+              b-icon.is_hand_blink(size="is-small" icon="hand-pointing-left" v-if="mi2_hand_p")
 
           b-menu-item.is_active_unset.important.cc_modal_handle(:class="base.bold_if(mi3_bold_p)" icon="numeric-3-circle-outline" @click="base.cc_modal_handle")
             template(#label)
               | 対局時計
-              b-icon.hand_blink(icon="hand-pointing-left" v-if="mi3_hand_p")
+              b-icon.is_hand_blink(size="is-small" icon="hand-pointing-left" v-if="mi3_hand_p")
 
         b-menu-list(label="局面操作")
           b-menu-item.is_active_unset(icon="undo"        label="1手戻す"        @click="base.force_sync_turn_previous_modal_handle")
@@ -117,16 +117,4 @@ export default {
 
   .important
     font-size: $size-5
-
-  .hand_blink
-    margin-left: 0.5rem
-    display: inline
-    font-weight: bold
-    color: $primary
-    animation: hand_blink 0.5s ease-in-out 0s infinite alternate
-    @keyframes hand_blink
-      0%
-        opacity: 0.0
-      100%
-        opacity: 1.0
 </style>
