de50299956dd39d648177b76b99687e56868b366
akicho8 <akicho8@gmail.com>
2022-03-07 10:31:18 +0900

    Revert "[feat] サービス利用規約更新"
    This reverts commit 5932fe00de444e84faf983500715c9bf5ada6b04.

--- nuxt_side/pages/about/terms.vue
+++ nuxt_side/pages/about/terms.vue
@@ -7,12 +7,8 @@
 
   MainSection
     .container
-      .content
-        ul.mt-0
-          li 重いからって連打してはいけない
-          li ぴよ将棋アプリから将棋ウォーズ棋譜検索に来れるからといってぴよ将棋と混同してはいけない
-          li 「KENTO」と混同してはいけない
-          li SHOGI-EXTENDの仕様についてHEROZに問い合わせてはいけない
+      p
+        | 重いからって連打しちゃだめ
 </template>
 
 <script>
