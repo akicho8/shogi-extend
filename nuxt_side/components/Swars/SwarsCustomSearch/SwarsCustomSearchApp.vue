d1b6c29c7aa029e15de6a1272135bca0b762bd33
akicho8 <akicho8@gmail.com>
2022-04-29 17:29:04 +0900

    [refactor] SwarsCustomSearchApp

--- nuxt_side/components/Swars/SwarsCustomSearch/SwarsCustomSearchApp.vue
+++ nuxt_side/components/Swars/SwarsCustomSearch/SwarsCustomSearchApp.vue
@@ -1,31 +1,19 @@
 <template lang="pug">
 .SwarsCustomSearchApp
-  //- b-loading(:active="$fetchState.pending")
-
   DebugBox(v-if="development_p")
-    //- p mounted_then_query_present_p: {{mounted_then_query_present_p}}
-
-  //- SwarsCustomSearchSidebar(:base="base")
+    p user_key: {{user_key}}
 
   MainNavbar(wrapper-class="container is-fluid")
     template(slot="brand")
       b-navbar-item(@click="base.back_handle")
         b-icon(icon="chevron-left")
-        //- b-navbar-item(tag="nuxt-link" :to="{}" @click.native="reset_handle")
-      b-navbar-item.has-text-weight-bold カスタム検索
-    //- template(slot="end")
-    //-   NavbarItemLogin
-    //-   NavbarItemProfileLink
-    //-   //- NavbarItemSidebarOpen(@click="sidebar_toggle")
+      b-navbar-item.has-text-weight-bold(@click="title_click_handle") カスタム検索
 
   MainSection
     .container.is-fluid
       .columns.form_block
         .column
           b-field.field_block.new_query_field(label="")
-            //- p.control
-            //-   span.button.is-static.is-fullwidth
-            //-     | {{new_query}}
             b-input(v-model.trim="new_query" disabled expanded size="is-medium")
             p.control
               b-button(@click="search_click_handle" type="is-primary" size="is-medium")
@@ -42,98 +30,13 @@
           SwarsCustomSearchCheckbox(:base="base" label1="勝敗"       :records="xi.judge_infos"  var_name="judge_keys")
           SwarsCustomSearchCheckbox(:base="base" label1="先後"       :records="xi.location_infos"  var_name="location_keys" last_only_if_full)
 
-          //- SimpleRadioButtons.field_block(:base="base" model_name="ChoiceXmodeInfo" var_name="xmode_key")
-          //- SimpleRadioButtons.field_block(:base="base" model_name="ChoiceJudgeInfo" var_name="judge_key")
-          //- SimpleRadioButtons.field_block(:base="base" model_name="ChoiceFinalInfo" var_name="final_key")
-          //- SimpleRadioButtons.field_block(:base="base" model_name="ChoicePresetInfo" var_name="preset_key")
-          //- SimpleRadioButtons.field_block(:base="base" model_name="ChoiceRuleInfo" var_name="rule_key")
-
           SwarsCustomSearchInputTag(:base="base" label="自分タグ" tags_var="my_tag_values" op_var="my_tag_values_op")
           SwarsCustomSearchInputTag(:base="base" label="相手タグ" tags_var="vs_tag_values" op_var="vs_tag_values_op")
-
-          //- SwarsCustomSearchInputTag(:base="base" label1="自分側 AND タグ" label2="自分側ですべて含む" var_name="my_tag_values"    )
-          //- SwarsCustomSearchInputTag(:base="base" label1="自分側 OR タグ"  label2="自分側でどれか含む" var_name="or_tag_values"     )
-          //- SwarsCustomSearchInputTag(:base="base" label1="相手側 AND タグ" label2="相手側ですべて含む" var_name="vs_my_tag_values" )
-          //- SwarsCustomSearchInputTag(:base="base" label1="相手側 OR タグ"  label2="相手側でどれか含む" var_name="vs_or_tag_values"  )
-
-          //- // ネイティブなselectは選択中の項目が選択された状態で開くため使いやすい
-          //- template(v-if="development_p")
-          //-   b-field.field_block(label="相手の棋力" v-if="xi")
-          //-     b-select(v-model="grade_keys" multiple @input="sound_play_click()")
-          //-       option(v-for="e in xi.grade_infos" :value="e.key") {{e.short_name}}
-          //-
-          //-     //- // https://buefy.org/documentation/dropdown/
-          //-     //- b-dropdown.dropdown_menu(position="is-bottom-left" hoverable)
-          //-     //-   b-icon.has-text-grey-light.is_clickable(slot="trigger" icon="dots-vertical")
-          //-     //-     b-dropdown-item(@click="func1_handle") クリックに反応
-          //-     //-     b-dropdown-item(separator)
-          //-     //-       b-dropdown-item(:href="xx_url") Aタグとして飛ぶ
-          //-     //-       b-dropdown-item アイコンつき
-          //-     //-       b-icon(icon="arrow-up-bold" size="is-small")
-          //-     //-         | 最大200件
-          //-
-          //- //- 使いづらい
-          //- template(v-if="development_p")
-          //-   b-field.field_block(label="相手の棋力" v-if="xi")
-          //-     .control
-          //-       b-dropdown(v-model="grade_keys" @active-change="e => e && sound_play_click()")
-          //-         template(#trigger)
-          //-           b-button(label="foo" icon-right="menu-down")
-          //-         template(v-for="e in xi.grade_infos")
-          //-           b-dropdown-item(:value="e" @click="sound_play_click()") {{e}}
 
           SwarsCustomSearchInputNumber(:base="base" label="開戦" xxx_enabled_var="critical_turn_enabled" xxx_value_var="critical_turn" xxx_compare_var="critical_turn_compare")
           SwarsCustomSearchInputNumber(:base="base" label="中盤" xxx_enabled_var="outbreak_turn_enabled" xxx_value_var="outbreak_turn" xxx_compare_var="outbreak_turn_compare")
           SwarsCustomSearchInputNumber(:base="base" label="手数" xxx_enabled_var="turn_max_enabled"      xxx_value_var="turn_max"      xxx_compare_var="turn_max_compare")
           SwarsCustomSearchInputNumber(:base="base" label="力差" xxx_enabled_var="grade_diff_enabled"    xxx_value_var="grade_diff"    xxx_compare_var="grade_diff_compare" :min="-9" :max="9" :message="grade_diff_message")
-
-          //- b-field.field_block(label="開戦")
-          //-   b-switch(v-model="critical_turn_enabled" @input="sound_play_toggle")
-          //-   b-numberinput(controls-position="compact" expanded v-model="critical_turn" :min="0" :max="200" :exponential="true" @input="sound_play_click()" :disabled="!critical_turn_enabled")
-          //-   b-select(v-model="critical_turn_compare" @input="sound_play_click()" :disabled="!critical_turn_enabled")
-          //-     option(v-for="e in CompareInfo.values" :value="e.key") {{e.name}}
-          //-
-          //- b-field.field_block(label="中盤")
-          //-   b-switch(v-model="outbreak_turn_enabled" @input="sound_play_toggle")
-          //-   b-numberinput(controls-position="compact" expanded v-model="outbreak_turn" :min="0" :max="200" :exponential="true" @input="sound_play_click()" :disabled="!outbreak_turn_enabled")
-          //-   b-select(v-model="outbreak_turn_compare" @input="sound_play_click()" :disabled="!outbreak_turn_enabled")
-          //-     option(v-for="e in CompareInfo.values" :value="e.key") {{e.name}}
-          //-
-          //- b-field.field_block(label="手数")
-          //-   b-switch(v-model="turn_max_enabled" @input="sound_play_toggle")
-          //-   b-numberinput(controls-position="compact" expanded v-model="turn_max" :min="0" :max="200" :exponential="true" @input="sound_play_click()" :disabled="!turn_max_enabled")
-          //-   b-select(v-model="turn_max_compare" @input="sound_play_click()" :disabled="!turn_max_enabled")
-          //-     option(v-for="e in CompareInfo.values" :value="e.key") {{e.name}}
-          //-
-          //- b-field.field_block(label="力差")
-          //-   b-switch(v-model="grade_diff_enabled" @input="sound_play_toggle")
-          //-   b-numberinput(controls-position="compact" expanded v-model="grade_diff" :min="-9" :max="9" :exponential="true" @input="sound_play_click()" :disabled="!grade_diff_enabled")
-          //-   b-select(v-model="grade_diff_compare" @input="sound_play_click()" :disabled="!grade_diff_enabled")
-          //-     option(v-for="e in CompareInfo.values" :value="e.key") {{e.name}}
-
-          //- b-field
-          //-   b-autocomplete#query(
-
-          //- b-field
-          //-   b-autocomplete#query(
-          //-     max-height="50vh"
-          //-     size="is-medium"
-          //-     v-model.trim="query"
-          //-     :data="search_input_complement_list"
-          //-     type="search"
-          //-     placeholder="ウォーズIDを入力"
-          //-     open-on-focus
-          //-     clearable
-          //-     expanded
-          //-     @select="search_select_handle"
-          //-     @keydown.native.enter="search_enter_handle"
-          //-     ref="main_search_form"
-          //-     )
-          //-   p.control
-          //-     b-button.search_click_handle(@click="search_click_handle" icon-left="magnify" size="is-medium")
-
-          //- SwarsCustomSearchBoard(:base="base" v-if="layout_info.key === 'is_layout_board'")
-          //- SwarsCustomSearchTable(:base="base" v-if="layout_info.key === 'is_layout_table'")
       SwarsCustomSearchDebugPanels(:base="base" v-if="development_p")
 </template>
 
@@ -145,11 +48,6 @@ import { app_chore       } from "./app_chore.js"
 import { app_search      } from "./app_search.js"
 import { app_storage     } from "./app_storage.js"
 
-// import { ChoiceXmodeInfo } from "./models/choice_xmode_info.js"
-// import { ChoiceJudgeInfo } from "./models/choice_judge_info.js"
-// import { ChoiceFinalInfo } from "./models/choice_final_info.js"
-// import { ChoicePresetInfo } from "./models/choice_preset_info.js"
-// import { ChoiceRuleInfo } from "./models/choice_rule_info.js"
 import { CompareInfo   } from "./models/compare_info.js"
 import { LogicalInfo   } from "./models/logical_info.js"
 
@@ -163,34 +61,18 @@ export default {
     app_chore,
     app_storage,
   ],
-
   props: {
     xi: { type: Object,  required: true, },
   },
-
   data() {
     return {
-      // xi: null,
     }
   },
-
-  // fetchOnServer: false,
-  // fetch() {
-  //   this.ga_click("カスタム検索")
-  //
-  //   let params = {
-  //     ...this.$route.query,
-  //   }
-  //
-  //   params = this.pc_url_params_clean(params)
-  //
-  //   return this.$axios.$get("/api/swars/custom_search_setup.json", {params}).then(xi => {
-  //     this.xi = xi
-  //     this.filtered_tags_rebuild("")  //
-  //   })
-  // },
-
   methods: {
+    title_click_handle() {
+      this.ls_reset()
+      this.pc_data_reset()
+    },
     search_click_handle() {
       this.sound_play_click()
       this.$router.push({name: "swars-search", query: {query: this.new_query}})
