030b56f645ccd7871bbaee282833ee96487b127a
akicho8 <akicho8@gmail.com>
2022-04-30 17:26:53 +0900

    [feat] 「相手で絞る」廃止

--- nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexSidebar.vue
+++ nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexSidebar.vue
@@ -51,20 +51,22 @@ b-sidebar.is-unselectable.SwarsBattleIndexSidebar(fullheight right overlay v-mod
                 :class="[{'has-text-weight-bold': base.per_info.per === e.per}, e.key]"
                 )
 
-          b-menu-item.is_active_unset.filter_set_menu_item(@click="sound_play_click()" :disabled="menu_item_disabled")
-            template(slot="label" slot-scope="props")
-              | {{base.QueryPresetInfo.field_label}}
-              b-icon.is-pulled-right(:icon="props.expanded ? 'menu-up' : 'menu-down'")
-            template(v-for="e in base.QueryPresetInfo.values")
-              SwarsBattleIndexMenuItemSelect(
-                v-if="e.available_p(base)"
-                :base="base"
-                :class="e.key"
-                :label="e.name"
-                :query_preset_info="e"
-                )
+          template(v-if="development_p")
+            b-menu-item.is_active_unset.filter_set_menu_item(@click="sound_play_click()" :disabled="menu_item_disabled")
+              template(slot="label" slot-scope="props")
+                | {{base.QueryPresetInfo.field_label}}
+                b-icon.is-pulled-right(:icon="props.expanded ? 'menu-up' : 'menu-down'")
+              template(v-for="e in base.QueryPresetInfo.values")
+                SwarsBattleIndexMenuItemSelect(
+                  v-if="e.available_p(base)"
+                  :base="base"
+                  :class="e.key"
+                  :label="e.name"
+                  :query_preset_info="e"
+                  )
 
-          b-menu-item.is_active_unset.vs_user_modal_handle(@click="base.vs_user_modal_handle" label="相手で絞る" :disabled="menu_item_disabled")
+          template(v-if="development_p")
+            b-menu-item.is_active_unset.vs_user_modal_handle(@click="base.vs_user_modal_handle" label="相手で絞る" :disabled="menu_item_disabled")
 
         b-menu-list(label="まとめて取得")
           b-menu-item.is_active_unset.swars_direct_download_handle(
