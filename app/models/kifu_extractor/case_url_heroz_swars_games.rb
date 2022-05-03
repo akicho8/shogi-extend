8843efac67c5e7ffe6877c04adfcbef40f3c0eb6
akicho8 <akicho8@gmail.com>
2022-05-03 09:23:15 +0900

    [fix][なんでも棋譜変換] ウォーズTweet文言をそのままコピペで読み取れない

--- app/models/kifu_extractor/case_url_heroz_swars_games.rb
+++ app/models/kifu_extractor/case_url_heroz_swars_games.rb
@@ -1,9 +1,14 @@
 # 将棋ウォーズ本家対局URL
 # rails r 'puts KifuExtractor.extract("https://shogiwars.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900")'
+# rails r 'puts KifuExtractor.extract("将棋ウォーズ棋譜 https://shogiwars.heroz.jp/games/Kato_Hifumi-SiroChannel-20200927_180900")'
+#
+# ・URLが行頭から開始していない場合 url_type? は false になる
+# ・なので行頭から開始していない場合のチェックも必要
+#
 module KifuExtractor
   class CaseUrlHerozSwarsGames < Extractor
     def resolve
-      if url_type?
+      if url_type? || swars_battle_url_match?
         if key = Swars::Battle.battle_key_extract(item.source)
           Swars::Battle.single_battle_import(key: key)
           if battle = Swars::Battle.find_by(key: key)
@@ -11,6 +16,12 @@ module KifuExtractor
           end
         end
       end
+    end
+
+    private
+
+    def swars_battle_url_match?
+      item.source.lines.count <= 2 && item.source.match?(/将棋ウォーズ棋譜|#shogiwars|#棋神解析/)
     end
   end
 end
