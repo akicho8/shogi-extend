2f47d73af528f72ff1a4ee4e6e3bc26bb02e3b47
akicho8 <akicho8@gmail.com>
2022-05-04 11:14:37 +0900

    [feat] FAQ レイアウト・文言 調整

--- nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexHelpBody.vue
+++ nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexHelpBody.vue
@@ -19,8 +19,11 @@
       article
         h2 初めて使ったんだが10件程度しか出てこないぞ
         ul
-          //- li 棋譜検索は直近1件の対局を毎回検討する人を対象としているのでそれでじゅうぶん
           li MENU→古い棋譜を補完
+      article
+        h2 ZIPでくれ
+        ul
+          li MENU→ダウンロード
       article
         h2 棋力別の勝率とかどこで見れる？
         ul
@@ -28,7 +31,7 @@
       article
         h2 プレイヤー情報って最大50件だけ？
         ul
-          li MENU→件数
+          li <i :class="dot_vertical_icon" />→件数
 
       template(v-if="true")
         article
@@ -55,37 +58,57 @@
     article
       h2 歯抜けにならないようにするには？
       ul
-        li 同じルールで連続11回対局する前に棋譜検索する
+        li 同じルールで連続11回対局する前に検索する
     article
       h2 対局を終えた直後に検索しても出てこないときがある
       ul
         li 最長3分待ってから検索する
     article
-      h2 ウォーズIDは知らんがウォーズの対局URLだけわかっている
+      h2 ウォーズの対局URLだけわかってる
       ul
         li そのまま検索覧に放り込む
     article
-      h2 まとめてダウンロードしたい
-      ul
-        li MENU→ダウンロード
-    article
-      h2 前回の続きからダウンロードしたい
+      h2 前回の続きからZIPでくれ
       ul
         li MENU→ダウンロード→前回の続きから
     article
-      h2 指定ウォーズIDの棋譜がすべて欲しい
+      h2 指定IDの棋譜がすべて欲しい
       ul
         li MENU→古い棋譜を補完→ZIPファイルの添付
 
     template(v-if="true")
       article
-        h2 一覧でページ送りが面倒
+        h2 SNSにスクショを投稿するとき相手の名前を隠すのが面倒
+        ul
+          li MENU→一覧<i :class="dot_vertical_icon" />→相手の名前
+      article
+        h2 相手より自分の名前を隠したい
+        ul
+          li MENU→一覧<i :class="dot_vertical_icon" />→自分の名前
+
+    template(v-if="true")
+      article
+        h2 棋譜を棋譜用紙にしたい
+        ul
+          li 詳細→MENU→棋譜用紙→PDFに印刷
+      article
+        h2 棋譜を印刷したい
+        ul
+          li 詳細→MENU→棋譜用紙→PDFに印刷→PDFを印刷
+      article
+        h2 会心譜を印刷して飾りたいがプリンタを持っていない
+        ul
+          li ネットプリント→コンビニ
+
+    template(v-if="true")
+      article
+        h2 ページ送りが面倒
         ul
           li MENU→件数
       article
         h2 終局図から目視で棋譜を探すには？
         ul
-          li MENU→局面→終局→件数→200
+          li MENU→終局→件数→200
       article
         h2 毎回棋譜コピーしてファイル保存するのが面倒
         ul
@@ -103,7 +126,7 @@
         ul
           li 詳細→MENU→ダウンロード→KIF
       article
-        h2 KIFデータへの直リンクが欲しい
+        h2 KIFデータへの直リンが欲しい
         ul
           li 詳細→MENU→表示→KIF
       article
@@ -123,31 +146,9 @@
         ul
           li 詳細→MENU→本家
       article
-        h2 棋譜を棋譜用紙にしたい
-        ul
-          li 詳細→MENU→棋譜用紙→PDFに印刷
-      article
-        h2 棋譜を印刷したい
-        ul
-          li 詳細→MENU→棋譜用紙→PDFに印刷→PDFを印刷
-      article
-        h2 会心譜を印刷して飾りたいがプリンタを持っていない
-        ul
-          li ネットプリント→コンビニ
-      article
         h2 対局の検討を元に詰将棋を作りたい
         ul
           li 詳細→MENU→共有将棋盤に転送→MENU→局面編集
-
-    template(v-if="true")
-      article
-        h2 SNSにスクショを投稿するとき相手の名前を隠すのが面倒
-        ul
-          li MENU→一覧<i :class="dot_vertical_icon" />→相手の名前
-      article
-        h2 相手より自分の名前を隠したい
-        ul
-          li MENU→一覧<i :class="dot_vertical_icon" />→自分の名前
 
     template(v-if="true")
       article
@@ -155,7 +156,7 @@
         ul
           li MENU→ウォーズIDを記録する
       article
-        h2 直近の対局をぴよ将棋で検討したいだけなのに毎回将棋ウォーズ棋譜検索に行くのが面倒
+        h2 直近の対局をぴよ将棋で検討したいだけなのに検索するのが面倒
         ul
           li MENU→外部アプリですぐ開く→ぴよ将棋
 
@@ -175,21 +176,26 @@
       article
         h2 横断検索したい
         ul
-          li 一般には公開してないけど検索条件を言ってくれれば可能な範囲で棋譜を用意します
+          li 一般には公開してないけど検索条件を伝えてくれれば可能な範囲で棋譜を用意します
       article
         h2 1手毎の消費時間を視覚的に確認したい
         ul
-          li 詳細
+          li 詳細→下
 
     article
-      h2 どっちが勝った？
+      h2 どっちが勝ち？
       ul
         li 名前の濃い方
 
     article
-      h2 平手の指導対局から学びたい
+      h2 指導対局の平手戦から学びたい
       ul
         li <i :class="home_icon" />→将棋ウォーズ十段の成績→名前タップ→MENU→カスタム検索→平手
+
+    article
+      h2 将棋ウォーズのイベント上位者の棋譜を見たい
+      ul
+        li <i :class="home_icon" />→将棋ウォーズイベント上位の成績
 
     article
       h2 特定IDをストーキングする
@@ -206,7 +212,7 @@
       ul
         li 詳細→MENU→動画作成
     article
-      h2 棋譜を問題にしたい
+      h2 棋譜の検討結果を問題にして自分用の問題集を作りたい
       ul
         li 詳細→MENU→問題作成
 
@@ -218,7 +224,7 @@
     article
       h2 ミニマリスト化
       ul
-        li MENU→一覧<i :class="dot_vertical_icon" />→すべてOFF→いちばん使うものだけON
+        li MENU→一覧<i :class="dot_vertical_icon" />→すべてOFF→使うのだけON
 
     template(v-if="true")
       article
@@ -261,6 +267,8 @@ export default {
 .SwarsBattleIndexHelpBody
   padding: 2rem
   article
+    word-break: break-all
+
     &:not(:first-child)
       margin-top: 1.5rem
     h2
