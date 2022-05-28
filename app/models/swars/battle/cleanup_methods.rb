eccf118920ea5f2893650af7cce1a704e1e2af26
akicho8 <akicho8@gmail.com>
2022-05-28 18:29:22 +0900

    [feat] 棋譜保持期間 30.days → 45.days

--- app/models/swars/battle/cleanup_methods.rb
+++ app/models/swars/battle/cleanup_methods.rb
@@ -6,7 +6,7 @@ module Swars
         # 削除対象
         scope :cleanup_scope, -> (params = {}) {
           params = {
-            expires_in: 30.days,
+            expires_in: 45.days,
             skip_users: (Rails.env.production? || Rails.env.staging?) ? Rails.application.credentials[:battles_destroy_skip_users] : ["devuser1"],
           }.merge(params)
 
