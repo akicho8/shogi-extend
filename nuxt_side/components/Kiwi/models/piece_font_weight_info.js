3dcc7f7bbfd38fb61c61f2c22c2111ca02c485a4
akicho8 <akicho8@gmail.com>
2022-07-04 22:22:05 +0900

    [refactor] cosmetic change

--- nuxt_side/components/Kiwi/models/piece_font_weight_info.js
+++ nuxt_side/components/Kiwi/models/piece_font_weight_info.js
@@ -6,8 +6,8 @@ export class PieceFontWeightInfo extends ApplicationMemoryRecord {
 
   static get define() {
     return [
-      { key: "is_piece_font_weight_auto", name: "おまかせ", type: "is-primary", message: "だいだい細字",                  },
-      { key: "is_piece_font_weight_normal",  name: "細字",     type: "is-primary", message: "あっさり (最後の指し手は太字)", },
+      { key: "is_piece_font_weight_auto",   name: "おまかせ", type: "is-primary", message: "だいだい細字",                  },
+      { key: "is_piece_font_weight_normal", name: "細字",     type: "is-primary", message: "あっさり (最後の指し手は太字)", },
       { key: "is_piece_font_weight_bold",   name: "太字",     type: "is-primary", message: "こってり",                      },
     ]
   }
