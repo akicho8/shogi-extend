a346f6466bd82681be3ec03bfbbca8db02889f32
akicho8 <akicho8@gmail.com>
2022-08-02 11:48:57 +0900

    [feat] orangain/shogi-piece-images 廃止

--- nuxt_side/pages/about/credit.vue
+++ nuxt_side/pages/about/credit.vue
@@ -10,7 +10,6 @@
       ExternalLink.mx-2(beep href="https://creativecommons.org/licenses/by-nc-sa/4.0/") (CC BY-NC-SA 4.0)
     li: ExternalLink(beep href="https://glyphwiki.org/") グリフウィキ
     li: ExternalLink(beep href="https://studio.beatnix.co.jp/kids-it/kids-programming/scratch/scratch-material/hiragana-katakana-minchyo/") コドモとアプリ
-    li: ExternalLink(beep href="https://github.com/orangain/shogi-piece-images") orangain/shogi-piece-images
     li: ExternalLink(beep href="https://twitter.com/Shogi_Zuan") 将棋図案駒 (404)
 
     //- li: ExternalLink(beep href="http://mucho.girly.jp/bona/") 将棋ｱﾌﾟﾘ用ｸﾘｴﾃｨﾌﾞｺﾓﾝｽﾞ画像 (404)
