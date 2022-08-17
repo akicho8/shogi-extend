301cae1747fdc25cbed1a390cfb0100c9e05a4dc
akicho8 <akicho8@gmail.com>
2022-08-17 19:41:28 +0900

    [fix] [test] テストこける

--- spec/system/share_board/本譜_リンクをクリックするとモーダルが出現する_spec.rb
+++ spec/system/share_board/本譜_リンクをクリックするとモーダルが出現する_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe type: :system, share_board_spec: true do
   it do
     visit_app(body: "position startpos")
     find("a", text: "本譜", exact_text: true).click   # 本譜のリンクをクリック
-    assert_has_selector(".ActionLogShowModal") # すると履歴と同じモーダルが出現する
+    assert_selector(".ActionLogShowModal") # すると履歴と同じモーダルが出現する
   end
 end
