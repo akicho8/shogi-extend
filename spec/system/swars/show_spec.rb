b429df008168bc4a10119926d42892627629869a
akicho8 <akicho8@gmail.com>
2022-09-12 20:41:52 +0900

    [test] [swars] 詳細での棋譜表示・DL・コピーとBODの手数

--- spec/system/swars/show_spec.rb
+++ spec/system/swars/show_spec.rb
@@ -20,7 +20,43 @@ RSpec.describe "詳細", type: :system, swars_spec: true do
     end
   end
 
-  describe "画像" do
+  describe "棋譜" do
+    before do
+      visit2 "/swars/battles/#{@key}"
+      hamburger_click
+    end
+    it "コピー" do
+      menu_item_sub_menu_click("棋譜コピー")
+      menu_item_click("CSA")
+      assert_text "コピーしました"
+    end
+    it "表示" do
+      menu_item_sub_menu_click("棋譜表示")
+      switch_to_window_by { menu_item_click("CSA") }
+      assert_text "%TORYO"
+    end
+    it "ダウンロード" do
+      file = Rails.root.join("tmp/#{@key}.csa")
+      FileUtils.rm_f(file)
+      menu_item_sub_menu_click("棋譜ダウンロード")
+      menu_item_click("CSA")
+      assert { file.exist? }
+    end
+  end
+
+  describe "BODだけ特殊で盤の手数の局面になる" do
+    it "works" do
+      visit2 "/swars/battles/#{@key}"
+      find(".ShogiPlayer .button.first").click # 0手目
+      find(".ShogiPlayer .button.next").click  # 1手目
+      hamburger_click
+      menu_item_sub_menu_click("棋譜表示")
+      switch_to_window_by { menu_item_click("BOD #1") }
+      assert_text "手数＝1"
+    end
+  end
+
+  describe "画像(共有将棋盤に飛ばすようにしたので普通の遷移では見れない)" do
     it "画像でも表示する" do
       visit2 "/swars/battles/#{@key}.png"
       assert_current_path "/swars/battles/#{@key}.png", ignore_query: true
@@ -29,38 +65,6 @@ RSpec.describe "詳細", type: :system, swars_spec: true do
     it "画像を反転して最後の局面を表示する" do
       visit2 "/swars/battles/#{@key}.png", turn: -1, viewpoint: "white"
       assert_current_path "/swars/battles/#{@key}.png", ignore_query: true
-    end
-  end
-
-  describe "棋譜" do
-    describe "表示" do
-      it "KIF" do
-        visit2 "/swars/battles/#{@key}"
-        hamburger_click
-        find(".menu_item_show").click
-        switch_to_window_by do
-          find(".menu_item_show .kif_utf8").click
-        end
-        assert_text "手数----指手---------消費時間--"
-      end
-    end
-    describe "コピー" do
-      it "KIF" do
-        visit2 "/swars/battles/#{@key}"
-        hamburger_click
-        find(".menu_item_copy").click
-        find(".menu_item_copy .kif_utf8").click
-        assert_text "コピーしました"
-      end
-    end
-    describe "ダウンロード" do
-      it "KIF" do
-        visit2 "/swars/battles/#{@key}"
-        hamburger_click
-        find(".menu_item_dl").click
-        find(".menu_item_dl .kif_utf8").click
-        assert_text "たぶんダウンロードしました"
-      end
     end
   end
 end
