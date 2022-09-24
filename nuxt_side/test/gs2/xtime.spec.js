9fb83f0e06b04a73b1110895f356b8c6f4715a7a
akicho8 <akicho8@gmail.com>
2022-09-24 18:41:58 +0900

    [fix] 3600秒がなぜか "10時間" になる

---
+++ nuxt_side/test/gs2/xtime.spec.js
@@ -0,0 +1,11 @@
+import { Xtime } from "@/components/models/core/xtime.js"
+import dayjs from "dayjs"
+
+describe("Xtime", () => {
+  test("time_format_human_hms", () => {
+    expect(Xtime.time_format_human_hms(0)).toEqual("0秒")
+    expect(Xtime.time_format_human_hms(90)).toEqual("1分30秒")
+    expect(Xtime.time_format_human_hms(60*59)).toEqual("59分0秒")
+    expect(Xtime.time_format_human_hms(60*60)).toEqual("1時間0分")
+  })
+})
