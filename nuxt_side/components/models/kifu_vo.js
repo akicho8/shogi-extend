82fdc7d7309989ee4579a59195c1d2ccf8d2dbe9
akicho8 <akicho8@gmail.com>
2022-09-25 18:47:37 +0900

    [refactor] kento_full_url を KifuVo に委譲

--- nuxt_side/components/models/kifu_vo.js
+++ nuxt_side/components/models/kifu_vo.js
@@ -1,9 +1,8 @@
 import { SfenParser } from "shogi-player/components/models/sfen_parser.js"
 import { Xcontainer } from "shogi-player/components/models/xcontainer.js"
+import { Gs2 } from "@/components/models/gs2.js"
 
 export class KifuVo {
-  static KYOKUMENPEDIA_URL_PREFIX = "http://kyokumen.jp/positions/"
-
   static create(...args) {
     return new this(...args)
   }
@@ -12,6 +11,41 @@ export class KifuVo {
     this.attributes = attributes
   }
 
+  //////////////////////////////////////////////////////////////////////////////// KENTO
+
+  get kento_full_url() {
+    Gs2.__assert__(this.attributes.sfen, "sfen is blank")
+
+    const info = SfenParser.parse(this.attributes.sfen)
+    const url = new URL("https://www.kento-shogi.com")
+
+    // initpos は position sfen と moves がない初期局面の sfen
+    url.searchParams.set("initpos", info.init_sfen_strip)
+
+    // 視点も対応してくれるかもしれないので入れとく
+    if (this.attributes.viewpoint) {
+      url.searchParams.set("viewpoint", this.attributes.viewpoint)
+    }
+
+    // moves は別のパラメータでスペースを . に置き換えている(KENTOの独自の工夫)
+    const { moves } = info.attributes
+    if (moves) {
+      url.searchParams.set("moves", moves.replace(/\s+/g, "."))
+    }
+
+    // #n が手数
+    if (this.attributes.turn != null) {
+      url.hash = this.attributes.turn
+    }
+
+    return url.toString()
+  }
+
+  //////////////////////////////////////////////////////////////////////////////// 局面ペディア
+
+  static KYOKUMENPEDIA_URL_PREFIX = "http://kyokumen.jp/positions/"
+
+  // ハイブリッド
   // "http://kyokumen.jp/positions/lnsgkgsnl/1r5b1/ppppppppp/9/9/7P1/PPPPPPP1P/1B5R1/LNSGKGSNL%20w%20-"
   get kpedia_url() {
     if (this.attributes.sfen.includes("moves")) {
