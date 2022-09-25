ea320f39eda9ac042804caaf298cdbe844c2e912
akicho8 <akicho8@gmail.com>
2022-09-25 17:00:00 +0900

    [fix] Vue の methods に format を定義するとエラーになる
    原因はわからない
    いったん回避する

--- nuxt_side/components/models/core/xformat.js
+++ nuxt_side/components/models/core/xformat.js
@@ -1,7 +1,8 @@
 import Extsprintf from "extsprintf"
 
 export const Xformat = {
-  format(...args) {
+  // format にすると何かと衝突する
+  sprintf(...args) {
     return Extsprintf.sprintf(...args)
   },
 }
--- nuxt_side/test/gs2/xformat.spec.js
+++ nuxt_side/test/gs2/xformat.spec.js
@@ -1,8 +1,8 @@
 import { Xformat } from "@/components/models/core/xformat.js"
 
 describe("Xformat", () => {
-  test("format", () => {
-    expect(Xformat.format("%02d", 1)).toEqual("01")
-    expect(Xformat.format("%j", {a:1})).toEqual("{ a: 1 }")
+  test("sprintf", () => {
+    expect(Xformat.sprintf("%02d", 1)).toEqual("01")
+    expect(Xformat.sprintf("%j", {a:1})).toEqual("{ a: 1 }")
   })
 })
