9a3757418b0e5f2d1f93520a2334c79437beda1f
akicho8 <akicho8@gmail.com>
2022-09-30 19:47:58 +0900

    [fix] シャウトモード 削除

--- nuxt_side/components/models/sound_preset_info.js
+++ nuxt_side/components/models/sound_preset_info.js
@@ -39,26 +39,6 @@ export class SoundPresetInfo extends ApplicationMemoryRecord {
       { key: "spon",                                 source: require("@/static/sound_effect/oto_logic/Onmtp-Pop01-4.mp3"),                                            volume: 0.50, },
       { key: "pon",                                  source: require("@/assets/UniversalSoundFX/CARTOON/POP_Mouth_mono.wav"),                                         volume: 0.40, },
       { key: "piece_put",                            source: require("@/assets/UniversalSoundFX/SPORTS/Table_Tennis/TABLE_TENNIS_Racket_Ball_Hit_07_Hard_mono.wav"),         volume: 0.20, },
-
-      // for kill_attack
-      { key: "SWORD_Hit_Armor_Hard_RR1_mono",        source: require("@/assets/UniversalSoundFX/WEAPONS/Melee/Swords/SWORD_Hit_Armor_Hard_RR1_mono.wav"),            volume: 0.20, },
-      { key: "SWORD_Hit_Armor_Hard_RR3_mono",        source: require("@/assets/UniversalSoundFX/WEAPONS/Melee/Swords/SWORD_Hit_Armor_Hard_RR3_mono.wav"),            volume: 0.20, },
-      { key: "SWORD_Whoosh_Hit_Armor_Hard_RR1_mono", source: require("@/assets/UniversalSoundFX/WEAPONS/Melee/Swords/SWORD_Whoosh_Hit_Armor_Hard_RR1_mono.wav"),     volume: 0.20, },
-      { key: "SWORD_Whoosh_Hit_Armor_Hard_RR2_mono", source: require("@/assets/UniversalSoundFX/WEAPONS/Melee/Swords/SWORD_Whoosh_Hit_Armor_Hard_RR2_mono.wav"),     volume: 0.20, },
-
-      // for move_or_appear
-      { key: "VOICE_Martial_Art_Shout_06_mono",      source: require("@/assets/UniversalSoundFX/VOICES/Martial_Arts_Male/VOICE_Martial_Art_Shout_06_mono.wav"),       volume: 0.50, },
-      { key: "VOICE_Martial_Art_Shout_07_mono",      source: require("@/assets/UniversalSoundFX/VOICES/Martial_Arts_Male/VOICE_Martial_Art_Shout_07_mono.wav"),       volume: 0.50, },
-      { key: "VOICE_Martial_Art_Shout_08_mono",      source: require("@/assets/UniversalSoundFX/VOICES/Martial_Arts_Male/VOICE_Martial_Art_Shout_08_mono.wav"),       volume: 0.50, },
-      { key: "VOICE_Martial_Art_Shout_12_mono",      source: require("@/assets/UniversalSoundFX/VOICES/Martial_Arts_Male/VOICE_Martial_Art_Shout_12_mono.wav"),       volume: 0.50, },
-
-      // for killed_and_death
-      { key: "SCREAM_Male_B_06_mono",                source: require("@/assets/UniversalSoundFX/VOICES/Screams/SCREAM_Male_B_06_mono.wav"),                volume: 0.60, },
-      { key: "SCREAM_Male_B_07_mono",                source: require("@/assets/UniversalSoundFX/VOICES/Screams/SCREAM_Male_B_07_mono.wav"),                volume: 0.60, },
-      { key: "SCREAM_Male_B_08_mono",                source: require("@/assets/UniversalSoundFX/VOICES/Screams/SCREAM_Male_B_08_mono.wav"),                volume: 0.60, },
-      { key: "GROAN_Male_Hurt_Long_mono",            source: require("@/assets/UniversalSoundFX/VOICES/Grunts_Groans_Hurt/GROAN_Male_Hurt_Long_mono.wav"), volume: 0.60, },
-      { key: "GROAN_Male_Hurt_Long_Pain_mono",       source: require("@/assets/UniversalSoundFX/VOICES/Grunts_Groans_Hurt/GROAN_Male_Hurt_Long_Pain_mono.wav"),       volume: 0.60, },
-      { key: "GRUNT_Male_B_Hurt_Medium_01_mono",     source: require("@/assets/UniversalSoundFX/VOICES/Grunts_Groans_Hurt/GRUNT_Male_B_Hurt_Medium_01_mono.wav"),     volume: 0.60, },
     ]
   }
 }
