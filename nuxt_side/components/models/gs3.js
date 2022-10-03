0c268e61dcf854ba0b729976d153080f4ac55d2a
akicho8 <akicho8@gmail.com>
2022-10-03 13:13:02 +0900

    [refactor] assert(__NUXT__) を入れておく

--- nuxt_side/components/models/gs3.js
+++ nuxt_side/components/models/gs3.js
@@ -1,6 +1,9 @@
+import { Xassertion } from "./core/xassertion.js"
+
 export const Gs3 = {
   as_full_url(path) {
     if (!path.match(/^http/)) {
+      Xassertion.__assert__(__NUXT__, "__NUXT__")
       path = __NUXT__.config.MY_SITE_URL + path
     }
     return path
