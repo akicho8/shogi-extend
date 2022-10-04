1af152d3de412d3ec0802a4f012187a9c75d067f
akicho8 <akicho8@gmail.com>
2022-10-04 18:26:23 +0900

    [fix] xtime.spec.js

--- nuxt_side/test/gs2/xtime.spec.js
+++ nuxt_side/test/gs2/xtime.spec.js
@@ -1,10 +1,10 @@
 import { Xtime } from "@/components/models/core/xtime.js"
 
 describe("Xtime", () => {
-  test("time_format_human_hms", () => {
-    expect(Xtime.time_format_human_hms(0)).toEqual("0秒")
-    expect(Xtime.time_format_human_hms(90)).toEqual("1分30秒")
-    expect(Xtime.time_format_human_hms(60*59)).toEqual("59分0秒")
-    expect(Xtime.time_format_human_hms(60*60)).toEqual("1時間0分")
+  test("xtime_format_human_hms", () => {
+    expect(Xtime.xtime_format_human_hms(0)).toEqual("0秒")
+    expect(Xtime.xtime_format_human_hms(90)).toEqual("1分30秒")
+    expect(Xtime.xtime_format_human_hms(60*59)).toEqual("59分0秒")
+    expect(Xtime.xtime_format_human_hms(60*60)).toEqual("1時間0分")
   })
 })
