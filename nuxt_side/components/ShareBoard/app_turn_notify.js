183f8e236997698e038739da27fbeed6db86ddc6
akicho8 <akicho8@gmail.com>
2022-10-05 11:10:13 +0900

    [feat] [share-board] 牛 → 削除

--- nuxt_side/components/ShareBoard/app_turn_notify.js
+++ nuxt_side/components/ShareBoard/app_turn_notify.js
@@ -9,11 +9,7 @@ export const app_turn_notify = {
       this.tn_counter += 1
       this.debug_alert("(通知効果音)")
       this.sound_play("notification")
-      // this.sound_moo()
       this.vibrate_long()
-    },
-    sound_moo() {
-      this.sound_play_random(["moo1", "moo2", "moo3"])
     },
   },
   computed: {
