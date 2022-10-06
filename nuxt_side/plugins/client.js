a0269781b20ca75ce8053105983648d8279ffce0
akicho8 <akicho8@gmail.com>
2022-10-06 13:37:14 +0900

    [fix] global mixin は一度だけの読み込みを考慮しないといけないらしい

--- nuxt_side/plugins/client.js
+++ nuxt_side/plugins/client.js
@@ -1,3 +1,15 @@
+// グローバルミックスイン
+// https://nuxtjs.org/ja/docs/directory-structure/plugins/#%E3%82%B0%E3%83%AD%E3%83%BC%E3%83%90%E3%83%AB%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9%E3%82%A4%E3%83%B3
+// グローバルミックスインは Nuxt プラグインで簡単に追加できますが、正しく処理しないとトラブルやメモリリークが発生する可能性があります。アプリケーションにグローバルミックスインを追加するときは、常にフラグを使用して複数回登録しないようにする必要があります：
+// import Vue from "vue"
+//
+// // 他の mixin と衝突しないように
+// // フラグの名前は必ずユニークなものにしてください。
+// if (!Vue.__my_mixin__) {
+//   Vue.__my_mixin__ = true
+//   Vue.mixin({ ... }) // ミックスインを設定する
+// }
+
 import Vue from "vue"
 
 import { vue_dialog            } from "./vue_dialog.js"
@@ -10,17 +22,20 @@ import { vue_talk              } from "./vue_talk.js"
 
 import { SoundUtil } from "@/components/models/sound_util.js"
 
-Vue.mixin({
-  mixins: [
-    vue_dialog,
-    vue_clipboard,
-    vue_actioncable,
-    vue_scroll,
-    vue_mounted_next,
-    vue_support_nuxt_side,
-    vue_talk,
-  ],
-  methods: {
-    ...SoundUtil,
-  },
-})
+if (!Vue.__client_js_mixin__) {
+  Vue.__client_js_mixin__ = true
+  Vue.mixin({
+    mixins: [
+      vue_dialog,
+      vue_clipboard,
+      vue_actioncable,
+      vue_scroll,
+      vue_mounted_next,
+      vue_support_nuxt_side,
+      vue_talk,
+    ],
+    methods: {
+      ...SoundUtil,
+    },
+  })
+}
