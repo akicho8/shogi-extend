9a402c0b4c7c7054bb575dcc9307075767238765
akicho8 <akicho8@gmail.com>
2022-10-14 22:22:48 +0900

    [feat] "position+" なら + を " " に置換する小細工廃止

--- app/models/concerns/share_board_methods.rb
+++ app/models/concerns/share_board_methods.rb
@@ -2,18 +2,8 @@ module ShareBoardMethods
   extend ActiveSupport::Concern
 
   class_methods do
-    # 本来スペースは + になり Rails が復元してくれる
-    # しかし Slack ? などでそれをさらにエスケープされると + は %2B になってしまい
-    # Rails が復元したときは %2B → + で終わる
-    # + を %2B にするのは CGI.escape と同じ処理なので CGI.unescape で戻す
-    DOUBLE_ESCAPE_MEASURES = true # 2重エスケープ対策をするか？
-
     def same_body_fetch(params)
       body = params[:body].presence || "position #{Bioshogi::Sfen::STARTPOS_EXPANSION}"
-      if DOUBLE_ESCAPE_MEASURES
-        if body.start_with?("position+")
-          body = CGI.unescape(body) # "+" → " "
-        end
       end
       body = DotSfen.unescape(body)
       sfen_hash = Digest::MD5.hexdigest(body)
