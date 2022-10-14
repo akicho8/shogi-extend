62406b26d2ae7e007847db9c402d8225219f2b57
akicho8 <akicho8@gmail.com>
2022-10-14 22:27:54 +0900

    [feat] [share-board] xbody 引数の考慮

--- app/models/concerns/share_board_methods.rb
+++ app/models/concerns/share_board_methods.rb
@@ -3,9 +3,14 @@ module ShareBoardMethods
 
   class_methods do
     def same_body_fetch(params)
-      body = params[:body].presence || "position #{Bioshogi::Sfen::STARTPOS_EXPANSION}"
+      body = nil
+      if v = params[:xbody].presence
+        body ||= Base64.urlsafe_decode64(v)
+      else
+        body ||= params[:body].presence
+        body ||= "position #{Bioshogi::Sfen::STARTPOS_EXPANSION}"
+        body = DotSfen.unescape(body)
       end
-      body = DotSfen.unescape(body)
       sfen_hash = Digest::MD5.hexdigest(body)
       record = find_by(sfen_hash: sfen_hash, use_key: :share_board)
       record ||= create!(kifu_body: body, use_key: :share_board)
