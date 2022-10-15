ecda0d8db2637ba578220a22eb59fa8cc9f8ea77
akicho8 <akicho8@gmail.com>
2022-10-15 20:20:09 +0900

    [chore] add comment

--- app/models/concerns/share_board_methods.rb
+++ app/models/concerns/share_board_methods.rb
@@ -11,6 +11,8 @@ module ShareBoardMethods
         body ||= "position #{Bioshogi::Sfen::STARTPOS_EXPANSION}"
         body = DotSfen.unescape(body) # sfen 以外が来るのも想定しておく
       end
+      # 例えば "68S" だったりすると SFEN ではないので sfen_hash とは一致しない
+      # body が綺麗な SFEN だったときだけキャッシュの効果がある
       sfen_hash = Digest::MD5.hexdigest(body)
       record = find_by(sfen_hash: sfen_hash, use_key: :share_board)
       record ||= create!(kifu_body: body, use_key: :share_board)
