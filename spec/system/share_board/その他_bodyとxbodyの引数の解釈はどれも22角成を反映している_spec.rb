e65e768fc9294f7334fcb4362688fe7fbb1977c7
akicho8 <akicho8@gmail.com>
2022-10-15 19:51:44 +0900

    [test] どの引数でも正しい盤面になっている

---
+++ spec/system/share_board/その他_bodyとxbodyの引数の解釈はどれも22角成を反映している_spec.rb
@@ -0,0 +1,24 @@
+require "#{__dir__}/shared_methods"
+
+RSpec.describe type: :system, share_board_spec: true do
+  describe "body" do
+    it "sfen" do
+      visit_app(body: SfenGenerator.santeme_kakunari)
+      assert_soldier_exist(:black, "B", true)
+    end
+    it "半角スペースをドットに置換したsfen" do
+      visit_app(body: DotSfen.escape(SfenGenerator.santeme_kakunari))
+      assert_soldier_exist(:black, "B", true)
+    end
+  end
+  describe "xbody" do
+    it "URL安全なbase64形式のSFENを与える" do
+      visit_app(xbody: SafeSfen.encode(SfenGenerator.santeme_kakunari))
+      assert_soldier_exist(:black, "B", true)
+    end
+    it "SFENでなくKI2でもかまわない(これで大きなテキストも安全に渡せる)" do
+      visit_app(xbody: SafeSfen.encode("76歩 34歩 22角成"))
+      assert_soldier_exist(:black, "B", true)
+    end
+  end
+end
