1c2f943fe1c04766b26e7754a9284993fe97892e
akicho8 <akicho8@gmail.com>
2022-10-15 19:55:55 +0900

    [refactor] 本譜_通常時に引数に棋譜があると本譜とする_spec.rb

--- spec/system/share_board/本譜_通常時に引数に棋譜があると本譜とする_spec.rb
+++ spec/system/share_board/本譜_通常時に引数に棋譜があると本譜とする_spec.rb
@@ -1,18 +1,18 @@
 require "#{__dir__}/shared_methods"
 
 RSpec.describe type: :system, share_board_spec: true do
-  it "bodyパラメータ" do
-    visit_app(body: DotSfen.escape("position startpos"))
+  it "body" do
+    visit_app(body: "position startpos")
     assert_honpu_link_exist
   end
 
-  it "xbodyパラメータ" do
-    visit_app(xbody: DotSfen.urlsafe_escape("position startpos"))
-    assert_honpu_link_exist
-  end
-
-  it "合言葉がある場合は登録しない" do
+  it "ただし合言葉がある場合は登録しない" do
     visit_app(body: "position startpos", room_code: "test_room")
     assert_no_selector("a", text: "本譜", exact_text: true)
+  end
+
+  it "xbody" do
+    visit_app(xbody: DotSfen.urlsafe_escape("position startpos"))
+    assert_honpu_link_exist
   end
 end
