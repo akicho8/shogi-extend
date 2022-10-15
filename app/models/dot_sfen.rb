e4a24d9d8a159ffbac21d530382fe4e55c9823db
akicho8 <akicho8@gmail.com>
2022-10-15 09:55:30 +0900

    [feat] DotSfen.urlsafe_* 追加

--- app/models/dot_sfen.rb
+++ app/models/dot_sfen.rb
@@ -20,4 +20,12 @@ module DotSfen
   def space_to_dot_replace(sfen)
     sfen.gsub(/\s+/, ".")
   end
+
+  def urlsafe_escape(sfen)
+    Base64.urlsafe_encode64(sfen)
+  end
+
+  def urlsafe_unescape(sfen)
+    Base64.urlsafe_decode64(sfen)
+  end
 end
