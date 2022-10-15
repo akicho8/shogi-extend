55c6ad522458c04e7de825a6319bd9363b41e9e0
akicho8 <akicho8@gmail.com>
2022-10-15 19:52:21 +0900

    [refactor] dot_sfen.rb

--- app/models/dot_sfen.rb
+++ app/models/dot_sfen.rb
@@ -16,16 +16,4 @@ module DotSfen
     end
     sfen
   end
-
-  def space_to_dot_replace(sfen)
-    sfen.gsub(/\s+/, ".")
-  end
-
-  def urlsafe_escape(sfen)
-    Base64.urlsafe_encode64(sfen)
-  end
-
-  def urlsafe_unescape(sfen)
-    Base64.urlsafe_decode64(sfen)
-  end
 end
