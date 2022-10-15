d4be0684a7ab63bf811c990b7a239006eb9eb422
akicho8 <akicho8@gmail.com>
2022-10-15 19:57:27 +0900

    [refactor] dot_sfen.js

--- nuxt_side/components/models/dot_sfen.js
+++ nuxt_side/components/models/dot_sfen.js
@@ -1,18 +1,14 @@
 export const DotSfen = {
-  // SFENの " " を "." に変更
   escape(sfen) {
-    return this.space_to_dot_replace(sfen)
+    if (sfen.startsWith("position ")) {
+      sfen = sfen.replace(/\s+/g, ".")
+    }
+    return sfen
   },
-
-  space_to_dot_replace(sfen) {
-    return sfen.replace(/\s+/g, ".")
-  },
-
-  // SFENの "." を " " に変更
-  // unescape(sfen) {
-  //   if (sfen.startsWith("position.")) {
-  //     sfen = sfen.replace(/\.+/, " ")
-  //   }
-  //   return sfen
-  // },
+  unescape(sfen) {
+    if (sfen.startsWith("position.")) {
+      sfen = sfen.replace(/\.+/g, " ")
+    }
+    return sfen
+  }
 }
