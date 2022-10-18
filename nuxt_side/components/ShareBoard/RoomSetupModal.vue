7fb511d0b0da0ccab52e3f1e718b8dce970a4613
akicho8 <akicho8@gmail.com>
2022-10-18 19:08:52 +0900

    [refactor] コメントアウトしていた使わないコード削除

--- nuxt_side/components/ShareBoard/RoomSetupModal.vue
+++ nuxt_side/components/ShareBoard/RoomSetupModal.vue
@@ -6,31 +6,6 @@
       b-tag.mx-2.has-text-weight-bold(type="is-success" v-if="base.ac_room && false") 入室中
       b-button(@click="base.room_url_copy_handle" icon-left="link" size="is-small" rounded v-if="present_p(base.ac_room)") 部屋のリンク
   .modal-card-body
-    .content(v-if="false")
-      ul
-        li
-          | 同じ合言葉を設定した人と部屋を共有します
-        li
-          | 合言葉を設定したら同じ合言葉を相手にも伝えよう
-          .has-text-grey.is-size-7
-            | メニューにある「部屋のリンク」を伝えてもよし
-          .is-flex.is-align-items-center(v-if="false")
-            b-button(@click="base.room_url_copy_handle" icon-left="link" outlined :disabled="!base.room_code") 部屋のリンク
-            span.ml-1 を伝えてもよし
-        li(v-if="false")
-          | <b>待った</b>や<b>反則の取り消し</b>は合意の上、当人が下の左矢印で局面を戻して指し直してください
-        li(v-if="false")
-          | 基本的に盤の同期は
-          b.is-size-5.mx-1 指したときだけ！
-          | ← これ重要
-          .has-text-grey.is-size-7
-            | あと「初期配置に戻す」と「1手戻す」のときも同期する
-
-        li(v-if="false")
-          | 指し手のログの行をタップするとそのときの局面にワープします
-          .has-text-grey.is-size-7
-            | 何か問題が起きたとき用で基本的には使わないでよい
-
     template(v-if="true || !base.ac_room")
       template(v-if="room_code_field_locked")
         b-field(key="room_code_field_locked_false")
@@ -39,8 +14,6 @@
       template(v-else)
         b-field(:label="label_wrap('合言葉')" label-position="on-border" key="room_code_field_locked_true")
           b-input.new_room_code(v-model.trim="new_room_code" :disabled="present_p(base.ac_room)" ref="new_room_code")
-
-      //- message="順番設定後に変更すると再度順番設定が必要になります"
       b-field(:label="label_wrap('ハンドルネーム')" label-position="on-border")
         b-input.new_user_name(v-model.trim="new_user_name" :disabled="present_p(base.ac_room)")
 
