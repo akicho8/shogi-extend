a45129b9ddc7e112d627289186a2a3afdc02ea6e
akicho8 <akicho8@gmail.com>
2022-10-30 17:38:42 +0900

    [refactor] app/models/win_range_info.rb は未使用

--- app/models/win_range_info.rb
+++
@@ -1,8 +0,0 @@
-class WinRangeInfo
-  include ApplicationMemoryRecord
-  memory_record [
-    { key: "格上", op: :>,  win_range: 0.0..0.5, rate_max: 0.5, },
-    { key: "同格", op: :==, win_range: 0.3..0.7, rate_max: 0.7, },
-    { key: "格下", op: :<,  win_range: 0.5..1.0, rate_max: 0.9, },
-  ]
-end
