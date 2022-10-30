c7cfd260eaabf3cd22f292127c87e4cc0d7ed523
akicho8 <akicho8@gmail.com>
2022-10-30 17:38:08 +0900

    [refactor] cosmetic change

--- app/models/use_info.rb
+++ app/models/use_info.rb
@@ -1,9 +1,9 @@
 class UseInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :basic,   name: "基本", },
-    { key: :adapter, name: "即席", },
-    { key: :share_board,  name: "リレー", },
+    { key: :basic,       name: "基本",     },
+    { key: :adapter,     name: "即席",     },
+    { key: :share_board, name: "リレー",   },
     { key: :kiwi_lemon,  name: "動画作成", },
   ]
 end
