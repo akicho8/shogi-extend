1758dfdd490e2f64fddd3a255078a706647079c0
akicho8 <akicho8@gmail.com>
2022-11-09 12:49:50 +0900

    [refactor] PiyoShogiUrlCreator のテスト改善

--- nuxt_side/test/piyo_shogi_url_creator.spec.js
+++ nuxt_side/test/piyo_shogi_url_creator.spec.js
@@ -2,8 +2,10 @@ import { PiyoShogiUrlCreator } from "@/components/models/piyo_shogi_url_creator.
 
 describe("PiyoShogiUrlCreator", () => {
   describe("ClassMethods", () => {
-    it("url_for", () => {
+    it("URLをエスケープしていて手数がないのでnumパラメータは入っていない", () => {
       expect(PiyoShogiUrlCreator.url_for({kif_url: "https://example.com/"})).toEqual("piyoshogi://?url=https%3A%2F%2Fexample.com%2F")
+    })
+    it("手数がある場合は異なるパラメータで埋める", () => {
       expect(PiyoShogiUrlCreator.url_for({kif_url: "https://example.com/", turn: 0})).toEqual("piyoshogi://?num=0&url=https%3A%2F%2Fexample.com%2F")
     })
   })
