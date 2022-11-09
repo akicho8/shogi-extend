a5a649a59bc6b2e089ab2f72b76ea8da62a17dbd
akicho8 <akicho8@gmail.com>
2022-11-09 20:26:11 +0900

    [test] [refactor] ApplicationRecord にある DB と関係ないものを StringUtil に移動

---
+++ app/models/string_util.rb
@@ -0,0 +1,52 @@
+module StringUtil
+  extend self
+
+  DELEGATE_METHODS = [
+    :hankaku_format,
+    :strip_tags,
+    :script_tag_escape,
+    :double_blank_lines_to_one_line,
+    :secure_random_urlsafe_base64_token,
+  ]
+
+  # 半角化
+  def hankaku_format(s)
+    s = s.gsub(/\p{Blank}+/, " ")
+    s = s.tr("０-９Ａ-Ｚａ-ｚ", "0-9A-Za-z")
+    s = s.tr("＃", "#")       # 問題番号を表わすのに「＃１」と書く人が実際にいたため
+    s = s.tr("（）", "()")    # 問題番号を表わすのに「（１）」と書く人がいそうなため
+    s = s.lines.collect(&:strip).join("\n")
+    s = s.strip
+  end
+
+  # タグを取る
+  def strip_tags(s)
+    ActionController::Base.helpers.strip_tags(s)
+  end
+
+  # HTMLタグのうち script のみをエスケープする
+  def script_tag_escape(s)
+    Loofah.fragment(s).scrub!(:escape).to_s
+  end
+
+  # 複数の空行を1つにする
+  def double_blank_lines_to_one_line(s)
+    s.gsub(/\R{3,}/, "\n\n")
+  end
+
+  # devise では欲しい長さの4/3倍の文字数が得られるのを予測して3/4倍しているがこれはいまいち
+  # 実際はおおよそ4/3倍なので指定の文字数に足りない場合がある
+  # なのでN文字欲しければN文字以上生成させて先頭からN文字拾えばよい
+  # わかりやすい名前はARの内部のメソッドとかぶりそうなので注意
+  # rails r 'tp 10.times.collect { ApplicationRecord.secure_random_urlsafe_base64_token }'
+  def secure_random_urlsafe_base64_token(length = 11)
+    if false
+      SecureRandom.urlsafe_base64(length).slice(0, length)
+    else
+      v = SecureRandom.urlsafe_base64(length * 2)
+      v = v.gsub(/[-_]/, "")
+      v = v.slice(/[a-z].{#{length-1}}/i)
+      v or raise "must not happen"
+    end
+  end
+end
