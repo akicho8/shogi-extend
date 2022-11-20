6e470b14f43a77028cac0fb0a73d690a2b0a70f0
akicho8 <akicho8@gmail.com>
2022-11-20 19:10:04 +0900

    [refactor] `this.[e]` ではエラーの原因がまったくわからない

--- nuxt_side/components/models/autoexec_methods.js
+++ nuxt_side/components/models/autoexec_methods.js
@@ -4,7 +4,11 @@ export const autoexec_methods = {
       this.$nextTick(() => {
         const s = this.$route.query.autoexec
         if (s) {
-          s.split(/[,\s]+/).forEach(e => this[e]())
+          s.split(/[,\s]+/).forEach(e => {
+            const func = this[e]
+            this.__assert__(this.present_p(func), `存在しないメソッドです : ${e}`)
+            func()
+          })
         }
       })
     },
