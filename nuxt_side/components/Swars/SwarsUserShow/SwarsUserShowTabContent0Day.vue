34ec7aebdd39216e4e96150469f6ac436de1fd97
akicho8 <akicho8@gmail.com>
2022-11-21 11:14:52 +0900

    [fix] 日毎のところだけ win lose が中央に寄っている

--- nuxt_side/components/Swars/SwarsUserShow/SwarsUserShowTabContent0Day.vue
+++ nuxt_side/components/Swars/SwarsUserShow/SwarsUserShowTabContent0Day.vue
@@ -13,7 +13,7 @@
             | {{$time.format_wday_name(row.battled_on)}}
       .columns.is-mobile.is-gapless
         .column.is-paddingless
-          WinLoseCircle(:info="row" size="is-small" :narrowed="true")
+          WinLoseCircle(:info="row" size="is-small" :narrowed="false")
         .column.is-paddingless.tactic_name_with_count_blocks(v-if="present_p(row.all_tags)")
           template(v-for="tag in row.all_tags")
             nuxt-link.tag_wrapper.has-text-weight-bold.is-size-6(:to="search_path({tag: tag.name})")
