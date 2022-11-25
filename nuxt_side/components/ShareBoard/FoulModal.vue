84ab6c719182ef5d02193dc3f5e3ae32d1cd604b
akicho8 <akicho8@gmail.com>
2022-11-25 15:04:50 +0900

    [feat] [share-board] 反則の場合も文言を厳しめにする

--- nuxt_side/components/ShareBoard/FoulModal.vue
+++ nuxt_side/components/ShareBoard/FoulModal.vue
@@ -2,10 +2,11 @@
 .modal-card
   .modal-card-head
     .modal-card-title
-      | {{reason}}で{{TheSb.current_location.name}}の勝ち！
+      | {{reason}}で{{current_location.name}}の勝ち！
   .modal-card-body
-    p 「1手戻す」で指し直せます
-    p(v-if="TheSb.give_up_button_show_p") 反則を受け入れる場合は投了しよう
+    p ルールを守りましょう
+    p 対戦相手が許可してくれた場合は「1手戻す」で指し直してエキシビジョンとしての対局を続行できます
+    p 反則を受け入れる場合は投了しましょう
   .modal-card-foot
     b-button(@click="close_handle" type="is-primary") 閉じる
 </template>
@@ -17,6 +18,14 @@ export default {
     foul_names: { type: Array,  required: true, },
   },
   inject: ["TheSb"],
+  data() {
+    return {
+      current_location: null, // モーダル発動時の先後
+    }
+  },
+  created() {
+    this.current_location = this.TheSb.current_location
+  },
   methods: {
     close_handle() {
       this.$sound.play_click()
