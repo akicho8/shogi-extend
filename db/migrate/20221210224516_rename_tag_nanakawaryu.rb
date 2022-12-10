21f71b024bcab2e9d230b16049a7bb0fdfa4444b
akicho8 <akicho8@gmail.com>
2022-12-10 22:50:54 +0900

    [fix][DB] 菜々河流→菜々河流△４四角 バトル再生成

---
+++ db/migrate/20221210224516_rename_tag_nanakawaryu.rb
@@ -0,0 +1,10 @@
+class RenameTagNanakawaryu < ActiveRecord::Migration[6.0]
+  def change
+    ActsAsTaggableOn::Tag.find_by(name: "菜々河流")&.update!(name: "菜々河流△４四角")
+    ["菜々河流△４四角", "菜々河流向かい飛車"].each do |e|
+      if tag = ActsAsTaggableOn::Tag.find_by(name: e)
+        tag.taggings.where(taggable_type: "Swars::Membership").order(created_at: :desc).in_batches.each_record { |e| e.taggable.battle.remake rescue nil }
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_03_152700) do
+ActiveRecord::Schema.define(version: 2022_12_10_224516) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
