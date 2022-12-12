ee01dc26e8e2fc2bab8e5e932d601c622d15a910
akicho8 <akicho8@gmail.com>
2022-12-12 11:32:19 +0900

    Revert "[feat] 相手の棋風を選択したら相手側で検索する"
    This reverts commit e72cb56623ccd2ad31cd98f1ca2d68faf43d0cd1.

--- nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexTableCellStyleBoth.vue
+++ nuxt_side/components/Swars/SwarsBattleIndex/SwarsBattleIndexTableCellStyleBoth.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 span.SwarsBattleIndexTableCellStyleBoth
-  SwarsBattleIndexTableCellStyleOne(search_column_name="棋風" :membership="memberships[0]")
+  SwarsBattleIndexTableCellStyleOne(search_column_name="棋風" :style_key="memberships[0].style_key")
   span.mx-1.has-text-grey-light vs
-  SwarsBattleIndexTableCellStyleOne(search_column_name="相手の棋風" :membership="memberships[1]")
+  SwarsBattleIndexTableCellStyleOne(search_column_name="相手の棋風" :style_key="memberships[1].style_key")
 </template>
 
 <script>
