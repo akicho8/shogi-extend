0394e85dff65d2f0a9060fd71ba34441b81cd810
akicho8 <akicho8@gmail.com>
2023-01-07 09:30:47 +0900

    [test] CPU対戦のテスト追加

---
+++ spec/controllers/api/cpu_battles_controller_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Api::CpuBattlesController, type: :controller do
+  it "show" do
+    get :show, params: { config_fetch: true }
+    assert { response.status == 200 }
+  end
+  it "start_trigger" do
+    post :create, params: { start_trigger: true }
+    assert { response.status == 204 }
+  end
+  it "i_give_up" do
+    post :create, params: { i_give_up: true }
+    assert { response.status == 200 }
+  end
+  it "candidate_sfen" do
+    post :create, params: { candidate_sfen: "68S", cpu_strategy_key: "オールラウンド", cpu_strategy_random_number: 0 }
+    assert { response.status == 200 }
+  end
+  it "kifu_body" do
+    post :create, params: { kifu_body: "68S", cpu_strategy_key: "オールラウンド", cpu_strategy_random_number: 0 }
+    assert { response.status == 200 }
+  end
+end
