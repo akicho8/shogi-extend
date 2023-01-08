ce2aee54342d77a6b64df02542756e6306984690
akicho8 <akicho8@gmail.com>
2023-01-08 14:28:36 +0900

    [test] [fix] テストと実装の不整合

--- spec/models/swars/distribution_ratio_spec.rb
+++ spec/models/swars/distribution_ratio_spec.rb
@@ -6,14 +6,15 @@ module Swars
       Battle.create!
       json = DistributionRatio.new.as_json
       expected = {
+        :name           => "2手目△３ニ飛戦法",
         :index          => 0,
         :count          => 1,
         :emission_ratio => 0.5,
         :diff_from_avg  => 0.49644128113879005,
         :rarity_key     => :rarity_key_N,
       }
-      assert { json[:items_hash]["2手目△３ニ飛戦法"] == expected }
-      assert { json[:items_hash]["居玉"] == nil }
+      assert { json[:items].find { |e| e[:name] == "2手目△３ニ飛戦法" } == expected }
+      assert { !json[:items].find { |e| e[:name] == "居玉" } }
       assert { json[:meta][:histogram] }
     end
   end
--- spec/models/swars/user_info/rarity_ratio_spec.rb
+++ spec/models/swars/user_info/rarity_ratio_spec.rb
@@ -13,7 +13,7 @@ module Swars
 
       it "to_chart" do
         assert { case1("棒銀").to_chart == [{:name=>"王道", :value=>2}, {:name=>"準王道", :value=>1}, {:name=>"準変態", :value=>0}, {:name=>"変態", :value=>0}] }
-        assert { case1("新米長玉").to_chart == [{:name=>"王道", :value=>0},{:name=>"準王道", :value=>0},{:name=>"準変態", :value=>1},{:name=>"変態", :value=>0}] }
+        assert { case1("新米長玉").to_chart == [{:name=>"王道", :value=>0}, {:name=>"準王道", :value=>0}, {:name=>"準変態", :value=>0}, {:name=>"変態", :value=>1}] }
       end
 
       it "majority?" do
