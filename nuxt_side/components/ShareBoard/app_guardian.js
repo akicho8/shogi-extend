a7996a95cca270a57e71768e854d3e459085e9e3
akicho8 <akicho8@gmail.com>
2023-01-11 09:48:30 +0900

    [refactor] twemoji の url に依存しているが使っていないコードを削除

--- nuxt_side/components/ShareBoard/app_guardian.js
+++ nuxt_side/components/ShareBoard/app_guardian.js
@@ -2,21 +2,12 @@ const PEPPER_DATE_FORMAT = "-" // 守護獣が変化するタイミング。毎�
 
 import _ from "lodash"
 import dayjs from "dayjs"
-import { parse as TwitterEmojiParser } from 'twemoji-parser'
 
 export const app_guardian = {
   methods: {
     // 指定番号の守護獣URL取得
     guardian_url_base_index(index) {
       return this.ary_cycle_at(this.guardian_list, index)
-    },
-
-    // 文字列から守護獣URLに変換
-    guardian_url_from_str(str) {
-      const guardian = this.guardian_from_str(str)
-      const entities = TwitterEmojiParser(guardian)
-      this.__assert__(this.present_p(entities), "this.present_p(entities)")
-      return entities[0].url
     },
 
     // 文字列から守護獣に変換
