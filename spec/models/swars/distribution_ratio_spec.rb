38199e0f23599b8b5e71743ceee0051c869547e4
akicho8 <akicho8@gmail.com>
2023-01-23 10:04:42 +0900

    [fix] [test] 戦法レア度のテストが転けやすい

--- spec/models/swars/distribution_ratio_spec.rb
+++ spec/models/swars/distribution_ratio_spec.rb
@@ -5,15 +5,11 @@ module Swars
     it "as_json" do
       Battle.create!
       json = DistributionRatio.new.as_json
-      expected = {
-        :name           => "2手目△３ニ飛戦法",
-        :index          => 0,
-        :count          => 1,
-        :emission_ratio => 0.5,
-        :diff_from_avg  => 0.49644128113879005,
-        :rarity_key     => :rarity_key_N,
-      }
-      assert { json[:items].find { |e| e[:name] == "2手目△３ニ飛戦法" } == expected }
+      item = json[:items].find { |e| e[:name] == "2手目△３ニ飛戦法" }
+      assert { item }
+      assert { item[:count]  == 1 }
+      assert { item[:emission_ratio]  == 0.5 }
+      assert { item[:rarity_key] == :rarity_key_N }
       assert { !json[:items].find { |e| e[:name] == "居玉" } }
       assert { json[:meta][:histogram] }
     end
