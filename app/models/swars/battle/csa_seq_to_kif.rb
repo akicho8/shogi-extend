9f5782115cc56c6ca0d65f7c79d3823053b47ab3
akicho8 <akicho8@gmail.com>
2023-03-04 17:37:50 +0900

    [refactor] record -> battle

--- app/models/swars/battle/csa_seq_to_kif.rb
+++ app/models/swars/battle/csa_seq_to_kif.rb
@@ -1,8 +1,8 @@
 module Swars
   class Battle
     class CsaSeqToKif
-      def initialize(record)
-        @record = record
+      def initialize(battle)
+        @battle = battle
       end
 
       def to_kif
@@ -16,13 +16,13 @@ module Swars
       private
 
       def render_header
-        @lines << ["N+", @record.memberships.first.name_with_grade].join
-        @lines << ["N-", @record.memberships.second.name_with_grade].join
-        @lines << ["$START_TIME", @record.battled_at.to_s(:csa_ymdhms)] * ":"
+        @lines << ["N+", @battle.memberships.first.name_with_grade].join
+        @lines << ["N-", @battle.memberships.second.name_with_grade].join
+        @lines << ["$START_TIME", @battle.battled_at.to_s(:csa_ymdhms)] * ":"
         @lines << ["$EVENT", "#{event_title}(#{event_types.join(' ')})"] * ":"
-        @lines << ["$TIME_LIMIT", @record.rule_info.csa_time_limit] * ":"
-        if @record.preset_info.handicap
-          @lines << @record.preset_info.to_board.to_csa.strip
+        @lines << ["$TIME_LIMIT", @battle.rule_info.csa_time_limit] * ":"
+        if @battle.preset_info.handicap
+          @lines << @battle.preset_info.to_board.to_csa.strip
           @lines << "-"
         else
           @lines << "+"
@@ -31,8 +31,8 @@ module Swars
 
       def render_body
         # 残り時間の並びから使用時間を求めつつ指し手と一緒に並べていく
-        life = [@record.rule_info.life_time] * @record.memberships.size
-        @record.csa_seq.each.with_index do |(op, t), i|
+        life = [@battle.rule_info.life_time] * @battle.memberships.size
+        @battle.csa_seq.each.with_index do |(op, t), i|
           i = i.modulo(life.size)
           used = life[i] - t
           life[i] = t
@@ -50,7 +50,7 @@ module Swars
       end
 
       def render_footer
-        @lines << "%" + @record.final_info.csa_last_action_key
+        @lines << "%" + @battle.final_info.csa_last_action_key
       end
 
       def event_title
@@ -59,12 +59,12 @@ module Swars
 
       def event_types
         ary = []
-        ary << @record.rule_info.long_name
-        if @record.xmode == Xmode.fetch("指導")
+        ary << @battle.rule_info.long_name
+        if @battle.xmode == Xmode.fetch("指導")
           ary << "指導対局"
         end
-        if @record.preset_info.handicap
-          ary << @record.preset_info.name
+        if @battle.preset_info.handicap
+          ary << @battle.preset_info.name
         end
         ary
       end
