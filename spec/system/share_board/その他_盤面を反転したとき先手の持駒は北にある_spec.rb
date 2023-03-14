4b4720fa07cf5f3e1256d98f7a92b3ba3678391a
akicho8 <akicho8@gmail.com>
2023-03-14 12:58:55 +0900

    [test] 盤面を反転したとき先手の持駒は北にある

---
+++ spec/system/share_board/その他_盤面を反転したとき先手の持駒は北にある_spec.rb
@@ -0,0 +1,8 @@
+require "#{__dir__}/shared_methods"
+
+RSpec.describe type: :system, share_board_spec: true do
+  it "works" do
+    visit_app(body: SfenGenerator.santeme_kakunari, viewpoint: "white") # 反転する
+    Capybara.assert_selector(".is_position_north .piece_B")             # 北側の駒台に角がある
+  end
+end
