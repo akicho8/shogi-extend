9a151f40570af0cc3f5361c30a6a82a98f0267f1
akicho8 <akicho8@gmail.com>
2023-03-21 17:33:05 +0900

    [fix] Rails root 側が動かない問題は .postcssrc.yml にあった

--- nuxt_side/nuxt.config.js
+++ nuxt_side/nuxt.config.js
@@ -440,6 +440,31 @@ const config = {
     //   // https://qiita.com/someone7140/items/5acfc94c63f16115ac99
     //   compact: false,
     // },
+
+    // 注意: なぜか ../.postcssrc.yml を見ているため
+    
+    
+    // ↓これを書くとろくなことにならない
+    // postcss: {
+    //   postcssOptions: {
+    //     // キーとしてプラグイン名を、値として引数を追加します
+    //     // プラグインは前もって npm か yarn で dependencies としてインストールしておきます
+    //     plugins: {
+    //       // 値として false を渡すことによりプラグインを無効化します
+    //       'postcss-url': false,
+    //       // lost: true,
+    //       // 'postcss-nested': false,
+    //       // 'postcss-responsive-type': false,
+    //       // 'postcss-hexrgba': false,
+    //     },
+    //   },
+    //   preset: {
+    //     // postcss-preset-env 設定を変更します
+    //     autoprefixer: {
+    //       grid: false,
+    //     },
+    //   },
+    // },
   },
 
   // https://nuxtjs.org/guide/runtime-config
