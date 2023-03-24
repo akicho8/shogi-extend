fa33044ba411b9e237c8fd6222ede15a93bc4d54
akicho8 <akicho8@gmail.com>
2023-03-24 12:07:48 +0900

    [chore] npm r qs urijs

--- nuxt_side/test/query_string_libs.spec.js
+++ nuxt_side/test/query_string_libs.spec.js
@@ -1,5 +1,3 @@
-const qs = require("qs")
-const URI = require("urijs")
 const StrictUriEncode = require("strict-uri-encode")
 const QueryString = require("query-string")
 
@@ -12,16 +10,8 @@ describe("URL関連", () => {
     it("strict-uri-encode", () => {
       expect(StrictUriEncode(str)).toEqual("%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~")
     })
-    it("qs", () => {
-      expect(qs.stringify({x: str})).toEqual("x=%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~")
-      expect(qs.stringify({x: str}, {format: "RFC3986"})).toEqual("x=%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~")
-      expect(qs.stringify({x: str}, {format: "RFC1738"})).toEqual("x=+%21%22%23%24%25%26%27()%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~")
-    })
     it("query-string", () => {
       expect(QueryString.stringifyUrl({url: "https://httpbin.org/get", query: {x: str}, fragmentIdentifier: 2})).toEqual("https://httpbin.org/get?x=%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~#2")
-    })
-    it("URI.js", () => {
-      expect(URI("https://httpbin.org/get").query({x: str}).toString()).toEqual("https://httpbin.org/get?x=+%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E_%60%7B%7C%7D~")
     })
   })
   describe("読み込み", () => {
