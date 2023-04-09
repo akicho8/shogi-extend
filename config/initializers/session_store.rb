c347a43229f168b32bf55c1ad4b2ec96fac634a3
akicho8 <akicho8@gmail.com>
2023-04-09 11:42:47 +0900

    [refactor] config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,12 @@
 # Be sure to restart your server when you modify this file.
+
+options = {
+  :key          => "_shogi_web_session",
+  :expire_after => 20.years,
+}
+
 if Rails.env.development?
-  Rails.application.config.session_store :cookie_store, key: '_shogi_web_session', expire_after: 20.years, domain: ENV["DOMAIN"] || "localhost"
-else
-  Rails.application.config.session_store :cookie_store, key: '_shogi_web_session', expire_after: 20.years
+  options[:domain] = ENV["DOMAIN"] || "localhost"
 end
+
+Rails.application.config.session_store(:cookie_store, options)
