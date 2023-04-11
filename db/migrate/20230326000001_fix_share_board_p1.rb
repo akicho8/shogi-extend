020b807165d2e216960d9f7cf762f40a77462cbb
akicho8 <akicho8@gmail.com>
2023-04-11 09:05:34 +0900

    [fix] migrate が通らないのを修正する

--- db/migrate/20230326000001_fix_share_board_p1.rb
+++ db/migrate/20230326000001_fix_share_board_p1.rb
@@ -1,9 +1,11 @@
 class FixShareBoardP1 < ActiveRecord::Migration[5.1]
   def up
     ShareBoard::Battle.all.each do |battle|
-      membership = battle.memberships.find_by!(judge: Judge.fetch(:win))
-      battle.win_location = membership.location
-      battle.save!
+      membership = battle.memberships.find_by(judge: Judge.fetch(:win))
+      if membership
+        battle.win_location = membership.location
+        battle.save!
+      end
     end
     tp ShareBoard::Battle
   end
