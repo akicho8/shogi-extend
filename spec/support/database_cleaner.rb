b18e8a83fac549d8150b9bd7a05794def7222817
akicho8 <akicho8@gmail.com>
2023-05-01 16:15:01 +0900

    [fix] [test] system テスト時に DatabaseCleaner の影響でテストが不安定になる問題を修正する

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,25 +1,30 @@
 if defined?(DatabaseCleaner)
   RSpec.configure do |config|
-    config.before(:suite) do
+    config.before(:suite) do |e|
       # ForeignKey.disabled
     end
 
     # デフォルト
-    config.before(:context) do
-      # tp :transaction
+    config.before(:context) do |e|
+      # if e.metadata[:type] == :system
+      # else
       DatabaseCleaner.strategy = :transaction
       DatabaseCleaner.clean_with(:transaction)
+      # end
     end
 
-    # SystemTest の場合はこれが「あと」に呼ばれるので truncation が有効になる
-    config.before(:context, type: :system) do
-      # tp :truncation
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.clean_with(:truncation)
-    end
+    # # SystemTest の場合はこれが「あと」に呼ばれるので truncation が有効になる
+    # config.before(:context, type: :system) do
+    #   DatabaseCleaner.strategy = :truncation
+    #   DatabaseCleaner.clean_with(:truncation)
+    # end
 
-    config.around(:example) do |ex|
-      DatabaseCleaner.cleaning { ex.run }
+    config.around(:example) do |e|
+      if e.metadata[:type] == :system
+        e.run
+      else
+        DatabaseCleaner.cleaning { e.run }
+      end
     end
   end
 end
