71c234a259f213cf5d170743351d9836670d2053
akicho8 <akicho8@gmail.com>
2023-05-04 14:10:46 +0900

    [feat] [share-board] 順番設定時にユーザーのステイタスを絵文字で表わす

--- nuxt_side/components/ShareBoard/order_mod/OrderTeamOne.vue
+++ nuxt_side/components/ShareBoard/order_mod/OrderTeamOne.vue
@@ -11,12 +11,16 @@
     @end="end_handle"
     )
     template(v-for="e in current_items")
-      li(:key="e.unique_key")
-        .text(v-text="e.to_s")
+      li(:key="e.unique_key" :class="name_class(e)" :style="name_style(e)")
+        .text
+          XemojiWrap(:str="name_emoji(e)" v-if="name_emoji(e)")
+          span.name
+            | {{e.to_s}}
 </template>
 
 <script>
 import VueDraggable from "vuedraggable"
+import { MemberStatusInfo } from "./member_status_info.js"
 
 export default {
   props: {
@@ -37,6 +41,8 @@ export default {
     current_items() { this.$emit("update:items", this.current_items) },
   },
   methods: {
+    // dnd
+
     start_handle() {
       this.TheSb.new_v.os_dnd_count += 1
     },
@@ -44,6 +50,42 @@ export default {
       this.TheSb.new_v.os_dnd_count -= 1
       this.TheSb.new_v.order_unit.cache_clear()
     },
+
+    // name
+
+    // 名前。ここに印を入れる案もある
+    name_emoji(item) {
+      return this.MemberStatusInfo.fetch(this.member_status_key_by(item)).emoji
+    },
+    name_class(item) {
+      return this.MemberStatusInfo.fetch(this.member_status_key_by(item)).css_class
+    },
+    // すべてのの名前のぷるぷる効果が同期していると変なのでずらす
+    name_style(item) {
+      const value = this.$gs.irand()
+      return { "animation-delay": `${value}s` }
+    },
+
+    member_status_key_by(item) {
+      // return "status_leave"
+      // return "status_disconnet"
+      // return "status_blur"
+
+      const member_info = this.TheSb.room_user_names_hash[item.to_s]
+      if (member_info == null) {
+        return "status_leave"
+      }
+      if (this.TheSb.member_is_window_blur(member_info)) {
+        return "status_blur"
+      }
+      if (this.TheSb.member_is_disconnect(member_info)) {
+        return "status_disconnet"
+      }
+      return "status_active"
+    },
+  },
+  computed: {
+    MemberStatusInfo() { return MemberStatusInfo }
   },
 }
 </script>
@@ -70,6 +112,26 @@ export default {
       border-radius: 2px
       padding: 0.25rem 0.75rem
       cursor: move
+
+      // overflow: hidden で切ったとき右のパッディングがなくなるため li に直接テキストを入れない
+      .text
+        display: flex
+        align-items: center
+        justify-content: center
+        gap: 0.25rem            // ドクロと名前の隙間
+
+        line-height: 2.5
+        overflow: hidden
+
+      // 絵文字で差があるため背景まで変更するとややこしくなる
+      &.status_good
+        // color: $grey
+        // background-color: $primary-light
+      &.status_bad
+        // color: $grey
+        // background-color: $white-bis
+
+      // ドラッグできることを感づかせるための施策
       box-shadow: 0px 0px 4px hsl(0, 0%, 90%) // 動かせると感じてもらいたいため少し浮いているように見せる
       animation: shake_up 0.05s ease-in-out 0s infinite alternate
       @keyframes shake_up
@@ -77,11 +139,6 @@ export default {
           transform: rotateZ(-2deg)
         100%
           transform: rotateZ(2deg)
-
-      // overflow: hidden で切ったとき右のパッディングがなくなるため li に直接テキストを入れない
-      .text
-        line-height: 2.5
-        overflow: hidden
 
     // 持ち上げられた元の要素
     .sortable-ghost
---
+++ nuxt_side/components/ShareBoard/order_mod/member_status_info.js
@@ -0,0 +1,13 @@
+import { ApplicationMemoryRecord } from "@/components/models/application_memory_record.js"
+
+export class MemberStatusInfo extends ApplicationMemoryRecord {
+  static get define() {
+    return [
+      // 順不同
+      { key: "status_active",    name: "元気",     emoji: "",    css_class: "status_good",   },
+      { key: "status_leave",     name: "完全離脱", emoji: "💀",  css_class: "status_bad", },
+      { key: "status_blur",      name: "よそ見中", emoji: "🙈",  css_class: "status_bad", },
+      { key: "status_disconnet", name: "反応なし", emoji: "😴",  css_class: "status_bad", },
+    ]
+  }
+}
