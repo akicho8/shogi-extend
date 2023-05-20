b42d56e6fdd65155527d69757bde08d6af9b89d8
akicho8 <akicho8@gmail.com>
2023-05-20 21:27:27 +0900

    [fix] [share-board] 続けて指す場合に「次も」と発言しないのを修正する

--- nuxt_side/components/ShareBoard/mod_sfen_share.js
+++ nuxt_side/components/ShareBoard/mod_sfen_share.js
@@ -152,12 +152,12 @@ export const mod_sfen_share = {
     },
     next_turn_call(params) {
       this.next_turn_message = null
-      if (this.$gs.blank_p(params.lmi.illegal_names)) {                  // 反則がなかった場合
+      if (this.$gs.blank_p(params.lmi.illegal_names)) {            // 反則がなかった場合
         if (this.yomiagable_p) {
           this.talk2(this.user_call_name(params.from_user_name), { // 「aliceさん」
             onend: () => this.talk2(params.lmi.yomiage, {          // 「7 6 ふー！」
-              onend: () => {                                      // 「次は〜」
-                if (params.next_user_name) {
+              onend: () => {                                       // 「次は〜」
+                if (params.next_user_name) {                       // 順番設定しているときだけ入っている
                   if (this.next_notify_p) {
                     if (this.user_name === params.next_user_name) {
                       this.tn_notify()
@@ -174,11 +174,33 @@ export const mod_sfen_share = {
     },
 
     next_turn_message_prefix(params) {
-      if (params.from_user_name === params.next_user_name) {
-        return "次も、"
+      this.$gs.assert(this.order_unit, "this.order_unit")
+
+      // console.log(this.tegoto)                                             // => 2
+      // console.log(params.lmi.next_turn_offset)                             // => 1
+      // console.log(params.from_user_name)                                   // => alice
+      // console.log(params.next_user_name)                                   // => bob
+      // console.log(this.turn_to_user_name(params.lmi.next_turn_offset - 2)) // => dave
+      // console.log(this.turn_to_user_name(params.lmi.next_turn_offset - 1)) // => alice
+      // console.log(this.turn_to_user_name(params.lmi.next_turn_offset - 0)) // => bob
+      // console.log(this.order_unit.order_state.state_name)                  // => O2State
+      // console.log(this.order_unit.order_state.teams[0].length)             // => 2
+      // console.log(this.order_unit.order_state.teams[1].length)             // => 2
+
+      if (this.order_unit.order_state.state_name === "O2State") {           // 正確なチーム分けモードなら
+        const turn = params.lmi.next_turn_offset
+        const location = this.turn_to_location(turn)                        // 渡ってきたこれから指す側のチームを求めて
+        if (this.order_unit.order_state.teams[location.code].length >= 2) { // そのチーム内にメンバーが2人以上いる場合は
+          const user_name = this.turn_to_user_name(turn - 2)                // 2手前の名前を求めて
+          if (params.next_user_name === user_name) {                        // 再度同じ人が指す場合には
+            return "次も、"
+          }
+        }
       } else {
-        return "次は、"
+        // O1State の場合は所属チームが曖昧になるため判定ができない
       }
+
+      return "次は、"
     },
 
     // 即時実行させたいエフェクト
---
+++ spec/system/share_board/順番設定_正確なチーム分けかつ自分チームに2人以上いたとき続けて指す場合は「次も」になる_spec.rb
@@ -0,0 +1,40 @@
+require "#{__dir__}/shared_methods"
+
+RSpec.describe type: :system, share_board_spec: true do
+  def case1(user_name)
+    visit_app({
+        :room_code            => :test_room,
+        :user_name            => user_name,
+        :fixed_member_names   => "a,b,c",
+        :fixed_order_names    => "a,b,c",
+        :fixed_order_state    => "to_o2_state", # [[a,c], [b]] のチーム分けになる
+        :handle_name_validate => "false",
+        :tegoto               => 2,
+      })
+  end
+
+  it "works" do
+    a_block do
+      case1("a")
+    end
+    b_block do
+      case1("b")
+    end
+    a_block do
+      piece_move_o("59", "58", "☗5八玉")
+      assert_system_variable(:next_turn_message, "次は、bさんの手番です")
+    end
+    b_block do
+      piece_move_o("51", "52", "☖5二玉")
+      assert_system_variable(:next_turn_message, "次も、aさんの手番です")
+    end
+    a_block do
+      piece_move_o("58", "59", "☗5九玉")
+      assert_system_variable(:next_turn_message, "次は、bさんの手番です")
+    end
+    b_block do
+      piece_move_o("52", "51", "☖5一玉")
+      assert_system_variable(:next_turn_message, "次は、cさんの手番です")
+    end
+  end
+end
