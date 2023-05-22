ccce0c7c6b6b547d18b439679e89c80302ad6f89
akicho8 <akicho8@gmail.com>
2023-05-22 11:51:13 +0900

    [feat] 全体で overscroll-behavior: none すると Mac PC では左フリックが効かなくなるため共有将棋盤内でのみ左右も禁止する

--- nuxt_side/components/ShareBoard/browser_slide_lock.js
+++ nuxt_side/components/ShareBoard/browser_slide_lock.js
@@ -1,10 +1,12 @@
+// 全体で overscroll-behavior: none すると Mac PC では左フリックが効かなくなるため共有将棋盤内でのみ左右も禁止する
+
 export const browser_slide_lock = {
-  // mounted() {
-  //   document.querySelector("html").setAttribute("style", "overscroll-behavior: none")
-  //   document.querySelector("body").setAttribute("style", "overscroll-behavior: none")
-  // },
-  // beforeDestroy() {
-  //   document.querySelector("html").setAttribute("style", "")
-  //   document.querySelector("body").setAttribute("style", "")
-  // },
+  mounted() {
+    document.querySelector("html").setAttribute("style", "overscroll-behavior: none")
+    document.querySelector("body").setAttribute("style", "overscroll-behavior: none")
+  },
+  beforeDestroy() {
+    document.querySelector("html").setAttribute("style", "")
+    document.querySelector("body").setAttribute("style", "")
+  },
 }
