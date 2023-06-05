305a82e0ecda58c1f6df2ff666fe9076813d9956
akicho8 <akicho8@gmail.com>
2023-06-06 08:13:21 +0900

    [refactor] nuxt_side/update は bin/setup-nuxt.sh に移動する

--- bin/setup-nuxt.sh
+++ bin/setup-nuxt.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 cd $(dirname $0)/..
 cd nuxt_side
+
+# 無条件に更新していいもの
+ncu /lodash/ -u
+ncu /howl/ -u
+ncu /buefy/ -u
+ncu /bulma/ -u
+ncu /actioncable/ -u
+ncu /pug/ -u
+ncu /shogi-player/ -u
+ncu /js-memory-record/ -u
+
+# 更新するとやばいもの
+# ncu /chart/ -u
+
 npm i
