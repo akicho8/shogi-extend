1fd01b15adff7b725c212a5901908ea67a1f66c6
akicho8 <akicho8@gmail.com>
2023-07-03 07:52:54 +0900

    [refactor] localStorage に保存できる変数の管理を共有将棋盤のようにする

--- nuxt_side/components/Wkbk/WkbkBookShow/mod_storage.js
+++ nuxt_side/components/Wkbk/WkbkBookShow/mod_storage.js
@@ -1,4 +1,7 @@
-import { ls_support_mixin        } from "@/components/models/ls_support_mixin.js"
+const STORAGE_VERSION = 0
+
+import { params_controller } from "@/components/params_controller.js"
+import { ParamInfo         } from "../models/param_info.js"
 
 import { ArticleTitleDisplayInfo } from "../models/article_title_display_info.js"
 import { MovesMatchInfo          } from "../models/moves_match_info.js"
@@ -7,34 +10,19 @@ import { ViewpointFlipInfo       } from "../models/viewpoint_flip_info.js"
 import { SoldierFlopInfo         } from "../models/soldier_flop_info.js"
 
 export const mod_storage = {
-  mixins: [
-    ls_support_mixin,
-  ],
+  mixins: [params_controller],
   data() {
     return {
-      article_title_display_key: null,
-      moves_match_key:           null,
-      correct_behavior_key:      null,
-      viewpoint_flip_key:        null,
-      soldier_flop_key:          null,
+      ...ParamInfo.null_value_data_hash,
     }
   },
-  beforeMount() {
-    this.ls_setup()
-  },
   computed: {
-    // ls_storage_key_suffix() {
-    //   return "v2"
-    // },
-    ls_default() {
-      return {
-        article_title_display_key: this.ArticleTitleDisplayInfo.values[0].key,
-        moves_match_key:           this.MovesMatchInfo.values[0].key,
-        correct_behavior_key:      this.CorrectBehaviorInfo.values[0].key,
-        viewpoint_flip_key:        this.ViewpointFlipInfo.values[0].key,
-        soldier_flop_key:          this.SoldierFlopInfo.values[0].key,
-      }
+    ParamInfo() { return ParamInfo },
+
+    ls_storage_key() {
+      return `WkbkBookShow/${STORAGE_VERSION}`
     },
+
     ArticleTitleDisplayInfo()    { return ArticleTitleDisplayInfo                                       },
     article_title_display_info() { return ArticleTitleDisplayInfo.fetch(this.article_title_display_key) },
 
