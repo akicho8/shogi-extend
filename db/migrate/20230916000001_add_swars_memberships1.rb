6f7dcaa682e690f122d7d2d9bfa81fe69213d4bd
akicho8 <akicho8@gmail.com>
2023-09-16 14:37:15 +0900

    [chore] [入玉宣言法] [production] 関連するレコードが600件ほどあるため作り直す

--- db/migrate/20230916000001_add_swars_memberships1.rb
+++ db/migrate/20230916000001_add_swars_memberships1.rb
@@ -4,5 +4,8 @@ class AddSwarsMemberships1 < ActiveRecord::Migration[6.0]
       t.integer :ek_score_without_cond, comment: "入玉宣言時の得点(仮)"
       t.integer :ek_score_with_cond, comment: "入玉宣言時の得点(条件考慮)"
     end
+
+    Swars::Membership.reset_column_information
+    Swars::Final.fetch("ENTERINGKING").battles.each(&:rename)
   end
 end
