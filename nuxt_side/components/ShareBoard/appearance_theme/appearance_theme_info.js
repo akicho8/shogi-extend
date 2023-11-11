58cd48c3d68632505629fe4a7e93b4bcb6049822
akicho8 <akicho8@gmail.com>
2023-11-11 14:19:54 +0900

    [refactor] cosmetic change

--- nuxt_side/components/ShareBoard/appearance_theme/appearance_theme_info.js
+++ nuxt_side/components/ShareBoard/appearance_theme/appearance_theme_info.js
@@ -6,8 +6,8 @@ export class AppearanceThemeInfo extends ApplicationMemoryRecord {
 
   static get define() {
     return [
-      { key: "is_appearance_theme_a", name: "ライト", navbar_type: "is-primary", sp_board_variant: "none",   sp_piece_variant: "nureyon",  },
-      { key: "is_appearance_theme_b", name: "ダーク", navbar_type: "is-black",   sp_board_variant: "none",   sp_piece_variant: "nureyon",  },
+      { key: "is_appearance_theme_a", name: "ライト", navbar_type: "is-primary", sp_board_variant: "none",        sp_piece_variant: "nureyon",  },
+      { key: "is_appearance_theme_b", name: "ダーク", navbar_type: "is-black",   sp_board_variant: "none",        sp_piece_variant: "nureyon",  },
       { key: "is_appearance_theme_c", name: "リアル", navbar_type: "is-black",   sp_board_variant: "wood_normal", sp_piece_variant: "portella", },
     ]
   }
