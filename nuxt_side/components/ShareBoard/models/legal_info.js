30c71c16400118b558b60a94682e1fb6f65ae269
akicho8 <akicho8@gmail.com>
2023-11-11 11:13:45 +0900

    [feat] [共有将棋盤] legal=loose オプションの説明を追記する

--- nuxt_side/components/ShareBoard/models/legal_info.js
+++ nuxt_side/components/ShareBoard/models/legal_info.js
@@ -2,7 +2,7 @@ import { ApplicationMemoryRecord } from "@/components/models/application_memory_
 
 export class LegalInfo extends ApplicationMemoryRecord {
   static field_label = "将棋のルール"
-  static field_message = "無視にすると禁じ手や手番の制約がなくなる。つまり自分の手番で相手の駒を操作できる。それを利用して後手のときも先手の駒を動かせばずっと先手側を操作できるので先手だけの囲いの手順の棋譜(SFENに限る)を作ったりするのが簡単になる。しかし反則のため他のアプリでは読めない棋譜になってしまう"
+  static field_message = "無視にすると禁じ手や手番の制約がなくなる。つまり自分の手番で相手の駒を操作できる。それを利用して後手のときも先手の駒を動かせばずっと先手側を操作できるので先手だけの囲いの手順の棋譜(SFENに限る)を作ったりするのが簡単になる。しかし反則のため他のアプリでは読めない棋譜になってしまう。URL に legal_key=loose を指定すると無視にした状態で開始する。"
 
   static get define() {
     return [
