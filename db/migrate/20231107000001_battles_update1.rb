9b3755b65054f9390336db70f2d36f8bd27b4ada
akicho8 <akicho8@gmail.com>
2023-11-12 15:16:52 +0900

    [fix] staging へのデプロイが失敗するのを回避する

--- db/migrate/20231107000001_battles_update1.rb
+++ db/migrate/20231107000001_battles_update1.rb
@@ -1,14 +1,14 @@
 class BattlesUpdate1 < ActiveRecord::Migration[6.0]
   def up
-    [
-      "bsplive",
-      "BOUYATETSU5",
-    ].each do |key|
-      if user = Swars::User.find_by(key: key)
-        user.battles.in_batches.each_record do |e|
-          e.remake rescue nil
-        end
-      end
-    end
+    # [
+    #   "bsplive",
+    #   "BOUYATETSU5",
+    # ].each do |key|
+    #   if user = Swars::User.find_by(key: key)
+    #     user.battles.in_batches.each_record do |e|
+    #       e.remake rescue nil
+    #     end
+    #   end
+    # end
   end
 end
