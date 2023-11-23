aea2c0020bf708cdc4a63180c835147f25625ecd
akicho8 <akicho8@gmail.com>
2023-11-23 13:45:38 +0900

    [feat] [admin] 短縮URLの利用状況を確認できるようにする

---
+++ app/models/backend_script/short_url_access_log_script.rb
@@ -0,0 +1,39 @@
+module BackendScript
+  class ShortUrlAccessLogScript < ::BackendScript::Base
+    self.category = "短縮URL"
+    self.script_name = "短縮URL - アクセス数"
+
+    def script_body
+      # 日別の問題作成回数を求める
+      access_log_hash = {}
+
+      model = ShortUrl::AccessLog
+      s = model.all
+      s = s.where(model.arel_table[:created_at].gteq(time_begin))
+      records = s.select([
+                           "DATE(#{MysqlUtil.column_tokyo_timezone_cast(:created_at)}) AS created_on",
+                           "COUNT(*)                              AS count_all",
+                         ].join(", ")).group("created_on")
+      access_log_hash = records.inject({}) { |a, e| a.merge(e[:created_on] => e) }
+
+      now = Time.current
+      current_length.times.collect do |i|
+        current = now.midnight - i.days
+        date = current.to_date
+        range = "2000-01-01".to_time ... current.tomorrow
+        row = {}
+        row["日付"]       = h.tag.span(current.to_fs(:ymd_j), :class => holiday_sunday_saturday_class(current))
+        row["アクセス数"] = access_log_hash[date]&.count_all
+        row
+      end
+    end
+
+    def time_begin
+      current_length.days.ago.midnight
+    end
+
+    def current_length
+      (params[:length].presence || 30).to_i
+    end
+  end
+end
