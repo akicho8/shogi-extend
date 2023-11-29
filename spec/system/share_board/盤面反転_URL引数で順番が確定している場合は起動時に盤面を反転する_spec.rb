cabcd5d86dd3cbb8ab67c561e9315d42e59cda93
akicho8 <akicho8@gmail.com>
2023-11-29 17:19:02 +0900

    [feat] [共有将棋盤] 盤面反転のタイミングを「対局開始」から「起動時」と「順番同期」のタイミングに変更する

--- spec/system/share_board/盤面反転_URL引数で順番が確定している場合は起動時に盤面を反転する_spec.rb
+++ spec/system/share_board/盤面反転_URL引数で順番が確定している場合は起動時に盤面を反転する_spec.rb
@@ -1,12 +1,12 @@
-# 対局時計のタイミングでは反転しないようにしたのでこのテストはスキップする
-
 require "#{__dir__}/shared_methods"
 
+# 順番設定を適用する引数で起動したため os_setup 内の sp_viewpoint_set_by_self_location だけで視点が反映される
+# order_copy_from_bc 内のテストにはなっていない
 RSpec.describe type: :system, share_board_spec: true do
   def visit_app2(fixed_order_state, b_or_w, user_name)
     visit_app({
         :room_code            => :test_room,
-        :user_name      => user_name,
+        :user_name            => user_name,
         :fixed_member_names   => "a,b",
         :fixed_order_names    => "a,b",
         :fixed_order_state    => fixed_order_state,
@@ -18,9 +18,6 @@ RSpec.describe type: :system, share_board_spec: true do
   def case1(fixed_order_state, b_or_w, a_side_location_key, b_side_location_key)
     a_block { visit_app2(fixed_order_state, b_or_w, "a") }
     b_block { visit_app2(fixed_order_state, b_or_w, "b") }
-    a_block do
-      clock_start # 対局時計PLAY
-    end
     a_block { assert_viewpoint(a_side_location_key) }
     b_block { assert_viewpoint(b_side_location_key) }
   end
@@ -34,8 +31,8 @@ RSpec.describe type: :system, share_board_spec: true do
   # | 2列  | 平手 | ▲ | △ |
   # |      | 落ち | ▲ | △ |
   # |------+------+----+----|
-  xit { case1(:to_o1_state, :black, :black, :white) }
-  xit { case1(:to_o1_state, :white, :white, :black) }
-  xit { case1(:to_o2_state, :black, :black, :white) }
-  xit { case1(:to_o2_state, :white, :black, :white) }
+  it { case1(:to_o1_state, :black, :black, :white) }
+  it { case1(:to_o1_state, :white, :white, :black) }
+  it { case1(:to_o2_state, :black, :black, :white) }
+  it { case1(:to_o2_state, :white, :black, :white) }
 end
---
+++ spec/system/share_board/盤面反転_順番設定ONのタイミングで盤面を反転する_spec.rb
@@ -0,0 +1,19 @@
+require "#{__dir__}/shared_methods"
+
+RSpec.describe type: :system, share_board_spec: true do
+  it "works" do
+    a_block do
+      room_setup("test_room", "alice")          # aliceが部屋を作る
+    end
+    b_block do
+      room_setup("test_room", "bob")            # bobも同じ部屋に入る
+    end
+    a_block do
+      order_set_on                              # 順番設定ON
+      assert_viewpoint(:black)
+    end
+    b_block do
+      assert_viewpoint(:white)                  # 順番設定を反映したタイミングで bob は後手なので盤面を反転する
+    end
+  end
+end
