0f50b558ea0158e3fa633b54c0b170327fc7e6be
akicho8 <akicho8@gmail.com>
2023-12-01 15:06:14 +0900

    [refactor] __KIFU_COPY_CACHE_FOR_IOS__ を simple_cache に置き換える

--- nuxt_side/components/models/simple_cache.js
+++ nuxt_side/components/models/simple_cache.js
@@ -1,9 +1,12 @@
+import { Gs } from "@/components/models/gs.js"
+
 export class SimpleCache {
   constructor() {
     this.cache = {}
   }
 
   fetch(key, block = () => {}) {
+    Gs.assert(key != null, "key != null")
     if (this.exist_p(key)) {
       return this.read(key)
     }
@@ -12,18 +15,23 @@ export class SimpleCache {
   }
 
   write(key, value) {
+    Gs.assert(key != null, "key != null")
     this.cache[key] = value
   }
 
   read(key) {
+    Gs.assert(key != null, "key != null")
     return this.cache[key]
   }
 
   delete(key) {
+    Gs.assert(key != null, "key != null")
     delete this.cache[key]
   }
 
+  // key in this.cache で判定する方法は逆に扱いにくいため値で判定する
   exist_p(key) {
-    return key in this.cache
+    Gs.assert(key != null, "key != null")
+    return this.read(key) != null
   }
 }
