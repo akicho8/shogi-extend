86bb7240706c9e9075c62767b59ff0745434be2c
akicho8 <akicho8@gmail.com>
2023-12-04 16:23:18 +0900

    [fix] [test] AlnumHash が 9 文字になる場合もあってテストがこけるのを修正する

--- spec/models/short_url/alnum_hash_spec.rb
+++ spec/models/short_url/alnum_hash_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 module ShortUrl
   RSpec.describe AlnumHash do
-    it "だいたい10から11文字になる" do
-      a = 100.times.collect { AlnumHash.call(SecureRandom.hex).size }.uniq.sort
-      assert { a == [10, 11] }
-    end
-
     it "works" do
       assert { AlnumHash.call("x") == "crBbpu5siIn" }
+    end
+
+    it "だいたい9〜11文字になる" do
+      a = 100.times.collect { AlnumHash.call(SecureRandom.hex).size }.uniq.sort
+      assert { (9..11).overlap?(Range.new(*a.minmax)) }
     end
   end
 end
