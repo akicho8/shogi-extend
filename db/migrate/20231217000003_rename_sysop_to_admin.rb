09dcdb4269f7d4c0a4e28a948b353f30bec486d3
akicho8 <akicho8@gmail.com>
2023-12-17 23:07:58 +0900

    [fix] rake db:seed できないのを修正する

--- db/migrate/20231217000003_rename_sysop_to_admin.rb
+++ db/migrate/20231217000003_rename_sysop_to_admin.rb
@@ -1,6 +1,6 @@
 class RenameSysopToAdmin < ActiveRecord::Migration[5.1]
   def up
-    if user = User.find_by(key: "admin")
+    if user = User.find_by(key: "sysop")
       user.key = "admin"
       user.save!
     end
