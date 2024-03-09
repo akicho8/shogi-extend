7d8be10a4bd6a26fd58e0cea3770443a8b2036f5
akicho8 <akicho8@gmail.com>
2024-03-09 23:24:53 +0900

    [fix] [faraday] 504 エラー時に例外が発生しないのを修正する

--- app/controllers/swars/exception_catch.rb
+++ app/controllers/swars/exception_catch.rb
@@ -1,3 +1,5 @@
+# ~/src/shogi-extend/nuxt_side/layouts/error.vue
+
 module Swars
   concern :ExceptionCatch do
     included do
@@ -14,9 +16,17 @@ module Swars
         render json: { message: "対応するデータが見つかりません" }, status: 404
       end
 
+      # Faraday の raise_error middleware で出るもの
+      rescue_from "Faraday::ServerError" do |exception|
+        AppLog.info(exception, data: exception.response)
+        render json: { message: "混み合っています<br>しばらくしてからアクセスしてください" }, status: exception.response_status
+      end
+
+      # Faraday の raise_error middleware を使わなかったこときに使っていたもの
+      # 504 のときに例外にならない？
       rescue_from "Faraday::ConnectionFailed" do |exception|
-        AppLog.critical(exception)
-        render json: { message: "混み合っています<br>しばらくしてからアクセスしてください" }, status: 408
+        AppLog.info(exception, data: exception.response)
+        render json: { message: "混み合っています<br>しばらくしてからアクセスしてください" }, status: exception.response_status
       end
 
       rescue_from "ActiveRecord::RecordNotUnique" do |exception| # 中身は「Mysql2::Error: Duplicate entry」
