bb7b32dfbaf4689f774ff02efc9a048d6b5b7135
akicho8 <akicho8@gmail.com>
2024-03-26 00:35:48 +0900

    [fix] [test] 遅いPCでテストが不安定になるため wait を追加する

--- spec/system/swars/index_spec.rb
+++ spec/system/swars/index_spec.rb
@@ -297,7 +297,7 @@ RSpec.describe "将棋ウォーズ棋譜検索", type: :system, swars_spec: true
         find(".post_handle").click                         # 「棋譜取得の予約」
 
         # モーダル発動
-        assert_text("予約しました(0件待ち)")
+        assert_text("予約しました(0件待ち)", wait: 10)
         find(".dialog.modal.is-active button").click       # 「OK」をクリック
 
         find(".crawler_run_handle_handle").click           # 「さばく」
