e96a2428c0c90f30585008de536aa27970255e08
akicho8 <akicho8@gmail.com>
2024-05-04 01:58:25 +0900

    [feat] 棋神判定の情報を更新する

---
+++ db/migrate/20240503000006_fix26.rb
@@ -0,0 +1,16 @@
+class Fix26 < ActiveRecord::Migration[6.0]
+  def up
+    ["十段", "九段", "八段", "七段", "六段"].each do |grade_key|
+      say_with_time "#{grade_key}" do
+        Swars::Grade[grade_key].memberships.where(ai_drop_total: nil).find_each { |e| e.think_columns_update2; e.save! }
+      end
+    end
+    Rails.application.credentials[:expert_import_user_keys].each do |user_key|
+      if user = Swars::User.find_by(key: user_key)
+        say_with_time "#{user_key}" do
+          user.memberships.memberships.where(ai_drop_total: nil).find_each { |e| e.think_columns_update2; e.save! }
+        end
+      end
+    end
+  end
+end
