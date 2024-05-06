6b33a4b11c814dae2dc9210851dec0d26d0f6439
akicho8 <akicho8@gmail.com>
2024-05-06 10:25:02 +0900

    [refactor] デバッグ用のコードをコメントとしていれておく

--- spec/support/swars_system_support.rb
+++ spec/support/swars_system_support.rb
@@ -3,12 +3,14 @@ module SwarsSystemSupport
 
   included do
     before(:context) do
+      # system "rails runner -e development 'tp Swars.info'"
       command = <<~EOT
         Swars::Battle.destroy_all
         Swars.setup
         Swars::Importer::AllRuleImporter.new(user_key: "DevUser1").run
       EOT
       %x(rails runner -e development '#{command}')
+      # system "rails runner -e development 'tp Swars.info'"
     end
   end
 end
