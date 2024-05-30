b5d91226d2c8f2c6c7b35318a011d3d907a4f9f4
akicho8 <akicho8@gmail.com>
2024-05-31 03:14:51 +0900

    [feat] [db] migrate の直近を全件更新する

---
+++ db/migrate/20240503000037_fix37.rb
@@ -0,0 +1,40 @@
+class Fix37 < ActiveRecord::Migration[6.0]
+  def up
+    Swars::Membership.reset_column_information
+    # Swars::Membership.update_all("ai_gear_freq = NULL")
+
+    # Rails.application.credentials[:expert_import_user_keys].each do |user_key|
+    #   say_with_time "#{user_key}" do
+    #     if user = Swars::User.find_by(key: user_key)
+    #       user.memberships.where(ai_gear_freq: nil).find_each { |e| e.ai_columns_set; e.save(validate: false) }
+    #     end
+    #   end
+    # end
+
+    # [
+    #   # "十段",
+    #   "九段",
+    #   "八段",
+    #   "七段",
+    #   "六段",
+    #   "五段",
+    # ].each do |grade_key|
+    #   say_with_time "#{grade_key}" do
+    #     s = Swars::Grade[grade_key].memberships
+    #     # s = Swars::Membership
+    #     s = s.joins(:battle)
+    #     s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
+    #     s = s.where(ai_gear_freq: nil)
+    #     s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
+    #   end
+    # end
+
+    say_with_time "all" do
+      s = Swars::Membership
+      s = s.joins(:battle)
+      s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
+      s = s.where(ai_gear_freq: nil)
+      s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
+    end
+  end
+end
