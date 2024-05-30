0692d8b68262e806aab9e3989eac46126a52a76c
akicho8 <akicho8@gmail.com>
2024-05-31 03:23:36 +0900

    [feat] [db] migrate の直近を全件更新する(その2)

--- db/migrate/20240503000038_fix38.rb
+++ db/migrate/20240503000038_fix38.rb
@@ -1,15 +1,17 @@
-class Fix37 < ActiveRecord::Migration[6.0]
+class Fix38 < ActiveRecord::Migration[6.0]
   def up
     Swars::Membership.reset_column_information
     # Swars::Membership.update_all("ai_gear_freq = NULL")
 
-    # Rails.application.credentials[:expert_import_user_keys].each do |user_key|
-    #   say_with_time "#{user_key}" do
-    #     if user = Swars::User.find_by(key: user_key)
-    #       user.memberships.where(ai_gear_freq: nil).find_each { |e| e.ai_columns_set; e.save(validate: false) }
-    #     end
-    #   end
-    # end
+    Rails.application.credentials[:expert_import_user_keys].each do |user_key|
+      say_with_time "#{user_key}" do
+        if user = Swars::User.find_by(key: user_key)
+          user.battles.limit(nil).order(id: :desc).in_batches.each_record do |e|
+            Retryable.retryable { e.remake }
+          end
+        end
+      end
+    end
 
     # [
     #   # "十段",
