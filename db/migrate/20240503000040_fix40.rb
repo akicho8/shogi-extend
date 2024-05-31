2b2761885f4527b3932a37c2ce0522a868695882
akicho8 <akicho8@gmail.com>
2024-05-31 10:17:35 +0900

    [feat] migrate の実行状況がわかるようにする

--- db/migrate/20240503000040_fix40.rb
+++ db/migrate/20240503000040_fix40.rb
@@ -5,38 +5,42 @@ class Fix40 < ActiveRecord::Migration[6.0]
 
     Rails.application.credentials[:expert_import_user_keys].each do |user_key|
       say_with_time "#{user_key}" do
+        AppLog.important("#{user_key} start")
         if user = Swars::User.find_by(key: user_key)
           user.battles.limit(200).order(id: :desc).in_batches.each_record do |e|
             Retryable.retryable { e.remake }
           end
         end
+        AppLog.important("#{user_key} done")
       end
     end
 
-    # [
-    #   # "十段",
-    #   "九段",
-    #   "八段",
-    #   "七段",
-    #   "六段",
-    #   "五段",
-    # ].each do |grade_key|
-    #   say_with_time "#{grade_key}" do
-    #     s = Swars::Grade[grade_key].memberships
-    #     # s = Swars::Membership
-    #     s = s.joins(:battle)
-    #     s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
-    #     s = s.where(ai_gear_freq: nil)
-    #     s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
-    #   end
-    # end
-
-    say_with_time "all" do
-      s = Swars::Membership
-      s = s.joins(:battle)
-      s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
-      s = s.where(ai_gear_freq: nil)
-      s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
+    [
+      # "十段",
+      "九段",
+      "八段",
+      "七段",
+      "六段",
+      "五段",
+    ].each do |grade_key|
+      say_with_time "#{grade_key}" do
+        AppLog.important(grade_key)
+        s = Swars::Grade[grade_key].memberships
+        # s = Swars::Membership
+        s = s.joins(:battle)
+        s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
+        s = s.where(ai_gear_freq: nil)
+        s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
+      end
     end
+
+    # say_with_time "all" do
+    #   s = Swars::Membership
+    #   s = s.joins(:battle)
+    #   s = s.where(Swars::Battle.arel_table[:battled_at].gteq(Time.parse("2024/05/23")))
+    #   s = s.where(ai_gear_freq: nil)
+    #   s.find_each { |e| e.ai_columns_set; e.save(validate: false) }
+    #   AppLog.important("all done")
+    # end
   end
 end
