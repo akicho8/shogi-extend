8979301c81f77b7e7685fef241b1783876a8b6d9
akicho8 <akicho8@gmail.com>
2024-05-31 19:02:23 +0900

    [feat] migrate をコメントアウトする

--- db/migrate/20240503000043_fix43.rb
+++ db/migrate/20240503000043_fix43.rb
@@ -1,18 +1,18 @@
 class Fix43 < ActiveRecord::Migration[6.0]
   def up
     unless Rails.env.local?
-      Rails.application.credentials[:expert_import_user_keys].each do |user_key|
-        say_with_time "#{user_key}" do
-          AppLog.important("#{user_key} start")
-          if user = Swars::User.find_by(key: user_key)
-            s = user.battles.limit(200).order(id: :desc)
-            s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/05/31 18:00")))
-            s.in_batches.each_record(&:remake)
-          end
-          AppLog.important("#{user_key} done")
-        end
-      end
-      raise "OK"
+      # Rails.application.credentials[:expert_import_user_keys].each do |user_key|
+      #   say_with_time "#{user_key}" do
+      #     AppLog.important("#{user_key} start")
+      #     if user = Swars::User.find_by(key: user_key)
+      #       s = user.battles.limit(200).order(battled_at: :desc)
+      #       s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/05/31 18:00")))
+      #       s.in_batches.each_record(&:remake)
+      #     end
+      #     AppLog.important("#{user_key} done")
+      #   end
+      # end
+      # raise "OK"
 
       # say_with_time "all" do
       #   s = Swars::Membership
