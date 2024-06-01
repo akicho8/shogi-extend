9666e6ec1530bbc9e3adbc9b704d012765d8ed47
akicho8 <akicho8@gmail.com>
2024-06-01 11:02:42 +0900

    [feat] クロール対象を調整する

--- experiment/swars/ai_cop/gear_observer.rb
+++
@@ -1,6 +0,0 @@
-require "./setup"
-Swars::AiCop::GearObserver.freq([])                            # => nil
-Swars::AiCop::GearObserver.freq([1, 2])                        # => 0.0
-Swars::AiCop::GearObserver.freq([1, 2, 2, 1])                  # => 0.0
-Swars::AiCop::GearObserver.freq([1, 2, 1])                     # => 0.3333333333333333
-Swars::AiCop::GearObserver.freq([1, 2, 1, 2, 1])               # => 0.4
--- experiment/swars/ai_cop/two_observer.rb
+++
@@ -1,4 +0,0 @@
-require "../setup"
-Swars::AiCop::TwoObserver.parse([]).count        # => 0
-Swars::AiCop::TwoObserver.parse([2]).count       # => 1
-Swars::AiCop::TwoObserver.parse([2, 1, 2]).count # => 2
