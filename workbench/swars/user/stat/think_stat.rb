1e8944e9a1c69f1f6b4973743de391e13320ab64
akicho8 <akicho8@gmail.com>
2024-06-14 01:45:04 +0900

    [feat] クロール対象を調整する

--- workbench/swars/user/stat/think_stat.rb
+++ workbench/swars/user/stat/think_stat.rb
@@ -1,4 +1,16 @@
 require "./setup"
-user = Swars::User["SugarHuuko"]
-_ { user.stat.think_stat.average } # => "131.72 ms"
-_ { user.stat.think_stat.max     } # => "21.55 ms"
+_ { Swars::User["SugarHuuko"].stat.think_stat.average } # => "231.97 ms"
+_ { Swars::User["SugarHuuko"].stat.think_stat.max     } # => "21.99 ms"
+
+options = {
+  sample_max: 200,
+}
+tp Swars::User::Vip.auto_crawl_user_keys.collect { |key|
+  if user = Swars::User[key]
+    think_stat = user.stat(options).think_stat
+    {
+      :key     => key,
+      :average => think_stat.average,
+    }
+  end
+}.compact.sort_by { |e| e[:average] }
