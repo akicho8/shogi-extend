aa7c4ca105a0ccf79edb25ff73da01fdd5ce4ee7
akicho8 <akicho8@gmail.com>
2024-06-25 23:19:04 +0900

    [fix] migrate が転けるのを直す

--- db/migrate/20240503000047_fix47.rb
+++ db/migrate/20240503000047_fix47.rb
@@ -1,6 +1,6 @@
 class Fix47 < ActiveRecord::Migration[6.0]
   def up
-    ActsAsTaggableOn::Tag.find_by!(name: "相振り").update!(name: "相振り飛車")
-    ActsAsTaggableOn::Tag.find_by!(name: "対振り").update!(name: "対振り飛車")
+    ActsAsTaggableOn::Tag.find_by(name: "相振り")&.update!(name: "相振り飛車")
+    ActsAsTaggableOn::Tag.find_by(name: "対振り")&.update!(name: "対振り飛車")
   end
 end
