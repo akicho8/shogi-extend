162f054fae3b039eeb7bd5b1869c7c7836740fb0
akicho8 <akicho8@gmail.com>
2024-07-21 00:10:18 +0900

    [fix] migrate が通らないのを修正する

--- db/migrate/20240720300054_fix54.rb
+++ db/migrate/20240720300054_fix54.rb
@@ -1,6 +1,6 @@
 class Fix54 < ActiveRecord::Migration[6.0]
   def up
-    remove_column :swars_memberships, :opponent_id
+    remove_column :swars_memberships, :opponent_id rescue nil
 
     change_table :swars_memberships do |t|
       t.belongs_to :opponent, null: true, index: { unique: true }, comment: "相手レコード"
