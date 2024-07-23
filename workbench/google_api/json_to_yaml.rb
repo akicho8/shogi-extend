2bc338aaaf942d16ff1e9daac38c53b14c69cf9d
akicho8 <akicho8@gmail.com>
2024-07-23 16:04:36 +0900

    [feat] [google] 認証情報を SHOGI-EXTEND-new の方に変更する

--- workbench/google_api/json_to_yaml.rb
+++ workbench/google_api/json_to_yaml.rb
@@ -1,8 +1,8 @@
 require "./setup"
 GoogleApi::JsonToYaml.new.call
 
-Rails.application.credentials.dig(:google_api, :development).to_s.size # => 2369
-Rails.application.credentials.dig(:google_api, :test).to_s.size        # => 2369
+Rails.application.credentials.dig(:google_api, :development).to_s.size # => 2339
+Rails.application.credentials.dig(:google_api, :test).to_s.size        # => 2339
 Rails.application.credentials.dig(:google_api, :production).to_s.size  # => 2342
 Rails.application.credentials.dig(:google_api, :staging).to_s.size     # => 2342
 
