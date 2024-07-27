e6755dd831d423520512853ecb72a0326c7b0fe0
akicho8 <akicho8@gmail.com>
2024-07-27 13:48:15 +0900

    [refactor] [quick_script] PrimaryErrorMessageMod にコメントを入れる

--- app/models/quick_script/middleware/primary_error_message_mod.rb
+++ app/models/quick_script/middleware/primary_error_message_mod.rb
@@ -3,8 +3,8 @@ module QuickScript
     # http://localhost:4000/lab/chore/status_code?status_code=404&primary_error_message=hello
     concern :PrimaryErrorMessageMod do
       prepended do
-        class_attribute :primary_error_message, default: nil
-        class_attribute :status_code, default: nil
+        class_attribute :primary_error_message, default: nil # error.vue で優先して表示するようにしている
+        class_attribute :status_code,           default: nil # Rails 側から返すコード
       end
 
       # CSR 用
