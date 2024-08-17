8c42e43334cf06e3f9f2da90e0c199c8a9c02d0b
akicho8 <akicho8@gmail.com>
2024-08-17 14:34:21 +0900

    [fix] BattleDownloadScript#summary

--- workbench/quick_script/swars/battle_download_script.rb
+++ workbench/quick_script/swars/battle_download_script.rb
@@ -1,23 +1,24 @@
 require "./setup"
 Swars::ZipDlLog.destroy_all
 User.admin.swars_zip_dl_logs.destroy_all
-exit
-# QuickScript::Swars::BattleDownloadScript.new({}, {current_user: User.admin}).one_record_download_for_debug
-# exit
-# User.admin = User.create!(email: "pinpon.ikeda+#{SecureRandom.hex}@gmail.com", confirmed_at: Time.current) # =>
-# User.admin.swars_zip_dl_logs.count   # =>
-
-user1 = Swars::User.create!
-swars_user_key = user1.key      # => 
-swars_battle_key = Swars::BattleKeyGenerator.new(seed: Swars::Battle.count).generate.to_s
-battle = Swars::Battle.create!(key: swars_battle_key) do |e|
-  e.memberships.build(user: user1)
-end
-battle                          # => 
-
-instance = QuickScript::Swars::BattleDownloadScript.new({swars_user_key: swars_user_key, scope_key: "recent", max_key: "200"}, {current_user: User.admin, background_mode: true})
-instance.dl_rest_count          # => 
-instance.call                   # => 
-instance.download_content.size  # => 
-instance.download_filename      # => 
-instance.dl_rest_count          # => 
+instance = QuickScript::Swars::BattleDownloadScript.new({query: "bsplive 勝敗:勝ち", scope_key: "recent", max_key: "1"}, {current_user: User.admin, background_mode: true})
+instance.dl_rest_count          # => 1
+instance.call                   # => #<Mail::Message:23520, Multipart: true, Headers: <Date: Sat, 17 Aug 2024 14:33:39 +0900>, <From: SHOGI-EXTEND <shogi.extend@gmail.com>>, <To: shogi.extend@gmail.com>, <Bcc: shogi.extend@gmail.com>, <Message-ID: <66c03633ef6c7_1799216bc291af@ikemac2023.local.mail>>, <Subject: [development] bsplive 八段の将棋ウォーズ棋譜ダウンロード(直近1件)>, <Mime-Version: 1.0>, <Content-Type: multipart/mixed; charset=UTF-8; boundary="--==_mimepart_66c03633ef1fd_1799216bc290b5">, <Content-Transfer-Encoding: 7bit>>
+instance.download_content.size  # => 1728
+instance.download_filename      # => "shogiwars-bsplive-1-20240612192106-kif-UTF-8.zip"
+instance.dl_rest_count          # => 1
+puts instance.summary
+# >> 2024-08-17T05:33:39.880Z pid=96658 tid=20j2 INFO: Sidekiq 7.1.6 connecting to Redis with options {:size=>10, :pool_name=>"internal", :url=>"redis://localhost:6379/4"}
+# >> |----------------------------------+--------------------------------------------------|
+# >> |               ログインユーザー名 | 運営                                             |
+# >> |     検索クエリ(ウォーズIDを含む) | bsplive 勝敗:勝ち                                |
+# >> | クエリから抽出した対象ウォーズID | bsplive                                          |
+# >> |                         対局総数 | 5017                                             |
+# >> |                 指定したスコープ | 直近                                             |
+# >> |                     指定した件数 | 1                                                |
+# >> |               実際に取得した件数 | 1                                                |
+# >> |                       文字コード | UTF-8                                            |
+# >> |                       ファイル名 | shogiwars-bsplive-1-20240612192106-kif-UTF-8.zip |
+# >> |                         処理時間 | 0.28s                                            |
+# >> |     今回を除くをダウンロード総数 | 1                                                |
+# >> |----------------------------------+--------------------------------------------------|
