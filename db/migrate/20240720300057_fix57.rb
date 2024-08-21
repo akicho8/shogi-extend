e93171c31c9b3ffde35d03436c7d2aa571d7d176
akicho8 <akicho8@gmail.com>
2024-08-21 20:17:23 +0900

    [fix] 負けているのに「背水の陣」タグがついているデータ不整合を修正する

---
+++ db/migrate/20240720300057_fix57.rb
@@ -0,0 +1,11 @@
+class Fix57 < ActiveRecord::Migration[6.0]
+  def change
+    AppLog.important Swars::Membership.where(judge: Judge[:lose]).tagged_with("背水の陣").count
+    memberships = Swars::Membership.where(judge: Judge[:lose]).tagged_with("背水の陣")
+    memberships.each do |e|
+      e.note_tag_list = e.note_tag_list - ["背水の陣"]
+      e.save!
+    end
+    AppLog.important Swars::Membership.where(judge: Judge[:lose]).tagged_with("背水の陣").count
+  end
+end
