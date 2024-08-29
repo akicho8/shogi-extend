4f09ae7d7d3a6741ac9c845e8c8e064c07ccc13d
akicho8 <akicho8@gmail.com>
2024-08-30 08:23:36 +0900

    [refactor] Rubyコードのバッチ処理をクラス化する

---
+++ app/models/main_batch.rb
@@ -0,0 +1,40 @@
+class MainBatch
+  def call
+    AppLog.important(subject: "cronを開始します", body: "OK")
+    public_send(Rails.env)
+    AppLog.important(subject: "cronは正常に終了しました", body: "OK")
+  end
+
+  def production
+    # 将棋ウォーズ棋譜検索クロール
+    Swars::Crawler::ExpertCrawler.run
+    Swars::Crawler::ReservationCrawler.run
+
+    Tsl::League.setup(verbose: false)
+    Kiwi::Lemon.background_job_for_cron   # 動画変換。job時間が 0...0 ならcronで実行する
+
+    # 削除シリーズ
+    Kiwi::Lemon.cleanup(execute: true)   # ライブラリ登録していないものを削除する(x-files以下の対応ファイルも削除する)
+    XfilesCleanup.new(execute: true).call # public/system/x-files 以下の古い png と rb を削除する
+    FreeBattle.cleanup(execute: true)
+    MediaBuilder.old_media_file_clean(execute: true, keep: 3)
+
+    # GeneralCleaner シリーズ
+    Swars::Battle.drop_scope1.cleaner(subject: "一般", execute: true).call  # 30分かかる
+    Swars::Battle.drop_scope2.cleaner(subject: "特別", execute: true).call
+    Swars::SearchLog.old_only(100.days).cleaner(subject: "棋譜検索ログ", execute: true).call
+    GoogleApi::ExpirationTracker.old_only(50.days).cleaner(subject: "スプレッドシート", execute: true).call
+    AppLog.old_only(2.weeks).cleaner(subject: "アプリログ", execute: true).call
+
+    # 集計
+    QuickScript::Swars::GradeStatScript.primary_aggregate_run
+    QuickScript::Swars::TacticStatScript.primary_aggregate_run
+
+    # チェック
+    Swars::SystemValidator.new.call
+  end
+
+  def staging
+    Swars::Crawler::ReservationCrawler.run
+  end
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -24,57 +24,9 @@ job_type :runner,  "cd :path && bin/rails runner -e :environment ':task' :output
 #   ].join(";")
 # end
 
-every("5 3 * * *") do
-  runner [
-    %(AppLog.important(subject: "cronを開始します", body: "OK")),
-
-    "Swars::Crawler::ExpertCrawler.run",
-    "Swars::Crawler::ReservationCrawler.run",
-
-    # %(AppLog.important(subject: "CRON", body: "ai_drop_total update")),
-    # 'Swars::Membership.where(Swars::Membership.arel_table[:created_at].gteq(7.days.ago)).where(ai_drop_total: nil).find_in_batches.with_index { |records, i| records.each {|e| e.ai_columns_set; e.save!(validate: false) rescue nil }; print "#{i} "; AppLog.important(subject: "ai_drop_total", body: i) }',
-
-    # %(AppLog.important(subject: "CRON", body: "耀龍四間飛車 update begin")),
-    # %(ActsAsTaggableOn::Tag.find_by(name: "耀龍四間飛車").taggings.where(taggable_type: "Swars::Membership").order(id: :desc).in_batches.each_record{|e|e.taggable.battle.rebuild rescue nil}),
-    # %(AppLog.important(subject: "CRON", body: "耀龍四間飛車 update end")),
-
-    # 全部0件
-    # "Swars::Membership.where(:op_user => nil).find_each{|e|e.save!}",
-    # "Swars::Battle.where(:sfen_hash => nil).find_each{|e|e.save!}",
-    # "FreeBattle.where(:sfen_hash => nil).find_each{|e|e.save!}",
-
-    "Tsl::League.setup(verbose: false)",
-    "QuickScript::Swars::GradeStatScript.primary_aggregate_run",
-    "QuickScript::Swars::TacticStatScript.primary_aggregate_run",
-
-    "Kiwi::Lemon.background_job_for_cron",   # 動画変換。job時間が 0...0 ならcronで実行する
-
-    # 削除シリーズ
-    %(Kiwi::Lemon.cleanup(execute: true)),   # ライブラリ登録していないものを削除する(x-files以下の対応ファイルも削除する)
-    %(XfilesCleanup.new(execute: true).call), # public/system/x-files 以下の古い png と rb を削除する
-    %(FreeBattle.cleanup(execute: true)),
-    %(MediaBuilder.old_media_file_clean(execute: true, keep: 3)),
-
-    # GeneralCleaner シリーズ
-    %(Swars::Battle.drop_scope1.cleaner(subject: "一般", execute: true).call),  # 30分かかる
-    %(Swars::Battle.drop_scope2.cleaner(subject: "特別", execute: true).call),
-    %(Swars::SearchLog.old_only(100.days).cleaner(subject: "棋譜検索ログ", execute: true).call),
-    %(GoogleApi::ExpirationTracker.old_only(50.days).cleaner(subject: "スプレッドシート", execute: true).call),
-    %(AppLog.old_only(2.weeks).cleaner(subject: "アプリログ", execute: true).call),
-
-    # チェック
-    %(Swars::SystemValidator.new.call),
-
-    # 通知
-    %(AppLog.important(subject: "cronは正常に終了しました", body: "OK")),
-  ].join(";")
-end
+every("5 3 * * *") { runner "MainBatch.new.call" }
 
 # every("0 18 * * *")  { runner "Kiwi::Lemon.background_job_for_cron" }
-
-if @environment == "staging"
-  every("5 3 * * *") { runner "Swars::Crawler::ReservationCrawler.run" }
-end
 
 if false
   every("0 * * * *") do
