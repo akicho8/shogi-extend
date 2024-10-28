9fa8d07efd68a9fee6a1649d613030586eb14811
akicho8 <akicho8@gmail.com>
2024-10-29 06:50:28 +0900

    [feat] update migrate_runner

--- app/models/migrate_runner.rb
+++ app/models/migrate_runner.rb
@@ -3,6 +3,7 @@
 # rails r MigrateRunner.new.call
 # rails r "tp Swars::Battle"
 # rails r "tp Swars::Membership"
+# RAILS_ENV=production nohup bundle exec bin/rails r 'MigrateRunner.new.call' &
 
 class MigrateRunner
   def call
@@ -46,71 +47,75 @@ class MigrateRunner
     # p Swars::Membership.where(judge_id: nil).count
   end
 
-  def step1_delete
-    [
-      "大隅囲い",
-      "三手囲い",
-      "高田流左玉",
-      "ロケット",
-      "手得角交換型",
-      "手損角交換型",
-      "角交換型",
-      "角換わり新型",
-      "新丸山ワクチン",
-      "矢倉左美濃急戦",
-    ].each do |name|
-      tag_delete(name)
-    end
-  end
+  # def step1_delete
+  #   [
+  #     "大隅囲い",
+  #     "三手囲い",
+  #     "高田流左玉",
+  #     "ロケット",
+  #     "手得角交換型",
+  #     "手損角交換型",
+  #     "角交換型",
+  #     "角換わり新型",
+  #     "新丸山ワクチン",
+  #     "矢倉左美濃急戦",
+  #   ].each do |name|
+  #     tag_delete(name)
+  #   end
+  # end
 
-  def step2_rename
-    list = {
-      "中田功XP"           => "コーヤン流三間飛車",
-      "平目"               => "ヒラメ戦法",
-      "急戦棒銀"           => "対矢倉急戦棒銀",
-      '▲3七銀戦法'        => "矢倉▲3七銀戦法",
-      "▲５五龍中飛車"     => "▲5五龍中飛車",
-      "▲３七銀戦法"       => "▲3七銀戦法",
-      "都成流△３一金"     => "都成流△3一金",
-      "△３三角型空中戦法" => "△3三角型空中戦法",
-      "△３三桂戦法"       => "△3三桂戦法",
-      "△２三歩戦法"       => "△2三歩戦法",
-      "△４五角戦法"       => "△4五角戦法",
-      "▲５七金戦法"       => "▲5七金戦法",
-      "△３三飛戦法"       => "△3三飛戦法",
-      "菜々河流△４四角"   => "菜々河流△4四角",
-      "天彦流▲６六角"     => "天彦流▲6六角",
-      "▲４六銀右急戦"     => "▲4六銀右急戦",
-      "▲４六銀左急戦"     => "▲4六銀左急戦",
-      "▲４五歩早仕掛け"   => "▲4五歩早仕掛け",
-      "超速▲３七銀"       => "超速▲3七銀戦法",
-      "超速▲3七銀"        => "超速▲3七銀戦法",
-      "初手７八銀戦法"     => "初手▲7八銀戦法",
-      "初手7八銀戦法"      => "初手▲7八銀戦法",
-      "初手３六歩戦法"     => "初手▲3六歩戦法",
-      "初手3六歩戦法"      => "初手▲3六歩戦法",
-      "2手目△３ニ飛戦法"  => "2手目△3ニ飛戦法",
-      "2手目△74歩戦法"    => "2手目△7四歩戦法",
-      "4手目△３三角戦法"  => "4手目△3三角戦法",
-      "2手目△62銀"        => "2手目△6二銀戦法",
-      "2手目△6二銀"       => "2手目△6二銀戦法",
-    }
-    list.each do |from, to|
-      p [from, to, :try]
-      if tag = ActsAsTaggableOn::Tag.find_by(name: from)
-        tag.update!(name: to)
-        p [from, to, :update]
-      end
-    end
-  end
+  # def step2_rename
+  #   list = {
+  #     "中田功XP"           => "コーヤン流三間飛車",
+  #     "平目"               => "ヒラメ戦法",
+  #     "急戦棒銀"           => "対矢倉急戦棒銀",
+  #     '▲3七銀戦法'        => "矢倉▲3七銀戦法",
+  #     "▲５五龍中飛車"     => "▲5五龍中飛車",
+  #     "▲３七銀戦法"       => "▲3七銀戦法",
+  #     "都成流△３一金"     => "都成流△3一金",
+  #     "△３三角型空中戦法" => "△3三角型空中戦法",
+  #     "△３三桂戦法"       => "△3三桂戦法",
+  #     "△２三歩戦法"       => "△2三歩戦法",
+  #     "△４五角戦法"       => "△4五角戦法",
+  #     "▲５七金戦法"       => "▲5七金戦法",
+  #     "△３三飛戦法"       => "△3三飛戦法",
+  #     "菜々河流△４四角"   => "菜々河流△4四角",
+  #     "天彦流▲６六角"     => "天彦流▲6六角",
+  #     "▲４六銀右急戦"     => "▲4六銀右急戦",
+  #     "▲４六銀左急戦"     => "▲4六銀左急戦",
+  #     "▲４五歩早仕掛け"   => "▲4五歩早仕掛け",
+  #     "超速▲３七銀"       => "超速▲3七銀戦法",
+  #     "超速▲3七銀"        => "超速▲3七銀戦法",
+  #     "初手７八銀戦法"     => "初手▲7八銀戦法",
+  #     "初手7八銀戦法"      => "初手▲7八銀戦法",
+  #     "初手３六歩戦法"     => "初手▲3六歩戦法",
+  #     "初手3六歩戦法"      => "初手▲3六歩戦法",
+  #     "2手目△３ニ飛戦法"  => "2手目△3ニ飛戦法",
+  #     "2手目△74歩戦法"    => "2手目△7四歩戦法",
+  #     "4手目△３三角戦法"  => "4手目△3三角戦法",
+  #     "2手目△62銀"        => "2手目△6二銀戦法",
+  #     "2手目△6二銀"       => "2手目△6二銀戦法",
+  #   }
+  #   list.each do |from, to|
+  #     # p [from, to, :try]
+  #     if tag = ActsAsTaggableOn::Tag.find_by(name: from)
+  #       begin
+  #         tag.update!(name: to)
+  #         p [from, to, :update]
+  #       rescue ActiveRecord::RecordInvalid
+  #         tag_delete(tag.name)
+  #       end
+  #     end
+  #   end
+  # end
 
   def step3_rebuild
     s = Swars::Battle.all
-    s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/10/28 12:00")))
     all_count = s.count.ceildiv(1000)
-    s.in_batches(use_ranges: true).each_with_index do |relation, batch|
+    s.in_batches(use_ranges: true).each_with_index do |s, batch|
       p [batch, all_count, batch.fdiv(all_count)]
-      relation.each(&:rebuild)
+      s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/10/28 12:25")))
+      s.each(&:rebuild)
       puts
     end
   end
