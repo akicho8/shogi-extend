bb0d0ab0e26c5d1d0dee7bb88c0e82434a6975f4
akicho8 <akicho8@gmail.com>
2024-10-29 17:29:45 +0900

    [fix] 戦法勝率ランキングで右玉を選択するとエラーになるのを修正する

--- spec/models/quick_script/swars/tactic_stat_script_spec.rb
+++ spec/models/quick_script/swars/tactic_stat_script_spec.rb
@@ -18,6 +18,9 @@ module QuickScript
         assert { case1(scope_key: :note)               }
         assert { case1(scope_key: :all)                }
 
+        assert { case1(scope_key: :right_king) == false }
+        assert { case1(scope_key: :technique)  == false }
+
         assert { case1(order_key: :win_rate) }
         assert { case1(order_key: :popular)  }
       end
