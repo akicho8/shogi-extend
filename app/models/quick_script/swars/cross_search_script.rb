4a1480f037437ef3a7bdf609aae8526909bcf4fe
akicho8 <akicho8@gmail.com>
2024-10-31 00:01:56 +0900

    [feat] 横断検索件数 最大500 → 200

--- app/models/quick_script/swars/cross_search_script.rb
+++ app/models/quick_script/swars/cross_search_script.rb
@@ -15,7 +15,7 @@ module QuickScript
       RANGE_SIZE_THRESHOLD = 30000   # N以上ならバックグラウンド実行する
       RANGE_SIZE_MAX       = 100000  # 対象件数は N 以下
       REQUEST_SIZE_DEFAULT = 50      # 抽出希望件数は N 以下
-      REQUEST_SIZE_MAX     = 500     # 抽出希望件数は N 以下
+      REQUEST_SIZE_MAX     = 200     # 抽出希望件数は N 以下
 
       def form_parts
         super + [
