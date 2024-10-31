b7c38e55440efce00bac63d662fb26e761a5d71f
akicho8 <akicho8@gmail.com>
2024-10-31 14:38:14 +0900

    [feat] メールのヘッダーに Rails.env を埋め込む

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "#{AppConfig[:admin_email_name]} <#{AppConfig[:admin_email]}>"
   default to: AppConfig[:admin_email]
+  default "X-Rails-Environment" => Rails.env
 
   layout "mailer"
 
