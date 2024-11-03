5e160bc644364d77f13dc134bbca17bb6faf0e21
akicho8 <akicho8@gmail.com>
2024-11-03 23:47:42 +0900

    [feat] [migrate_runner] step3_rebuild のみ

--- app/models/migrate_runner.rb
+++ app/models/migrate_runner.rb
@@ -113,18 +113,18 @@ class MigrateRunner
   #   end
   # end
 
-  # def step3_rebuild
-  #   s = Swars::Battle.all
-  #   batch_size = 1000
-  #   all_count = s.count.ceildiv(batch_size)
-  #   s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
-  #     p [batch, all_count, batch.fdiv(all_count)]
-  #     # s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/10/28 12:25")))
-  #     s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
-  #     s.each { |e| e.rebuild(tries: 1) }
-  #     puts
-  #   end
-  # end
+  def step3_rebuild
+    s = Swars::Battle.all
+    batch_size = 1000
+    all_count = s.count.ceildiv(batch_size)
+    s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
+      p [batch, all_count, batch.fdiv(all_count)]
+      # s = s.where(Swars::Battle.arel_table[:updated_at].lt(Time.parse("2024/10/28 12:25")))
+      s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
+      s.each { |e| e.rebuild(tries: 1) }
+      puts
+    end
+  end
 
   # def step4
   #   [
@@ -136,34 +136,37 @@ class MigrateRunner
   #   end
   # end
 
-  # def step5_onirokuryu_dokkan_bisya
+  # def step5_鬼六流ドッカン飛車_更新
   #   if e = ActsAsTaggableOn::Tag.find_by(name: "原始中飛車")
   #     batch_size = 100
   #     all_count = e.taggings.where(taggable_type: "Swars::Membership").count
   #     e.taggings.in_batches(of: batch_size, order: :desc).each_with_index do |relation, batch|
   #       p [batch, all_count, batch.fdiv(all_count)]
-  #       battle_ids = relation.where(taggable_type: "Swars::Membership").collect { |e| e.taggable.battle_id }.uniq
-  #       Swars::Battle.find(battle_ids).each(&:rebuild)
+  #       battle_ids = relation.where(taggable_type: "Swars::Membership").collect { |e| e.taggable&.battle_id }.compact.uniq
+  #       s = Swars::Battle.where(id: battle_ids)
+  #       s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
+  #       s.each(&:rebuild)
+  #       puts
   #     end
   #   end
   # end
 
-  def step6_rebuild_for_auto_crawl_user_keys
-    ::Swars::User::Vip.auto_crawl_user_keys.each do |user_key|
-      p user_key
-      if user = Swars::User[user_key]
-        s = user.battles
-        batch_size = 1000
-        all_count = s.count.ceildiv(batch_size)
-        s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
-          p [batch, all_count, batch.fdiv(all_count)]
-          s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
-          s.each { |e| e.rebuild(tries: 1) }
-          puts
-        end
-      end
-    end
-  end
+  # def step6_rebuild_for_auto_crawl_user_keys
+  #   ::Swars::User::Vip.auto_crawl_user_keys.each do |user_key|
+  #     p user_key
+  #     if user = Swars::User[user_key]
+  #       s = user.battles
+  #       batch_size = 1000
+  #       all_count = s.count.ceildiv(batch_size)
+  #       s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
+  #         p [batch, all_count, batch.fdiv(all_count)]
+  #         s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
+  #         s.each { |e| e.rebuild(tries: 1) }
+  #         puts
+  #       end
+  #     end
+  #   end
+  # end
 
   private
 
