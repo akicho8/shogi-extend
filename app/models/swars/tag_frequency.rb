2dd4e653c0c6d7ad9e9e903289685443ce381368
akicho8 <akicho8@gmail.com>
2024-11-05 21:41:50 +0900

    [feat] 戦法分布数のハッシュは種類ごとに生成するように変更する

--- app/models/swars/tag_frequency.rb
+++ app/models/swars/tag_frequency.rb
@@ -1,9 +1,13 @@
 # http://localhost:3000/api/swars/tag_frequency
 # https://www.shogi-extend.com/api/swars/tag_frequency
+# cap production deploy:upload FILES=app/models/swars/tag_frequency.rb
+# Swars::TagFrequency.new.call
 module Swars
   class TagFrequency
     SAMPLE_MAX = 50000
     EXPIRES_IN = Rails.env.local? ? 0 : 1.days
+    VERSION    = 1
+    BATCH_SIZE = 1000
 
     def initialize(options = {})
       @options = {
@@ -11,7 +15,7 @@ module Swars
     end
 
     def as_json(*)
-      Rails.cache.fetch("tag_frequency", expires_in: EXPIRES_IN) do
+      Rails.cache.fetch("tag_frequency/#{VERSION}", expires_in: EXPIRES_IN) do
         counts_hash
       end
     end
@@ -20,20 +24,24 @@ module Swars
       counts_hash
     end
 
+    def call(...)
+      as_json(...)
+    end
+
     def counts_hash
       @counts_hash ||= yield_self do
-        count = 0
-        hv = Hash.new(0)
-        Swars::Membership.in_batches(order: :desc) do |group|
-          group.all_tag_counts.each do |tag|
-            hv[tag.name] += tag.count
+        Bioshogi::Analysis::TacticInfo.each_with_object({}) do |e, m|
+          hv = Hash.new(0)
+          Swars::Membership.in_batches(of: BATCH_SIZE, order: :desc).each_with_index do |relation, batch|
+            if batch >= sample_max.ceildiv(BATCH_SIZE)
+              break
+            end
+            relation.tag_counts_on("#{e.key}_tags", at_least: 1).each do |tag|
+              hv[tag.name] += tag.count
+            end
           end
-          count += group.count
-          if count >= sample_max
-            break
-          end
+          m[e.key] = hv.sort_by { |_, v| -v }.to_h
         end
-        hv.sort_by { |k, v| -v }.to_h
       end
     end
 
