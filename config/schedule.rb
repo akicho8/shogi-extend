a335a1dfa3ca4e3370ab262c5743378f6654f949
akicho8 <akicho8@gmail.com>
2024-11-08 20:04:55 +0900

    [chore] [cron] MainBatch は2時45分から発動させる

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,7 +24,7 @@ job_type :runner,  "cd :path && bin/rails runner -e :environment ':task' :output
 #   ].join(";")
 # end
 
-every("40 2 * * *") { runner "MainBatch.new.call" }
+every("45 2 * * *") { runner "MainBatch.new.call" }
 
 # every("0 18 * * *")  { runner "Kiwi::Lemon.background_job_for_cron" }
 
