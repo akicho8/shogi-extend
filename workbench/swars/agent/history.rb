40caac331b39f38b72bf4cfe2a866e07038ec6fb
akicho8 <akicho8@gmail.com>
2024-12-25 22:47:10 +0900

    [fix] [暫定] ウォーズの不具合で消費時間が nil になっている場合があるため 0 にする

---
+++ workbench/swars/agent/history.rb
@@ -0,0 +1,3 @@
+require "./setup"
+pp Swars::Agent::History.new(user_key: "testarossa00").fetch
+# >> [全3件][新0件][最後]
--- workbench/swars/importer/all_rule_importer.rb
+++ workbench/swars/importer/all_rule_importer.rb
@@ -1,12 +1,25 @@
 require "./setup"
-Swars::Importer::AllRuleImporter.new(user_key: "bsplive").run
-Swars::User["bsplive"].hard_crawled_at # => Tue, 26 Nov 2024 18:17:42.000000000 JST +09:00
-# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=&page=1&user_id=bsplive
-# >> bsplive P1 10分 [全3件][新0件][最後]
+# Swars::Importer::AllRuleImporter.new(user_key: "bsplive").run
+# Swars::User["bsplive"].hard_crawled_at # => Tue, 26 Nov 2024 18:17:42.000000000 JST +09:00
+
+Swars::Importer::AllRuleImporter.new(user_key: "th_1230", remote_run: true, page_max: 1).run
+Swars::User["th_1230"].hard_crawled_at # => Wed, 27 Nov 2024 07:43:49.000000000 JST +09:00
+# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=&page=1&user_id=th_1230
+# >> th_1230 P1 10分 [全10件][新9件][続く]
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-skullna-20241225_224230
+# >> [fetch][record] https://shogiwars.heroz.jp/games/KKKRRRYYY-th_1230-20241225_205830
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-kamishiro_san-20241225_193834
+# >> [fetch][record] https://shogiwars.heroz.jp/games/hiro8305-th_1230-20241225_123027
+# >> [fetch][record] https://shogiwars.heroz.jp/games/knzGISH-th_1230-20241225_080208
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-pideto-20241225_075318
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-kingdomon-20241224_224838
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-youstate-20241224_222129
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-minarai610MVP17-20241224_214509
+# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=sb&page=1&user_id=th_1230
+# >> th_1230 P1 3分 [全2件][新2件][最後]
 # >> 最後のページと思われるので終わる
-# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=sb&page=1&user_id=bsplive
-# >> bsplive P1 3分 [全3件][新0件][最後]
-# >> 最後のページと思われるので終わる
-# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=s1&page=1&user_id=bsplive
-# >> bsplive P1 10秒 [全3件][新0件][最後]
+# >> [fetch][record] https://shogiwars.heroz.jp/games/th_1230-hitoribotti0711-20241224_131849
+# >> [fetch][record] https://shogiwars.heroz.jp/games/Houndtruth17-th_1230-20241211_075119
+# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=s1&page=1&user_id=th_1230
+# >> th_1230 P1 10秒 [全0件][新0件][最後]
 # >> 最後のページと思われるので終わる
