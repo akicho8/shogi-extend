11e1ec3583eea22ff59aae6b9c132d4f6f3deedc
akicho8 <akicho8@gmail.com>
2024-12-30 22:30:07 +0900

    [fix] [test] 対象者は10分ルールをしないためテストが失敗するのを修正する

--- workbench/swars/agent/history.rb
+++ workbench/swars/agent/history.rb
@@ -1,3 +1,4 @@
 require "./setup"
-pp Swars::Agent::History.new(user_key: "testarossa00").fetch
-# >> [全3件][新0件][最後]
+Swars::Agent::History.new(user_key: "testarossa00").fetch # => [全3件][新3件][最後]
+Swars::Agent::History.new(user_key: "abacus10", remote_run: true, verbose: true).fetch # => [全10件][新9件][続く]
+# >> [fetch][history] https://shogiwars.heroz.jp/games/history?gtype=&page=1&user_id=abacus10
