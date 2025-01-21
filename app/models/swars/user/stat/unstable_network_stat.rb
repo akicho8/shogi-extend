d072654d6f2d20fb01b7a421f502d3d71915cd30
akicho8 <akicho8@gmail.com>
2025-01-21 19:59:21 +0900

    [chore] add comment

--- app/models/swars/user/stat/unstable_network_stat.rb
+++ app/models/swars/user/stat/unstable_network_stat.rb
@@ -22,7 +22,7 @@ module Swars
           s = ids_scope
           s = s.joins(:battle => :final)
           s = s.joins(:judge)
-          s = s.where(Swars::Battle.arel_table[:turn_max].lt(Config.establish_gteq))
+          s = s.where(Swars::Battle.arel_table[:turn_max].lt(Config.establish_gteq)) # 手数が0か1
           s = s.where(Swars::Final.arel_table[:key].eq(:DISCONNECT))
           s = s.where(Judge.arel_table[:key].eq(:lose))
           s.count
