9e2cc5de240ea8099d2c6e093b29cfcf89ef2f33
akicho8 <akicho8@gmail.com>
2025-02-19 20:40:14 +0900

    [test] 実行環境によって並びが変わるため並びを固定する

--- spec/models/swars/user/stat/matrix_stat_spec.rb
+++ spec/models/swars/user/stat/matrix_stat_spec.rb
@@ -10,83 +10,76 @@ module Swars
         end
       end
 
+      def sorted(hash_ary)
+        hash_ary.sort_by { |e| [e[:appear_ratio], e[:tag]] }
+      end
+
+      # 実行環境によって並びが変わるため
+      def case2(method, expected)
+        assert { sorted(@user.stat.matrix_stat.public_send(method)) == sorted(expected) }
+      end
+
       before do
         @user = User.create!
         case1
       end
 
       it "my_attack_items" do
-        assert do
-          @user.stat.matrix_stat.my_attack_items == [
-            {:tag => :"目くらまし戦法", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :"アヒル戦法",     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-          ]
-        end
+        case2 :my_attack_items, [
+          {:tag => :"目くらまし戦法", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :"アヒル戦法",     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+        ]
       end
 
       it "vs_attack_items" do
-        assert do
-          @user.stat.matrix_stat.vs_attack_items == [
-            {:tag => :"四間飛車", :appear_ratio => 1.0, :judge_counts => {:lose => 0, :win => 1}},
-          ]
-        end
+        case2 :vs_attack_items, [
+          {:tag => :"四間飛車", :appear_ratio => 1.0, :judge_counts => {:lose => 0, :win => 1}},
+        ]
       end
 
       it "my_defense_items" do
-        assert do
-          @user.stat.matrix_stat.my_defense_items == [
-            {:tag => :"アヒル囲い", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-          ]
-        end
+        case2 :my_defense_items, [
+          {:tag => :"アヒル囲い", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+        ]
       end
 
       it "vs_defense_items" do
-        assert do
-          @user.stat.matrix_stat.vs_defense_items == [
-            {:tag => :"美濃囲い", :appear_ratio => 1.0, :judge_counts => {:lose => 0, :win => 1}},
-          ]
-        end
+        case2 :vs_defense_items, [
+          {:tag => :"美濃囲い", :appear_ratio => 1.0, :judge_counts => {:lose => 0, :win => 1}},
+        ]
       end
 
       it "my_technique_items" do
-        assert do
-          @user.stat.matrix_stat.my_technique_items == [
-            {:tag => :"下段の香",    :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :"3段ロケット", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :"垂れ歩",      :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-          ]
-        end
+        case2 :my_technique_items, [
+          {:tag => :"下段の香",    :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :"3段ロケット", :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :"垂れ歩",      :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+        ]
       end
 
       it "vs_technique_items" do
-        assert do
-          @user.stat.matrix_stat.vs_technique_items == []
-        end
+        case2 :vs_technique_items, []
       end
 
       it "my_note_items" do
-        assert do
-          @user.stat.matrix_stat.my_note_items == [
-            {:tag => :居飛車,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :対振り飛車, :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :対抗形,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :ロケット,   :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :急戦,       :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-            {:tag => :短手数,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
-          ]
-        end
+        case2 :my_note_items, [
+          {:tag => :居飛車,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :対振り飛車, :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :対抗形,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :ロケット,   :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :急戦,       :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :短手数,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+        ]
       end
 
       it "vs_note_items" do
-        assert do
-          @user.stat.matrix_stat.vs_note_items == [
-            {:tag => :対抗形,   :appear_ratio => 1.0,:judge_counts => {:win => 1,:lose => 0}},
-            {:tag => :急戦,     :appear_ratio => 1.0,:judge_counts => {:win => 1,:lose => 0}},
-            {:tag => :短手数,   :appear_ratio => 1.0,:judge_counts => {:win => 1,:lose => 0}},
-            {:tag => :振り飛車, :appear_ratio => 1.0,:judge_counts => {:win => 1,:lose => 0}},
-            {:tag => :対居飛車, :appear_ratio => 1.0,:judge_counts => {:win => 1,:lose => 0}},
-          ]
-        end
+        case2 :vs_note_items, [
+          {:tag => :対抗形,   :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :急戦,     :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :短手数,   :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :振り飛車, :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+          {:tag => :対居飛車, :appear_ratio => 1.0, :judge_counts => {:win => 1, :lose => 0}},
+        ]
       end
     end
   end
