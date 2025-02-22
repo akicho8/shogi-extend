5c247d5e99ad1b77c4430416e8b8eacb21e127c3
akicho8 <akicho8@gmail.com>
2025-02-22 17:54:18 +0900

    [feat] [共有将棋盤] [ActionCable] connected か reconnected で記録を分ける

--- app/models/action_cable_event_info.rb
+++ app/models/action_cable_event_info.rb
@@ -3,6 +3,7 @@ class ActionCableEventInfo
   memory_record [
     { key: :initialized,  name: "初期", short_name: "初", },
     { key: :connected,    name: "接続", short_name: "接", },
+    { key: :reconnected,  name: "再接", short_name: "再", },
     { key: :disconnected, name: "切断", short_name: "切", },
     { key: :rejected,     name: "拒否", short_name: "拒", },
     { key: :received,     name: "受信", short_name: "受", },
