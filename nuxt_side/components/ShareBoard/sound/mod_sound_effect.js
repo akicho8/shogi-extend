8653d7bc31d0a9440817df485839a89c314caf6d
akicho8 <akicho8@gmail.com>
2025-02-22 16:19:43 +0900

    [feat] [共有将棋盤] 投了時にBGMを鳴らしてみる

--- nuxt_side/components/ShareBoard/sound/mod_sound_effect.js
+++ nuxt_side/components/ShareBoard/sound/mod_sound_effect.js
@@ -37,6 +37,18 @@ export const mod_sound_effect = {
 
     ////////////////////////////////////////////////////////////////////////////////
 
+    toryo_bgm_call() {
+      this.$sound.play_random([
+        "bgm_battle_toryo1",
+        "bgm_battle_toryo2",
+        "bgm_battle_toryo3",
+        "bgm_battle_toryo4",
+        "bgm_battle_toryo5",
+      ])
+    },
+
+    ////////////////////////////////////////////////////////////////////////////////
+
     beat_call(type) {
       if (this.vibration_mode_info.key === "vibration_mode_on") {
         this.$beat.call(type)
