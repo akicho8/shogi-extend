e685746bb56cce0971b338e3d61b5da2e9744444
akicho8 <akicho8@gmail.com>
2025-03-01 15:21:03 +0900

    [refactor] history_access_patterns を別ファイル化する

---
+++ app/models/swars/importer/history_access_patterns.rb
@@ -0,0 +1,17 @@
+module Swars
+  module Importer
+    HistoryAccessPatterns = [
+      { imode_key: nil,          xmode_key: "野良", rule_key: "10分", },
+      { imode_key: nil,          xmode_key: "野良", rule_key: "3分",  },
+      { imode_key: nil,          xmode_key: "野良", rule_key: "10秒", },
+      { imode_key: nil,          xmode_key: "友達", rule_key: "10分", },
+      { imode_key: nil,          xmode_key: "友達", rule_key: "3分",  },
+      { imode_key: nil,          xmode_key: "友達", rule_key: "10秒", },
+      { imode_key: nil,          xmode_key: "指導", rule_key: "10分", },
+      { imode_key: nil,          xmode_key: "大会", rule_key: "10分", },
+      { imode_key: nil,          xmode_key: "大会", rule_key: "3分",  },
+      { imode_key: nil,          xmode_key: "大会", rule_key: "10秒", },
+      { imode_key: "スプリント", xmode_key: nil,    rule_key: nil,    },
+    ]
+  end
+end
