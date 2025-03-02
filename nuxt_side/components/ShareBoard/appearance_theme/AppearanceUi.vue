64128770fa2502f66fe931c2960bf2efaaeef3aa
akicho8 <akicho8@gmail.com>
2025-03-02 17:29:44 +0900

    [feat] [共有将棋盤] スタイル設定の幅の最小を0にできるようにする

--- nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
+++ nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
@@ -2,7 +2,7 @@
 .AppearanceUi.box
   .title.is-6 スタイル設定
   SimpleRadioButton(:base="SB" model_name="AppearanceThemeInfo" var_name="appearance_theme_key" custom-class="is-small" element_size="is-small")
-  SimpleSlider.is-hidden-touch(:base="SB" label="盤の大きさ" var_name="board_width" :min="60" :max="100" :step="1.0")
+  SimpleSlider.is-hidden-touch(:base="SB" label="盤の大きさ" var_name="board_width" :min="0" :max="100" :step="1.0")
 </template>
 
 <script>
