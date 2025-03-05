93b716a84f92588593803cdca6ba9975d16c93ae
akicho8 <akicho8@gmail.com>
2025-03-05 23:07:47 +0900

    [feat] [共有将棋盤] 開始BGMは鳴らさない

--- nuxt_side/components/ShareBoard/clock/mod_clock_box.js
+++ nuxt_side/components/ShareBoard/clock/mod_clock_box.js
@@ -424,7 +424,9 @@ export const mod_clock_box = {
           this.tl_alert(`${this.user_name}から開始を${this.user_name}だけに通知`)
         }
         this.$sound.play("se_niwatori")
-        Gs.delay_block(1.6, () => this.$sound.play("bgm_opening1"))
+        if (this.debug_mode_p) {
+          Gs.delay_block(1.6, () => this.$sound.play("bgm_opening2"))
+        }
       }
     },
 
