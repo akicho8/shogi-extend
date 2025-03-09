18a5afee307a1c6af72841650d2121bdbf7010f0
akicho8 <akicho8@gmail.com>
2025-03-09 13:12:54 +0900

    [feat] [admin] Google Chrome で iCloud パスワードを有効にすると自動入力候補が出なくなるため明示的に指定してみる

--- app/models/form_box/input_builder/type_string.rb
+++ app/models/form_box/input_builder/type_string.rb
@@ -8,6 +8,12 @@ module FormBox
       def tag_build
         h.send(input_tag, key, default, form_controll)
       end
+
+      def form_controll
+        super.merge({
+            :autocomplete => "on", # Google Chrome で iCloud パスワードを有効にすると自動入力候補が出なくなるため明示的に指定してみる
+          })
+      end
     end
   end
 end
