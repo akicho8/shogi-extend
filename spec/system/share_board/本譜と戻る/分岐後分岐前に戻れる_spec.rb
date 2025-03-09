4a6ed002dab50ff3bf5cb02af3df02cf4f6b5c53
akicho8 <akicho8@gmail.com>
2025-03-09 12:25:26 +0900

    [fix] 仕様とテストの不整合を修正する

--- spec/system/share_board/本譜と戻る/分岐後分岐前に戻れる_spec.rb
+++ spec/system/share_board/本譜と戻る/分岐後分岐前に戻れる_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "分岐したとき戻れる", type: :system, share_board_spec: t
     assert_honpu_return_off              # 「戻る」ボタンはない
     piece_move("77", "76")               # そこで分岐する
     assert_turn(1)                       # 1手目になる
-    assert_honpu_open_on                 # そのとき「本譜」ボタンはあり、
+    assert_honpu_open_off                # そのとき「本譜」ボタンは消えて、
     assert_honpu_return_on               # 「戻る」ボタンが出る
     find(".honpu_return_button").click   # 「戻る」ボタンを押すと、
     assert_turn(0)                       # 0手目に戻る
