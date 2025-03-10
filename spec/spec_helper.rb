913586a59a3603b022dd3c794f0b3e8e620f3cab
akicho8 <akicho8@gmail.com>
2025-03-10 11:46:56 +0900

    [fix] [test] 実行順序がおかしいので seed を設定するのをやめる

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -110,7 +110,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   # config.order = :random
-  config.seed = 0
+  # config.seed = 0
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
