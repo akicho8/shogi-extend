cc94dd8a45dc6a1fe247d518601bde07ef332b59
akicho8 <akicho8@gmail.com>
2025-03-16 14:59:10 +0900

    [test] [swars] [search] Imode (通常 スプリント) の検索を追加する

--- spec/models/swars/battle/search_spec.rb
+++ spec/models/swars/battle/search_spec.rb
@@ -59,6 +59,10 @@ module Swars
         assert { Battle.find_all_by_query("xmode:野良,友達").count == 2 }
       end
 
+      it "imode" do
+        ImodeInfo.each { |e| Battle.create!(imode_key: e.key) }
+        assert { Battle.find_all_by_query("imode:スプリント").count == 1 }
+      end
       it "rule" do
         RuleInfo.each { |e| Battle.create!(rule_key: e.key) }
         assert { Battle.find_all_by_query("rule:10分,3分").count == 2 }
