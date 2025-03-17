92eeebf78e422a1974294d12b8e6248685341b2a
akicho8 <akicho8@gmail.com>
2025-03-17 17:25:55 +0900

    [fix] [test] 不安定な GoogleApi::ExpirationTracker のテストを改善する

--- spec/models/google_api/expiration_tracker_spec.rb
+++ spec/models/google_api/expiration_tracker_spec.rb
@@ -1,12 +1,22 @@
 require "rails_helper"
 
 RSpec.describe GoogleApi::ExpirationTracker, type: :model do
+  before do
+    Timecop.return do
+      GoogleApi::ExpirationTracker.destroy_all
+    end
+  end
+
+  after do
+    Timecop.return do
+      GoogleApi::ExpirationTracker.destroy_all
+    end
+  end
+
   it "プレッドシートを作ったあとで削除する" do
     Timecop.return do
-      assert { GoogleApi::ExpirationTracker.count == 0 }
       GoogleApi::Facade.new(rows: [{id: 1}]).call
       assert { GoogleApi::ExpirationTracker.count == 1 }
-      GoogleApi::ExpirationTracker.destroy_all
     end
   end
 end
--- workbench/google_api/expiration_tracker.rb
+++ workbench/google_api/expiration_tracker.rb
@@ -1,8 +1,8 @@
 require "./setup"
 GoogleApi::ExpirationTracker.delete_all
 GoogleApi::ExpirationTracker.count          # => 0
-GoogleApi::Facade.new(rows: [{id: 1}]).call # => "https://docs.google.com/spreadsheets/d/1fvp4AWulCdcqdnYUX5LJYsdGg_37heG3_Cs3eTIujNs/edit"
+GoogleApi::Facade.new(rows: [{id: 1}]).call # => "https://docs.google.com/spreadsheets/d/1M0VsErGIZQpezy4lOUEmZvJaLJyhhBilGrhygePGnJI/edit"
 GoogleApi::ExpirationTracker.count          # => 1
 GoogleApi::ExpirationTracker.destroy_all
 GoogleApi::ExpirationTracker.count          # => 0
-# >> 2024-07-15T03:33:22.846Z pid=58380 tid=1bxs INFO: Sidekiq 7.1.6 connecting to Redis with options {:size=>10, :pool_name=>"internal", :url=>"redis://localhost:6379/4"}
+# >> 2025-03-17T08:23:03.128Z pid=16732 tid=e9g INFO: Sidekiq 7.3.9 connecting to Redis with options {size: 10, pool_name: "internal", url: "redis://localhost:6379/4"}
