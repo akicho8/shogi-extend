4982bd57a12298522f31df05328fac65211541cb
akicho8 <akicho8@gmail.com>
2025-04-15 23:13:44 +0900

    [refactor] [swars] 取り込み条件を明示する

--- app/models/swars/importer/history_access_patterns.rb
+++ app/models/swars/importer/history_access_patterns.rb
@@ -1,17 +1,25 @@
 module Swars
   module Importer
     HistoryAccessPatterns = [
-      { imode_key: nil,          xmode_key: "野良", rule_key: "10分", },
-      { imode_key: nil,          xmode_key: "野良", rule_key: "3分",  },
-      { imode_key: nil,          xmode_key: "野良", rule_key: "10秒", },
-      { imode_key: nil,          xmode_key: "友達", rule_key: "10分", },
-      { imode_key: nil,          xmode_key: "友達", rule_key: "3分",  },
-      { imode_key: nil,          xmode_key: "友達", rule_key: "10秒", },
-      { imode_key: nil,          xmode_key: "指導", rule_key: "10分", },
-      { imode_key: nil,          xmode_key: "大会", rule_key: "10分", },
-      { imode_key: nil,          xmode_key: "大会", rule_key: "3分",  },
-      { imode_key: nil,          xmode_key: "大会", rule_key: "10秒", },
-      { imode_key: "スプリント", xmode_key: nil,    rule_key: nil,    },
+      { imode_key: "通常",       xmode_key: "野良", rule_key: "10分", },
+      { imode_key: "通常",       xmode_key: "野良", rule_key: "3分",  },
+      { imode_key: "通常",       xmode_key: "野良", rule_key: "10秒", },
+
+      { imode_key: "通常",       xmode_key: "友達", rule_key: "10分", },
+      { imode_key: "通常",       xmode_key: "友達", rule_key: "3分",  },
+      { imode_key: "通常",       xmode_key: "友達", rule_key: "10秒", },
+
+      { imode_key: "通常",       xmode_key: "指導", rule_key: "10分", },
+
+      { imode_key: "通常",       xmode_key: "大会", rule_key: "10分", },
+      { imode_key: "通常",       xmode_key: "大会", rule_key: "3分",  },
+      { imode_key: "通常",       xmode_key: "大会", rule_key: "10秒", },
+
+      { imode_key: "スプリント", xmode_key: "野良", rule_key: "3分",  },
+
+      # { imode_key: "スプリント", xmode_key: "友達", rule_key: "10分", },
+      # { imode_key: "スプリント", xmode_key: "友達", rule_key: "3分",  },
+      # { imode_key: "スプリント", xmode_key: "友達", rule_key: "10秒", },
     ]
   end
 end
