688a42dadde7f13834dae7225aadfbb73006e331
akicho8 <akicho8@gmail.com>
2025-05-24 21:47:26 +0900

    [fix] [R] 出力しているHTMLファイル名が違う

--- app/models/quick_script/swars/turn_average.R
+++ app/models/quick_script/swars/turn_average.R
@@ -112,7 +112,7 @@ p <- config(p, displayModeBar = TRUE)
 if (interactive()) {
   p
 } else {
-  full_path <- "~/src/shogi-extend/nuxt_side/static/lab/swars/turn-average-overlay.html"
+  full_path <- "~/src/shogi-extend/nuxt_side/static/lab/swars/turn-average.html"
   htmlwidgets::saveWidget(p, full_path, selfcontained = TRUE)
   system(sprintf("open -a 'Google Chrome' %s", full_path))
 }
