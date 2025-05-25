6224ea07ec76cd49e2c9c9c0662b7be26203a201
akicho8 <akicho8@gmail.com>
2025-05-25 14:33:02 +0900

    [fix] FinalInfo.fetch("切断") が引けない

--- app/models/swars/final_info.rb
+++ app/models/swars/final_info.rb
@@ -27,13 +27,13 @@ module Swars
   class FinalInfo
     include ApplicationMemoryRecord
     memory_record [
-      { key: :TORYO,         name: "投了",             secondary_key: nil,        label_color: nil,       csa_last_action_key: :TORYO,      draw: false, toryo_or_tsumi: true,  chart_required: true,  grade_segment_label: "投了",             },
-      { key: :TIMEOUT,       name: "時間切れ",         secondary_key: nil,        label_color: nil,       csa_last_action_key: :TIME_UP,    draw: false, toryo_or_tsumi: false, chart_required: true,  grade_segment_label: "時間切れ",         },
-      { key: :CHECKMATE,     name: "詰み",             secondary_key: nil,        label_color: nil,       csa_last_action_key: :TSUMI,      draw: false, toryo_or_tsumi: true,  chart_required: true,  grade_segment_label: "詰まされ",         },
-      { key: :DISCONNECT,    name: "切断",             secondary_key: "通信不調", label_color: "danger",  csa_last_action_key: :CHUDAN,     draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "切断逃亡",         }, # 今の手番が負けと判断してしまうと、自分の手番で相手が切断すると自分が負けてしまうというエッジケースがある。
-      { key: :ENTERINGKING,  name: "入玉",             secondary_key: nil,        label_color: "primary", csa_last_action_key: :KACHI,      draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "入玉",             },
-      { key: :DRAW_SENNICHI, name: "千日手",           secondary_key: nil,        label_color: "danger",  csa_last_action_key: :SENNICHITE, draw: true,  toryo_or_tsumi: false, chart_required: false, grade_segment_label: nil,                }, # これだけは (SENTE|GOTE)_WIN の型で来てない
-      { key: :OUTE_SENNICHI, name: "連続王手の千日手", secondary_key: nil,        label_color: "danger",  csa_last_action_key: :SENNICHITE, draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "連続王手の千日手", }, # 連続王手の千日手 https://ja.wikipedia.org/wiki/%E5%8D%83%E6%97%A5%E6%89%8B#%E9%80%A3%E7%B6%9A%E7%8E%8B%E6%89%8B%E3%81%AE%E5%8D%83%E6%97%A5%E6%89%8B
+      { key: :TORYO,         name: "投了",             alias_name: nil,        label_color: nil,       csa_last_action_key: :TORYO,      draw: false, toryo_or_tsumi: true,  chart_required: true,  grade_segment_label: "投了",             },
+      { key: :TIMEOUT,       name: "時間切れ",         alias_name: nil,        label_color: nil,       csa_last_action_key: :TIME_UP,    draw: false, toryo_or_tsumi: false, chart_required: true,  grade_segment_label: "時間切れ",         },
+      { key: :CHECKMATE,     name: "詰み",             alias_name: nil,        label_color: nil,       csa_last_action_key: :TSUMI,      draw: false, toryo_or_tsumi: true,  chart_required: true,  grade_segment_label: "詰まされ",         },
+      { key: :DISCONNECT,    name: "切断",             alias_name: "通信不調", label_color: "danger",  csa_last_action_key: :CHUDAN,     draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "切断逃亡",         }, # 今の手番が負けと判断してしまうと、自分の手番で相手が切断すると自分が負けてしまうというエッジケースがある。
+      { key: :ENTERINGKING,  name: "入玉",             alias_name: nil,        label_color: "primary", csa_last_action_key: :KACHI,      draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "入玉",             },
+      { key: :DRAW_SENNICHI, name: "千日手",           alias_name: nil,        label_color: "danger",  csa_last_action_key: :SENNICHITE, draw: true,  toryo_or_tsumi: false, chart_required: false, grade_segment_label: nil,                }, # これだけは (SENTE|GOTE)_WIN の型で来てない
+      { key: :OUTE_SENNICHI, name: "連続王手の千日手", alias_name: nil,        label_color: "danger",  csa_last_action_key: :SENNICHITE, draw: false, toryo_or_tsumi: false, chart_required: false, grade_segment_label: "連続王手の千日手", }, # 連続王手の千日手 https://ja.wikipedia.org/wiki/%E5%8D%83%E6%97%A5%E6%89%8B#%E9%80%A3%E7%B6%9A%E7%8E%8B%E6%89%8B%E3%81%AE%E5%8D%83%E6%97%A5%E6%89%8B
     ]
 
     prepend AliasMod
@@ -52,6 +52,10 @@ module Swars
 
     def csa_footer
       "%#{csa_last_action_key}"
+    end
+
+    def secondary_key
+      [name, alias_name]
     end
 
     # def name2(judge_key)
--- spec/models/swars/final_info_spec.rb
+++ spec/models/swars/final_info_spec.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Swars::FinalInfo, type: :model, swars_spec: true do
+  it "works" do
+    assert { Swars::FinalInfo.fetch("切断") }
+  end
+
   it "通信不調で切断が引ける" do
-    assert { Swars::FinalInfo.fetch("通信不調").name == "切断" }
-    assert { Swars::FinalInfo.fetch(:"通信不調").name == "切断" }
+    assert { Swars::FinalInfo.fetch("通信不調") }
+    assert { Swars::FinalInfo.fetch(:"通信不調") }
   end
 end
