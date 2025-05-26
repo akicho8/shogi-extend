b4bdadb63a9a72e7db29566e9ffbdacb02640000
akicho8 <akicho8@gmail.com>
2025-05-26 13:22:56 +0900

    [refactor] JudgeInfo#flip

--- app/models/judge_info.rb
+++ app/models/judge_info.rb
@@ -1,14 +1,21 @@
 class JudgeInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: :win,  name: "勝ち",     ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold",   },
-    { key: :lose, name: "負け",     ox_mark: "●", one_char: "負", css_class: "has-text-weight-normal", },
-    { key: :draw, name: "引き分け", ox_mark: "─", one_char: "△", css_class: "has-text-weight-normal", },
+    { key: :win,  name: "勝ち",     short_name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold",   flip_key: :lose, },
+    { key: :lose, name: "負け",     short_name: "負け", ox_mark: "●", one_char: "負", css_class: "has-text-weight-normal", flip_key: :win,  },
+    { key: :draw, name: "引き分け", short_name: "引分", ox_mark: "─", one_char: "△", css_class: "has-text-weight-normal", flip_key: nil,   },
   ]
 
   prepend AliasMod
 
   class << self
+    def key_cast(key)
+      if key.respond_to?(:downcase)
+        key = key.downcase
+      end
+      key
+    end
+
     def zero_default_hash
       @zero_default_hash ||= each_with_object({}) { |e, m|
         m[e.key] = 0
@@ -20,25 +27,16 @@ class JudgeInfo
     end
   end
 
-  class << self
-    def key_cast(key)
-      if key.respond_to?(:downcase)
-        key = key.downcase
-      end
-      key
-    end
-  end
-
   def swars_memberships
     Swars::Membership.where(judge_key: key)
   end
 
   def flip
-    if key == :draw
-      return self
+    if flip_key
+      self.class.fetch(flip_key)
+    else
+      self
     end
-
-    self.class.fetch(key == :win ? :lose : :win)
   end
 
   def secondary_key
--- workbench/judge_info.rb
+++ workbench/judge_info.rb
@@ -1,4 +1,6 @@
 require "#{__dir__}/setup"
-JudgeInfo[:Win]                 # => #<JudgeInfo:0x0000000124670f50 @attributes={key: :win, name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", code: 0}>
-JudgeInfo["Win"]                # => #<JudgeInfo:0x0000000124670f50 @attributes={key: :win, name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", code: 0}>
-JudgeInfo["WIN"]                # => #<JudgeInfo:0x0000000124670f50 @attributes={key: :win, name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", code: 0}>
+JudgeInfo[:Win]                 # => #<JudgeInfo:0x000000011fcf10a0 @attributes={key: :win, name: "勝ち", short_name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", flip_key: :lose, code: 0}>
+JudgeInfo["Win"]                # => #<JudgeInfo:0x000000011fcf10a0 @attributes={key: :win, name: "勝ち", short_name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", flip_key: :lose, code: 0}>
+JudgeInfo["WIN"]                # => #<JudgeInfo:0x000000011fcf10a0 @attributes={key: :win, name: "勝ち", short_name: "勝ち", ox_mark: "○", one_char: "勝", css_class: "has-text-weight-bold", flip_key: :lose, code: 0}>
+JudgeInfo.zero_default_hash.merge(a: 1) # => {win: 0, lose: 0, draw: 0, a: 1}
+JudgeInfo[:win].flip.key        # => :lose
