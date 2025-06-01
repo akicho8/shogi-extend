9cd7d2a158c5b76da9b29da662a58a40c9316f20
akicho8 <akicho8@gmail.com>
2025-06-01 09:43:45 +0900

    [feat] [swars] 棋譜保持期間を調整する

--- app/models/swars/battle/cleaner_methods.rb
+++ app/models/swars/battle/cleaner_methods.rb
@@ -7,25 +7,25 @@ module Swars
   class Battle
     concern :CleanerMethods do
       included do
-        scope :old_only, -> expires_in { where(arel_table[:accessed_at].lteq(expires_in.seconds.ago)) }        # 古いもの
+        scope :old_only, -> expires_in { where(arel_table[:accessed_at].lteq(expires_in.seconds.ago)) }          # 古いもの
 
-        scope :pro_only,    -> { joins(:memberships).merge(Membership.pro_only).distinct   }                       # PROの対局のみ
-        scope :pro_except,  -> { where.not(id: Membership.pro_only.select(:battle_id)) }                       # PROの対局を除外する
+        scope :pro_only,   -> { joins(:memberships).merge(Membership.pro_only).distinct   }                      # PROの対局のみ
+        scope :pro_except, -> { where.not(id: Membership.pro_only.select(:battle_id)) }                          # PROの対局を除外する
 
-        scope :ban_only,    -> { joins(:memberships).merge(Membership.ban_only).distinct   }                       # 垢BANになった人との対局のみ
-        scope :ban_except,  -> { where.not(id: Membership.ban_only.select(:battle_id)) }                       # 垢BANになった人との対局を除外する
+        scope :ban_only,   -> { joins(:memberships).merge(Membership.ban_only).distinct   }                      # 垢BANになった人との対局のみ
+        scope :ban_except, -> { where.not(id: Membership.ban_only.select(:battle_id)) }                          # 垢BANになった人との対局を除外する
 
         scope :user_only,   -> user_keys { joins(:memberships).merge(Membership.user_only(user_keys)).distinct } # ユーザーを絞る
-        scope :user_except, -> user_keys { where.not(id: Membership.user_only(user_keys).select(:battle_id)) } # ユーザーを省く
+        scope :user_except, -> user_keys { where.not(id: Membership.user_only(user_keys).select(:battle_id)) }   # ユーザーを省く
 
-        scope :xmode_only,   -> xmode_keys {     where(xmode: Xmode.where(key: xmode_keys)) }                  # 特定の対局モードのみ
-        scope :xmode_except, -> xmode_keys { where.not(xmode: Xmode.where(key: xmode_keys)) }                  # 特定の対局モードを除く
+        scope :xmode_only,   -> xmode_keys {     where(xmode: Xmode.where(key: xmode_keys)) }                    # 特定の対局モードのみ
+        scope :xmode_except, -> xmode_keys { where.not(xmode: Xmode.where(key: xmode_keys)) }                    # 特定の対局モードを除く
 
-        scope :coaching_only,   -> { xmode_only("指導")   }                                                    # 指導対局のみ
-        scope :coaching_except, -> { xmode_except("指導") }                                                    # 指導対局を除く
+        scope :coaching_only,   -> { xmode_only("指導")   }                                                      # 指導対局のみ
+        scope :coaching_except, -> { xmode_except("指導") }                                                      # 指導対局を除く
 
-        scope :imode_only,   -> imode_keys {     where(imode: Imode.where(key: imode_keys)) }                  # 特定の開始モードのみ
-        scope :imode_except, -> imode_keys { where.not(imode: Imode.where(key: imode_keys)) }                  # 特定の開始モードを除く
+        scope :imode_only,   -> imode_keys {     where(imode: Imode.where(key: imode_keys)) }                    # 特定の開始モードのみ
+        scope :imode_except, -> imode_keys { where.not(imode: Imode.where(key: imode_keys)) }                    # 特定の開始モードを除く
 
         # 削除対象
         scope :cleaner_scope, -> (options = {}) {
@@ -63,12 +63,14 @@ module Swars
           s
         }
 
-        # 一般ユーザーの対局は60日後に消す
+        # 消さない棋譜に集計が偏るため集計では直近3ヶ月とした方がいいかもしれない
+
+        # 一般ユーザーの対局は90日後に消す
         scope :drop_scope1, -> (options = {}) {
           options = {
             :xmode_only  => "野良",
             :ban_except  => false,
-            :old_only    => Rails.env.local? ? 0.days : 60.days,
+            :old_only    => Rails.env.local? ? 0.days : 3.month,
             :user_except => Swars::User::Vip.long_time_keep_user_keys + Swars::User::Vip.protected_user_keys,
           }.merge(options)
           cleaner_scope(options)
@@ -79,7 +81,7 @@ module Swars
           options = {
             :xmode_only  => "野良",
             :ban_except  => false,
-            :old_only    => Rails.env.local? ? 0.days : 120.days,
+            :old_only    => Rails.env.local? ? 0.days : 4.month,
             :user_only   => Swars::User::Vip.long_time_keep_user_keys, # こちらに含まれていても
             :user_except => Swars::User::Vip.protected_user_keys,      # さらにこちらで除外される
           }.merge(options)
@@ -90,12 +92,24 @@ module Swars
       class_methods do
         # rails r 'Swars::Battle.cleaner1.call'
         def cleaner1(options = {})
+          cleaner_options_old_only_validation!(options)
           drop_scope1(options).cleaner({ subject: "一般" }.merge(options))
         end
 
         # rails r 'Swars::Battle.cleaner2.call'
         def cleaner2(options = {})
+          cleaner_options_old_only_validation!(options)
           drop_scope2(options).cleaner({ subject: "VIP" }.merge(options))
+        end
+
+        private
+
+        def cleaner_options_old_only_validation!(options)
+          if Rails.env.production? || Rails.env.staging?
+            if options[:old_only] < Config[:battle_keep_days]
+              raise "削除までの猶予は共通保持期間よりも長めにすること : (#{options[:old_only]} < #{Config[:battle_keep_days]})"
+            end
+          end
         end
       end
     end
--- app/models/swars/config.rb
+++ app/models/swars/config.rb
@@ -1,5 +1,7 @@
 module Swars
   module Config
+    ################################################################################ プレイヤー情報
+
     # turn_max
     mattr_accessor(:establish_gteq)          {  2 } # N手以上で双方の通信が正常と見なす
     mattr_accessor(:sennitite_eq)            { 12 } # N手で開幕千日手と見なす
@@ -10,5 +12,9 @@ module Swars
     mattr_accessor(:master_count_gteq)       { 5  } # N局以上あったときに率で判定する(技Aが1勝0敗のとき技Aを究めたは不自然なため)
     # 棋力差
     mattr_accessor(:gdiff_penalty_threshold) { 10 } # 恐怖の級位者と見なす段級差
+
+    ################################################################################ 削除
+
+    mattr_accessor(:battle_keep_days) { 3.month } # 最低限保持する日数
   end
 end
