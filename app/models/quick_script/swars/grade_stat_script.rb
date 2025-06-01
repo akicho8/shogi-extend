b69139c0f132a0fec3eea57828bcd9b9ebcdc36d
akicho8 <akicho8@gmail.com>
2025-06-01 11:57:57 +0900

    [fix] 公開の /lab/swars/grade-stat が 404 になるため新URLに飛ばす

---
+++ app/models/quick_script/swars/grade_stat_script.rb
@@ -0,0 +1,13 @@
+module QuickScript
+  module Swars
+    class GradeStatScript < Base
+      self.title = "新URLに飛ぶ"
+      self.description = "/lab/swars/grade-stat から新しいURLにリダイレクトする"
+      self.qs_invisible = true
+
+      def call
+        redirect_to "/lab/swars/grade-standard-score"
+      end
+    end
+  end
+end
