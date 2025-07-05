53885574ef348659c30ca795e0d4e981c695927d
akicho8 <akicho8@gmail.com>
2025-07-05 10:46:52 +0900

    [feat] [共有将棋盤] N手毎交代の最大を999とする

--- nuxt_side/components/models/change_per_info.js
+++ nuxt_side/components/models/change_per_info.js
@@ -5,7 +5,7 @@ export class ChangePerInfo extends ApplicationMemoryRecord {
   static field_message = "オンラインでは移動する手間がかからないため1手毎を推奨する"
   static input_type = "numberinput"
   static min = 1
-  static max = 100
+  static max = 999
   static hint_messages = [
     "N回指したら交代します。リアル対局と違って移動の手間がかからないので1回にしておくのが無難です。",
   ]
