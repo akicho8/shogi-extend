9c958dd6c5d01915cb119fce00866d7b030c5bcb
akicho8 <akicho8@gmail.com>
2025-07-05 10:47:24 +0900

    [feat] [共有将棋盤] 順番設定の説明を改善する

--- nuxt_side/components/models/illegal_behavior_info.js
+++ nuxt_side/components/models/illegal_behavior_info.js
@@ -20,7 +20,7 @@ export class IllegalBehaviorInfo extends ApplicationMemoryRecord {
       {
         key: "is_illegal_behavior_auto",
         name: "したら負け",
-        message: "緊張感あり。逆転の余地あり。面白さ増↑ (推奨)",
+        message: "緊張感↑ 逆転の余地↑ 盛り上がり↑ (推奨)",
         type: "is-primary",
         sp_illegal_validate: true,
         sp_illegal_cancel: false,
@@ -29,7 +29,7 @@ export class IllegalBehaviorInfo extends ApplicationMemoryRecord {
       }, {
         key: "is_illegal_behavior_newbie",
         name: "できない",
-        message: "緊張感なし。逆転の余地なし。面白さ減↓ (非推奨)",
+        message: "緊張感↓ 逆転の余地↓ 盛り上がり↓ (超初心者向け)",
         type: "is-warning",
         sp_illegal_validate: true,
         sp_illegal_cancel: true,
