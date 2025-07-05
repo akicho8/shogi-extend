07767a07b5b93033d1468fdeae95d2823f969a39
akicho8 <akicho8@gmail.com>
2025-07-05 10:36:32 +0900

    [feat] [共有将棋盤] 深考時間の説明をより簡潔にする

--- nuxt_side/components/ShareBoard/clock/ClockBoxInputTable.vue
+++ nuxt_side/components/ShareBoard/clock/ClockBoxInputTable.vue
@@ -23,12 +23,13 @@ table.ClockBoxInputTable(:class="{cc_unique_p: SB.cc_unique_p}")
         //- https://buefy.org/documentation/tooltip
         b-tooltip(position="is-right" multilined)
           template(v-slot:content)
-            p.title.is-6.mb-0 深考時間とは？
-            ul
-              li 回復しない最後の持ち時間で、秒読みが0になった後で消費します
-              li うっかり時間切れの救済用ではなく、勝負所の局面で<b>じっくり考えるため</b>の時間です
-              li <b>数分程度</b>あると、より深い終盤戦が楽しめるでしょう
+            //- p.title.is-6.mb-0 深考時間とは？
+            .is-flex.is-flex-direction-column
+              p 回復しない最後の持ち時間で、秒読みが0になった後で消費します
+              p うっかり切れ負けの救済用ではなく、勝負所で<b>じっくり考えるため</b>の時間です
+              p <b>数分程度</b>あると、より深い終盤戦が楽しめるでしょう
           | 深考時間(秒)
+          b-icon.has-text-info(icon="help-circle-outline" size="is-small")
       td(v-for="e in SB.cc_params")
         b-numberinput.initial_extra_sec(v-bind="input_default_attrs" v-model="e.initial_extra_sec" :max="60*60" @input="SB.cc_input_handle")
     tr
@@ -98,12 +99,15 @@ export default {
   .b-tooltip.is-multiline
     .tooltip-content
       padding: 1rem
+      width: 15rem
+
       text-align: left
       line-height: 1.5
-      width: 15rem
-      .title
-        color: inherit
-        white-space: nowrap
-      li
-        margin-top: 0.8rem
+
+      .is-flex
+        gap: 0.75rem
+
+      // .title
+      //   color: inherit
+      //   white-space: nowrap
 </style>
