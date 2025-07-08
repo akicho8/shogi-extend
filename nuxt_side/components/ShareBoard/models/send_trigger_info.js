f3a6b1fc6e2aca78b8f3b9d81d9ee026e51eab46
akicho8 <akicho8@gmail.com>
2025-07-08 20:06:32 +0900

    [共有将棋盤] [設定] [文言改善] 発言送信トリガー

--- nuxt_side/components/ShareBoard/models/send_trigger_info.js
+++ nuxt_side/components/ShareBoard/models/send_trigger_info.js
@@ -1,7 +1,7 @@
 import { ApplicationMemoryRecord } from "@/components/models/application_memory_record.js"
 
 export class SendTriggerInfo extends ApplicationMemoryRecord {
-  static field_label = "発言送信トリガー"
+  static field_label = "チャットの発言の送信方法"
   static field_message = "チャットの発言をどのキーで送信できるようにするか？"
 
   static get define() {
