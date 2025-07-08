d5f35c75701b480c997cc03f15206c8a323877cc
akicho8 <akicho8@gmail.com>
2025-07-08 19:50:56 +0900

    [feat] [共有将棋盤] [文言改善] Androidのみ対応 → Androidのみ

--- nuxt_side/components/ShareBoard/models/vibration_mode_info.js
+++ nuxt_side/components/ShareBoard/models/vibration_mode_info.js
@@ -2,12 +2,12 @@ import { ApplicationMemoryRecord } from "@/components/models/application_memory_
 
 export class VibrationModeInfo extends ApplicationMemoryRecord {
   static field_label = "振動"
-  static field_message = "随所でスマホを振動させるか？ (Androidのみ対応)"
+  static field_message = "随所でスマホを振動させるか？ (Androidのみ)"
 
   static get define() {
     return [
-      { key: "vibration_mode_on",  name: "あり", type: "is-primary",  message: null, },
-      { key: "vibration_mode_off", name: "なし", type: "is-primary",  message: null, },
+      { key: "vibration_mode_on",  name: "あり", type: "is-primary", message: null, },
+      { key: "vibration_mode_off", name: "なし", type: "is-primary", message: null, },
     ]
   }
 }
