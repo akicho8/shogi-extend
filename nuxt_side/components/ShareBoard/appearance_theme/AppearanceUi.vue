e7fe46a2773a39a921ae1a8168b8655105581a7e
akicho8 <akicho8@gmail.com>
2025-07-19 20:56:06 +0900

    [feat] [共有将棋盤] グリッド線の太さオプションは悪手なので隠す

--- nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
+++ nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
@@ -4,7 +4,7 @@
   .ui_parts
     SimpleRadioButton(:base="SB" model_name="AppearanceThemeInfo" var_name="appearance_theme_key" custom-class="is-small" element_size="is-small")
     SimpleSlider.is-hidden-touch(:base="SB" label="盤の大きさ" var_name="sb_board_width" :min="0" :max="100" :step="1.0")
-    SimpleSlider(:base="SB" label="グリッドの太さ" var_name="sb_grid_stroke" :min="0" :max="3" :step="0.5")
+    SimpleSlider(:base="SB" label="グリッドの太さ" var_name="sb_grid_stroke" :min="0" :max="3" :step="0.5" v-if="SB.debug_mode_p")
     SimpleRadioButton.is-hidden-tablet(:base="SB" model_name="MobileVerticalInfo" var_name="mobile_vertical_key" custom-class="is-small" element_size="is-small")
 </template>
 
