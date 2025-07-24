5fa6cc5461880bf96998c886fdb925f518ca8901
akicho8 <akicho8@gmail.com>
2025-07-24 20:16:50 +0900

    [feat] 棋譜ダウンロード件数を調整する

--- app/models/quick_script/swars/battle_download_script/max_info.rb
+++ app/models/quick_script/swars/battle_download_script/max_info.rb
@@ -3,9 +3,9 @@ class QuickScript::Swars::BattleDownloadScript
     include ApplicationMemoryRecord
     memory_record [
       { key: "1",   name: "1",   value: 1,   el_message: "1件なら対局詳細からダウンロードした方がZIPにならないぶん簡単です", },
-      { key: "50",  name: "50",  value: 50,  el_message: "50件取得する",                                                     },
-      { key: "100", name: "100", value: 100, el_message: "100件取得する",                                                    },
-      { key: "200", name: "200", value: 200, el_message: "201件以上取得するときは「古い棋譜の補完」のほうを使ってください",  },
+      { key: "50",  name: "50",  value: 50,  el_message: "これ以上取得したいときは「古い棋譜の補完」を使ってください",       },
+      # { key: "100", name: "100", value: 100, el_message: "100件取得する",                                                    },
+      # { key: "200", name: "200", value: 200, el_message: "201件以上取得するときは「古い棋譜の補完」のほうを使ってください",  },
     ]
   end
 end
