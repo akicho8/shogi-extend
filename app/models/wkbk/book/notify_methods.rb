ba2e775203ad8a0d75f995097bca1469a3e9708c
akicho8 <akicho8@gmail.com>
2025-08-02 17:39:51 +0900

    [feat] [将棋ドリル] 問題集や問題の作成通知のログレベルを下げる

--- app/models/wkbk/book/notify_methods.rb
+++ app/models/wkbk/book/notify_methods.rb
@@ -3,7 +3,7 @@ module Wkbk
     concern :NotifyMethods do
       # rails r 'Wkbk::Book.first.notify'
       def notify
-        AppLog.important(emoji: ":問題集:", subject: mail_subject, body: mail_body)
+        AppLog.info(emoji: ":問題集:", subject: mail_subject, body: mail_body)
       end
 
       def status_name
