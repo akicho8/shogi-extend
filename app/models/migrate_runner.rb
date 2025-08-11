dc889155cc1827d781a1dda3fa9ac877e0442965
akicho8 <akicho8@gmail.com>
2025-08-11 11:38:41 +0900

    [feat] [MigrateRunner] VIPユーザー対局全再ビルド

--- app/models/migrate_runner.rb
+++ app/models/migrate_runner.rb
@@ -154,25 +154,25 @@ class MigrateRunner
   #   end
   # end
 
-  # def step6_rebuild_for_auto_crawl_user_keys
-  #   ::Swars::User::Vip.auto_crawl_user_keys.each.with_index do |user_key, i|
-  #     if i.modulo(50).zero?
-  #       AppLog.important("#{i} / #{::Swars::User::Vip.auto_crawl_user_keys.size}")
-  #     end
-  #     p user_key
-  #     if user = Swars::User[user_key]
-  #       s = user.battles
-  #       batch_size = 1000
-  #       all_count = s.count.ceildiv(batch_size)
-  #       s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
-  #         p [batch, all_count, batch.fdiv(all_count)]
-  #         # s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
-  #         s.each { |e| e.rebuild(tries: 1) }
-  #         puts
-  #       end
-  #     end
-  #   end
-  # end
+  def step6_rebuild_for_auto_crawl_user_keys
+    ::Swars::User::Vip.auto_crawl_user_keys.each.with_index do |user_key, i|
+      if i.modulo(50).zero?
+        AppLog.important("#{i} / #{::Swars::User::Vip.auto_crawl_user_keys.size}")
+      end
+      p user_key
+      if user = Swars::User[user_key]
+        s = user.battles
+        batch_size = 1000
+        all_count = s.count.ceildiv(batch_size)
+        s.in_batches(order: :desc, of: batch_size).each_with_index do |s, batch|
+          p [batch, all_count, batch.fdiv(all_count)]
+          s = s.where.not(analysis_version: Bioshogi::ANALYSIS_VERSION)
+          s.each { |e| e.rebuild(tries: 1) }
+          puts
+        end
+      end
+    end
+  end
 
   # def step1
   #   Swars::Rule.unscoped.find_each do |rule|
