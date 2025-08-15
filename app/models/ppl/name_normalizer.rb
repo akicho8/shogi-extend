a17ddedd5862ef998264b2b16cf5248bc4a3d5bb
akicho8 <akicho8@gmail.com>
2025-08-15 16:01:05 +0900

    [fix] [奨励会三段リーグ早見表] 「木村」と「木村一」は同一自分らしい

--- app/models/ppl/name_normalizer.rb
+++ app/models/ppl/name_normalizer.rb
@@ -8,6 +8,7 @@ module Ppl
       str = str.gsub(/\A森安\z/, "森安正")
       str = str.gsub(/\A石田\z/, "石田和")
       str = str.gsub(/\A廣瀬章人\z/, "広瀬章人")
+      str = str.gsub(/\A木村\z/, "木村一")
 
       str = str.gsub("泰煕", "泰熙")
       str = str.gsub(/[剣劔]持/, "剱持")
