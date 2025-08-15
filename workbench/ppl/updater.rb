3b5a672d962b5655279bc8f89b7618b11d632552
akicho8 <akicho8@gmail.com>
2025-08-15 16:01:25 +0900

    [refactor] [奨励会三段リーグ早見表] workbench

--- workbench/ppl/updater.rb
+++ workbench/ppl/updater.rb
@@ -1,9 +1,13 @@
 require "#{__dir__}/setup"
 Ppl.setup_for_workbench
 
-# ネットから
-# Ppl::Updater.resume_crawling
+# ネットからDB
+#  Ppl::Updater.resume_crawling
 
-# JSONから
+# ネットからJSON
+# all = (28..77).collect { |e| Ppl::Spider.new(season_number: e).call }
+# Pathname("all.json").write(all.to_json)
+
+# JSONからDB
 rows = JSON.parse(Pathname("all.json").read, symbolize_names: true)
 rows.each.with_index { |e, i| Ppl::Updater.update_raw(i, e) }
