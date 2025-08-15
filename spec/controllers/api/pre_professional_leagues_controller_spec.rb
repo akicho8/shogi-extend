ba115804ecd2616ecdd9d9f2149d5f0d95e2fa2c
akicho8 <akicho8@gmail.com>
2025-08-15 20:10:18 +0900

    [refactor] 不要なテストを削除する

--- spec/controllers/api/pre_professional_leagues_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Api::PreProfessionalLeaguesController, type: :controller, swars_spec: true do
-  it "works" do
-    get :show, params: { generation: -1 }
-    assert { response.status == 404 }
-  end
-end
