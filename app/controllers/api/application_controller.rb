bdae6d2b2c15fed6d3bdcbf10aad348174b78c05
akicho8 <akicho8@gmail.com>
2025-08-24 08:20:57 +0900

    [refactor] controller にある html_fetch は使われていないので削除する

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -8,24 +8,6 @@ module Api
       end
     end
 
-    # FIXME: なんだこれ？ いらなかったら消す
-    def html_fetch(url, options = {})
-      options = {
-        expires_in: 1.hour,
-      }.merge(options)
-
-      key = [:html_fetch, Digest::MD5.hexdigest(url)].join
-      Rails.cache.fetch(key, options) do
-        Rails.logger.debug("html_fetch: #{url}")
-        begin
-          URI(url).read.toutf8
-        rescue SocketError => error
-          AppLog.critical(error)
-          ""
-        end
-      end
-    end
-
     def api_log!
       ApiOnelineLogger.new(self).perform
     end
