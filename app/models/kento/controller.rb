ab62334ae9f931efcc5b647d574649d0cab75a94
akicho8 <akicho8@gmail.com>
2025-08-30 11:55:01 +0900

    [feat] KENTO からの DOS 攻撃対策を入れる (が、IP が異なるため意味がない)

--- app/models/kento/controller.rb
+++ app/models/kento/controller.rb
@@ -1,5 +1,14 @@
 module Kento
   concern :Controller do
+    # Amazon EC2 からのアクセスは IP がバラバラなため攻撃を防ぐことができない
+    # if Rails.env.local?
+    #   included do
+    #     rate_limit to: 1, within: 1.second, only: :index, by: -> { request.remote_ip }, :if => -> {
+    #       params[:format_type] == "kento" && current_swars_user
+    #     }
+    #   end
+    # end
+
     # http://localhost:3000/w.json?query=DevUser1&format_type=kento
     # https://www.shogi-extend.com/w.json?query=kinakom0chi&format_type=kento
     def index
