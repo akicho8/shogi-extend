88df1c631b15c2a39984c80a61dedf46e90a5791
akicho8 <akicho8@gmail.com>
2025-08-30 13:26:14 +0900

    [test] [三段リーグ] テストを仕様に合わせる

--- spec/models/ppl/medieval_spider_spec.rb
+++ spec/models/ppl/medieval_spider_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Ppl::MedievalSpider, type: :model do
   it "works" do
     rows = Ppl::MedievalSpider.new(season_key_vo: Ppl::SeasonKeyVo["1"], take_size: 1, verbose: false, sleep: 0, promotion_count_gteq: 0).call
     row = rows.sole
-    assert { row[:result_key] == "維"               }
+    assert { row[:result_key] == "維持"             }
     assert { row[:name]       == "村松央一"         }
     assert { row[:age]        == 28                 }
     assert { row[:win]        == 7                  }
