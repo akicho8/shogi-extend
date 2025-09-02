c0f9c26385aeb65e99fab4d86ee73722092c7bce
akicho8 <akicho8@gmail.com>
2025-09-02 23:03:30 +0900

    [test] 仕様とテストの不整合を修正する

--- spec/system/share_board/順番設定_下のわかりにくいオプションの説明_spec.rb
+++ spec/system/share_board/順番設定_下のわかりにくいオプションの説明_spec.rb
@@ -13,6 +13,6 @@ RSpec.describe type: :system, share_board_spec: true do
     os_modal_open_handle    # 「順番設定」モーダルを開く
     os_switch_toggle   # 右上の有効スイッチをクリック
     case1("反則", "反則は「二歩」「王手放置」「駒ワープ」「死に駒」のみが対象です")
-    case1("N手毎交代", "N回指したら交代します")
+    case1("N回指したら交代する", "1回にしておくのが無難")
   end
 end
