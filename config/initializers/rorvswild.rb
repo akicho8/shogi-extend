dff24ad8115d52f2c6d8d848cc1a0b3bef31b1fe
akicho8 <akicho8@gmail.com>
2025-09-04 23:46:23 +0900

    [fix] Rorvswild の設定で失敗している

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,1 +1,4 @@
-RorVsWild.start(api_key: Rails.application.credentials[:rorvswild][Rails.env][:api_key])
+if defined? RorVsWild
+  RorVsWild.start(api_key: Rails.application.credentials[:rorvswild][Rails.env][:api_key])
+end
+
