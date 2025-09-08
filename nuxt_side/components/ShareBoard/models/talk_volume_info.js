304f8be58aa483bb1678c3f4e2ee409f4273ff23
akicho8 <akicho8@gmail.com>
2025-09-08 11:04:51 +0900

    [feat] [共有将棋盤] 文言を改善する

--- nuxt_side/components/ShareBoard/models/talk_volume_info.js
+++ nuxt_side/components/ShareBoard/models/talk_volume_info.js
@@ -3,7 +3,7 @@ import { Gs } from "@/components/models/gs.js"
 
 export class TalkVolumeInfo extends ApplicationMemoryRecord {
   static field_label = "申し伝え音量"
-  static field_message = "これを 0 に設定するのは避けてください。実況の妨げになる場合は、「指し手の読み上げ」と「手番のお知らせ」だけを OFF にするのが適切です。これを 0 にしてしまうと、重要な通知まで聞こえなくなり、想定しているUXに支障をきたすおそれがあります（初期値: 0.5）"
+  static field_message = "これを 0 に設定するのは避けてください。実況の妨げになる場合は、「指し手の読み上げ」と「手番のお知らせ」だけを OFF にするのが適切です。これを 0 にしてしまうと重要な通知まで聞こえなくなり、想定のUXに支障をきたすおそれがあります（初期値: 0.5）"
   static input_type = "slider"
   static min = 0.0
   static step = 0.1
