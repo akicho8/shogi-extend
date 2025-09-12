d3522ba21be2865822a946f30433bfe9bd99f297
akicho8 <akicho8@gmail.com>
2025-09-12 15:21:50 +0900

    [feat] [符号の鬼] 40回間違えたら停止とする

--- nuxt_side/components/XyMaster/models/rule_info.js
+++ nuxt_side/components/XyMaster/models/rule_info.js
@@ -3,10 +3,10 @@ import { ApplicationMemoryRecord } from "@/components/models/application_memory_
 export class RuleInfo extends ApplicationMemoryRecord {
   static get define() {
     return [
-      { key: "rule100t",  name: "☗100問TAP", o_count_max: 100, viewpoint: "black", input_mode: "is_input_mode_tap", time_limit: 60*3.5, play_break_miss_count: 50, },
-      { key: "rule100tw", name: "☖100問TAP", o_count_max: 100, viewpoint: "white", input_mode: "is_input_mode_tap", time_limit: 60*3.5, play_break_miss_count: 50, },
-      { key: "rule100",   name: "☗100問",    o_count_max: 100, viewpoint: "black", input_mode: "is_input_mode_kb",  time_limit: 60*3.5, play_break_miss_count: 50, },
-      { key: "rule100w",  name: "☖100問",    o_count_max: 100, viewpoint: "white", input_mode: "is_input_mode_kb",  time_limit: 60*3.5, play_break_miss_count: 50, },
+      { key: "rule100t",  name: "☗100問TAP", o_count_max: 100, viewpoint: "black", input_mode: "is_input_mode_tap", time_limit: 60*3.5, break_miss_gteq: 50, },
+      { key: "rule100tw", name: "☖100問TAP", o_count_max: 100, viewpoint: "white", input_mode: "is_input_mode_tap", time_limit: 60*3.5, break_miss_gteq: 50, },
+      { key: "rule100",   name: "☗100問",    o_count_max: 100, viewpoint: "black", input_mode: "is_input_mode_kb",  time_limit: 60*3.5, break_miss_gteq: 50, },
+      { key: "rule100w",  name: "☖100問",    o_count_max: 100, viewpoint: "white", input_mode: "is_input_mode_kb",  time_limit: 60*3.5, break_miss_gteq: 50, },
     ]
   }
 
@@ -17,6 +17,6 @@ export class RuleInfo extends ApplicationMemoryRecord {
 
   // ミスの許容を超えたか？
   too_many_miss_p(x_count) {
-    return x_count >= this.play_break_miss_count
+    return x_count >= this.break_miss_gteq
   }
 }
--- nuxt_side/components/XyMasterLite/models/rule_info.js
+++ nuxt_side/components/XyMasterLite/models/rule_info.js
@@ -3,8 +3,8 @@ import { ApplicationMemoryRecord } from "@/components/models/application_memory_
 export class RuleInfo extends ApplicationMemoryRecord {
   static get define() {
     return [
-      { key: "rule100t",  name: "☗100問TAP", viewpoint: "black", time_limit: 60*3.5, play_break_miss_count: 50, },
-      { key: "rule100tw", name: "☖100問TAP", viewpoint: "white", time_limit: 60*3.5, play_break_miss_count: 50, },
+      { key: "rule100t",  name: "☗100問TAP", viewpoint: "black", time_limit: 60*3.5, break_miss_gteq: 40, },
+      { key: "rule100tw", name: "☖100問TAP", viewpoint: "white", time_limit: 60*3.5, break_miss_gteq: 40, },
     ]
   }
 }
