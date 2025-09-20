578aa15d8af4f3d87712fe3a865aef7fe062d197
akicho8 <akicho8@gmail.com>
2025-09-20 19:00:13 +0900

    [feat] 木目テクスチャ集に統一する

--- spec/system/gallery_spec.rb
+++ spec/system/gallery_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe "将棋盤木目テクスチャ集", type: :system do
+RSpec.describe "木目テクスチャ集", type: :system do
   it "works" do
     visit2 "/gallery"
-    assert_text("将棋盤木目テクスチャ集")
+    assert_text("木目テクスチャ集")
     first(".pagination-next").click
     first(".pagination-previous").click
   end
