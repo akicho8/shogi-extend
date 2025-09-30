5c870cb24b9379012733ad68e3d346558157fc4a
akicho8 <akicho8@gmail.com>
2025-09-30 18:42:57 +0900

    [style] cosmetic change

--- spec/system/share_board/対局時計_個別設定_片方を変更したとき片方は連動しない_spec.rb
+++ spec/system/share_board/対局時計_個別設定_片方を変更したとき片方は連動しない_spec.rb
@@ -1,15 +1,15 @@
 require "#{__dir__}/shared_methods"
 
-RSpec.describe type: :system, share_board_spec: true do
+RSpec.describe "対局時計_個別設定_片方を変更したとき片方は連動しない", type: :system, share_board_spec: true do
   it "works" do
     a_block do
       room_setup("test_room", "alice")
       clock_open
-      clock_box_form_set(:black, 1, 2, 3, 4)             # aliceが時計を設定する
-      find(".cbm_cc_unique_mode_sete_handle").click      # 個別設定を押す
-      clock_box_form_eq(:white, 1, 2, 3, 4)     # ▲側が△にコピーされている
-      clock_box_form_set(:white, 5, 6, 7, 8)             # △側を更新する
-      clock_box_form_eq(:black, 1, 2, 3, 4)     # ▲側は変更されていない (内容は同じだが別のメモリを指している)
+      clock_box_form_set(:black, 1, 2, 3, 4)        # aliceが時計を設定する
+      find(".cbm_cc_unique_mode_sete_handle").click # 個別設定を押す
+      clock_box_form_eq(:white, 1, 2, 3, 4)         # ▲側が△にコピーされている
+      clock_box_form_set(:white, 5, 6, 7, 8)        # △側を更新する
+      clock_box_form_eq(:black, 1, 2, 3, 4)         # ▲側は変更されていない (内容は同じだが別のメモリを指している)
     end
     b_block do
       room_setup("test_room", "bob")
