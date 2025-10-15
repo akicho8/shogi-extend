b92bdd47126b699eb4d966327e4b06da69197a80
akicho8 <akicho8@gmail.com>
2025-10-15 19:00:22 +0900

    [test] [共有将棋盤] before ブロックの重い初期化処理を除外する

--- spec/system/share_board/shared_methods.rb
+++ spec/system/share_board/shared_methods.rb
@@ -8,7 +8,7 @@ Pathname(__dir__).glob("shared_methods/*.rb") { |e| require(e) }
 RSpec.configure do |config|
   config.include(AliceBobCarol, type: :system, share_board_spec: true)
   config.include(SharedMethods, type: :system, share_board_spec: true)
-  config.before(:example, type: :system, share_board_spec: true) do
-    setup_share_board
-  end
+  # config.before(:example, type: :system, share_board_spec: true) do
+  #   setup_share_board
+  # end
 end
