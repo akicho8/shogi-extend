9e6450d738137fd0b7fc71175c34e2c4be6bdd6f
akicho8 <akicho8@gmail.com>
2025-10-22 15:25:21 +0900

    [test] [fix] [共有将棋盤] 全件表示しようとして対局履歴が重い

--- spec/system/share_board/投了_ランキングに反映する_spec.rb
+++ spec/system/share_board/投了_ランキングに反映する_spec.rb
@@ -1,28 +1,22 @@
 require "#{__dir__}/shared_methods"
 
 RSpec.describe __FILE__, type: :system, share_board_spec: true do
-  def case1(user_name)
+  it "works" do
+    setup_share_board
     visit_room({
-        :user_name    => user_name,
+        :user_name    => :a,
         :fixed_member => "a,b",
         :fixed_order  => "a,b",
         :room_after_create => :cc_auto_start_10m,
       })
-  end
-
-  it "works" do
-    window_a { case1(:a) }
-    window_b { case1(:b) }
-    window_a do
-      give_up_run
-      sidebar_open
-      menu_item_click("対局履歴")
-      within(".modal") do
-        assert_text(:a)
-        assert_text(:b)
-      end
-      find(".permalink").click
-      assert_text("順位")
+    give_up_run
+    sidebar_open
+    menu_item_click("対局履歴")
+    within(".modal") do
+      assert_text(:a)
+      assert_text(:b)
     end
+    find(".permalink").click
+    assert_text("順位")
   end
 end
