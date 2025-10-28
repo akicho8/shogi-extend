c7e568e7aa47b67c7bbd0a706c3441aaa618135c
akicho8 <akicho8@gmail.com>
2025-10-28 20:26:50 +0900

    [refactor] [共有将棋盤] /help_mod -> /help

--- nuxt_side/components/ShareBoard/help/mod_help.js
+++ nuxt_side/components/ShareBoard/help/mod_help.js
@@ -2,7 +2,7 @@ import SbHelpModal from "./SbHelpModal.vue"
 
 export const mod_help = {
   methods: {
-    general_help_modal_handle() {
+    general_help_modal_open_handle() {
       this.sidebar_p = false
       this.sfx_click()
       this.modal_card_open({
