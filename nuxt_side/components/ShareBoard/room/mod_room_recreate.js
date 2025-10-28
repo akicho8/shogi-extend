6670dbd8257a6b19f4d8a1f7e24c5d3758bb485f
akicho8 <akicho8@gmail.com>
2025-10-29 00:04:47 +0900

    [fix] [共有将棋盤] 「オフラインになりました」と発音することで余計に状況が悪化する

--- nuxt_side/components/ShareBoard/room/mod_room_recreate.js
+++ nuxt_side/components/ShareBoard/room/mod_room_recreate.js
@@ -32,7 +32,7 @@ export const mod_room_recreate = {
       }
     },
     internet_off_trigger() {
-      this.toast_ng("オフラインになりました")
+      this.toast_ng("オフラインになりました", {talk: false})
       if (this.ac_room) {
         this.room_recreate_modal_open_handle()
       }
