51caf97c0c4d40117ed7e72a924fff994c71a5b7
akicho8 <akicho8@gmail.com>
2025-10-28 19:14:10 +0900

    [refactor] [共有将棋盤] [よそ見] focus, blur を用いた古いコードを削除する

--- nuxt_side/components/ShareBoard/window_active_detector.js
+++ nuxt_side/components/ShareBoard/window_active_detector.js
@@ -6,69 +6,38 @@
 // 2021-11-23 スマホの focus, blur 問題
 // ・上下にスクロールした時点で blur になってしまう
 // ・なので visibilitychange に変更
-//
-const USE_VISIBILITYCHANGE = true
 
 export const window_active_detector = {
   data() {
     return {
-      window_active_p: null, // ここで this.native_window_active_p() を呼ぶと SSR でひっかる
+      window_active_p: null, // ここで this.__native_window_active_p() を呼ぶと SSR でひっかる
     }
   },
 
   // client でしか呼ばれないのでここで呼ぶ
   mounted() {
-    this.window_active_p = this.native_window_active_p()
-
-    if (USE_VISIBILITYCHANGE) {
-      document.addEventListener("visibilitychange", this.visibilitychange_hook)
-    } else {
-      window.addEventListener("focus", this.window_focus_hook)
-      window.addEventListener("blur", this.window_blur_hook)
-    }
+    this.window_active_p = this.__native_window_active_p()
+    document.addEventListener("visibilitychange", this.__visibilitychange_hook)
   },
 
   beforeDestroy() {
-    if (USE_VISIBILITYCHANGE) {
-      document.removeEventListener("visibilitychange", this.visibilitychange_hook)
-    } else {
-      window.removeEventListener("focus", this.window_focus_hook)
-      window.removeEventListener("blur", this.window_blur_hook)
-    }
+    document.removeEventListener("visibilitychange", this.__visibilitychange_hook)
   },
 
   methods: {
     // private
-    native_window_active_p() {
-      if (USE_VISIBILITYCHANGE) {
-        return document.visibilityState === "visible"
-      } else {
-        return document.hasFocus()
-      }
+    __native_window_active_p() {
+      return document.visibilityState === "visible"
     },
 
-    window_focus_hook() {
-      if (this.debug_mode_p) {
-        this.tl_add("HOOK", "focus")
-      }
-      this.window_active_change_hook(true)
-    },
-
-    window_blur_hook() {
-      if (this.debug_mode_p) {
-        this.tl_add("HOOK", "blur")
-      }
-      this.window_active_change_hook(false)
-    },
-
-    visibilitychange_hook() {
+    __visibilitychange_hook() {
       if (this.debug_mode_p) {
         this.tl_add("HOOK", document.visibilityState)
       }
-      this.window_active_change_hook(this.native_window_active_p())
+      this.__window_active_change_hook(this.__native_window_active_p())
     },
 
-    window_active_change_hook(focus_p) {
+    __window_active_change_hook(focus_p) {
       this.window_active_p = focus_p
       if (focus_p) {
         if (this.window_focus_user_after_hook) {
