2672564176de1716f3bf657f9034ea5943b07c1e
akicho8 <akicho8@gmail.com>
2025-10-28 22:07:13 +0900

    [test] [fix] FIXED_ORDER_STATE が定数になって Syntax error

--- spec/system/share_board/盤面反転_URL引数で順番が確定している場合は起動時に盤面を反転する_spec.rb
+++ spec/system/share_board/盤面反転_URL引数で順番が確定している場合は起動時に盤面を反転する_spec.rb
@@ -3,19 +3,19 @@ require "#{__dir__}/shared_methods"
 # 順番設定を適用する引数で起動したため os_setup 内の sp_viewpoint_set_by_self_location だけで視点が反映される
 # order_copy_from_bc 内のテストにはなっていない
 RSpec.describe __FILE__, type: :system, share_board_spec: true do
-  def case1(FIXED_ORDER_STATE, b_or_w, user_name)
+  def case1(fixed_order_state, b_or_w, user_name)
     visit_room({
         :user_name            => user_name,
         :FIXED_MEMBER   => "a,b",
         :FIXED_ORDER    => "a,b",
-        :FIXED_ORDER_STATE    => FIXED_ORDER_STATE,
+        :FIXED_ORDER_STATE    => fixed_order_state,
         :body                 => SfenGenerator.start_from(b_or_w)
       })
   end
 
-  def case2(FIXED_ORDER_STATE, b_or_w, a_side_location_key, b_side_location_key)
-    window_a { case1(FIXED_ORDER_STATE, b_or_w, "a") }
-    window_b { case1(FIXED_ORDER_STATE, b_or_w, "b") }
+  def case2(fixed_order_state, b_or_w, a_side_location_key, b_side_location_key)
+    window_a { case1(fixed_order_state, b_or_w, "a") }
+    window_b { case1(fixed_order_state, b_or_w, "b") }
     window_a { assert_viewpoint(a_side_location_key) }
     window_b { assert_viewpoint(b_side_location_key) }
   end
