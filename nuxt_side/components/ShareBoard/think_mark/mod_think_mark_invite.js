1d1ac0a6f0b3e00d480d08f51c747e8021c593a2
akicho8 <akicho8@gmail.com>
2025-11-14 15:35:44 +0900

    [fix] [共有将棋盤] 観戦者への発言に観戦者の名前が正しく呼ばれていない

--- nuxt_side/components/ShareBoard/think_mark/mod_think_mark_invite.js
+++ nuxt_side/components/ShareBoard/think_mark/mod_think_mark_invite.js
@@ -19,14 +19,15 @@ export const mod_think_mark_invite = {
     },
 
     think_mark_invite_dialog_show() {
-      const message = "${this.my_call_name}へ。暇だったら右クリックで自分の考えた指し手を示そう。"
+      const message = `観戦者の${this.my_call_name}へ。暇だったら右クリで自分の考えた指し手を示そう。`
+      this.sfx_play("se_notification")
       this.sb_talk(message)
       this.think_mark_invite_dialog_close()
       this.think_mark_invite_dialog_instance = this.dialog_confirm({
-        title: `${this.my_call_name}へ`,
+        title: `観戦者の${this.my_call_name}へ`,
         message: `
-<p>暇だったら右クリックで自分の考えた指し手を示そう</p>
-<p class="is-size-7">スマホの場合は右上の鉛筆を有効にしよう</p>`,
+<p>暇だったら右クリで自分の考えた指し手を示そう</p>
+<p class="is-size-7 mt-4">スマホの人は右上の鉛筆を押そう</p>`,
         confirmText: "わかった",
         cancelText: "やだ",
         focusOn: "confirm",
