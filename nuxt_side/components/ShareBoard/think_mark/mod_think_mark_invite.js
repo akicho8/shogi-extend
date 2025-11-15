7c9ee287ee2e49888f1b65c2c485b391aa2dd5cb
akicho8 <akicho8@gmail.com>
2025-11-15 16:17:30 +0900

    [feat] [共有将棋盤] [思考印] 観戦者に思考印を使ってもらうモーダルに画像を入れる

--- nuxt_side/components/ShareBoard/think_mark/mod_think_mark_invite.js
+++ nuxt_side/components/ShareBoard/think_mark/mod_think_mark_invite.js
@@ -1,4 +1,5 @@
 import { GX } from "@/components/models/gx.js"
+const share_board_think_mark_invite_png = require("@/assets/share_board/think_mark_invite.png")
 
 export const mod_think_mark_invite = {
   data() {
@@ -19,17 +20,19 @@ export const mod_think_mark_invite = {
     },
 
     think_mark_invite_dialog_show() {
-      const message = `観戦者の${this.my_call_name}へ。暇だったら右クリで自分の考えた指し手を示そう。`
       this.sfx_play("se_notification")
-      this.sb_talk(message)
       this.think_mark_invite_dialog_close()
       this.think_mark_invite_dialog_instance = this.dialog_confirm({
         title: `観戦者の${this.my_call_name}へ`,
-        message: `
-<p>暇だったら右クリで自分の考えた指し手を示そう</p>
-<p class="is-size-7 mt-4">スマホの人は右上の鉛筆を押そう</p>`,
+        message: [
+          `<div class="content">`,
+          /**/ `<img src="${share_board_think_mark_invite_png}" />`,
+          /**/ `<p class="mt-3">右クリックで自分の考えを示そう</p>`,
+          /**/ `<p class="mt-2 is-size-7 has-text-grey">スマホの人は右上の鉛筆を押してから</p>`,
+          `</div>`,
+        ].join(""),
         confirmText: "わかった",
-        cancelText: "やだ",
+        cancelText: "見てるだけ",
         focusOn: "confirm",
         onConfirm: () => this.sfx_play("o"),
         onCancel: ()  => this.sfx_play("x"),
