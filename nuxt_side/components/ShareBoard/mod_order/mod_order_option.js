0a5d9bdb5e86326383bbcabb14aeb2b5454fee9e
akicho8 <akicho8@gmail.com>
2025-11-18 18:46:02 +0900

    [style] cosmetic change

--- nuxt_side/components/ShareBoard/mod_order/mod_order_option.js
+++ nuxt_side/components/ShareBoard/mod_order/mod_order_option.js
@@ -1,15 +1,15 @@
-import { ChangePerInfo       } from "@/components/models/change_per_info.js"
-import { FoulModeInfo } from "@/components/models/foul_mode_info.js"
+import { ChangePerInfo  } from "@/components/models/change_per_info.js"
+import { FoulModeInfo   } from "@/components/models/foul_mode_info.js"
 import { AutoResignInfo } from "@/components/models/auto_resign_info.js"
 
 export const mod_order_option = {
   computed: {
-    ChangePerInfo()         { return ChangePerInfo                                      },
+    ChangePerInfo()    { return ChangePerInfo                              },
 
-    FoulModeInfo()   { return FoulModeInfo                                },
-    foul_mode_info() { return FoulModeInfo.fetch(this.foul_mode_key)  },
+    FoulModeInfo()     { return FoulModeInfo                               },
+    foul_mode_info()   { return FoulModeInfo.fetch(this.foul_mode_key)     },
 
-    AutoResignInfo()   { return AutoResignInfo                                },
-    auto_resign_info() { return AutoResignInfo.fetch(this.auto_resign_key)  },
+    AutoResignInfo()   { return AutoResignInfo                             },
+    auto_resign_info() { return AutoResignInfo.fetch(this.auto_resign_key) },
   },
 }
