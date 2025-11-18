792b62d3a2d16bbc54db0e0e6f2d25dce5319fa2
akicho8 <akicho8@gmail.com>
2025-11-18 21:12:01 +0900

    [refactor] [共有将棋盤] 持駒レイアウト関連を利用者視点での言葉に変更する

--- nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
+++ nuxt_side/components/ShareBoard/appearance_theme/AppearanceUi.vue
@@ -5,7 +5,11 @@
     SimpleRadioButton(:base="SB" model_name="AppearanceThemeInfo" var_name="appearance_theme_key" custom-class="is-small" element_size="is-small")
     SimpleSlider.is-hidden-touch(:base="SB" label="盤の大きさ" var_name="sb_board_width" :min="0" :max="100" :step="1.0")
     SimpleSlider(:base="SB" label="グリッドの太さ" var_name="sb_grid_stroke" :min="0" :max="3" :step="0.5" v-if="SB.debug_mode_p")
-    SimpleRadioButton.is-hidden-tablet(:base="SB" model_name="MobileLayoutInfo" var_name="mobile_layout_key" custom-class="is-small" element_size="is-small")
+
+    // もともと .is-hidden-tablet を入れてスマホでのみ表示していたがやめた。
+    // PC利用者は比較的リテラシーが高く、スマホで画面が見切れている人にいろいろアドバイスしたりしている人もいた。
+    // だからPC利用者にこそこの設定が存在することを知ってもらうようにする。
+    SimpleRadioButton(:base="SB" model_name="MobileLayoutInfo" var_name="mobile_layout_key" custom-class="is-small" element_size="is-small")
 </template>
 
 <script>
@@ -26,4 +30,3 @@ export default {
     flex-direction: column
     gap: 0.75rem
 </style>
-
--- nuxt_side/components/ShareBoard/appearance_theme/mobile_layout_info.js
+++ nuxt_side/components/ShareBoard/appearance_theme/mobile_layout_info.js
@@ -1,13 +1,13 @@
 import { ApplicationMemoryRecord } from "@/components/models/application_memory_record.js"
 
 export class MobileLayoutInfo extends ApplicationMemoryRecord {
-  static field_label = "持駒レイアウト (スマホ利用時)"
+  static field_label = "画面レイアウト (スマホ利用時)"
   static field_message = ""
 
   static get define() {
     return [
-      { key: "ml_vertical",   name: "上下", sp_mobile_vertical: true,  type: "is-primary", message: "盤の横幅を最大化する (推奨)", },
-      { key: "ml_horizontal", name: "左右", sp_mobile_vertical: false, type: "is-warning", message: "画面比率が特殊なスマホ向け",  },
+      { key: "ml_vertical",   name: "基本", sp_mobile_vertical: true,  type: "is-primary", message: "盤の横幅を最大化する (推奨)", },
+      { key: "ml_horizontal", name: "縮小", sp_mobile_vertical: false, type: "is-warning", message: "持駒が見切れる端末用",        },
     ]
   }
 }
