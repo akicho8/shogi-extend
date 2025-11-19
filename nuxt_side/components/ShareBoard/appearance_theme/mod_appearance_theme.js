28acd34119beebb43928dbb1e55c2f1c6d05cd9e
akicho8 <akicho8@gmail.com>
2025-11-19 18:21:21 +0900

    [style] cosmetic change

--- nuxt_side/components/ShareBoard/appearance_theme/mod_appearance_theme.js
+++ nuxt_side/components/ShareBoard/appearance_theme/mod_appearance_theme.js
@@ -1,16 +1,16 @@
 import { AppearanceThemeInfo } from "./appearance_theme_info.js"
-import { MobileLayoutInfo } from "./mobile_layout_info.js"
-import { DesktopLayoutInfo } from "./desktop_layout_info.js"
+import { MobileLayoutInfo    } from "./mobile_layout_info.js"
+import { DesktopLayoutInfo   } from "./desktop_layout_info.js"
 
 export const mod_appearance_theme = {
   computed: {
     AppearanceThemeInfo()   { return AppearanceThemeInfo                                  },
     appearance_theme_info() { return AppearanceThemeInfo.fetch(this.appearance_theme_key) },
 
-    MobileLayoutInfo()   { return MobileLayoutInfo                                  },
-    mobile_layout_info() { return MobileLayoutInfo.fetch(this.mobile_layout_key) },
+    MobileLayoutInfo()      { return MobileLayoutInfo                                     },
+    mobile_layout_info()    { return MobileLayoutInfo.fetch(this.mobile_layout_key)       },
 
-    DesktopLayoutInfo()   { return DesktopLayoutInfo                                  },
-    desktop_layout_info() { return DesktopLayoutInfo.fetch(this.desktop_layout_key) },
+    DesktopLayoutInfo()     { return DesktopLayoutInfo                                    },
+    desktop_layout_info()   { return DesktopLayoutInfo.fetch(this.desktop_layout_key)     },
   },
 }
