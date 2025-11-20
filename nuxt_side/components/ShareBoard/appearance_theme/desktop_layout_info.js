2fc279272e0fe09d5e92ae869a4d8975c782ed02
akicho8 <akicho8@gmail.com>
2025-11-20 12:25:11 +0900

    [feat] [共有将棋盤] [スタイル設定] [推奨盤サイズ] 80% → 90%

--- nuxt_side/components/ShareBoard/appearance_theme/desktop_layout_info.js
+++ nuxt_side/components/ShareBoard/appearance_theme/desktop_layout_info.js
@@ -6,7 +6,7 @@ export class DesktopLayoutInfo extends ApplicationMemoryRecord {
 
   static get define() {
     return [
-      { key: "dl_horizontal", name: "横長", sp_layout: "horizontal", type: "is-primary", message: "左右に持駒を配置する (推奨盤サイズ<b>80%</b>)", },
+      { key: "dl_horizontal", name: "横長", sp_layout: "horizontal", type: "is-primary", message: "左右に持駒を配置する (推奨盤サイズ<b>90%</b>)", },
       { key: "dl_vertical",   name: "縦長", sp_layout: "vertical",   type: "is-warning", message: "上下に持駒を配置する (推奨盤サイズ<b>60%</b>)", },
     ]
   }
