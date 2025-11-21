1bccb6e297942eecad12796e42950e42b627e10b
akicho8 <akicho8@gmail.com>
2025-11-21 18:42:34 +0900

    [feat] [共有将棋盤] [chat] 発言送信方法のカスタマイズは需要がないため消す

--- nuxt_side/components/ShareBoard/general_setting/setting_root_info/setting_tab_ui.js
+++ nuxt_side/components/ShareBoard/general_setting/setting_root_info/setting_tab_ui.js
@@ -3,8 +3,8 @@ import { SettingTabBase } from "./setting_tab_base.js"
 export class SettingTabUi extends SettingTabBase {
   static get define() {
     return [
-      { key: "lift_cancel_action", show: true, },
-      { key: "send_trigger_key",   show: true, },
+      { key: "lift_cancel_action", show: true,  },
+      { key: "send_trigger_key",   show: false, },
     ]
   }
 }
