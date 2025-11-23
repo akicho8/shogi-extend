c6482391074bad821f6d5f6bd3f4fcc501c77918
akicho8 <akicho8@gmail.com>
2025-11-23 10:26:18 +0900

    [refactor] [共有将棋盤] 文言改善

--- nuxt_side/components/ShareBoard/chat/chat_content_scale_info.js
+++ nuxt_side/components/ShareBoard/chat/chat_content_scale_info.js
@@ -6,8 +6,8 @@ export class ChatContentScaleInfo extends ApplicationMemoryRecord {
 
   static get define() {
     return [
-      { key: "ccs_small", name: "小さめ", type: "is-primary", message: "デフォルト",               full_screen_p: false, },
-      { key: "ccs_large", name: "大きめ", type: "is-warning", message: "大きめというかフルサイズ", full_screen_p: true,  },
+      { key: "ccs_small", name: "小さめ", type: "is-primary", message: "デフォルト",         full_screen_p: false, },
+      { key: "ccs_large", name: "大きめ", type: "is-warning", message: "大きめというか最大", full_screen_p: true,  },
     ]
   }
 }
