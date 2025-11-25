1346e530e7256d808f79936df3150a34cc1996c1
akicho8 <akicho8@gmail.com>
2025-11-25 19:26:05 +0900

    [refactor] [共有将棋盤] デバッグ用メニューの項目を見やすくする

--- nuxt_side/components/ShareBoard/debug/SbDebug.vue
+++ nuxt_side/components/ShareBoard/debug/SbDebug.vue
@@ -4,14 +4,14 @@
     .panel
       .panel-heading
         | コンテキスト変更1
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c")                                                                 順番🔴 時計🔴 入室 a
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b")                                                 順番🟢 時計🔴 先手 a
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&room_after_create=cc_auto_start_10m")                 順番🔴 時計🟢 先手 a
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m") 順番🟢 時計🟢 先手 a
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=b&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m") 順番🟢 時計🟢 後手 b
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=c&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m") 順番🟢 時計🟢 観戦 c
-      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&FIXED_ORDER_SWAP=true&room_after_create=cc_auto_start_10m") 順番🟢 時計🟢 後手 a ※先手不明
-      a.panel-block(href="?self_vs_self_enable_p=true&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&FIXED_ORDER_SWAP=true&room_after_create=cc_auto_start_10m") 順番🟢 時計🟢 後手 a ※先手不明 自分vs自分 可
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c")                                                                     🔴順番 🔴時計 入室 a
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b")                                                     🟢順番 🔴時計 先手 a
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&room_after_create=cc_auto_start_10m")                                 🔴順番 🟢時計 先手 a
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m")                 🟢順番 🟢時計 先手 a
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=b&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m")                 🟢順番 🟢時計 後手 b
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=c&FIXED_MEMBER=a,b,c&FIXED_ORDER=a,b&room_after_create=cc_auto_start_10m")                 🟢順番 🟢時計 観戦 c
+      a.panel-block(href="?self_vs_self_enable_p=false&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&FIXED_ORDER_SWAP=true&room_after_create=cc_auto_start_10m") 🟢順番 🟢時計 後手 a ※先手不明
+      a.panel-block(href="?self_vs_self_enable_p=true&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&FIXED_ORDER_SWAP=true&room_after_create=cc_auto_start_10m")  🟢順番 🟢時計 後手 a ※先手不明 自分vs自分 可
   .column.is-2
     .panel
       .panel-heading
