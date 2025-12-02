bb1a0c2ef8dafa516ee40a1bb8bbca2dc42bea35
akicho8 <akicho8@gmail.com>
2025-12-02 23:37:47 +0900

    [test] [共有将棋盤] 全体シャッフルは1人のとき表示されていないため先後入替に変更する

--- spec/system/share_board/順番設定/更新を押さずに閉じたら確認する_spec.rb
+++ spec/system/share_board/順番設定/更新を押さずに閉じたら確認する_spec.rb
@@ -3,9 +3,9 @@ require "#{__dir__}/setup"
 RSpec.describe __FILE__, type: :system, share_board_spec: true do
   it "works" do
     visit_room(user_name: :a)
-    os_modal_open                                                 # 「順番設定」モーダルを開く
-    os_switch_toggle                                                # 右上の有効スイッチをクリック
-    find(:button, text: "全体ｼｬｯﾌﾙ", exact_text: true).click        # 全体シャッフルする
+    os_modal_open                                               # 「順番設定」モーダルを開く
+    os_switch_toggle                                            # 右上の有効スイッチをクリック
+    find(".swap_handle").click                                  # a が先後反転した
     os_modal_close                                              # 閉じる (ヘッダーに置いている) とするがダイアログが表示される
     find(:button, text: "確定せずに閉じる", exact_text: true).click # 無視して閉じる
   end
