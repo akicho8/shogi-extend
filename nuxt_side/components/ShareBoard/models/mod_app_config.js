8f64bfba5a2caf701f964b956b52fec8d0ba89ea
akicho8 <akicho8@gmail.com>
2025-12-04 21:48:08 +0900

    [feat] [共有将棋盤] デフォルトアバターを毎日変化させる

--- nuxt_side/components/ShareBoard/models/mod_app_config.js
+++ nuxt_side/components/ShareBoard/models/mod_app_config.js
@@ -27,7 +27,7 @@ export const AppConfig = {
 
   avatar: {
     profile_image_first_use: true,            // プロフィール画像を優先的に使うか？
-    pepper_date_format: "-",                  // アバターが変化するタイミング。毎日なら"YYYY-MM-DD"。固定でいいなら "-"。空にすると秒単位の時間になるので注意
+    pepper_date_format: "YYYY-MM-DD",         // アバターが変化するタイミング。毎日なら"YYYY-MM-DD"。固定でいいなら "-"。空にすると秒単位の時間になるので注意
     avatar_history_take: 10,                  // アバターを履歴に残す件数
   },
 
