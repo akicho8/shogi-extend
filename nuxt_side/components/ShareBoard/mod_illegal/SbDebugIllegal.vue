76ceb1eeec86b25683b275ae2e7ea114ab39a90f
akicho8 <akicho8@gmail.com>
2025-12-16 21:07:49 +0900

    [fix] [共有将棋盤] [debug] URL に埋め込んだ SFEN が読めない

--- nuxt_side/components/ShareBoard/mod_illegal/SbDebugIllegal.vue
+++ nuxt_side/components/ShareBoard/mod_illegal/SbDebugIllegal.vue
@@ -14,7 +14,7 @@
         .panel-heading
           | 反則テスト
         a.panel-block(href="?body=position sfen 7k1/5Gb2/7SL/8K/6s1P/9/9/9/8L b GNP 1&RS_FEATURE=false&self_vs_self_enable_p=true&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&room_after_create=cc_auto_start_10m&foul_mode_key=block") 二歩・自殺手・ピン外し自殺手・駒ワープ・死に駒・王手放置・打ち歩詰め
-        a.panel-block(href="?body=position sfen 9/9/9/9/9/6+R2/6+b1k/9/8K b P 1&RS_FEATURE=false&self_vs_self_enable_p=true&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&room_after_create=cc_auto_start_10m&foul_mode_key=block") 王手解除せず
+        a.panel-block(href="?xbody=cG9zaXRpb24gc2ZlbiA4ay82YjFsLzVSK3JHMS84Sy84UC85LzkvOS85IGIgUCAx&RS_FEATURE=false&self_vs_self_enable_p=true&room_restore_feature_p=false&room_key=dev_room&user_name=a&FIXED_MEMBER=a&FIXED_ORDER=a&room_after_create=cc_auto_start_10m&foul_mode_key=block") 王手解除せず
     .column.is-4
       .panel.assert_var
         .panel-heading
