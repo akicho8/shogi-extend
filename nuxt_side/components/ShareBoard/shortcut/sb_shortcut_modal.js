c9d98ea3cae74d16b6358584ac756ec2b8d5f38b
akicho8 <akicho8@gmail.com>
2025-12-28 10:58:05 +0900

    [fix] [共有将棋盤] [ショートカットモーダル] モーダル外クリックやESCで閉じない

--- nuxt_side/components/ShareBoard/shortcut/sb_shortcut_modal.js
+++ nuxt_side/components/ShareBoard/shortcut/sb_shortcut_modal.js
@@ -37,6 +37,7 @@ export const sb_shortcut_modal = {
       this.shortcut_modal_close()
       this.shortcut_modal_instance = this.modal_card_open({
         component: SbShortcutModal,
+        canCancel: ["outside", "escape"],
         onCancel: () => {
           this.sfx_click()
           this.shortcut_modal_close()
