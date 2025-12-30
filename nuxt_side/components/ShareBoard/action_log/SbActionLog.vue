7f75885c0237e7f738e739bf0075c5d33e91a822
akicho8 <akicho8@gmail.com>
2025-12-30 11:35:53 +0900

    [feat] [共有将棋盤] 王手しているときは履歴に王手を表示する

--- nuxt_side/components/ShareBoard/action_log/SbActionLog.vue
+++ nuxt_side/components/ShareBoard/action_log/SbActionLog.vue
@@ -8,21 +8,26 @@
     .SbAvatarLines
       template(v-for="(e, i) in SB.action_logs")
         SbAvatarLine.is-clickable(:info="e" tag="a" :key="e.unique_key" @click="SB.al_click_handle(e)" :xprofile_show_p="false")
+          // タグ的なもの
           template(v-if="e.label")
-            template(v-if="e.label && e.label_type")
+            template(v-if="e.label_type")
               b-tag.flex_item(:type="e.label_type" size="is-small") {{e.label}}
             template(v-else)
               .flex_item {{e.label}}
 
+          // 指し手
           template(v-if="e.last_move_info_attrs")
             .flex_item {{e.last_move_info_attrs.next_turn_offset}}
             .flex_item {{e.last_move_info_attrs.kif_without_from}}
 
-          template(v-for="e in e.illegal_hv_list")
-            b-tag.flex_item(type="is-danger" size="is-small") {{e.illegal_info.name}}
-
-          template(v-if="e.checkmate_stat && e.checkmate_stat.yes_or_no === 'yes'")
+          // 表示優先度: 反則←詰み←王手
+          template(v-if="$GX.present_p(e.illegal_hv_list)")
+            template(v-for="e in e.illegal_hv_list")
+              b-tag.flex_item(type="is-danger" size="is-small") {{e.illegal_info.name}}
+          template(v-else-if="e.checkmate_stat && e.checkmate_stat.yes_or_no === 'yes'")
             b-tag.flex_item(type="is-danger" size="is-small") 詰み
+          template(v-else-if="e.op_king_check")
+            b-tag.flex_item(type="is-danger" size="is-small") 王手
 
           .flex_item.is-size-7(v-if="'elapsed_sec' in e") {{-e.elapsed_sec}}秒
 
