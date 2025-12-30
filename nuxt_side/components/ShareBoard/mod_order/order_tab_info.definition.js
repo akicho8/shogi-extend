7b801f4c7ef3e690850783dc88790e1f37f1a9d8
akicho8 <akicho8@gmail.com>
2025-12-30 11:11:51 +0900

    [refactor] [共有将棋盤] [順番設定] [実験的] AMR用のデータを外部JSとする

---
+++ nuxt_side/components/ShareBoard/mod_order/order_tab_info.definition.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { key: "order_tab_main", name: "順番",   },
+  { key: "order_tab_fes",  name: "投票",   },
+  { key: "order_tab_rule", name: "ルール", },
+]
---
+++ nuxt_side/test/share_board/mod_order/order_tab_info.spec.js
@@ -0,0 +1,7 @@
+import { OrderTabInfo } from "@/components/ShareBoard/mod_order/order_tab_info.js"
+
+describe("OrderTabInfo", () => {
+  test("works", () => {
+    expect(OrderTabInfo.values.length > 0).toEqual(true)
+  })
+})
