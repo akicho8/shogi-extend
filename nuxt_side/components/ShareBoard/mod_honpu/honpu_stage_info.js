588766751863c691408c8ed76abc7ba4080cd9c1
akicho8 <akicho8@gmail.com>
2026-04-03 07:11:48 +0900

    Revert "[refactor] [ShareBoard] 本譜に戻るボタンがあるときは本譜を表示しない"
    This reverts commit 734d948564fd01c8339213e5618b123383ca8bdf.

--- nuxt_side/components/ShareBoard/mod_honpu/honpu_stage_info.js
+++ nuxt_side/components/ShareBoard/mod_honpu/honpu_stage_info.js
@@ -8,24 +8,18 @@ export class HonpuStageInfo extends ApplicationMemoryRecord {
         name: "本譜のみ",
         kifu_copy_message: "本譜をコピーしました",
         help_message: null,
-        honpu_hyouji: false,
-        honpuni_modoreru: false,
       },
       {
         key: "hs_branching",
         name: "本譜分岐",
         kifu_copy_message: "コピーしましたがこれは本譜ではありません",
         help_message: "棋譜が本譜から分岐しています (本譜が必要であれば本譜に戻してください)",
-        honpu_hyouji: true,
-        honpuni_modoreru: true,
       },
       {
         key: "hs_honpu_none",
         name: "本譜なし",
         kifu_copy_message: "コピーしました",
         help_message: null,
-        honpu_hyouji: false,
-        honpuni_modoreru: false,
       },
     ]
   }
