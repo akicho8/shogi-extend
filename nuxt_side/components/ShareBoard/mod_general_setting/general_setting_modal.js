32b4a390911d80ec6dc2e509090b701cf39b0db7
akicho8 <akicho8@gmail.com>
2026-04-03 17:19:41 +0900

    [refactor] [ShareBoard] general_setting_modal cosmetic change

--- nuxt_side/components/ShareBoard/mod_general_setting/general_setting_modal.js
+++ nuxt_side/components/ShareBoard/mod_general_setting/general_setting_modal.js
@@ -1,33 +1,26 @@
 import GeneralSettingModal from "./GeneralSettingModal.vue"
 
 export const general_setting_modal = {
-  data() {
-    return {
-    }
-  },
-
   beforeDestroy() {
     this.general_setting_modal_close()
   },
 
   methods: {
     general_setting_modal_shortcut_handle() {
-      if (!this.general_setting_modal_instance) {
-        this.general_setting_modal_open_handle()
-      } else {
+      if (this.general_setting_modal_instance) {
         this.general_setting_modal_close_handle()
+      } else {
+        this.general_setting_modal_open_handle()
       }
       return true
     },
 
     general_setting_modal_open_handle() {
-      // this.sidebar_close()
       this.sfx_click()
       this.general_setting_modal_open()
     },
 
     general_setting_modal_close_handle() {
-      // this.sidebar_close()
       this.sfx_click()
       this.general_setting_modal_close()
     },
