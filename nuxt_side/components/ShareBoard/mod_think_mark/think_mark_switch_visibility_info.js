916882e20e854abf12038ccb8c20244f45b24898
akicho8 <akicho8@gmail.com>
2026-04-05 07:36:06 +0900

    [refactor] [ShareBoard] 文言を改善する

--- nuxt_side/components/ShareBoard/mod_think_mark/think_mark_switch_visibility_info.js
+++ nuxt_side/components/ShareBoard/mod_think_mark/think_mark_switch_visibility_info.js
@@ -1,13 +1,13 @@
 import { ApplicationMemoryRecord } from "@/components/models/application_memory_record.js"
 
 export class ThinkMarkSwitchVisibilityInfo extends ApplicationMemoryRecord {
-  static field_label = "自分が対局中のときも思考印の切り替えアイコンを表示"
+  static field_label = "自分の対局中に思考印の切り替えアイコンを表示"
   static field_message = ""
 
   static get define() {
     return [
-      { key: "tmsv_hidden",  name: "しない", type: "is-primary", message: "初心者向け (表示しなくても右クリックで思考印は書けるしショートカットキーでも切り替え可能)", },
-      { key: "tmsv_visible", name: "する",   type: "is-primary", message: "玄人向け (スマホでは UI がないと思考印モードを切り替えることができない)", },
+      { key: "tmsv_hidden",  name: "しない", type: "is-primary", message: "スマホまたは初心者向け (PCであれば表示しなくても右クリックで思考印は書けるしショートカットキーでも、切り替えることができる)", },
+      { key: "tmsv_visible", name: "する",   type: "is-primary", message: "玄人向け (注意: スマホではアイコンがないと思考印モードを有効にすることができない)", },
     ]
   }
 }
