cfac1007aebc176e6bd530d3f2b4ed18192be8d2
akicho8 <akicho8@gmail.com>
2026-04-19 19:06:48 +0900

    [test] [fix] [スタイル設定] ハイブリッドなどを追加したあたらしいスタイル設定に合わせる

--- spec/system/share_board/補助機能_スタイル_spec.rb
+++ spec/system/share_board/補助機能_スタイル_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe __FILE__, type: :system, share_board_spec: true do
     visit_app
     sidebar_open
     find(".appearance_modal_open_handle").click
-    find(:label, text: "ライト", exact_text: true).click
-    find(:label, text: "ダーク", exact_text: true).click
+    find(:label, text: "モダン", exact_text: true).click
     find(:label, text: "リアル", exact_text: true).click
+    find(:label, text: "ハイブリッド", exact_text: true).click
   end
 end
