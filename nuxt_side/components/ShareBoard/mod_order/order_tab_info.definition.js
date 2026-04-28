043a6f74482fa000c5d3ec7c3383412f580fc36f
akicho8 <akicho8@gmail.com>
2026-04-28 14:55:12 +0900

    [refactor] [共有将棋盤] order_tab_info.definition.js を消す

--- nuxt_side/components/ShareBoard/mod_order/order_tab_info.definition.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { key: "order_tab_main", name: "対局者", },
-  { key: "order_tab_rule", name: "反則等", },
-  { key: "order_tab_fes",  name: "投票",   },
-]
--- nuxt_side/components/ShareBoard/mod_order/order_tab_info.js
+++ nuxt_side/components/ShareBoard/mod_order/order_tab_info.js
@@ -1,5 +1,11 @@
 import { ApplicationMemoryRecord } from "@/components/models/application_memory_record.js"
 
 export class OrderTabInfo extends ApplicationMemoryRecord {
-  static get define() { return require("./order_tab_info.definition.js") }
+  static get define() {
+    return [
+      { key: "order_tab_main", name: "対局者", },
+      { key: "order_tab_rule", name: "ルール", },
+      { key: "order_tab_fes",  name: "投票",   },
+    ]
+  }
 }
