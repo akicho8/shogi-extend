5c53c32ad19c17f75b1780c5fba7f485f9eabe4b
akicho8 <akicho8@gmail.com>
2026-05-01 19:44:51 +0900

    [refactor] cosmetic change

--- nuxt_side/components/ShareBoard/mod_appearance/appearance_modal.js
+++ nuxt_side/components/ShareBoard/mod_appearance/appearance_modal.js
@@ -1,25 +1,18 @@
 import AppearanceModal from "./AppearanceModal.vue"
 
 export const appearance_modal = {
-  data() {
-    return {
-    }
-  },
-
   beforeDestroy() {
     this.appearance_modal_close()
   },
 
   methods: {
     appearance_modal_open_handle() {
-      // this.sidebar_close()
       this.sfx_click()
       this.appearance_modal_open()
       this.general_setting_modal_close()
     },
 
     appearance_modal_close_handle() {
-      // this.sidebar_close()
       this.sfx_click()
       this.appearance_modal_close()
     },
