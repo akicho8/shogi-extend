25b80ec3287241dc75eb4321726e794cfe0a8589
akicho8 <akicho8@gmail.com>
2026-05-04 19:28:04 +0900

    [test] [fix] [ShareBoard] モーダル内の b-dropdown だけを正しく選択できない

--- spec/system/share_board/エクポート/画像ダウンロード_spec.rb
+++ spec/system/share_board/エクポート/画像ダウンロード_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe __FILE__, type: :system, share_board_spec: true do
     find(".image_download_modal_handle").click                     # 開く
     assert_selector(".ImageDownloadModal")                         # モーダルが開いている
 
-    find(".image_size_key_dropdown").click                         # サイズ変更ドロップダウンを開く
+    find(".ImageDownloadModal .image_size_key_dropdown").click     # サイズ変更ドロップダウンを開く
     find(".is_image_size_1080x810").click                          # サイズ確定
 
-    find(".SbColorThemeDropdown").click                            # 配色変更ドロップダウンを開く
+    find(".ImageDownloadModal .SbColorThemeDropdown").click        # 配色変更ドロップダウンを開く
     find(".is_color_theme_real").click                             # 配色確定
 
     find(".download_handle", wait: 5).click                        # ダウンロード実行 (waitは遅いPCのフレーキー対策)
