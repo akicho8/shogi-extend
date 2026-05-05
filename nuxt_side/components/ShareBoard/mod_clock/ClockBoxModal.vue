2ba2ae51c1a9a97768b52e5f05b775edc9750577
akicho8 <akicho8@gmail.com>
2026-05-06 08:48:13 +0900

    [refactor] [ShareBoard] ごく一部の人のために時計のUIを変更したが情報過多なので元に戻す

--- nuxt_side/components/ShareBoard/mod_clock/ClockBoxModal.vue
+++ nuxt_side/components/ShareBoard/mod_clock/ClockBoxModal.vue
@@ -55,12 +55,12 @@
     template(v-if="SB.clock_box")
       .buttons
         template(v-if="SB.clock_box.stop_p")
-          b-button.play_button(title="対局開始" @click="SB.cbm_play_handle" icon-left="play" type="is-primary") 対局開始
+          b-button.play_button(title="対局開始" @click="SB.cbm_play_handle" icon-left="play" type="is-primary")
         template(v-if="SB.clock_box.play_p")
           b-button.pause_button(title="一時停止" @click="SB.cbm_pause_handle" icon-left="pause" type="is-primary")
         template(v-if="SB.clock_box.pause_p")
           b-button.stop_button(title="停止" @click="SB.cbm_stop_handle" icon-left="stop")
-          b-button.resume_button(title="再開" @click="SB.cbm_resume_handle" icon-left="play-pause" type="is-primary") 再開
+          b-button.resume_button(title="再開" @click="SB.cbm_resume_handle" icon-left="play-pause" type="is-primary")
 </template>
 
 <script>
