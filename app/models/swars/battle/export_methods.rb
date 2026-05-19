9a1302b98513861219a6186219c32d913f9967ad
akicho8 <akicho8@gmail.com>
2026-05-19 22:33:54 +0900

    [fix] syntax error

--- app/models/swars/battle/export_methods.rb
+++ app/models/swars/battle/export_methods.rb
@@ -15,24 +15,24 @@ module Swars
           Swars::Battle.limit(options[:limit]).find_each.with_index do |battle, i|
             begin
               battle.to_all.each do |ext, body|
-              prefix = Digest::MD5.hexdigest(battle.key.to_s).slice(...2)
-              path = output_dir.join(prefix, battle.key, "#{battle.key}.#{ext}")
-              if path.exist?
-                if options[:verbose]
-                  print "."
-                  STDOUT.flush
+                prefix = Digest::MD5.hexdigest(battle.key.to_s).slice(...2)
+                path = output_dir.join(prefix, battle.key, "#{battle.key}.#{ext}")
+                if path.exist?
+                  if options[:verbose]
+                    print "."
+                    STDOUT.flush
+                  end
+                  next
                 end
-                next
+                path.dirname.mkpath
+                path.write(body)
+                ratio = i.fdiv(all_count) * 100
+                if options[:verbose]
+                  puts "[#{i}/#{all_count}][#{ratio.round(2)}]: #{path.basename}"
+                end
               end
-              path.dirname.mkpath
-              path.write(body)
-              ratio = i.fdiv(all_count) * 100
-              if options[:verbose]
-                puts "[#{i}/#{all_count}][#{ratio.round(2)}]: #{path.basename}"
-              end
-            end
             rescue Bioshogi::BioshogiError => error
-              output_dir.join("error_files.txt").write("#{battle.key} #{error}\n" mode: "a")
+              output_dir.join("error_files.txt").write("#{battle.key} #{error}\n", mode: "a")
             end
           end
         end
